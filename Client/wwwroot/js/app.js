034061e6641450892dbdceb45fc455a8aef5bf39
Vladislav Karamfilov <vladislav.karamfilov@gmail.com>
2020-05-31 11:20:22 +0300

    Splitter progress.

---
+++ Client/wwwroot/js/app.js
@@ -0,0 +1,28 @@
+﻿window.App = window.App || {};
+
+(function () {
+    window.App.initSplitter = function (container1Id, container2Id) {
+        if (container1Id &&
+            container2Id &&
+            document.getElementById(container1Id) &&
+            document.getElementById(container2Id)) {
+
+            Split(['#' + container1Id, '#' + container2Id], {
+                elementStyle: (dimension, size, gutterSize) => ({
+                    'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+                }),
+                gutterStyle: (dimension, gutterSize) => ({
+                    'flex-basis': `${gutterSize}px`,
+                }),
+                onDrag: () => {
+                    var oldEditorElement = document.getElementById('user-code-editor');
+                    if (oldEditorElement && oldEditorElement.childNodes) {
+                        oldEditorElement.childNodes.forEach(c => oldEditorElement.removeChild(c));
+                    }
+
+                    window.initEditor('user-code-editor');
+                }
+            });
+        }
+    };
+}());
