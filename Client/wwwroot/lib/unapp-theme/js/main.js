bf4113d495e2c0d49bc8f18cf770b3a927306598
Vladislav Karamfilov <vladislav.karamfilov@gmail.com>
2020-06-01 23:04:46 +0300

    Progress on using Bootstrap 4

--- Client/wwwroot/lib/unapp-theme/js/main.js
+++ Client/wwwroot/lib/unapp-theme/js/main.js
@@ -45,8 +45,8 @@
 
 	var offcanvasMenu = function() {
 
-		$('#page').prepend('<div id="colorlib-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle colorlib-nav-white"><i></i></a>');
+		$('#unapp-page').prepend('<div id="colorlib-offcanvas" />');
+		$('#unapp-page').prepend('<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle colorlib-nav-white"><i></i></a>');
 		var clone1 = $('.menu-1 > ul').clone();
 		$('#colorlib-offcanvas').append(clone1);
 		var clone2 = $('.menu-2 > ul').clone();
