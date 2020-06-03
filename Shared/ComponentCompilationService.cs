1c00dc627478e7a807bbcb2cda7a311ec34442a8
KristianMariyanov <kristian.mariyanov@gmail.com>
2020-06-03 22:11:56 +0300

    refactor compilation service

--- Shared/ComponentCompilationService.cs
+++ Shared/ComponentCompilationService.cs
@@ -20,6 +20,12 @@ namespace BlazorRepl.Shared
 {
     public class ComponentCompilationService
     {
+        private const string LineEnding = "\n";
+        private const string DefaultRootNamespace = "BlazorRepl.UserComponents";
+        private const string WorkingDirectory = "x:\\BlazorRepl";
+
+        private static readonly string PathSeparator = Path.DirectorySeparatorChar.ToString();
+
         public static async Task Init(HttpClient httpClient)
         {
             var basicReferenceAssemblyRoots = new[]
@@ -74,40 +80,17 @@ namespace BlazorRepl.Shared
                 }));
 
             return streams;
-
         }
 
         private static CSharpParseOptions CSharpParseOptions { get; set; }
 
-        // Used to force a specific style of line-endings for testing. This matters
-        // for the baseline tests that exercise line mappings. Even though we normalize
-        // newlines for testing, the difference between platforms affects the data through
-        // the *count* of characters written.
-        internal virtual string LineEnding { get; } = "\n";
-
-        internal virtual string DefaultRootNamespace { get; } = "BlazorRepl.UserComponents";
-
-        internal virtual RazorConfiguration Configuration { get; }
+        private RazorConfiguration Configuration { get; }
             = RazorConfiguration.Create(RazorLanguageVersion.Latest, "MVC-3.0", Array.Empty<RazorExtension>());
 
-        internal virtual VirtualRazorProjectFileSystem FileSystem { get; } = new VirtualRazorProjectFileSystem();
-
-        internal List<RazorProjectItem> AdditionalRazorItems { get; } = new List<RazorProjectItem>();
-
-        internal List<SyntaxTree> AdditionalSyntaxTrees { get; } = new List<SyntaxTree>();
-
-        internal virtual bool DesignTime { get; }
-
-        internal virtual string PathSeparator { get; } = Path.DirectorySeparatorChar.ToString();
-
-        internal virtual string WorkingDirectory { get; } = "x:\\BlazorRepl";
-
-        internal virtual bool NormalizeSourceLineEndings { get; } = true;
-
-        internal virtual string FileKind { get; } = FileKinds.Component;
+        private VirtualRazorProjectFileSystem FileSystem { get; } = new VirtualRazorProjectFileSystem();
 
         // Creating the initial compilation + reading references is on the order of 250ms without caching
-        // so making sure it doesn't happen for each test.
+        // so making sure it doesn't happen for each run.
         private static CSharpCompilation BaseCompilation;
 
         public async Task<CompileToAssemblyResult> CompileToAssembly(
@@ -142,11 +125,6 @@ namespace BlazorRepl.Shared
                 .Where(d => d.Severity > DiagnosticSeverity.Info)
                 .ToList();
 
-            foreach (var diagnostic in diagnostics)
-            {
-                Console.WriteLine(diagnostic);
-            }
-
             var result = new CompileToAssemblyResult
             {
                 Compilation = compilation,
@@ -164,12 +142,12 @@ namespace BlazorRepl.Shared
             return result;
         }
 
-        protected static CSharpSyntaxTree Parse(string text, string path = null)
+        private static CSharpSyntaxTree Parse(string text, string path = null)
         {
             return (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(text, CSharpParseOptions, path: path);
         }
 
-        protected async Task<CompileToCSharpResult> CompileToCSharp(
+        private async Task<CompileToCSharpResult> CompileToCSharp(
             string cshtmlRelativePath,
             string cshtmlContent,
             CSharpCompilation compilation,
@@ -179,26 +157,17 @@ namespace BlazorRepl.Shared
             // This mirrors what the build does.
             var projectEngine = CreateProjectEngine(Array.Empty<MetadataReference>());
 
-            RazorCodeDocument codeDocument;
-            foreach (var item in AdditionalRazorItems)
-            {
-                // Result of generating declarations
-                codeDocument = projectEngine.ProcessDeclarationOnly(item);
-
-                var syntaxTree = Parse(codeDocument.GetCSharpDocument().GeneratedCode, path: item.FilePath);
-                AdditionalSyntaxTrees.Add(syntaxTree);
-            }
-
             // Result of generating declarations
             var projectItem = CreateProjectItem(cshtmlRelativePath, cshtmlContent);
 
-            codeDocument = projectEngine.ProcessDeclarationOnly(projectItem);
+            var codeDocument = projectEngine.ProcessDeclarationOnly(projectItem);
+            var cSharpDocument = codeDocument.GetCSharpDocument();
 
             var declaration = new CompileToCSharpResult
             {
-                BaseCompilation = compilation.AddSyntaxTrees(AdditionalSyntaxTrees),
-                Code = codeDocument.GetCSharpDocument().GeneratedCode,
-                Diagnostics = codeDocument.GetCSharpDocument().Diagnostics.Select(CompilationDiagnostic.FromRazorDiagnostic).ToList(),
+                BaseCompilation = compilation,
+                Code = cSharpDocument.GeneratedCode,
+                Diagnostics = cSharpDocument.Diagnostics.Select(CompilationDiagnostic.FromRazorDiagnostic).ToList(),
             };
 
             // Result of doing 'temp' compilation
@@ -212,32 +181,21 @@ namespace BlazorRepl.Shared
             var references = compilation.References.Concat(new[] { tempAssembly.Compilation.ToMetadataReference() }).ToArray();
             projectEngine = CreateProjectEngine(references);
 
-            // Now update the any additional files
-            foreach (var item in AdditionalRazorItems)
-            {
-                // Result of generating declarations
-                codeDocument = DesignTime ? projectEngine.ProcessDesignTime(item) : projectEngine.Process(item);
-
-                // Replace the 'declaration' syntax tree
-                var syntaxTree = Parse(codeDocument.GetCSharpDocument().GeneratedCode, path: item.FilePath);
-                AdditionalSyntaxTrees.RemoveAll(st => st.FilePath == item.FilePath);
-                AdditionalSyntaxTrees.Add(syntaxTree);
-            }
-
             await (updateStatusFunc?.Invoke("Preparing Project") ?? Task.CompletedTask);
 
             // Result of real code generation for the document
-            codeDocument = DesignTime ? projectEngine.ProcessDesignTime(projectItem) : projectEngine.Process(projectItem);
+            codeDocument = projectEngine.Process(projectItem);
+            cSharpDocument = codeDocument.GetCSharpDocument();
 
             return new CompileToCSharpResult
             {
-                BaseCompilation = compilation.AddSyntaxTrees(AdditionalSyntaxTrees),
-                Code = codeDocument.GetCSharpDocument().GeneratedCode,
-                Diagnostics = codeDocument.GetCSharpDocument().Diagnostics.Select(CompilationDiagnostic.FromRazorDiagnostic).ToList(),
+                BaseCompilation = compilation,
+                Code = cSharpDocument.GeneratedCode,
+                Diagnostics = cSharpDocument.Diagnostics.Select(CompilationDiagnostic.FromRazorDiagnostic).ToList(),
             };
         }
 
-        internal RazorProjectItem CreateProjectItem(string cshtmlRelativePath, string cshtmlContent)
+        private RazorProjectItem CreateProjectItem(string cshtmlRelativePath, string cshtmlContent)
         {
             var fullPath = WorkingDirectory + PathSeparator + cshtmlRelativePath;
 
@@ -248,17 +206,14 @@ namespace BlazorRepl.Shared
                 filePath = '/' + filePath;
             }
 
-            if (NormalizeSourceLineEndings)
-            {
-                cshtmlContent = cshtmlContent.Replace("\r", "").Replace("\n", LineEnding);
-            }
+            cshtmlContent = cshtmlContent.Replace("\r", "").Replace("\n", LineEnding);
 
             return new VirtualProjectItem(
                 WorkingDirectory,
                 filePath,
                 fullPath,
                 cshtmlRelativePath,
-                FileKind,
+                FileKinds.Component,
                 Encoding.UTF8.GetBytes(cshtmlContent.TrimStart()));
         }
 
@@ -271,10 +226,7 @@ namespace BlazorRepl.Shared
                 // Turn off checksums, we're testing code generation.
                 b.Features.Add(new SuppressChecksum());
 
-                if (LineEnding != null)
-                {
-                    b.Phases.Insert(0, new ForceLineEndingPhase(LineEnding));
-                }
+                b.Phases.Insert(0, new ForceLineEndingPhase(LineEnding));
 
                 // Features that use Roslyn are mandatory for components
                 CompilerFeatures.Register(b);
