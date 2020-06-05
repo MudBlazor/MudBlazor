2f851717c73e16e7f99dec15223e15c070662d5c
KristianMariyanov <kristian.mariyanov@gmail.com>
2020-06-05 21:43:30 +0300

    extract separate components for error list and loader

---
+++ Client/Components/ErrorList.razor.cs
@@ -0,0 +1,46 @@
+﻿using BlazorRepl.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRepl.Client.Components
+{
+    public partial class ErrorList : ComponentBase
+    {
+        [Parameter]
+        public IReadOnlyCollection<CompilationDiagnostic> Diagnostics { get; set; } = Array.Empty<CompilationDiagnostic>();
+
+        [Parameter]
+        public int UserComponentCodeStartLine { get; set; }
+        
+        [Parameter]
+        public bool Show { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> ShowChanged { get; set; }
+
+        public int ErrorsCount { get; set; }
+
+        public int WarningsCount { get; set; }
+
+        public bool ShowIcon => Diagnostics.Any();
+
+        protected override Task OnInitializedAsync()
+        {
+            ErrorsCount = Diagnostics.Count(d => d.Severity == DiagnosticSeverity.Error);
+            WarningsCount = Diagnostics.Count(d => d.Severity == DiagnosticSeverity.Warning);
+
+            return base.OnInitializedAsync();
+        }
+
+        public void ToggleDiagnostics()
+        {
+            Show = !Show;
+            ShowChanged.InvokeAsync(Show);
+        }
+
+    }
+}
---
+++ Client/Components/ReplLoader.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRepl.Client.Components
+{
+    public partial class ReplLoader : ComponentBase
+    {
+        [Parameter]
+        public bool Show { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+    }
+}
