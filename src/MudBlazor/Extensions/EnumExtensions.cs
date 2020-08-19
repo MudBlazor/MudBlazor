668f751e7d8529c4cf5e1a1bea1e2fc0d569e680
Garderoben <jonny@gardnet.nu>
2020-08-19 17:37:19 +0200

    Added EnumExtensions to convert Enum Descriptions

---
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazor.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescriptionString(this Position val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+
+        public static string ToDescriptionString(this Color val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+
+        public static string ToDescriptionString(this Variant val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+
+        public static string ToDescriptionString(this Edge val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+
+        public static string ToDescriptionString(this Size val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}
