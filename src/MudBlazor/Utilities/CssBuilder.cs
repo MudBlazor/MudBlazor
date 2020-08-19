aeb2d3c3227834d1a49f1e345dd92f7f9110d632
Garderoben <jonny@gardnet.nu>
2020-08-19 17:46:29 +0200

    Added CssBuilder as utilities for the components.

---
+++ src/MudBlazor/Utilities/CssBuilder.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MudBlazor.Utilities
+{
+    public struct CssBuilder
+    {
+        private string stringBuffer;
+
+        /// <summary>
+        /// Creates a CssBuilder used to define conditional CSS classes used in a component.
+        /// Call Build() to return the completed CSS Classes as a string. 
+        /// </summary>
+        /// <param name="value"></param>
+        public static CssBuilder Default(string value) => new CssBuilder(value);
+
+        /// <summary>
+        /// Creates an Empty CssBuilder used to define conditional CSS classes used in a component.
+        /// Call Build() to return the completed CSS Classes as a string. 
+        /// </summary>
+        public static CssBuilder Empty() => new CssBuilder();
+
+        /// <summary>
+        /// Creates a CssBuilder used to define conditional CSS classes used in a component.
+        /// Call Build() to return the completed CSS Classes as a string. 
+        /// </summary>
+        /// <param name="value"></param>
+        public CssBuilder(string value) => stringBuffer = value;
+
+        /// <summary>
+        /// Adds a raw string to the builder that will be concatenated with the next class or value added to the builder.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddValue(string value)
+        {
+            stringBuffer += value;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a CSS Class to the builder with space separator.
+        /// </summary>
+        /// <param name="value">CSS Class to add</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(string value) => AddValue(" " + value);
+
+        /// <summary>
+        /// Adds a conditional CSS Class to the builder with space separator.
+        /// </summary>
+        /// <param name="value">CSS Class to conditionally add.</param>
+        /// <param name="when">Condition in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(string value, bool when = true) => when ? this.AddClass(value) : this;
+
+        /// <summary>
+        /// Adds a conditional CSS Class to the builder with space separator.
+        /// </summary>
+        /// <param name="value">CSS Class to conditionally add.</param>
+        /// <param name="when">Condition in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(string value, Func<bool> when = null) => this.AddClass(value, when());
+
+        /// <summary>
+        /// Adds a conditional CSS Class to the builder with space separator.
+        /// </summary>
+        /// <param name="value">Function that returns a CSS Class to conditionally add.</param>
+        /// <param name="when">Condition in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(Func<string> value, bool when = true) => when ? this.AddClass(value()) : this;
+
+        /// <summary>
+        /// Adds a conditional CSS Class to the builder with space separator.
+        /// </summary>
+        /// <param name="value">Function that returns a CSS Class to conditionally add.</param>
+        /// <param name="when">Condition in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(Func<string> value, Func<bool> when = null) => this.AddClass(value, when());
+
+        /// <summary>
+        /// Adds a conditional nested CssBuilder to the builder with space separator.
+        /// </summary>
+        /// <param name="value">CSS Class to conditionally add.</param>
+        /// <param name="when">Condition in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(CssBuilder builder, bool when = true) => when ? this.AddClass(builder.Build()) : this;
+
+        /// <summary>
+        /// Adds a conditional CSS Class to the builder with space separator.
+        /// </summary>
+        /// <param name="value">CSS Class to conditionally add.</param>
+        /// <param name="when">Condition in which the CSS Class is added.</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClass(CssBuilder builder, Func<bool> when = null) => this.AddClass(builder, when());
+
+        /// <summary>
+        /// Adds a conditional CSS Class when it exists in a dictionary to the builder with space separator.
+        /// Null safe operation.
+        /// </summary>
+        /// <param name="additionalAttributes">Additional Attribute splat parameters</param>
+        /// <returns>CssBuilder</returns>
+        public CssBuilder AddClassFromAttributes(IReadOnlyDictionary<string, object> additionalAttributes) =>
+            additionalAttributes == null ? this :
+            additionalAttributes.TryGetValue("class", out var c) ? AddClass(c.ToString()) : this;
+
+        /// <summary>
+        /// Finalize the completed CSS Classes as a string.
+        /// </summary>
+        /// <returns>string</returns>
+        public string Build()
+        {
+            // String buffer finalization code
+            return stringBuffer != null ? stringBuffer.Trim() : string.Empty;
+        }
+
+        // ToString should only and always call Build to finalize the rendered string.
+        public override string ToString() => Build();
+
+    }
+}
