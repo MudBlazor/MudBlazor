c21a38e1d411afc3ca8377b844a0b042b7233d27
Garderoben <jonny@gardnet.nu>
2020-08-24 00:24:21 +0200

    Started working on the list/nav components.

---
+++ src/MudBlazor/Components/List/ListItem.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public class ComponentBaseListItem : ComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-list-item")
+          .AddClass(Class)
+        .Build();
+
+        [Parameter] public string Class { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public string Href { get; set; } = "";
+        [Parameter] public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
