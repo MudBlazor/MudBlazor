3147ce4a623c461d843bfa145ce32b7cbd130a73
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2020-08-24 22:03:02 +0300

    MainLayout now has a back-end file.

---
+++ Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorRepl.Client.Shared
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using BlazorRepl.Client.Components;
+    using BlazorRepl.Core;
+    using Microsoft.AspNetCore.Components;
+
+    public partial class MainLayout
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public PageNotifications PageNotificationsComponent { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ComponentCompilationService.Init(this.HttpClient);
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
