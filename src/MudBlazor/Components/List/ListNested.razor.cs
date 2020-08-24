485b4938cfa766ce02c03f76c91e3a26bdc87b3d
Garderoben <jonny@gardnet.nu>
2020-08-24 18:03:27 +0200

    Finished the NestedLists or "submenus"

--- src/MudBlazor/Components/List/ListNested.razor.cs
+++ src/MudBlazor/Components/List/ListNested.razor.cs
@@ -4,17 +4,25 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public class ComponentBaseListSubItem : ComponentBase
+    public class ComponentBaseListNested : ComponentBase
     {
         protected string Classname =>
-        new CssBuilder("mud-list-item")
+        new CssBuilder("mud-nested-list")
+          .AddClass("expanded", Expanded)
           .AddClass(Class)
         .Build();
 
         [Parameter] public string Class { get; set; }
+        [Parameter] public string Title { get; set; }
         [Parameter] public bool Disabled { get; set; }
+        [Parameter] public bool Expanded { get; set; }
         [Parameter] public string Href { get; set; } = "";
         [Parameter] public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
         [Parameter] public RenderFragment ChildContent { get; set; }
+
+        protected void ExpandedToggle()
+        {
+            Expanded = !Expanded;
+        }
     }
 }
