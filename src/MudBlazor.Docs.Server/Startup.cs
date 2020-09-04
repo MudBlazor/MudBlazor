b836deff97124371a2ce90974b76e7c59cb90189
Garderoben <jonny@gardnet.nu>
2020-09-05 00:36:33 +0200

    Continued work on documentation and structuring scss files for the docs project.

--- src/MudBlazor.Docs.Server/Startup.cs
+++ src/MudBlazor.Docs.Server/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MudBlazor.Docs.Extensions;
 
 namespace MudBlazor.Docs.Server
 {
@@ -27,6 +28,7 @@ namespace MudBlazor.Docs.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.TryAddDocsViewServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/MudBlazor.Docs/Extensions/DocsViewExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Skclusive.Markdown.Component;
+
+namespace MudBlazor.Docs.Extensions
+{
+    public static class DocsViewExtension
+    {
+        public static void TryAddDocsViewServices(this IServiceCollection services)
+        {
+            services.TryAddMarkdownServices();
+        }
+    }
+}
