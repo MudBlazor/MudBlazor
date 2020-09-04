8beb2a42a32b5d4dff286b095d8565afb483f03f
Garderoben <jonny@gardnet.nu>
2020-09-04 16:12:59 +0200

    Added Typography component

---
+++ src/MudBlazor/Components/Typography/Typography.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+
+namespace MudBlazor
+{
+    public class ComponentBaseTypography : ComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-typography")
+          .AddClass($"mud-typography-{Typo.ToDescriptionString()}")
+          .AddClass("mud-typography-gutterbottom")
+          .AddClass(Class)
+        .Build();
+
+        [Parameter] public Typo Typo { get; set; } = Typo.body1;
+        [Parameter] public string Class { get; set; }
+        [Parameter]  public RenderFragment ChildContent { get; set; }
+    }
+}
