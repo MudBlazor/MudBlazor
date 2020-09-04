005278c8c6214647e95eaca60f5c95f4c2569872
Garderoben <jonny@gardnet.nu>
2020-09-04 16:11:37 +0200

    Cleaned up the Server side demo so it now uses the Docs project for all pages.

--- src/MudBlazor.Docs.Server/Startup.cs
+++ src/MudBlazor.Docs.Server/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MudBlazor.Docs.Server.Data;
 
 namespace MudBlazor.Docs.Server
 {
@@ -28,7 +27,6 @@ namespace MudBlazor.Docs.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
