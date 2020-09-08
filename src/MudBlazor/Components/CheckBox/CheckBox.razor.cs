d440de219be67144788b1dd7bcdbe36c282e72d4
Garderoben <jonny@gardnet.nu>
2020-09-08 02:06:20 +0200

    Started working on CheckBox component

---
+++ src/MudBlazor/Components/CheckBox/CheckBox.razor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+
+namespace MudBlazor
+{
+    public class ComponentBaseCheckBox : ComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-button-root mud-icon-button mud-checkbox")
+          .AddClass(Class)
+        .Build();
+
+        [Parameter] public string Label { get; set; }
+
+        [Parameter] public bool Disabled { get; set; }
+
+        [Parameter] public string Class { get; set; }
+
+        [Parameter] public bool Checked { get; set; }
+
+        public void CheckboxClicked(object aChecked)
+        {
+            if((bool)aChecked)
+            {
+                Checked = true;
+            }else
+            {
+                Checked = false;
+            }
+            StateHasChanged();
+        }
+    }
+}
