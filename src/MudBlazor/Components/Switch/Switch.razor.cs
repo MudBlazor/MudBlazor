9073be4c890a1ef5cc7703041691103a57c8d38a
Garderoben <jonny@gardnet.nu>
2020-09-12 02:15:09 +0200

    Added Input Switch Component

---
+++ src/MudBlazor/Components/Switch/Switch.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+
+namespace MudBlazor
+{
+    public class ComponentBaseSwitch : ComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-button-root mud-icon-button mud-switch-base")
+            .AddClass($"mud-ripple mud-ripple-switch", !DisableRipple)
+            .AddClass($"mud-switch-{Color.ToDescriptionString()}")
+            .AddClass($"mud-switch-disabled", Disabled)
+            .AddClass($"mud-checked", Checked)
+          .AddClass(Class)
+        .Build();
+
+        [Parameter] public Color Color { get; set; } = Color.Default;
+        [Parameter] public string Label { get; set; }
+        [Parameter] public bool DisableRipple { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public string Class { get; set; }
+        [Parameter]
+        public EventCallback<bool> CheckedChanged { get; set; }
+
+        private bool _checked;
+        [Parameter] public bool Checked
+        {
+            get => _checked;
+            set
+            {
+                if (value != _checked)
+                {
+                    _checked = value;
+                    CheckedChanged.InvokeAsync(value);
+                }
+            }   
+        }
+    }
+}
