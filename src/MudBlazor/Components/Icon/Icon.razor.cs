7096fdc49c5c4e1c996846de6500567d2f41d55a
Garderoben <jonny@gardnet.nu>
2020-09-12 20:55:49 +0200

    Added Icon size option to Icon component

--- src/MudBlazor/Components/Icon/Icon.razor.cs
+++ src/MudBlazor/Components/Icon/Icon.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Extensions;
 using MudBlazor.Utilities;
 
 namespace MudBlazor
@@ -10,12 +11,13 @@ namespace MudBlazor
         protected string Classname =>
         new CssBuilder("")
           .AddClass($"mud-svg-icon-root", !WebFont)
+          .AddClass($"mud-icon-size-{IconSize.ToDescriptionString()}")
           .AddClass(Class)
         .Build();
 
         [Parameter] public string Icon { get; set; }
         [Parameter] public bool WebFont { get; set; }
-
+        [Parameter] public IconSize IconSize { get; set; } = IconSize.Default;
         [Parameter] public string Class { get; set; }
 
         [Parameter] public RenderFragment ChildContent { get; set; }
---
+++ src/MudBlazor/Enums/IconSize.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazor
+{
+    public enum IconSize
+    {
+        [Description("inherit")]
+        Inherit,
+        [Description("small")]
+        Small,
+        [Description("default")]
+        Default,
+        [Description("large")]
+        Large
+    }
+}
