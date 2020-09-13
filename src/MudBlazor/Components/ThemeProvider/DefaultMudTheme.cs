bb1d5c91a3d98ca796372694ef4b357293db37ce
Garderoben <jonny@gardnet.nu>
2020-09-14 00:34:47 +0200

    Added AppBar, Drawer and Surface to MudColors and expanded MudTheme and DefaultMudTheme.

--- src/MudBlazor/Components/ThemeProvider/DefaultMudTheme.cs
+++ src/MudBlazor/Components/ThemeProvider/DefaultMudTheme.cs
@@ -2,12 +2,15 @@
 {
     public class DefaultMudTheme
     {
-        public string Color_Text_Default { get; set; } = "#000";
+        public string Color_Text_Default { get; set; } = "#2f2f2f";
 
         public string Color_OnSurface { get; set; } = "#444";
         public string Color_Surface { get; set; } = "#fff";
         public string Color_Background { get; set; } = "#fff";
-        
+        public string Color_AppBar { get; set; } = "#594ae2";
+        public string Color_OnAppBar { get; set; } = "#fff";
+        public string Color_Drawer { get; set; } = "#fff";
+        public string Color_OnDrawer { get; set; } = "#2f2f2f";
         public string Color_Default { get; set; } = "#000";
         public string Color_OnDefault { get; set; } = "#000";
         public string Color_Primary { get; set; } = "#594ae2";
@@ -18,7 +21,9 @@
         public string Color_Success { get; set; } = "#4caf50";
         public string Color_Warning { get; set; } = "#ff9800";
         public string Color_Danger { get; set; } = "#FF4644";
-        public string Color_Dark { get; set; } = "#494454";
+        public string Color_Dark { get; set; } = "#272c34";
+        public string Color_Background_Grey { get; set; } = "#f5f5f5";
+        public string Color_Border_Default { get; set; } = "#e0e0e0";
         public string BorderRadius_Default { get; set; } = "4px";
     }
 }
