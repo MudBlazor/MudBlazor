df2c88d4f85fcaad54f83ac64e6a682b459361de
Garderoben <jonny@gardnet.nu>
2020-09-14 07:28:56 +0200

    Started with TextField input

---
+++ src/MudBlazor/Components/TextField/TextField.razor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MudBlazor
+{
+    public class ComponentBaseTextField : ComponentBase
+    {
+        protected string LabelClassname =>
+        new CssBuilder()
+           .AddClass($"mud-input-label-shrink mud-focused", Focused)
+          .AddClass(Class)
+        .Build();
+        protected string Classname =>
+        new CssBuilder()
+           .AddClass($"mud-focused", Focused)
+          .AddClass(Class)
+        .Build();
+
+        [Parameter] public string Label { get; set; }
+
+        [Parameter] public string Placeholder { get; set; }
+
+        [Parameter] public bool ReadOnly { get; set; }
+
+        [Parameter] public string Class { get; set; }
+
+        [Parameter] public bool Disabled { get; set; }
+
+        [Parameter] public string Value { get; set; }
+
+        [Parameter] public Variant Variant { get; set; } = Variant.Text;
+
+        private bool Focused { get; set; }
+
+        protected async Task OnClickHandler(MouseEventArgs ev)
+        {
+            Focused = true;
+        }
+
+    }
+}
