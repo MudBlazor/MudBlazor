814a7a439f01613281fb30d9f111a1d4aeed5c00
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-09-14 22:08:54 +0200

    Added SimpleTable, an easy to use material design wrapper around <table>

---
+++ src/MudBlazor/Components/Table/SimpleTable.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+
+
+namespace MudBlazor
+{
+    public class ComponentBaseSimpleTable : ComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder()
+          .AddClass(Class)
+        .Build();
+        [Parameter] public string Class { get; set; }
+
+        // todo: implement
+        [Parameter] public bool Dense { get; set; }
+
+        // todo: implement
+        [Parameter] public bool StickyHeader { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+    }
+}
