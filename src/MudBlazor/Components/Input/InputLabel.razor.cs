af1444acb7e9feb8ce7e009adda2ea748456f686
Garderoben <jonny@gardnet.nu>
2020-09-16 17:58:38 +0200

    Added InputLabel component and css

---
+++ src/MudBlazor/Components/Input/InputLabel.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+
+namespace MudBlazor
+{
+    public class ComponentBaseInputLabel : MudBaseInputTextComponent
+    {
+        protected string Classname =>
+       new CssBuilder("mud-input-label")
+         .AddClass("mud-input-label-animated")
+         .AddClass($"mud-input-label-{Variant.ToDescriptionString()}")
+         .AddClass(Class)
+       .Build();
+
+        [Parameter] public string Class { get; set; }
+    }
+}
