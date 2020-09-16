70b1a4106f6d3705c7db2ee289882ef74d6e828d
Garderoben <jonny@gardnet.nu>
2020-09-16 17:58:20 +0200

    Added Input Component and css

---
+++ src/MudBlazor/Components/Input/Input.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+
+namespace MudBlazor
+{
+    public class ComponentBaseInput : MudBaseInputTextComponent
+    {
+        protected string Classname =>
+       new CssBuilder("mud-input-root")
+         .AddClass("mud-input-underline")
+         .AddClass("mud-disabled", Disabled)
+         .AddClass("mud-error", Error)
+         .AddClass(Class)
+       .Build();
+
+        protected string InputClassname =>
+       new CssBuilder("mud-input")
+         .AddClass("mud-input-underline")
+         .AddClass("mud-disabled", Disabled)
+         .AddClass("mud-error", Error)
+         .AddClass(Class)
+       .Build();
+
+        [Parameter] public string Class { get; set; }
+
+        protected string InputType => new CssBuilder().AddClass(Type.ToDescriptionString()).Build();
+    }
+}
