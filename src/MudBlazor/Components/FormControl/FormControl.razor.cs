3caf93387ac090b303955ac1d0e50950e9c0caf7
Garderoben <jonny@gardnet.nu>
2020-09-16 17:57:41 +0200

    Added FormControl component.

---
+++ src/MudBlazor/Components/FormControl/FormControl.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public class ComponentBaseFormControl : ComponentBase
+    {
+        protected string Classname =>
+       new CssBuilder("mud-form-control")
+         .AddClass(Class)
+       .Build();
+
+        [Parameter] public string Class { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+    }
+}
