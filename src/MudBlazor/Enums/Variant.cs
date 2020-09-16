bac97a74462e39eb94f272270849abdd47eaa028
Garderoben <jonny@gardnet.nu>
2020-09-16 17:59:52 +0200

    Removed regular, dense and contained from Enum Variant. Contained will now be Filled and regular will be set as default in components who use it. Dense will be a bool.

--- src/MudBlazor/Enums/Variant.cs
+++ src/MudBlazor/Enums/Variant.cs
@@ -4,17 +4,11 @@ namespace MudBlazor
 {
     public enum Variant
     {
-        [Description("regular")]
-        Regular,
-        [Description("dense")]
-        Dense,
         [Description("text")]
         Text,
-        [Description("outlined")]
-        Outlined,
-        [Description("contained")]
-        Contained,
         [Description("filled")]
         Filled,
+        [Description("outlined")]
+        Outlined
     }
 }
