6730b5b853c4a637363cecc5c89be355565e5f10
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-09-19 22:28:33 +0200

    Table: added Style and renamed RenderFragments

--- src/MudBlazor/Components/Table/MudTable.razor.cs
+++ src/MudBlazor/Components/Table/MudTable.razor.cs
@@ -6,7 +6,7 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public partial class MudTable : ComponentBase
+    public partial class MudTable : MudComponentBase
     {
         protected string Classname =>
         new CssBuilder("mud-table")
@@ -17,20 +17,17 @@ namespace MudBlazor
           .AddClass(Class)
         .Build();
 
-        [Parameter] public string Class { get; set; }
         [Parameter] public int Elevation { set; get; } = 1;
         [Parameter] public bool Square { get; set; }
         [Parameter] public bool Outlined { get; set; }
         [Parameter] public bool Dense { get; set; }
         [Parameter] public string Title { get; set; }
         [Parameter] public bool FixedHeader { get; set; }
-        [Parameter] public bool HideHeader { get; set; } = false;
         [Parameter] public int RowsPerPage { get; set; } = 10;
-        [Parameter] public RenderFragment MudTableToolBar { get; set; }
-        [Parameter] public RenderFragment MudTableHeader { get; set; }
-        [Parameter] public RenderFragment MudTableBody { get; set; }
-        [Parameter] public RenderFragment ChildContent { get; set; }
-
+        [Parameter] public RenderFragment ToolBarContent { get; set; }
+        [Parameter] public RenderFragment HeaderContent { get; set; }
+        [Parameter] public RenderFragment BodyContent { get; set; }
+        [Parameter] public RenderFragment PagerContent { get; set; }
 
         public void NavigateTo(Page page)
         {
--- src/MudBlazor/Components/Table/MudTablePager.razor.cs
+++ src/MudBlazor/Components/Table/MudTablePager.razor.cs
@@ -6,7 +6,8 @@ namespace MudBlazor
     public partial class MudTablePager : ComponentBase
     {
         [CascadingParameter] public MudTable Table { get; set; }
-        [Parameter] public bool DisabelRowsPerPage { get; set; }
+
+        [Parameter] public bool DisableRowsPerPage { get; set; }
 
         [Parameter] public List<int> RowsPerPage { get; set; } = new List<int>() { 10, 25, 50, 100 };
 
