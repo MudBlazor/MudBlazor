504911a379cb35ea6412495b36f2bf98671e6c66
Garderoben <jonny@gardnet.nu>
2020-09-19 21:06:21 +0200

    Css fixes to MudTable

--- src/MudBlazor/Components/Table/MudTable.razor.cs
+++ src/MudBlazor/Components/Table/MudTable.razor.cs
@@ -9,11 +9,17 @@ namespace MudBlazor
     public partial class MudTable : ComponentBase
     {
         protected string Classname =>
-        new CssBuilder()
+        new CssBuilder("mud-table")
+           .AddClass($"mud-table-outlined", Outlined)
+           .AddClass($"mud-table-square", Square)
+           .AddClass($"mud-elevation-{Elevation.ToString()}", !Outlined)
           .AddClass(Class)
         .Build();
-        [Parameter] public string Class { get; set; }
 
+        [Parameter] public string Class { get; set; }
+        [Parameter] public int Elevation { set; get; } = 1;
+        [Parameter] public bool Square { get; set; }
+        [Parameter] public bool Outlined { get; set; }
         [Parameter] public bool Dense { get; set; }
         [Parameter] public string Title { get; set; }
         [Parameter] public bool FixedHeader { get; set; }
