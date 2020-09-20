aef83b8de8287705fbaec916c4a1f0e0d49494e6
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-09-20 15:01:56 +0200

    Table: implemented pagination

--- src/MudBlazor/Components/Table/MudTable.razor.cs
+++ src/MudBlazor/Components/Table/MudTable.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.Utilities;
 
@@ -6,8 +7,10 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public class MudTableBase : MudComponentBase
+    public abstract class MudTableBase : MudComponentBase
     {
+        // note: the MudTable code is split. Everything that has nothing to do with the type parameter of MudTable<T> is here in MudTableBase
+
         protected string Classname =>
         new CssBuilder("mud-table")
            .AddClass($"mud-table-dense", Dense)
@@ -25,6 +28,8 @@ namespace MudBlazor
         [Parameter] public bool Hover { get; set; }
         [Parameter] public bool FixedHeader { get; set; }
         [Parameter] public int RowsPerPage { get; set; } = 10;
+        [Parameter] public int CurrentPage { get; set; } = 0;
+
         [Parameter] public RenderFragment ToolBarContent { get; set; }
         [Parameter] public RenderFragment HeaderContent { get; set; }
         //[Parameter] public RenderFragment<T> RowTemplate { get; set; } <-- see MudTable.razor
@@ -32,12 +37,30 @@ namespace MudBlazor
 
         public void NavigateTo(Page page)
         {
-         
+            switch (page)
+            {
+                case Page.First:
+                    CurrentPage = 0;
+                    break;
+                case Page.Last:
+                    CurrentPage = Math.Max(0, NumPages-1);
+                    break;
+                case Page.Next:
+                    CurrentPage = Math.Min(NumPages - 1, CurrentPage +1);
+                    break;
+                case Page.Previous:
+                    CurrentPage = Math.Max(0, CurrentPage-1);
+                    break;
+            }
+            StateHasChanged();
         }
 
         public void SetRowsPerPage(int size)
         {
             RowsPerPage = size;
+            StateHasChanged();
         }
+
+        protected abstract int NumPages { get; }
     }
 }
