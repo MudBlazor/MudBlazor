400c0753c49fc69108913238c7d025a0c4ed7b37
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-09-20 22:20:38 +0200

    MudTypography -> MudText

--- src/MudBlazor/Components/Typography/MudText.razor.cs
+++ src/MudBlazor/Components/Typography/MudText.razor.cs
@@ -6,8 +6,19 @@ using MudBlazor.Extensions;
 
 namespace MudBlazor
 {
-    public partial class MudText : MudTypography
+    public partial class MudText : MudComponentBase
     {
+        protected string Classname =>
+        new CssBuilder("mud-typography")
+          .AddClass($"mud-typography-{Typo.ToDescriptionString()}")
+          .AddClass("mud-typography-gutterbottom", GutterBottom)
+          .AddClass($"mud-typography-align-{Align.ToDescriptionString()}", Align != Align.Inherit)
+          .AddClass(Class)
+        .Build();
 
+        [Parameter] public Typo Typo { get; set; } = Typo.body1;
+        [Parameter] public Align Align { get; set; } = Align.Inherit;
+        [Parameter] public bool GutterBottom { get; set; } = true;
+        [Parameter]  public RenderFragment ChildContent { get; set; }
     }
 }
