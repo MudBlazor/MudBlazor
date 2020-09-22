a325dc57d2c5f66b339ff67466062ed65c0ad95d
Garderoben <jonny@gardnet.nu>
2020-09-22 21:05:49 +0200

    Added basic slider component

---
+++ src/MudBlazor/Components/Slider/MudSlider.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MudBlazor
+{
+    public class ComponentBaseMudSlider : MudComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-slider")
+          .AddClass(Class)
+        .Build();
+
+        private int _value;
+        [Parameter] public int Min { get; set; } = 0;
+        [Parameter] public int Max { get; set; } = 100;
+        [Parameter] public int Step { get; set; } = 1;
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public EventCallback<int> ValueChanged { get; set; }
+        [Parameter]
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (value != _value)
+                {
+                    _value = value;
+                    ValueChanged.InvokeAsync(value);
+                }
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            if (Value == 0)
+            {
+                Value = (Max - Min) / 2;
+            }
+        }
+    }
+}
