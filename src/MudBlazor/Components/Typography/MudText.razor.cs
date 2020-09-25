d1e33755d7d33906346e7220037b874c9c68ab4b
Garderoben <jonny@gardnet.nu>
2020-09-26 00:13:26 +0200

    Added color option to MudText component

--- src/MudBlazor/Components/Typography/MudText.razor.cs
+++ src/MudBlazor/Components/Typography/MudText.razor.cs
@@ -11,6 +11,7 @@ namespace MudBlazor
         protected string Classname =>
         new CssBuilder("mud-typography")
           .AddClass($"mud-typography-{Typo.ToDescriptionString()}")
+          .AddClass($"mud-typography-color-{Color.ToDescriptionString()}")
           .AddClass("mud-typography-gutterbottom", GutterBottom)
           .AddClass($"mud-typography-align-{Align.ToDescriptionString()}", Align != Align.Inherit)
           .AddClass(Class)
@@ -18,6 +19,7 @@ namespace MudBlazor
 
         [Parameter] public Typo Typo { get; set; } = Typo.body1;
         [Parameter] public Align Align { get; set; } = Align.Inherit;
+        [Parameter] public Color Color { get; set; } = Color.Inherit;
         [Parameter] public bool GutterBottom { get; set; } = true;
         [Parameter]  public RenderFragment ChildContent { get; set; }
     }
