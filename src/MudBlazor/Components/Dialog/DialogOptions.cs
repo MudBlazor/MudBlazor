a2cdf7e7f2747f7ed96f180f0f712f0fba2cdc6f
Garderoben <jonny@gardnet.nu>
2020-09-26 17:14:30 +0200

    Moved Dialog Position into Dialog Options. Added MaxWidth option to Dialog.

--- src/MudBlazor/Components/Dialog/DialogOptions.cs
+++ src/MudBlazor/Components/Dialog/DialogOptions.cs
@@ -7,13 +7,36 @@
 // See https://github.com/Blazored
 
 
+using System.ComponentModel;
+
 namespace MudBlazor.Dialog
 {
     public class DialogOptions
     {
         public DialogPosition? Position { get; set; }
+
+        public MaxWidth? MaxWidth { get; set; }
+
         public bool? DisableBackdropClick { get; set; }
         public bool? NoHeader { get; set; }
         public bool? CloseButton { get; set; }
+        public bool? FullScreen { get; set; }
+        public bool? FullWidth { get; set; }
+    }
+
+    public enum DialogPosition
+    {
+        [Description("center")]
+        Center,
+        [Description("topleft")]
+        TopLeft,
+        [Description("topright")]
+        TopRight,
+        [Description("bottomleft")]
+        BottomLeft,
+        [Description("bottomright")]
+        BottomRight,
+        [Description("center")]
+        Custom
     }
 }
--- src/MudBlazor/Extensions/EnumExtensions.cs
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -93,5 +93,17 @@ namespace MudBlazor.Extensions
             var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
             return attributes.Length > 0 ? attributes[0].Description : string.Empty;
         }
+
+        public static string ToDescriptionString(this Dialog.DialogPosition val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+
+        public static string ToDescriptionString(this Margin val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
     }
 }
