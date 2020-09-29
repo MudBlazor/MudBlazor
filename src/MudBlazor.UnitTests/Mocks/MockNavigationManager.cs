d8d4e3c029d586eb1f97fdaf0be333bcf65d4ecc
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-09-29 20:39:39 +0200

    Unit tests: injected services that were required by the components

---
+++ src/MudBlazor.UnitTests/Mocks/MockNavigationManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace MudBlazor.UnitTests.Mocks
+{
+    public class MockNavigationManager
+        : NavigationManager
+    {
+        public MockNavigationManager() : base() =>
+            this.Initialize("http://localhost:2112/", "http://localhost:2112/test");
+
+        protected override void NavigateToCore(string uri, bool forceLoad) =>
+            this.WasNavigateInvoked = true;
+
+        public bool WasNavigateInvoked { get; private set; }
+    }
+}
