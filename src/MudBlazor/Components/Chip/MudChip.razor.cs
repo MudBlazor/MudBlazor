7d2ae3bbf78dc3942b346639c579f9f8d2e93fed
Jonny <jonny@gardnet.nu>
2020-10-02 11:45:13 +0200

    Added basic MudChip component

---
+++ src/MudBlazor/Components/Chip/MudChip.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using MudBlazor.Extensions;
+
+namespace MudBlazor
+{
+    public partial class MudChip : MudComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-chip")
+          .AddClass($"mud-chip-color-{Color.ToDescriptionString()}")
+          .AddClass(Class)
+        .Build();
+
+        [Parameter] public Color Color { get; set; } = Color.Default;
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
