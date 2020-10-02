1fb4112d2977931f93cf6410c55b0ea06bc2074f
Jonny <jonny@gardnet.nu>
2020-10-02 11:45:59 +0200

    Added DividerType to Divider component

---
+++ src/MudBlazor/Enums/DividerType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazor
+{
+    public enum DividerType
+    {
+        [Description("fullwidth")]
+        FullWidth,
+        [Description("inset")]
+        Inset,
+        [Description("middle")]
+        Middle
+    }
+}
