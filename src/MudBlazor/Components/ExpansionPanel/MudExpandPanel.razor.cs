b6a4fc61477b877730b452624d810ce7b0ebbdc2
Jonny <jonny@gardnet.nu>
2020-10-10 04:38:23 +0200

    Added new component MudExpansionPanel

---
+++ src/MudBlazor/Components/ExpansionPanel/MudExpandPanel.razor.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using System;
+using System.Linq;
+
+namespace MudBlazor
+{
+    public partial class MudExpandPanel : MudComponentBase
+    {
+        [CascadingParameter] private MudExpansionPanels Parent { get; set; }
+
+        protected string Classname =>
+        new CssBuilder("mud-expand-panel")
+            .AddClass("mud-panel-expanded", Expanded)
+            .AddClass("mud-panel-next-expanded", NextPanelExpanded)
+            .AddClass("mud-disabled", Disabled)
+            .AddClass($"mud-elevation-{Parent.Elevation.ToString()}")
+        .Build();
+
+        [Parameter] public string Text { get; set; }
+        [Parameter] public bool Expanded { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        private bool NextPanelExpanded { get; set; }
+        private int ActivePanelIndex { get; set; }
+        private int PreviousPanelIndex { get; set; }
+        public void ExpandPanel()
+        {
+            if (!Disabled)
+            {
+                if (Parent.MultipleExpand)
+                {
+                    SetExpanded();
+                    SetPreviousPanelIndex();
+                }
+                else
+                {
+                    SetExpanded();
+                    SetPreviousPanelIndex();
+                    if (Parent.ExpandedPanelSet)
+                    {
+                        Parent.ExpandPanel();
+                    }
+                    Parent.SetExpandedPanel(ActivePanelIndex);
+                }
+            }
+        }
+
+        public void SetExpanded()
+        {
+            Expanded = !Expanded;
+        }
+
+        public void SetNextPanelExpanded()
+        {
+            NextPanelExpanded = !NextPanelExpanded;
+        }
+
+        public void SetPreviousPanelIndex()
+        {
+            if (ActivePanelIndex >= 1)
+            {
+                PreviousPanelIndex = ActivePanelIndex - 1;
+                var PreviousPanel = Parent.Panels.ElementAt(PreviousPanelIndex);
+                Parent.AddPreviousPanelClass(PreviousPanel);
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            if (Parent == null)
+                throw new ArgumentNullException(nameof(Parent), "ExpandPanel must exist within a ExpansionPanels component");
+            base.OnInitialized();
+            Parent.AddPanel(this);
+            ActivePanelIndex = Parent.Panels.IndexOf(this);
+        }
+    }
+}
---
+++ src/MudBlazor/Components/ExpansionPanel/MudExpansionPanels.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MudBlazor
+{
+    public partial class MudExpansionPanels : MudComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-expansion-panels")
+            .AddClass($"mud-expansion-panels-square", Square)
+        .Build();
+
+        [Parameter] public bool Square { get; set; }
+        [Parameter] public bool MultipleExpand { get; set; }
+        [Parameter] public int Elevation { set; get; } = 1;
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        public bool ExpandedPanelSet { get; set; }
+        public int ExpandedPanelIndex { get; set; }
+        public List<MudExpandPanel> Panels = new List<MudExpandPanel>();
+
+        internal void AddPanel(MudExpandPanel expandPanel)
+        {
+            Panels.Add(expandPanel);
+            StateHasChanged();
+        }
+
+        internal void AddPreviousPanelClass(MudExpandPanel expandPanel)
+        {
+            expandPanel.SetNextPanelExpanded();
+            StateHasChanged();
+        }
+
+        internal void SetExpandedPanel(int index)
+        {
+            ExpandedPanelIndex = index;
+            ExpandedPanelSet = true;
+        }
+
+        internal void ExpandPanel()
+        {
+            Panels.ElementAt(ExpandedPanelIndex).SetExpanded();
+            Panels.ElementAt(ExpandedPanelIndex).SetPreviousPanelIndex();
+            StateHasChanged();
+        }
+    }
+}
