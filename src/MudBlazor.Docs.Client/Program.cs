eb8f2b132faca33d376b9513d5d1d1643fa9d392
Jonny <jonny@gardnet.nu>
2020-10-11 13:32:47 +0200

    Added MudBlazor services to Client project, it now works again!

--- src/MudBlazor.Docs.Client/Program.cs
+++ src/MudBlazor.Docs.Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Docs.Extensions;
 
 namespace MudBlazor.Docs.Client
 {
@@ -18,7 +19,8 @@ namespace MudBlazor.Docs.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.TryAddDocsViewServices();
+            
             await builder.Build().RunAsync();
         }
     }
