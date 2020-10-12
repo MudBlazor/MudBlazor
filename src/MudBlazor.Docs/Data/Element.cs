0d4006ef3688473ab7fa9de56cff393ad3306418
Jonny Larsson <jonny@gardnet.nu>
2020-10-12 15:38:45 +0200

    Restructured Docs project, removed using and added to import
    Removed unused Code files.

---
+++ src/MudBlazor.Docs/Data/Element.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace MudBlazor.Docs.Data
+{
+    public class Element
+    {
+        [JsonProperty("group")]
+        public string Group { get; set; }
+
+        [JsonProperty("position")]
+        public int Position { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("number")]
+        public int Number { get; set; }
+
+        [JsonProperty("small")]
+        public string Sign { get; set; }
+
+        [JsonProperty("molar")]
+        public double Molar { get; set; }
+
+    }
+}
--- src/MudBlazor.Docs/Models/MudIcons.cs
+++ src/MudBlazor.Docs/Models/MudIcons.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace MudBlazor.Docs.Models
 {
     public class MudIcons
