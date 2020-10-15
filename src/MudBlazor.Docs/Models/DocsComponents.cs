6c01f51353c314dc271df4a96b34f8d5fa8087b3
Jonny Larsson <jonny@gardnet.nu>
2020-10-15 17:07:02 +0200

    Added API pages for components.

---
+++ src/MudBlazor.Docs/Models/DocsComponents.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MudBlazor.Docs.Models
+{
+    public class DocsComponents
+    {
+        private List<MudComponent> _mudComponents;
+
+        public DocsComponents()
+        {
+            _mudComponents = new List<MudComponent>();
+        }
+
+        public DocsComponents AddItem(string name, Type component)
+        {
+            var componentItem = new MudComponent
+            {
+                Name = name,
+                Link = name.ToLower().Replace(" ", ""),
+                Component = component
+            };
+
+            _mudComponents.Add(componentItem);
+
+            return this;
+        }
+
+        internal List<MudComponent> ToList()
+        {
+            var Item = _mudComponents.ToList();
+
+            return Item.ToList();
+        }
+    }
+}
---
+++ src/MudBlazor.Docs/Models/MudComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MudBlazor.Docs.Models
+{
+    public class MudComponent
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public Type Component { get; set; }
+
+    }
+}
