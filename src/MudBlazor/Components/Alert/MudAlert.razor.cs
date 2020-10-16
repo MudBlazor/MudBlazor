8a38ddc37faae57a2a27243f7109417310d11c81
Jonny Larsson <jonny@gardnet.nu>
2020-10-16 23:38:49 +0200

    Added all comments to MudAlert

--- src/MudBlazor/Components/Alert/MudAlert.razor.cs
+++ src/MudBlazor/Components/Alert/MudAlert.razor.cs
@@ -18,13 +18,44 @@ namespace MudBlazor
           .AddClass(Class)
         .Build();
 
+        /// <summary>
+        /// The higher the number, the heavier the drop-shadow. 0 for no shadow.
+        /// </summary>
         [Parameter] public int Elevation { set; get; } = 0;
+
+        /// <summary>
+        /// If true, rounded corners are disabled.
+        /// </summary>
         [Parameter] public bool Square { get; set; }
+
+        /// <summary>
+        /// If true, compact padding will be used.
+        /// </summary>
         [Parameter] public bool Dense { get; set; }
+
+        /// <summary>
+        /// If true, no alert icon will be used.
+        /// </summary>
         [Parameter] public bool NoIcon { get; set; }
+
+        /// <summary>
+        /// The severity of the alert. This defines the color and icon used.
+        /// </summary>
         [Parameter] public Severity Severity { get; set; } = Severity.Normal;
+
+        /// <summary>
+        /// The variant to use.
+        /// </summary>
         [Parameter] public Variant Variant { get; set; } = Variant.Text;
+
+        /// <summary>
+        /// User class names, separated by space.
+        /// </summary>
         [Parameter] public string Class { set; get; }
+
+        /// <summary>
+        /// Child content of the component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
         public string Icon;
