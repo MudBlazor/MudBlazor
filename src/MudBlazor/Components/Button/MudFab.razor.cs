9f119bd77ef7b78888a84dafaf9e2ff11cdfb30b
Jonny Larsson <jonny@gardnet.nu>
2020-10-16 23:39:10 +0200

    Added all comments to MudFab

--- src/MudBlazor/Components/Button/MudFab.razor.cs
+++ src/MudBlazor/Components/Button/MudFab.razor.cs
@@ -19,18 +19,39 @@ namespace MudBlazor
           .AddClass(Class)
         .Build();
 
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
         [Parameter] public Color Color { get; set; } = Color.Default;
 
+        /// <summary>
+        /// The Size of the component.
+        /// </summary>
         [Parameter] public Size Size { get; set; } = Size.Large;
 
+        /// <summary>
+        /// If applied Icon will be added to the component.
+        /// </summary>
         [Parameter] public string Icon { get; set; }
 
+        /// <summary>
+        /// If applied the text will be added to the component.
+        /// </summary>
         [Parameter] public string Label { get; set; }
 
+        /// <summary>
+        /// If true, the button will be disabled.
+        /// </summary>
         [Parameter] public bool Disabled { get; set; }
 
+        /// <summary>
+        /// If true, no drop-shadow will be used.
+        /// </summary>
         [Parameter] public bool DisableElevation { get; set; }
 
+        /// <summary>
+        /// If true, disables ripple effect.
+        /// </summary>
         [Parameter] public bool DisableRipple { get; set; }
 
     }
