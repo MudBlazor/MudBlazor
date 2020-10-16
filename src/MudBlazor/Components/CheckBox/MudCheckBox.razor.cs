b383c966c15cc7ff96aacdbe00580f32e300c4a3
Jonny Larsson <jonny@gardnet.nu>
2020-10-16 23:39:50 +0200

    Added all comments to MudCheckBox

--- src/MudBlazor/Components/CheckBox/MudCheckBox.razor.cs
+++ src/MudBlazor/Components/CheckBox/MudCheckBox.razor.cs
@@ -24,12 +24,34 @@ namespace MudBlazor
           .AddClass(Class)
         .Build();
 
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
         [Parameter] public Color Color { get; set; } = Color.Default;
+
+        /// <summary>
+        /// If applied the text will be added to the component.
+        /// </summary>
         [Parameter] public string Label { get; set; }
+
+        /// <summary>
+        /// If true, disables ripple effect.
+        /// </summary>
         [Parameter] public bool DisableRipple { get; set; }
+
+        /// <summary>
+        /// If true, the checkbox will be disabled.
+        /// </summary>
         [Parameter] public bool Disabled { get; set; }
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
+        /// <summary>
+        /// A callback when CheckedChanges.
+        /// </summary>
         [Parameter]
         public EventCallback<bool> CheckedChanged { get; set; }
 
