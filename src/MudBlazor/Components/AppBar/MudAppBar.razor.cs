f2a46631ec30866df7504b6f9945fd5b06293276
Jonny Larsson <jonny@gardnet.nu>
2020-10-16 23:38:38 +0200

    Added all comments to MudAppBar

--- src/MudBlazor/Components/AppBar/MudAppBar.razor.cs
+++ src/MudBlazor/Components/AppBar/MudAppBar.razor.cs
@@ -19,14 +19,29 @@ namespace MudBlazor
                 .AddClass(Class)
                 .Build();
 
+        /// <summary>
+        /// The higher the number, the heavier the drop-shadow. 0 for no shadow.
+        /// </summary>
         [Parameter] public int Elevation { set; get; } = 4;
 
+        /// <summary>
+        /// If true, compact padding will be used.
+        /// </summary>
         [Parameter] public bool Dense { get; set; }
 
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
         [Parameter] public Color Color { get; set; } = Color.Primary;
 
+        /// <summary>
+        /// The positioning type. The behavior of the different options is described in the MDN web docs. Note: sticky is not universally supported and will fall back to static when unavailable.
+        /// </summary>
         [Parameter] public Position Position { get; set; } = Position.Fixed;
 
+        /// <summary>
+        /// Child content of the component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
         [CascadingParameter] LayoutState LayoutState { get; set; }
