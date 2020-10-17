d50012ae8e83b5f83397d7656a613049e695a1b7
Jonny Larsson <jonny@gardnet.nu>
2020-10-17 02:07:22 +0200

    Added all comments to MudFormControl

--- src/MudBlazor/Components/FormControl/MudFormControl.razor.cs
+++ src/MudBlazor/Components/FormControl/MudFormControl.razor.cs
@@ -13,10 +13,27 @@ namespace MudBlazor
          .AddClass(Class)
        .Build();
 
+        /// <summary>
+        /// If string has value, label text will be added.
+        /// </summary>
         [Parameter] public string Label { get; set; }
+
+        /// <summary>
+        /// If true, the component will take up the full width of its container.
+        /// </summary>
         [Parameter] public bool FullWidth { get; set; }
+
+        /// <summary>
+        /// If string has value, helper text will be added.
+        /// </summary>
         [Parameter] public string HelperText { get; set; }
+        /// <summary>
+        /// 	If dense or normal, will adjust vertical spacing of this and contained components.
+        /// </summary>
         [Parameter] public Margin Margin { get; set; } = Margin.None;
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
     }
