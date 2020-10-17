cd786200688b54b2366500779cd0a9f7a8bf30c2
Jonny Larsson <jonny@gardnet.nu>
2020-10-17 04:45:17 +0200

    Added all comments to MudSider

--- src/MudBlazor/Components/Slider/MudSlider.razor.cs
+++ src/MudBlazor/Components/Slider/MudSlider.razor.cs
@@ -14,10 +14,30 @@ namespace MudBlazor
         .Build();
 
         private int _value;
+
+        /// <summary>
+        /// The minimum allowed value of the slider. Should not be equal to max.
+        /// </summary>
         [Parameter] public int Min { get; set; } = 0;
+
+        /// <summary>
+        /// The maximum allowed value of the slider. Should not be equal to min.
+        /// </summary>
+        /// 
         [Parameter] public int Max { get; set; } = 100;
+        /// <summary>
+        /// How many steps the slider should take on eatch move.
+        /// </summary>
+        /// 
         [Parameter] public int Step { get; set; } = 1;
+        /// <summary>
+        /// If true, the slider will be disabled.
+        /// </summary>
+        /// 
         [Parameter] public bool Disabled { get; set; }
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
         [Parameter] public EventCallback<int> ValueChanged { get; set; }
 
