b07cb1addc3778a7ed3996caec54e97481ffd961
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-10-17 19:34:52 +0200

    Added links between api and component pages

---
+++ src/MudBlazor.Docs/Models/ApiLink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MudBlazor.Docs.Extensions;
+
+namespace MudBlazor.Docs.Models
+{
+    public static class ApiLink
+    {
+        public static string GetApiLinkFor(Type type)
+        {
+            if (!SpecialCaseComponents.TryGetValue(type, out var component))
+                component = type.ToString().Replace("MudBlazor.Mud", "").ToKebabCase();
+            string href = $"api/{component}";
+            return href;
+        }
+
+        public static string GetComponentLinkFor(Type type)
+        {
+            if (!SpecialCaseComponents.TryGetValue(type, out var component))
+                component = type.ToString().Replace("MudBlazor.Mud", "").ToKebabCase();
+            string href = $"components/{component}";
+            return href;
+        }
+
+        static Dictionary<Type, string> SpecialCaseComponents = new Dictionary<Type, string>()
+        {
+            [typeof(MudTable<T>)] = "table",
+        };
+    }
+}
