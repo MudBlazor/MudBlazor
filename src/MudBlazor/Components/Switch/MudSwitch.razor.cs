d45efde8ea02c7665a89dcf8d676b1196b1d7224
Jonny Larsson <jonny@gardnet.nu>
2020-10-17 04:45:23 +0200

    Added all comments to MudSwitch

--- src/MudBlazor/Components/Switch/MudSwitch.razor.cs
+++ src/MudBlazor/Components/Switch/MudSwitch.razor.cs
@@ -25,9 +25,24 @@ namespace MudBlazor
           .AddClass(Class)
         .Build();
 
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
         [Parameter] public Color Color { get; set; } = Color.Default;
+
+        /// <summary>
+        /// The text/label will be displayed next to the switch if set.
+        /// </summary>
         [Parameter] public string Label { get; set; }
+
+        /// <summary>
+        /// If true, disables ripple effect.
+        /// </summary>
         [Parameter] public bool DisableRipple { get; set; }
+
+        /// <summary>
+        /// If true, the switch will be disabled.
+        /// </summary>
         [Parameter] public bool Disabled { get; set; }
 
         [Parameter] public EventCallback<bool> CheckedChanged { get; set; }
