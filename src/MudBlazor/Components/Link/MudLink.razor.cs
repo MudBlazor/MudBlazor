d6ce0bdc709ea8770a08b8260136d1fd615b9084
Jonny Larsson <jonny@gardnet.nu>
2020-10-17 02:07:47 +0200

    Added all comments to MudLink

--- src/MudBlazor/Components/Link/MudLink.razor.cs
+++ src/MudBlazor/Components/Link/MudLink.razor.cs
@@ -19,16 +19,32 @@ namespace MudBlazor
           .AddClass(Class)
         .Build();
 
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
         [Parameter] public Color Color { get; set; } = Color.Primary;
 
+        /// <summary>
+        /// Typography variant to use.
+        /// </summary>
         [Parameter] public Typo Typo { get; set; } = Typo.body1;
 
+        /// <summary>
+        /// Controls when the link should have an underline.
+        /// </summary>
         [Parameter] public Underline Underline { get; set; } = Underline.Hover;
 
+        /// <summary>
+        /// The URL, which is the actual link.
+        /// </summary>
         [Parameter] public string Href { get; set; }
+
 
         [Parameter] public string Target { get; set; }
 
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
     }
 }
