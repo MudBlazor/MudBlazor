c573bc44be7f6506f4299df70394dbc7c7e105e1
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-10-19 19:09:02 +0200

    Revert "Removed Snippets, it is obsolete"
    This reverts commit 1577e45eb712f225abf88fd1b87619a064e0e641.

---
+++ src/MudBlazor.Docs/Models/Snippets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MudBlazor.Docs.Models
+{
+
+    [Obsolete("We don'T need that any more")]
+    public static partial class Snippets
+    {
+        public static string GetCode(string component)
+        {
+            var field= typeof(Snippets).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField)
+                .FirstOrDefault(f => f.Name == component);
+            if (field==null)
+                return $"Snippet for component '{component}' not found!";
+            return (string)field.GetValue(null);
+        }
+    }
+}
