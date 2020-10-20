577961c4e595ad76ee21ad6ea9e7d886369251e5
Jonny Larsson <jonny@gardnet.nu>
2020-10-20 15:53:17 +0200

    Added comments on MudBaseSelectItem

--- src/MudBlazor/Base/MudBaseSelectItem.cs
+++ src/MudBlazor/Base/MudBaseSelectItem.cs
@@ -9,11 +9,34 @@ namespace MudBlazor
 {
     public abstract class MudBaseSelectItem : MudComponentBase
     {
+        /// <summary>
+        /// If true, the input element will be disabled.
+        /// </summary>
         [Parameter] public bool Disabled { get; set; }
+
+        /// <summary>
+        /// If true, disables ripple effect.
+        /// </summary>
         [Parameter] public bool DisableRipple { get; set; }
+
+        /// <summary>
+        /// Link to a URL when clicked.
+        /// </summary>
         [Parameter] public string Href { get; set; }
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Command parameter.
+        /// </summary>
         [Parameter] public object CommandParameter { get; set; }
+
+        /// <summary>
+        /// Command executed when the user clicks on an element.
+        /// </summary>
         [Parameter] public ICommand Command { get; set; }
         [Inject] public Microsoft.AspNetCore.Components.NavigationManager UriHelper { get; set; }
 
