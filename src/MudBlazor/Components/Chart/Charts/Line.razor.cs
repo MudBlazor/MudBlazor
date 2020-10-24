ba0adbc8fd704745a87035553d11cd3b2c059f29
Jonny Larsson <jonny@gardnet.nu>
2020-10-24 14:40:40 +0200

    Added Base for Line Chart

---
+++ src/MudBlazor/Components/Chart/Charts/Line.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using MudBlazor.Charts.Models;
+
+namespace MudBlazor.Charts
+{
+    public class LineBase : MudChartBase
+    {
+        public List<ChartSegment> Segments = new List<ChartSegment>();
+        public List<ChartLegend> Legends = new List<ChartLegend>();
+
+        protected override void OnInitialized()
+        {
+            
+        }
+    }
+}
