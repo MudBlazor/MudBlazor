9979169f5543dd5dce6d2571ca720c7fc208b518
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2020-10-24 10:44:39 -0700

    Added Button Type

--- src/MudBlazor/Extensions/EnumExtensions.cs
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -135,5 +135,11 @@ namespace MudBlazor.Extensions
             var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
             return attributes.Length > 0 ? attributes[0].Description : string.Empty;
         }
+
+        public static string ToDescriptionString(this ButtonType val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
     }
 }
