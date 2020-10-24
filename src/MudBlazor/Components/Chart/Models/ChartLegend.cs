62b5b0cb3df1e6cf1c829d485ebf828f25e3f276
Jonny Larsson <jonny@gardnet.nu>
2020-10-24 14:39:35 +0200

    Models for ChartLegend and ChartSegments

---
+++ src/MudBlazor/Components/Chart/Models/ChartLegend.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MudBlazor.Charts.Models
+{
+    public class ChartLegend
+    {
+        public int Index { get; set; }
+        public string Labels { get; set; }
+        public string Data { get; set; }
+    }
+}
