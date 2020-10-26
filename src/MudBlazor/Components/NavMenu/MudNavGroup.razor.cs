41d99b2d5b3a3668c305610e83dcc028d1057a52
Jonny Larsson <jonny@gardnet.nu>
2020-10-26 20:02:21 +0100

    Added support for NavMenu Icons and Expander icon.

--- src/MudBlazor/Components/NavMenu/MudNavGroup.razor.cs
+++ src/MudBlazor/Components/NavMenu/MudNavGroup.razor.cs
@@ -12,6 +12,11 @@ namespace MudBlazor
         .Build();
 
         [Parameter] public string Title { get; set; }
+
+        /// <summary>
+        /// Icon to use if set.
+        /// </summary>
+        [Parameter] public string Icon { get; set; }
         [Parameter] public bool Disabled { get; set; }
         [Parameter] public bool DisableRipple { get; set; }
         [Parameter] public bool Expanded { get; set; }
