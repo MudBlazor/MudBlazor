a94e304d4dfb2a6ad1622bc75ea2e9c3c4e06213
Jonny Larsson <jonny@gardnet.nu>
2020-10-27 19:23:33 +0100

    Added working/moving pointer to TimePicker and fixed landscape text

--- src/MudBlazor/Components/TimePicker/MudTimePicker.razor.cs
+++ src/MudBlazor/Components/TimePicker/MudTimePicker.razor.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace MudBlazor
 {
@@ -46,6 +47,12 @@ namespace MudBlazor
             StateHasChanged();
         }
 
+        protected string ToolbarClass =>
+        new CssBuilder()
+          .AddClass($"mud-picker-timepicker-toolbar")
+          .AddClass($"mud-picker-timepicker-toolbar-landscape", Orientation == Orientation.Landscape && PickerVariant == PickerVariant.Static)
+        .Build();
+
         private string GetClockPinColor()
         {
            return $"mud-picker-time-clock-pin mud-color-{Color.ToDescriptionString()}";
@@ -58,6 +65,79 @@ namespace MudBlazor
         private string GetClockPointerThumbColor()
         {
             return $"mud-picker-time-clock-pointer-thumb mud-color-{Color.ToDescriptionString()}";
+        }
+
+        private string GetPointerRotation()
+        {
+            return $"rotateZ({TimeSet.Degrees.ToString()}deg);";
+        }
+
+        private string GetPointerHeight()
+        {
+            return $"{TimeSet.Height.ToString()}%;";
+        }
+
+        private SetTime TimeSet = new SetTime();
+
+        protected override void OnInitialized()
+        {
+            if (AmPm)
+            {
+                TimeSet.Hour = 1;
+                TimeSet.Minute = 37;
+                TimeSet.Degrees = 30;
+                TimeSet.Height = 40;
+            }
+            else
+            {
+                TimeSet.Hour = 13;
+                TimeSet.Minute = 37;
+                TimeSet.Degrees = 30;
+                TimeSet.Height = 40;
+            }
+        }
+
+        public bool MouseDown { get; set; }
+
+        private void OnMouseDown(MouseEventArgs e)
+        {
+            MouseDown = true;
+        }
+
+        private void OnMouseUp(MouseEventArgs e)
+        {
+            MouseDown = false;
+        }
+
+        private void OnMouseOverHour(SetTime setTime)
+        {
+            if(MouseDown)
+            {
+                TimeSet.Degrees = setTime.Degrees;
+                TimeSet.Height = setTime.Height;
+                TimeSet.Hour = setTime.Hour;
+            }
+        }
+
+        private void OnMouseOverMinute(SetTime setTime)
+        {
+            if(MouseDown)
+            {
+                TimeSet.Degrees = setTime.Degrees;
+                TimeSet.Height = 40;
+                TimeSet.Minute = setTime.Minute;
+            }
+        }
+
+        private class SetTime
+        {
+            public int Degrees { get; set; }
+
+            public int Height { get; set; }
+
+            public int Hour { get; set; }
+
+            public int Minute { get; set; }
         }
     }
 }
