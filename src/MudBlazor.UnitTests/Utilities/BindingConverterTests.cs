ba70ac0490711e193276bbd8aa79234338d34d96
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-10-30 00:07:31 +0100

    Added DefaultConverter<T> which can convert most types to and from string

---
+++ src/MudBlazor.UnitTests/Utilities/BindingConverterTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MudBlazor.UnitTests.Utilities
+{
+    [TestFixture]
+    public class BindingConverterTests
+    {
+        [Test]
+        public void DefaultConverterTest()
+        {
+            var c1 = new DefaultConverter<string>();
+            c1.Set("hello").Should().Be("hello");
+            c1.Get("hello").Should().Be("hello");
+            var c2 = new DefaultConverter<int>();
+            c2.Set(17).Should().Be("17");
+            c2.Get("17").Should().Be(17);
+            var c3 = new DefaultConverter<int?>();
+            c3.Set(17).Should().Be("17");
+            c3.Get("17").Should().Be(17);
+            c3.Set(null).Should().Be(null);
+            c3.Get(null).Should().Be(null);
+            var c4 = new DefaultConverter<double?>();
+            c4.Set(1.7).Should().Be("1.7");
+            c4.Get("1.7").Should().Be(1.7);
+            c4.Set(null).Should().Be(null);
+            c4.Get(null).Should().Be(null);
+            c4.Culture = CultureInfo.GetCultureInfo("de-AT");
+            c4.Set(1.7).Should().Be("1,7");
+            c4.Get("1,7").Should().Be(1.7);
+            var c5 = new DefaultConverter<ButtonType>();
+            c5.Set(ButtonType.Button).Should().Be("Button");
+            c5.Get("Button").Should().Be(ButtonType.Button);
+            var c6 = new DefaultConverter<DateTime?>();
+            var date = DateTime.Today;
+            c6.Get(c6.Set(date)).Should().Be(date);
+            c6.Set(null).Should().Be(null);
+            c6.Get(null).Should().Be(null);
+            var c7 = new DefaultConverter<TimeSpan?>();
+            var time = DateTime.Now.TimeOfDay;
+            c7.Get(c7.Set(time)).Should().Be(time);
+            c7.Set(null).Should().Be(null);
+            c7.Get(null).Should().Be(null);
+        }
+    }
+}
---
+++ src/MudBlazor/Utilities/BindingConverters/DefaultConverter.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Globalization;
+
+namespace MudBlazor
+{
+    
+    /// <summary>
+    /// A universal T to string binding converter
+    /// </summary>
+    public class DefaultConverter<T> : Converter<T>
+    {
+       
+        public DefaultConverter()
+        {
+            SetFunc = OnSet;
+            GetFunc = OnGet;
+        }
+
+        private T OnGet(string value)
+        {
+            try
+            {
+                if (typeof(T) == typeof(string))
+                {
+                    return (T)(object)value;
+                }
+                else if (typeof(T) == typeof(int) || typeof(T) == typeof(int?))
+                {
+                    if (int.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
+                        return (T)(object)parsedValue;
+                    OnError?.Invoke("Not a valid number");
+                }
+                else if (typeof(T) == typeof(double) || typeof(T) == typeof(double?))
+                {
+                    if (double.TryParse(value, NumberStyles.Any, Culture, out var parsedValue))
+                        return (T)(object)parsedValue;
+                    OnError?.Invoke("Not a valid number");
+                }
+                else if (typeof(T) == typeof(Guid))
+                {
+                    if (Guid.TryParse(value, out var parsedValue))
+                        return (T)(object)parsedValue;
+                    OnError?.Invoke("Not a valid GUID");
+                }
+                else if (typeof(T).IsEnum)
+                {
+                    if (Enum.TryParse(typeof(T), value, out var parsedValue))
+                        return (T)parsedValue;
+                    OnError?.Invoke("Not a value of " + typeof(T).Name);
+                }
+                else if (typeof(T) == typeof(DateTime) || typeof(T) == typeof(DateTime?))
+                {
+                    if (DateTime.TryParse(value, Culture,  DateTimeStyles.None, out var parsedValue))
+                        return (T)(object)parsedValue;
+                    OnError?.Invoke("Not a valid date time");
+                }
+                else if (typeof(T) == typeof(TimeSpan) || typeof(T) == typeof(TimeSpan?))
+                {
+                    if (TimeSpan.TryParse(value, Culture, out var parsedValue))
+                        return (T)(object)parsedValue;
+                    OnError?.Invoke("Not a valid date time");
+                }
+            }
+            catch (Exception e)
+            {
+                OnError?.Invoke("Conversion error: "+e.Message);
+                return default(T);
+            }
+            OnError?.Invoke($"Conversion to type {typeof(T)} not implemented");
+            return default(T);
+        }
+
+        private string OnSet(T arg)
+        {
+            if (arg == null)
+                return null;
+            try
+            {
+                if (typeof(T) == typeof(string))
+                {
+                    return (string)(object)arg;
+                }
+                else if (typeof(T) == typeof(int) )
+                {
+                    var value = (int) (object) arg;
+                    return value.ToString(Culture);
+                }
+                else if (typeof(T) == typeof(int?))
+                {
+                    var value = (int?) (object) arg;
+                    if (value == null)
+                        return null;
+                    return value.Value.ToString(Culture);
+                }
+                else if (typeof(T) == typeof(double))
+                {
+                    var value = (double) (object) arg;
+                    return value.ToString(Culture);
+                }
+                else if (typeof(T) == typeof(double?))
+                {
+                    var value = (double?) (object) arg;
+                    if (value == null)
+                        return null;
+                    return value.Value.ToString(Culture);
+                }
+                else if (typeof(T) == typeof(Guid))
+                {
+                    var value = (Guid) (object) arg;
+                    return value.ToString();
+                }
+                else if (typeof(T).IsEnum)
+                {
+                    var value = (Enum) (object) arg;
+                    return value.ToString();
+                }
+                else if (typeof(T) == typeof(DateTime))
+                {
+                    var value = (DateTime) (object) arg;
+                    return value.ToString(Culture);
+                }  
+                else if (typeof(T) == typeof(DateTime?))
+                {
+                    var value = (DateTime?) (object) arg;
+                    if (value == null)
+                        return null;
+                    return value.Value.ToString(Culture);
+                }               
+                else if (typeof(T) == typeof(TimeSpan))
+                {
+                    var value = (TimeSpan) (object) arg;
+                    return value.ToString();
+                }  
+                else if (typeof(T) == typeof(TimeSpan?))
+                {
+                    var value = (TimeSpan?) (object) arg;
+                    if (value == null)
+                        return null;
+                    return value.Value.ToString();
+                }               
+                return arg.ToString( );
+            }
+            catch (FormatException e)
+            {
+                OnError?.Invoke("Conversion error: "+e.Message);
+                return null;
+            }
+        }
+    }
+}
