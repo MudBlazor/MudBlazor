0d4194c7ada8a56d447704ad1804903f24a1fa74
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-10-29 20:49:37 +0100

    Added binding converters

---
+++ src/MudBlazor/Utilities/BindingConverters/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MudBlazor
+{
+    public class Converter<T, U>
+    {
+        public Func<T, U> SetFunc { get; set; } 
+        public Func<U, T> GetFunc { get; set; }
+
+        public U Set(T value)
+        {
+            if (SetFunc==null)
+                return default(U);
+            return SetFunc(value);
+        }
+        
+        public T Get(U value)
+        {
+            if (GetFunc==null)
+                return default(T);
+            return GetFunc(value);
+        }
+    }
+
+    public class Converter<T> : Converter<T, string>
+    {
+        
+    }
+}
