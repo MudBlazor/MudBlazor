6955eb9e7a299950e7aff1f2bc6f2ccd3c254015
Henrique Clausing <hclausing@hotmail.com>
2020-11-04 10:07:59 -0300

    MudTextInput default style for table cell, by @Garderoben

--- src/MudBlazor/Extensions/EnumExtensions.cs
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -141,5 +141,12 @@ namespace MudBlazor.Extensions
             var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
             return attributes.Length > 0 ? attributes[0].Description : string.Empty;
         }
+
+        public static string ToDescriptionString(this LegendPosition val)
+        {
+            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+
     }
 }
