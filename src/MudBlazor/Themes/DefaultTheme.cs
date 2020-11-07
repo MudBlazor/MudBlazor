199cc7d1a1500c986258d7b838e047d70dafcaca
Jonny Larsson <jonny@gardnet.nu>
2020-11-07 21:35:33 +0100

    Added the new MudTheme classes and defaults

---
+++ src/MudBlazor/Themes/DefaultTheme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MudBlazor.Theme.Defaults;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public class DefaultTheme : MudTheme
+    {
+        public DefaultTheme()
+        {
+            Breakpoints = new Breakpoints();
+            Palette = new Palette();
+            LayoutProperties = new LayoutProperties();
+            Shadow = new Shadow();
+            ZIndex = new ZIndex();
+        }
+    }
+}
---
+++ src/MudBlazor/Themes/MudTheme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MudBlazor.Theme.Defaults;
+
+namespace MudBlazor
+{
+    public class MudTheme
+    {
+        public Breakpoints Breakpoints { get; set; }
+        public Palette Palette { get; set; }
+        public Shadow Shadow { get; set; }
+        //public Typography Typography { get; set; }
+        public LayoutProperties LayoutProperties { get; set; }
+        public ZIndex ZIndex { get; set; }
+    }
+}
