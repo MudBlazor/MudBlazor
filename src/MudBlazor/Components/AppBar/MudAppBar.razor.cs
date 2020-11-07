a5618750710aced06cd80400324990f1fbf153b4
Jonny Larsson <jonny@gardnet.nu>
2020-11-07 21:48:21 +0100

    Added Drawer Anchor  to MudLayout and Layout state for  test

--- src/MudBlazor/Components/AppBar/MudAppBar.razor.cs
+++ src/MudBlazor/Components/AppBar/MudAppBar.razor.cs
@@ -12,10 +12,12 @@ namespace MudBlazor
         protected string Classname =>
             new CssBuilder("mud-appbar")
                 .AddClass($"mud-appbar-position-{Position.ToDescriptionString()}")
-                .AddClass($"mud-appbar-drawer-open", LayoutState?.DrawerOpen)
-                .AddClass($"mud-appbar-drawer-clipped", LayoutState?.DrawerClipped)
+                .AddClass($"mud-left", LayoutState.DrawerAnchor == Anchor.Left)
+                .AddClass($"mud-right", LayoutState.DrawerAnchor == Anchor.Right)
+                .AddClass($"mud-appbar-drawer-open", LayoutState.DrawerOpen)
+                .AddClass($"mud-appbar-drawer-clipped", LayoutState.DrawerClipped)
                 .AddClass($"mud-elevation-{Elevation.ToString()}")
-                .AddClass($"mud-appbar-color-{Color.ToDescriptionString()}")
+                .AddClass($"mud-appbar-color-{Color.ToDescriptionString()}", Color != Color.Default)
                 .AddClass(Class)
                 .Build();
 
@@ -32,7 +34,7 @@ namespace MudBlazor
         /// <summary>
         /// The color of the component. It supports the theme colors.
         /// </summary>
-        [Parameter] public Color Color { get; set; } = Color.Primary;
+        [Parameter] public Color Color { get; set; } = Color.Default;
 
         /// <summary>
         /// The positioning type. The behavior of the different options is described in the MDN web docs. Note: sticky is not universally supported and will fall back to static when unavailable.
--- src/MudBlazor/Components/Layout/LayoutState.cs
+++ src/MudBlazor/Components/Layout/LayoutState.cs
@@ -4,5 +4,6 @@
     {
         public bool DrawerOpen { get; set; }
         public bool DrawerClipped { get; set; }
+        public Anchor DrawerAnchor { get; set; }
     }
 }
