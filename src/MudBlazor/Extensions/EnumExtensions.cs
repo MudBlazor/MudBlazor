50b28ffd5bc29a55d8698172acf5e0eeefa07d2b
Jonny Larsson <jonny@gardnet.nu>
2020-11-09 12:44:41 +0100

    Removed Position Enum

--- src/MudBlazor/Extensions/EnumExtensions.cs
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -4,12 +4,6 @@ namespace MudBlazor.Extensions
 {
     public static class EnumExtensions
     {
-        public static string ToDescriptionString(this Position val)
-        {
-            var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
-        }
-
         public static string ToDescriptionString(this Color val)
         {
             var attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
