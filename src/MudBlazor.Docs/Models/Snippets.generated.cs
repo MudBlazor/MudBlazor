d4fc910a6ae6dde0ebc1db1831432cde4ddd47a7
Jonny Larsson <jonny@gardnet.nu>
2020-11-11 19:55:10 +0100

    Generated code examples.

--- src/MudBlazor.Docs/Models/Snippets.generated.cs
+++ src/MudBlazor.Docs/Models/Snippets.generated.cs
@@ -3165,14 +3165,14 @@ public const string OverviewThemesMultipleExample = @"<MudThemeProvider Theme=""
             Surface = ""#373740"",
             DrawerBackground = ""#27272f"",
             DrawerText = ""rgba(255,255,255, 0.50)"",
-            DrawerIcon = ""rgba(255,255,255, 0.50)""
+            DrawerIcon = ""rgba(255,255,255, 0.50)"",
             AppbarBackground = ""#27272f"",
             AppbarText = ""rgba(255,255,255, 0.70)"",
             TextPrimary = ""rgba(255,255,255, 0.70)"",
             TextSecondary = ""rgba(255,255,255, 0.50)"",
             ActionDefault = ""#adadb1"",
             ActionDisabled = ""rgba(255,255,255, 0.26)"",
-            ActionDisabledBackground = ""rgba(255,255,255, 0.12)"",
+            ActionDisabledBackground = ""rgba(255,255,255, 0.12)""
         }
     };
 }";
