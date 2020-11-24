29c0125eb18cbeef95008afa886ad3624d95e970
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-11-24 18:20:11 +0100

    ResizeListenerService: changed from 300ms to 100ms per default because it responds too lazily to window resizing

--- src/MudBlazor/Services/ResizeListener/ResizeOptions.cs
+++ src/MudBlazor/Services/ResizeListener/ResizeOptions.cs
@@ -6,7 +6,7 @@
         /// Rate in milliseconds that the browsers `resize()` event should report a change.
         /// Setting this value too low can cause poor application performance.
         /// </summary>
-        public int ReportRate { get; set; } = 300;
+        public int ReportRate { get; set; } = 100;
 
         /// <summary>
         /// Report resize events and media queries in the browser's console.
