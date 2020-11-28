cb5e6f5de7a2010cc254ca3c2cf9b8d90b006f79
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-11-28 15:02:43 +0100

    Added a form validation test

---
+++ src/MudBlazor.UnitTests/Components/FormTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.UnitTests.Mocks;
+using NUnit.Framework;
+using static MudBlazor.UnitTests.SelectWithEnumTest;
+
+namespace MudBlazor.UnitTests
+{
+
+    [TestFixture]
+    public class FormTests
+    {
+        /// <summary>
+        /// Setting the required textfield's value should set IsValid true
+        /// Clearing the value of a required textfield should set form's IsValid to false.
+        /// </summary>
+        [Test]
+        public async Task FormIsValidTest() {
+            using var ctx = new Bunit.TestContext();
+            var comp = ctx.RenderComponent<FormIsValidTest>();
+            Console.WriteLine(comp.Markup);
+            var form = comp.FindComponent<MudForm>().Instance;
+            var textField = comp.FindComponent<MudTextField<string>>().Instance;
+            // check initial state: form should not be valid, but text field does not display an error initially!
+            form.IsValid.Should().Be(false);
+            textField.Error.Should().BeFalse();
+            textField.ErrorText.Should().BeNullOrEmpty();
+            await comp.InvokeAsync(()=> textField.Value = "Marilyn Manson");
+            form.IsValid.Should().Be(true);
+            form.Errors.Length.Should().Be(0);
+            textField.Error.Should().BeFalse();
+            textField.ErrorText.Should().BeNullOrEmpty();
+            // clear value to null
+            await comp.InvokeAsync(() => textField.Value = null);
+            form.IsValid.Should().Be(false);
+            form.Errors.Length.Should().Be(1);
+            form.Errors[0].Should().Be("Enter a rock star");
+            textField.Error.Should().BeTrue();
+            textField.ErrorText.Should().Be("Enter a rock star");
+            // set value to "" -> should also be an error
+            await comp.InvokeAsync(() => textField.Value = "");
+            form.IsValid.Should().Be(false);
+            form.Errors.Length.Should().Be(1);
+            form.Errors[0].Should().Be("Enter a rock star");
+            textField.Error.Should().BeTrue();
+            textField.ErrorText.Should().Be("Enter a rock star");
+            //
+            await comp.InvokeAsync(() => textField.Value = "Kurt Cobain");
+            form.IsValid.Should().Be(true);
+            form.Errors.Length.Should().Be(0);
+            textField.Error.Should().BeFalse();
+            textField.ErrorText.Should().BeNullOrEmpty();
+        }
+
+    }
+}
