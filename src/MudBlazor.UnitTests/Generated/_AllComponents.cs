336b51c381b51ecbba6655db04bd876ea0bc34ad
Jonny Larsson <jonny@gardnet.nu>
2020-12-10 21:01:10 +0100

    Added more functionality to MudOverlay

--- src/MudBlazor.UnitTests/Generated/_AllComponents.cs
+++ src/MudBlazor.UnitTests/Generated/_AllComponents.cs
@@ -1481,6 +1481,18 @@ namespace MudBlazor.UnitTests.Components
 
 
         [Test]
+        public void OverlayZIndexExample_Test()
+        {
+                using var ctx = new Bunit.TestContext();
+                ctx.Services.AddSingleton<NavigationManager>(new MockNavigationManager());
+                ctx.Services.AddSingleton<IDialogService>(new DialogService());
+                ctx.Services.AddSingleton<ISnackbar>(new MockSnackbar());
+                ctx.Services.AddSingleton<IResizeListenerService>(new MockResizeListenerService());
+                var comp = ctx.RenderComponent<OverlayZIndexExample>();
+        }
+
+
+        [Test]
         public void PaperComponentExample_Test()
         {
                 using var ctx = new Bunit.TestContext();
