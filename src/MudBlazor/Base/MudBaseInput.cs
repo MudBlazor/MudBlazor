22b3b30539419b214bc6e7b9e86ee557c8e3ef4a
Mike Surcouf <mikes@surcouf.co.uk>
2020-12-11 18:22:43 +0000

    Fix CS0105 Repeated namespace

--- src/MudBlazor/Base/MudBaseInput.cs
+++ src/MudBlazor/Base/MudBaseInput.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using MudBlazor.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Interfaces;
 using MudBlazor.Utilities;
 using System.Globalization;
 
