93d43d3a86533d0f626cdd13438d3daac18761ae
Porkopek <porkopek@users.noreply.github.com>
2020-12-11 22:06:01 +0000

    Added Class, Style and UserAttributes to MudTable elements

---
+++ src/MudBlazor/Components/Table/MudTd.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudTd : MudComponentBase
+    {
+
+        protected string Classname => new CssBuilder("mud-table-cell")
+       .AddClass(Class).Build();
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [Parameter] public string DataLabel { get; set; }
+    }
+}
