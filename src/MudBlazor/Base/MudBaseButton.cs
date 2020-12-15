0ccefc460cb5dad803247c0daa51434c99d82c7e
Porkopek <porkopek@users.noreply.github.com>
2020-12-15 20:53:26 +0000

    Changed: all buttons are controlled by MudElement

--- src/MudBlazor/Base/MudBaseButton.cs
+++ src/MudBlazor/Base/MudBaseButton.cs
@@ -16,6 +16,12 @@ namespace MudBlazor
         [Inject] public IJSRuntime JsRuntime { get; set; }
 
         /// <summary>
+        /// The HTML element that will be rendered in the root by the component
+        /// </summary>
+        [Parameter] public string HtmlTag { get; set; }
+
+
+        /// <summary>
         /// The button Type (Button, Submit, Refresh)
         /// </summary>
         [Parameter] public ButtonType ButtonType { get; set; }
@@ -51,22 +57,28 @@ namespace MudBlazor
         [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
 
         protected async Task OnClickHandler(MouseEventArgs ev)
-        {
-            if (Link != null)
-            {
-                if (string.IsNullOrWhiteSpace(Target))
-                    UriHelper.NavigateTo(Link, ForceLoad);
-                else
-                    await JsRuntime.InvokeVoidAsync("blazorOpen", new object[2] { Link, Target });
-            }
-            else
-            {
+        {          
                 await OnClick.InvokeAsync(ev);
                 if (Command?.CanExecute(CommandParameter) ?? false)
                 {
                     Command.Execute(CommandParameter);
-                }
+                }            
+        }
+
+        protected override void OnInitialized()
+        {
+            //default tag for a MudButton is "button"
+            if (string.IsNullOrWhiteSpace(HtmlTag))
+            {
+                HtmlTag = "button";
             }
+
+            //But if Link property is set, it changes to an anchor element automatically
+            if (!string.IsNullOrWhiteSpace(Link))
+            {
+                HtmlTag = "a";
+            }
+            base.OnInitialized();
         }
     }
 }
