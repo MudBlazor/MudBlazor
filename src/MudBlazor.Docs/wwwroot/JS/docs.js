63b0c6f9c985e54eab3e31e189c5083d4984c4af
Porkopek <porkopek@users.noreply.github.com>
2020-12-27 13:55:44 +0000

    Feature: scroll to active NavLink in NavMenu

---
+++ src/MudBlazor.Docs/wwwroot/JS/docs.js
@@ -0,0 +1,21 @@
+﻿//General functions for Docs page 
+var mudBlazorDocs = {
+
+    //scrolls to the active nav link in the NavMenu
+    scrollToActiveNavLink: function () {
+        let element = document.querySelector('.mud-nav-link.active');
+        if (!element) return;
+        element.scrollIntoView({ block: 'center', behavior:'smooth' })
+    }
+}
+
+
+//Functions for examples
+var mudBlazorExamples = {
+
+    //focus an element
+    focusElement: function (el) {
+        el.focus();
+    }
+}
+
