976c339b1bc11314dd203f596a4a89deaabb7778
Porkopek <porkopek@users.noreply.github.com>
2020-12-28 02:24:12 +0000

    Moved code from NavMenu.razor to .razor.cs

---
+++ src/MudBlazor.Docs/Shared/NavMenu.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using MudBlazor.Docs.Extensions;
+using MudBlazor.Docs.Models;
+using MudBlazor.Docs.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazor.Docs.Shared
+{
+    public partial class NavMenu
+    {
+        [Inject] IMenuService MenuService { get; set; }
+       
+        //sections are "getting-started","components", "api", ...
+        string _section;
+
+        //component links are the part of the url that tells us what component is featured
+        string _componentLink;
+
+        
+
+        public void Refresh()
+        {
+            _section = NavMan.GetSection();
+            _componentLink = NavMan.GetComponentLink();
+            StateHasChanged();
+        }
+        
+        
+
+        bool IsSubGroupExpanded(MudComponent item)
+        {
+            #region comment about is subgroup expanded
+            //if the route contains any of the links of the subgroup, then the subgroup
+            //should be expanded
+            //Example:
+            //subgroup: form inputs & controls
+            //the subgroup "form inputs & controls" should be open if the current page has in the route
+            //a component included in the subgroup elements, that in this case are autocomplete, form, field,
+            //radio, select...
+            //this route `/components/autocomplete` should open the subgroup "form inputs..."
+            #endregion
+            return item.GroupItems.Elements.Any(i => i.Link == _componentLink);
+        }
+    }
+}
