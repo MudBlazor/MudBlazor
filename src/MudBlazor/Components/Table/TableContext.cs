f39783aa7ae027fde4d9ff22d0835f7e960dfa04
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-01-01 20:59:00 +0100

    Table: fixed the warnings (#404)

--- src/MudBlazor/Components/Table/TableContext.cs
+++ src/MudBlazor/Components/Table/TableContext.cs
@@ -40,7 +40,7 @@ namespace MudBlazor
             {
                 var row = pair.Value;
                 var item = pair.Key;
-                row.IsChecked = Selection.Contains(item);
+                row.SetChecked(Selection.Contains(item), notify:true);
             }
             // update header checkbox
             if (HeaderRow!=null)
@@ -77,7 +77,7 @@ namespace MudBlazor
         {
             CurrentSortLabel = label;
             if (label.SortDirection == SortDirection.None && override_direction_none)
-                label.SortDirection = SortDirection.Ascending;
+                label.SetSortDirection( SortDirection.Ascending);
             SortDirection = label.SortDirection;
             SortBy = label.SortBy; 
             UpdateSortLabels(label);
@@ -103,7 +103,7 @@ namespace MudBlazor
                 return;
             UpdateSortLabels(initial_sortlabel);
             // this will trigger initial sorting of the table
-            initial_sortlabel.SortDirection = initial_sortlabel.InitialDirection;
+            initial_sortlabel.SetSortDirection( initial_sortlabel.InitialDirection);
         }
 
 
