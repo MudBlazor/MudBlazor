4c617d224bdba0f744eda0b2af38fdc9a3de53a1
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-01-02 09:17:01 +0100

    Removed obsolete namespaces

--- src/MudBlazor/RemovedNamespaces/Components.Select.cs
+++
@@ -1,9 +0,0 @@
-namespace MudBlazor.Components.Select
-{
-    // This namespace has been removed.
-    // This file serves as a placeholder
-    // to prevent breaking existing using statements
-    public class Removed
-    {
-    }
-}
