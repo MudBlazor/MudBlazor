bad45a23623f8694ec4f418e0b15f1394f90bf47
Codinkat <46028532+Codinkat@users.noreply.github.com>
2021-01-03 20:06:26 -0500

    Add focus trap component

---
+++ src/MudBlazor/Components/FocusTrap/MudFocusTrap.razor.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+namespace MudBlazor
+{
+    public partial class MudFocusTrap
+    {
+        protected ElementReference _firstBumper;
+        protected ElementReference _lastBumper;
+        protected ElementReference _root;
+
+        private bool _tabDown;
+        private bool _shiftDown;
+        private bool _disabled;
+
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
+
+        /// <summary>
+        /// Child content of the component.
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// If true, the focus will no longer loops inside the element.
+        /// </summary>
+        [Parameter] public bool Disabled
+        {
+            get => _disabled;
+            set
+            {
+                if (_disabled != value)
+                {
+                    _disabled = value;
+
+                    if (!_disabled)
+                        FocusFirst();
+                }
+            }
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender && !_disabled)
+                FocusFirst();
+        }
+
+        private void OnBumperFocus(FocusEventArgs args)
+        {
+            if (_tabDown)
+            {
+                if (_shiftDown)
+                    FocusLast();
+                else
+                    FocusFirst();
+            }
+        }
+
+        private void FocusFirst()
+        {
+            JsRuntime.InvokeVoidAsync("elementReference.focusFirst", _root, 1);
+        }
+
+        private void FocusLast()
+        {
+            JsRuntime.InvokeVoidAsync("elementReference.focusLast", _root, 1);
+        }
+
+        private void OnRootKeyDown(KeyboardEventArgs args)
+        {
+            HandleKeyEvent(args, true);
+        }
+
+        private void OnRootKeyUp(KeyboardEventArgs args)
+        {
+            HandleKeyEvent(args, false);
+        }
+
+        private void HandleKeyEvent(KeyboardEventArgs args, bool isDown)
+        {
+            switch (args.Key)
+            {
+                case "Tab":
+                    _tabDown = isDown;
+                    break;
+
+                case "Shift":
+                    _shiftDown = isDown;
+                    break;
+            }
+        }
+    }
+}
--- src/MudBlazor/TScripts/elementReference.js
+++ src/MudBlazor/TScripts/elementReference.js
@@ -1,6 +1,38 @@
 ﻿// functions that can be called on html element references
 window.elementReference = {
     blur: function (element) {
-            element.blur();
+        element.blur();
+    },
+    focus: function (element) {
+        element.focus();
+    },
+    focusFirst: function (element, skip = 0) {
+        var tabbables = getTabbableElements(element);
+        if (tabbables.length <= skip)
+            element.focus();
+        else
+            tabbables[skip].focus();
+    },
+    focusLast: function (element, skip = 0) {
+        var tabbables = getTabbableElements(element);
+        if (tabbables.length <= skip)
+            element.focus();
+        else
+            tabbables[tabbables.length - skip - 1].focus();
     }
+};
+
+function getTabbableElements(element) {
+    return element.querySelectorAll(
+        "a[href]:not([tabindex='-1'])," +
+        "area[href]:not([tabindex='-1'])," +
+        "button:not([disabled]):not([tabindex='-1'])," +
+        "input:not([disabled]):not([tabindex='-1']):not([type='hidden'])," +
+        "select:not([disabled]):not([tabindex='-1'])," +
+        "textarea:not([disabled]):not([tabindex='-1'])," +
+        "iframe:not([tabindex='-1'])," +
+        "details:not([tabindex='-1'])," +
+        "[tabindex]:not([tabindex='-1'])," +
+        "[contentEditable=true]:not([tabindex='-1']"
+    );
 };
