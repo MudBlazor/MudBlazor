bded088e3305dde48945d94c09c24fde534a7789
zHaytam <haytam.zanid@gmail.com>
2021-01-07 19:01:50 +0100

    Add Item icons and docs/demo

--- src/MudBlazor/Components/Breadcrumbs/BreadcrumbItem.cs
+++ src/MudBlazor/Components/Breadcrumbs/BreadcrumbItem.cs
@@ -5,12 +5,14 @@
         public string Text { get; }
         public string Href { get; }
         public bool Disabled { get; }
+        public string Icon { get; }
 
-        public BreadcrumbItem(string text, string href, bool disabled = false)
+        public BreadcrumbItem(string text, string href, bool disabled = false, string icon = null)
         {
             Text = text;
             Disabled = disabled;
             Href = href;
+            Icon = icon;
         }
     }
 }
