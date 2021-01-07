4db530943fbdf5a53cb55b795815876d8c685adf
zHaytam <haytam.zanid@gmail.com>
2021-01-07 11:11:32 +0100

    Add basic MudBreadcrumbs component

---
+++ src/MudBlazor/Components/Breadcrumbs/BreadcrumbItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MudBlazor
+{
+    public class BreadcrumbItem
+    {
+        public string Text { get; }
+        public string Href { get; }
+        public bool Disabled { get; }
+
+        public BreadcrumbItem(string text, string href, bool disabled = false)
+        {
+            Text = text;
+            Disabled = disabled;
+            Href = href;
+        }
+    }
+}
---
+++ src/MudBlazor/Components/Breadcrumbs/MudBreadcrumbs.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+using System.Collections.Generic;
+
+namespace MudBlazor
+{
+    public partial class MudBreadcrumbs : MudComponentBase
+    {
+        [Parameter] public List<BreadcrumbItem> Items { get; set; }
+
+        [Parameter] public string Separator { get; set; } = "/";
+
+        private static string GetItemClassname(BreadcrumbItem item)
+        {
+            return new CssBuilder("mud-breadcrumb-item")
+                .AddClass("mud-disabled", item.Disabled)
+                .Build();
+        }
+    }
+}
