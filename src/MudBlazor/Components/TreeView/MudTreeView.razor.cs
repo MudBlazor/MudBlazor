f275457186087fa6cf3beca08a027b27ddb9f68b
tungi52 <tungi52@gmail.com>
2021-01-08 12:57:08 +0100

    TreeView templating improvements

--- src/MudBlazor/Components/TreeView/MudTreeView.razor.cs
+++ src/MudBlazor/Components/TreeView/MudTreeView.razor.cs
@@ -7,11 +7,14 @@ namespace MudBlazor
 {
     public partial class MudTreeView : MudComponentBase
     {
-        private MudTreeViewItem activeItem;
+        private MudTreeViewItem _activatedItem;
+        private HashSet<MudTreeViewItem> _selectedItems;
+        private List<MudTreeViewItem> childItems = new List<MudTreeViewItem>();
 
         protected string Classname =>
         new CssBuilder("mud-treeview")
           .AddClass("mud-treeview-canhover", CanHover)
+          .AddClass("mud-treeview-canactivate", CanActivate)
           .AddClass("mud-treeview-expand-on-click", ExpandOnClick)
           .AddClass(Class)
         .Build();
@@ -28,29 +31,109 @@ namespace MudBlazor
         [Parameter]
         public bool CanHover { get; set; }
 
+        [Parameter]
+        public MudTreeViewItem ActivatedItem
+        {
+            get => _activatedItem;
+            set
+            {
+                if (_activatedItem == value)
+                    return;
+
+                InvokeAsync(() => UpdateActivatedItem(_activatedItem, true));
+            }
+        }
+
+        [Parameter]
+        public HashSet<MudTreeViewItem> SelectedItems
+        {
+            get => _selectedItems;
+            set
+            {
+                if (_selectedItems == value)
+                    return;
+
+                if (_selectedItems != null)
+                {
+                    foreach (var item in _selectedItems)
+                    {
+                        _ = item.Select(false);
+                    }
+                }
+
+                if (value != null)
+                {
+                    foreach (var item in value)
+                    {
+                        _ = item.Select(true);
+                    }
+                }
+
+                _selectedItems = value;
+                SelectedItemsChanged.InvokeAsync(_selectedItems);
+            }
+        }
+
+        [Parameter]
+        public EventCallback<MudTreeViewItem> ActivatedItemChanged { get; set; }
+
+        [Parameter]
+        public EventCallback<HashSet<MudTreeViewItem>> SelectedItemsChanged { get; set; }
+
         /// <summary>
         /// Child content of component.
         /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
-        [CascadingParameter] public MudTreeView MudTreeRoot { get; set; }
+        [CascadingParameter] MudTreeView MudTreeRoot { get; set; }
 
         public MudTreeView()
         {
             MudTreeRoot = this;
         }
 
-        internal async Task UpdateActivation(MudTreeViewItem item)
+        internal async Task UpdateActivatedItem(MudTreeViewItem item, bool requestedValue)
         {
-            if (activeItem == item)
+            if ((_activatedItem == item && requestedValue) ||
+                (_activatedItem != item && !requestedValue))
                 return;
 
-            if (activeItem != null)
+            if (_activatedItem == item && !requestedValue)
             {
-                await activeItem.Deactivate();
+                _activatedItem = null;
+                await item.Activate(requestedValue);
+                await ActivatedItemChanged.InvokeAsync(_activatedItem);
+                return;
             }
 
-            activeItem = item;
+            if (_activatedItem != null)
+            {
+                await _activatedItem.Activate(false);
+            }
+
+            _activatedItem = item;
+            await item?.Activate(requestedValue);
+            await ActivatedItemChanged.InvokeAsync(item);
         }
+
+        internal async Task UpdateSelectedItems()
+        {
+            if (_selectedItems == null)
+                _selectedItems = new HashSet<MudTreeViewItem>();
+
+            //collect selected items
+            _selectedItems.Clear();
+            foreach (var item in childItems)
+            {
+                foreach (var selectedItem in item.GetSelectedItems())
+                {
+                    _selectedItems.Add(selectedItem);
+                }
+            }
+
+            await SelectedItemsChanged.InvokeAsync(_selectedItems);
+        }
+
+        internal void AddChild(MudTreeViewItem item) => childItems.Add(item);
     }
 }
