c93f62fff1401d393954ccc58dff089e549f9f37
Codinkat <46028532+Codinkat@users.noreply.github.com>
2021-01-10 17:01:48 -0500

    Add missing code-behind file of Avatar component

---
+++ src/MudBlazor/Components/Avatar/MudAvatar.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.Extensions;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    partial class MudAvatar : MudComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-avatar")
+          .AddClass($"mud-avatar-{Size.ToDescriptionString()}")
+          .AddClass($"mud-avatar-rounded", Rounded)
+          .AddClass($"mud-avatar-square", Square)
+          .AddClass($"mud-theme-{Color.ToDescriptionString()}")
+          .AddClass(Class)
+        .Build();
+
+        /// <summary>
+        /// If true, border-radius is set to 0.
+        /// </summary>
+        [Parameter] public bool Square { get; set; }
+
+        /// <summary>
+        /// If true, border-radius is set to the themes default value.
+        /// </summary>
+        [Parameter] public bool Rounded { get; set; }
+
+        /// <summary>
+        /// Link to image, if set a image will be displayed instead of text.
+        /// </summary>
+        [Parameter] public string Image { get; set; }
+
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
+        [Parameter] public Color Color { get; set; } = Color.Default;
+
+        /// <summary>
+        /// The Size of the MudAvatar.
+        /// </summary>
+        [Parameter] public Size Size { get; set; } = Size.Medium;
+
+        /// <summary>
+        /// Child content of the component.
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
