4fceb67523a122066c7ebaeda331c8d54549a3c8
Mike Surcouf <mikes@surcouf.co.uk>
2021-01-12 17:07:09 +0000

    Remove depreciated CodeBase

--- src/MudBlazor.Docs/Models/XmlDocumentation.cs
+++ src/MudBlazor.Docs/Models/XmlDocumentation.cs
@@ -181,7 +181,7 @@ namespace MudBlazor.Docs.Models
 		/// <returns>The file path of the assembly.</returns>
 		public static string GetDirectoryPath(this Assembly assembly)
 		{
-			string codeBase = assembly.CodeBase;
+			string codeBase = "file://" + assembly.Location;
 			UriBuilder uri = new UriBuilder(codeBase);
 			string path = Uri.UnescapeDataString(uri.Path);
 			return Path.GetDirectoryName(path);
