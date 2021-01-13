d4499b802b966d7a4c661eacc86f2d6579a0fbff
Mike Surcouf <mikes@surcouf.co.uk>
2021-01-13 21:39:44 +0000

    Refactor Treeview after PR

--- src/MudBlazor/Components/TreeView/MudTreeViewItemBuilder.razor.cs
+++ src/MudBlazor/Components/TreeView/MudTreeViewItemBuilder.razor.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Components;
 
 namespace MudBlazor
 {
