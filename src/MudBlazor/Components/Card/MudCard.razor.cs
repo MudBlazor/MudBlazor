174ca94c6a3ec12887553b29d2721289409bfb08
Codinkat <46028532+Codinkat@users.noreply.github.com>
2021-01-17 10:02:06 -0500

    Add missing card codebehind files

---
+++ src/MudBlazor/Components/Card/MudCard.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudCard : MudComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-card")
+          .AddClass(Class)
+        .Build();
+
+        /// <summary>
+        /// The higher the number, the heavier the drop-shadow. 0 for no shadow.
+        /// </summary>
+        [Parameter] public int Elevation { set; get; } = 1;
+
+        /// <summary>
+        /// If true, border-radius is set to 0.
+        /// </summary>
+        [Parameter] public bool Square { get; set; }
+
+        /// <summary>
+        /// If true, card will be outlined.
+        /// </summary>
+        [Parameter] public bool Outlined { get; set; }
+
+        /// <summary>
+        /// Child content of the component.
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
