2cd0b6f04e42734f63840ca498fdc21e83e9b139
Mike Surcouf <mikes@surcouf.co.uk>
2021-01-19 00:06:27 +0000

    Speed up Docs.Compiler

--- src/MudBlazor.Docs.Compiler/Program.cs
+++ src/MudBlazor.Docs.Compiler/Program.cs
@@ -1,15 +1,21 @@
-﻿namespace MudBlazor.Docs.Compiler
+﻿using System;
+using System.Diagnostics;
+
+namespace MudBlazor.Docs.Compiler
 {
     public class Program
     {
         public static int Main()
         {
+            var stopWatch = Stopwatch.StartNew();
             var success =
                 new CodeSnippets().Execute()
                 && new DocStrings().Execute()
                 && new ExamplesMarkup().Execute()
                 && new TestsForExamples().Execute()
                 && new TestsForApiPages().Execute();
+
+            Console.WriteLine($"Docs.Compiler completed in {stopWatch.ElapsedMilliseconds} msecs");
             return success ? 0 : 1;
         }
     }
