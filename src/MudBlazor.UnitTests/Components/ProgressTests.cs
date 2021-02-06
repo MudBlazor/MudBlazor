32dd4469d171a02447d9f93a2b5c74ac7d40de9d
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-02-06 14:47:13 +0100

    Fixed linear deterministic ProgressBar (#752)

--- src/MudBlazor.UnitTests/Components/ProgressTests.cs
+++ src/MudBlazor.UnitTests/Components/ProgressTests.cs
@@ -1,6 +1,7 @@
 ﻿#pragma warning disable CS1998 // async without await
 #pragma warning disable IDE1006 // leading underscore
 
+using System;
 using Bunit;
 using FluentAssertions;
 using NUnit.Framework;
@@ -35,6 +36,7 @@ namespace MudBlazor.UnitTests
                     x.Add(y => y.Value, -400);
                     x.Add(y => y.BufferValue, 400);
                 });
+            Console.WriteLine(comp.Markup);
             // checking range conversion
             comp.Instance.GetValuePercent().Should().Be(10);
             comp.Instance.GetBufferPercent().Should().Be(90);
@@ -44,6 +46,18 @@ namespace MudBlazor.UnitTests
             comp.SetParam(x => x.Min, -500.0);
             comp.SetParam(x => x.Max, 0.0);
             comp.Instance.GetBufferPercent().Should().Be(100);
+            comp.SetParam(x => x.Min, 0.0);
+            comp.SetParam(x => x.Max, 100.0);
+            comp.SetParam(x=>x.Value, 100.0);
+            comp.Instance.GetValuePercent().Should().Be(100);
+            comp.SetParam(x => x.Value, -2.0);
+            comp.SetParam(x => x.Min, -7.0);
+            comp.SetParam(x => x.Max, 7.0);
+            comp.SetParam(x=>x.Buffer, false);
+            var percent = (-2 - (-7)) / 14.0 * 100;
+            comp.Instance.GetValuePercent().Should().Be(percent);
+            comp.Find("div.mud-progress-linear-bar").MarkupMatches(
+                $"<div class=\"mud-progress-linear-bar mud-default mud-progress-linear-bar-1-determinate\" style=\"transform: translateX(-{Math.Round(100 - percent)}%);\"></div>");
         }
     }
 }
