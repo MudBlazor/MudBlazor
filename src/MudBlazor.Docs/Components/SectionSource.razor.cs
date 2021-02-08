3ce4c0abf3c745f1bccf7df3e4832aa9bf7d8a8f
Mike Surcouf <mikes@surcouf.co.uk>
2021-02-08 13:10:37 +0000

    Switch to url transfer of snippets

---
+++ src/MudBlazor.Docs/Components/SectionSource.razor.cs
@@ -0,0 +1,110 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+using MudBlazor.Docs.Models;
+using MudBlazor.Docs.Extensions;
+
+using System;
+using Microsoft.JSInterop;
+using System.Linq;
+
+namespace MudBlazor.Docs.Components
+{
+    public partial class SectionSource
+    {
+        [Inject]
+        protected IJSRuntime _jsRuntime { get; set; }
+
+        [Parameter] public string Title { get; set; }
+
+        [Parameter] public string Code { get; set; }
+
+        [Parameter] public string Class { get; set; }
+
+        [Parameter] public string GitHubFolderName { get; set; }
+
+        [Parameter] public bool ShowCode { get; set; } = true;
+
+        [Parameter] public bool HideButtons { get; set; }
+
+        [Parameter] public bool NoToolbar { get; set; }
+
+        private string GitHubSourceCode { get; set; }
+
+        public string TooltipSourceCodeText { get; set; }
+
+        private string showCodeExampleString { get; set; } = "Show code example";
+        private string hideCodeExampleString { get; set; } = "Hide code example";
+        private string showComponentCodeExampleString { get; set; } = "Show component code example";
+        private string hideComponentCodeExampleString { get; set; } = "Hide component code example";
+
+        private async Task CopyTextToClipboard()
+        {
+            await _jsRuntime.InvokeVoidAsync("clipboardCopy.copyText", Snippets.GetCode(Code));
+        }
+
+        public void OnShowCode()
+        {
+            if (!string.IsNullOrEmpty(Code))
+            {
+                ShowCode = !ShowCode;
+                if (ShowCode)
+                {
+                    TooltipSourceCodeText = hideCodeExampleString;
+                }
+                else
+                {
+                    TooltipSourceCodeText = showCodeExampleString;
+                }
+            }
+        }
+
+        private Type CodeType => Type.GetType("MudBlazor.Docs.Examples.Markup." + Code + "Code");
+
+        RenderFragment CodeComponent() => builder =>
+        {
+            try
+            {
+                var key = typeof(SectionSource).Assembly.GetManifestResourceNames().FirstOrDefault(x => x.Contains($"{Code}Code.html"));
+                using (var stream = typeof(SectionSource).Assembly.GetManifestResourceStream(key))
+                using (var reader = new StreamReader(stream))
+                {
+                    builder.AddMarkupContent(0, reader.ReadToEnd());
+                }
+            }
+            catch (Exception)
+            {
+                // todo: log this
+            }
+        };
+
+        protected virtual async void EditOnTryMudBlazor()
+        {
+            // We use a seperator that wont be in code so we can send 2 files later
+            var codeFile = "__Main.razor" + (char)31 + Snippets.GetCode(Code);
+            var codeFileEncoded = codeFile.ToCompressedEncodedUrl();
+            // var tryMudBlazorLocation = "https://localhost:5001/";
+            var tryMudBlazorLocation = "https://try.mudblazor.com/";
+            var url = $"{tryMudBlazorLocation}snippet/{codeFileEncoded}";
+            await _jsRuntime.InvokeAsync<string>("open", url, "_blank");
+        }
+
+        protected override void OnInitialized()
+        {
+            if (!string.IsNullOrEmpty(GitHubFolderName))
+            {
+                var gitHubLink = "https://github.com/";
+                GitHubSourceCode = $"{gitHubLink}Garderoben/MudBlazor/blob/master/src/MudBlazor.Docs/Pages/Components/{GitHubFolderName}/Examples/{Code}.razor";
+            }
+            if (ShowCode)
+            {
+                TooltipSourceCodeText = hideCodeExampleString;
+            }
+            else
+            {
+                TooltipSourceCodeText = showCodeExampleString;
+            }
+        }
+    }
+}
