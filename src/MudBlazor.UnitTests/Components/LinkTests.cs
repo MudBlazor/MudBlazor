bcfbbda550f10ed6e91c5bf6a349768ab15d77e3
Heisenberg74 <marc744@me.com>
2021-02-11 11:24:51 +0100

    Add tests

---
+++ src/MudBlazor.UnitTests/Components/LinkTests.cs
@@ -0,0 +1,47 @@
+﻿#pragma warning disable CS1998 // async without await
+#pragma warning disable IDE1006 // leading underscore
+
+using System;
+using System.Threading.Tasks;
+using Bunit;
+using FluentAssertions;
+using NUnit.Framework;
+using static Bunit.ComponentParameterFactory;
+
+namespace MudBlazor.UnitTests
+{
+    [TestFixture]
+    public class LinkTests
+    {
+        private Bunit.TestContext ctx;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctx = new Bunit.TestContext();
+            ctx.AddTestServices();
+        }
+
+        /// <summary>
+        /// When Target is not empty, rel attribute should be equals to "noopener noreferrer" on the a element
+        /// </summary>
+        [TestCase(null, "")]
+        [TestCase("", "")]
+        [TestCase("_self", "noopener noreferrer")]
+        [TestCase("_blank", "noopener noreferrer")]
+        [TestCase("_parent", "noopener noreferrer")]
+        [TestCase("_top", "noopener noreferrer")]
+        [TestCase("myFrameName", "noopener noreferrer")]
+        public async Task TextFieldMultiline_CheckRenderedText(string target, string expectedRel)
+        {
+            var comp = ctx.RenderComponent<MudNavLink>(new[]
+            {
+               Parameter(nameof(MudNavLink.Target), target),
+            });
+            // print the generated html
+            Console.WriteLine(comp.Markup);
+            // select elements needed for the test
+            comp.Find("a").GetAttribute("rel").Should().Be(expectedRel);
+        }
+    }
+}
