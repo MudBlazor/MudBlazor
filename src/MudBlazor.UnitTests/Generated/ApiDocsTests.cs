972044a0d16c3835a1ad502ce8f6b7ad48a71268
Mike Surcouf <mikes@surcouf.co.uk>
2021-02-13 12:23:40 +0000

    Remove unecessary usings

--- src/MudBlazor.UnitTests/Generated/ApiDocsTests.cs
+++ src/MudBlazor.UnitTests/Generated/ApiDocsTests.cs
@@ -1,21 +1,11 @@
-using System;
 using System.Net.Http;
 using Bunit;
-using Bunit.Rendering;
-using Bunit.TestDoubles;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
-using MudBlazor.Charts;
-using MudBlazor.Docs.Examples;
-using MudBlazor.Docs.Components;
-using MudBlazor.Docs.Wireframes;
-using MudBlazor.Internal;
 using MudBlazor.Services;
-using MudBlazor.UnitTests;
 using MudBlazor.UnitTests.Mocks;
 using NUnit.Framework;
 using Toolbelt.Blazor.HeadElement;
-using ComponentParameter = Bunit.ComponentParameter;
 
 namespace MudBlazor.UnitTests.Components
 {
@@ -40,7 +30,7 @@ namespace MudBlazor.UnitTests.Components
             ctx.Services.AddSingleton<IDomService>(new MockDomService());
             ctx.Services.AddScoped(sp => new HttpClient());
         }
-        
+
         [TearDown]
         public void TearDown() => ctx.Dispose();
     }
