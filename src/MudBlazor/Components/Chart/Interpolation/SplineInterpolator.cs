1de8c1e18246540488263c43b70e68381d1db788
Mike Surcouf <mikes@surcouf.co.uk>
2021-03-16 17:37:18 +0000

    Tidy-up interpolated charts

--- src/MudBlazor/Components/Chart/Interpolation/SplineInterpolator.cs
+++ src/MudBlazor/Components/Chart/Interpolation/SplineInterpolator.cs
@@ -11,10 +11,10 @@ namespace MudBlazor.Components.Chart
         protected readonly int n;
         protected double[] a, b, c, d, h;
 
-        public double[] givenYs { get; set; }
-        public double[] givenXs { get; set; }
-        public double[] interpolatedXs { get; set ; }
-        public double[] interpolatedYs { get; set; }
+        public double[] GivenYs { get; set; }
+        public double[] GivenXs { get; set; }
+        public double[] InterpolatedXs { get; set; }
+        public double[] InterpolatedYs { get; set; }
         public bool InterpolationRequired { get; set; } = true;
 
         public SplineInterpolator(double[] xs, double[] ys, int resolution = 10)
@@ -31,16 +31,16 @@ namespace MudBlazor.Components.Chart
             if (resolution < 1)
                 throw new ArgumentException("resolution must be 1 or greater");
 
-            givenXs = xs;
-            givenYs = ys;
+            GivenXs = xs;
+            GivenYs = ys;
             n = xs.Length;
 
-            interpolatedXs = new double[n * resolution];
-            interpolatedYs = new double[n * resolution];
+            InterpolatedXs = new double[n * resolution];
+            InterpolatedYs = new double[n * resolution];
         }
         public void Interpolate()
         {
-            int resolution = interpolatedXs.Length / n;
+            int resolution = InterpolatedXs.Length / n;
             for (int i = 0; i < h.Length; i++)
             {
                 for (int k = 0; k < resolution; k++)
@@ -51,8 +51,8 @@ namespace MudBlazor.Components.Chart
                     double termC = c[i] * deltaX * deltaX;
                     double termD = d[i] * deltaX * deltaX * deltaX;
                     int interpolatedIndex = i * resolution + k;
-                    interpolatedXs[interpolatedIndex] = deltaX + givenXs[i];
-                    interpolatedYs[interpolatedIndex] = termA + termB + termC + termD;
+                    InterpolatedXs[interpolatedIndex] = deltaX + GivenXs[i];
+                    InterpolatedYs[interpolatedIndex] = termA + termB + termC + termD;
                 }
             }
 
@@ -62,12 +62,12 @@ namespace MudBlazor.Components.Chart
             int pointsToKeep = resolution * (n - 1) + 1;
             double[] interpolatedXsCopy = new double[pointsToKeep];
             double[] interpolatedYsCopy = new double[pointsToKeep];
-            Array.Copy(interpolatedXs, 0, interpolatedXsCopy, 0, pointsToKeep - 1);
-            Array.Copy(interpolatedYs, 0, interpolatedYsCopy, 0, pointsToKeep - 1);
-            interpolatedXs = interpolatedXsCopy;
-            interpolatedYs = interpolatedYsCopy;
-            interpolatedXs[pointsToKeep - 1] = givenXs[n - 1];
-            interpolatedYs[pointsToKeep - 1] = givenYs[n - 1];
+            Array.Copy(InterpolatedXs, 0, interpolatedXsCopy, 0, pointsToKeep - 1);
+            Array.Copy(InterpolatedYs, 0, interpolatedYsCopy, 0, pointsToKeep - 1);
+            InterpolatedXs = interpolatedXsCopy;
+            InterpolatedYs = interpolatedYsCopy;
+            InterpolatedXs[pointsToKeep - 1] = GivenXs[n - 1];
+            InterpolatedYs[pointsToKeep - 1] = GivenYs[n - 1];
         }
 
         public double Integrate()
