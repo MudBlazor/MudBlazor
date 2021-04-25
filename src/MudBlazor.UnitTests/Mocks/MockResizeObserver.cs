344e039087b7671d5d9648e459b1a41717e2dd9d
Benjamin Kappel <b.kappel90@outlook.de>
2021-04-25 06:59:56 -0500

    Added test to proof #1389 is fixed

--- src/MudBlazor.UnitTests/Mocks/MockResizeObserver.cs
+++ src/MudBlazor.UnitTests/Mocks/MockResizeObserver.cs
@@ -59,6 +59,8 @@ namespace MudBlazor.UnitTests.Mocks
 
         public async Task<BoundingClientRect> Observe(ElementReference element) => (await Observe(new[] { element })).FirstOrDefault();
 
+        private Boolean _firstBatchProcessed = false;
+
         public Task<IEnumerable<BoundingClientRect>> Observe(IEnumerable<ElementReference> elements)
         {
             List<BoundingClientRect> result = new List<BoundingClientRect>();
@@ -66,7 +68,7 @@ namespace MudBlazor.UnitTests.Mocks
             {
                 var size = PanelSize;
                 // last element is alaways TabsContentSize
-                if (item.Id == elements.Last().Id && elements.Count() > 1)
+                if (item.Id == elements.Last().Id && _firstBatchProcessed == false)
                 {
                     size = PanelTotalSize;
                 }
@@ -77,6 +79,8 @@ namespace MudBlazor.UnitTests.Mocks
                 }
                 _cachedValues.Add(item, rect);
             }
+
+            _firstBatchProcessed = true;
 
             return Task.FromResult<IEnumerable<BoundingClientRect>>(result);
         }
