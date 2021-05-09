e238e52193ebb64536392df10eac9273d4ab4de8
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-05-09 21:08:17 +0200

    Charts: added simple chart selection test

---
+++ src/MudBlazor.UnitTests/Components/ChartTests.cs
@@ -0,0 +1,48 @@
+﻿#pragma warning disable CS1998 // async without await
+#pragma warning disable IDE1006 // leading underscore
+
+using System;
+using Bunit;
+using FluentAssertions;
+using MudBlazor.Docs.Examples;
+using MudBlazor.UnitTests.TestComponents;
+using NUnit.Framework;
+
+namespace MudBlazor.UnitTests.Components
+{
+
+    [TestFixture]
+    public class ChartTests
+    {
+        private Bunit.TestContext ctx;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctx = new Bunit.TestContext();
+            ctx.AddTestServices();
+        }
+
+        [TearDown]
+        public void TearDown() => ctx.Dispose();
+
+        /// <summary>
+        /// single checkbox, initialized false, check -  uncheck
+        /// </summary>
+        [Test]
+        public void PieChartSelectionTest()
+        {
+            var comp = ctx.RenderComponent<PieExample1>();
+            // print the generated html
+            Console.WriteLine(comp.Markup);
+            comp.Find("h6").InnerHtml.Trim().Should().Be("Selected portion of the chart: -1");
+            // now click something and see that the selected index changes:
+            comp.FindAll("path.mud-chart-serie")[0].Click();
+            comp.Find("h6").InnerHtml.Trim().Should().Be("Selected portion of the chart: 0");
+            comp.FindAll("path.mud-chart-serie")[3].Click();
+            comp.Find("h6").InnerHtml.Trim().Should().Be("Selected portion of the chart: 3");
+        }
+
+
+    }
+}
