cf9986aff4342b6478559baf3550641b5de28b1c
Jonas B <jonasmitb@gmail.com>
2021-05-20 18:37:56 +0200

    ButtonGroup component added

--- src/MudBlazor.Docs/Services/MenuService.cs
+++ src/MudBlazor.Docs/Services/MenuService.cs
@@ -96,6 +96,7 @@ namespace MudBlazor.Docs.Services
             //Buttons
             .AddNavGroup("Buttons", false, new DocsComponents()
                 .AddItem("Button", typeof(MudButton))
+                .AddItem("Button Group", typeof(MudButtonGroup))
                 .AddItem("IconButton", typeof(MudIconButton))
                 .AddItem("ToggleIconButton", typeof(MudToggleIconButton))
                 .AddItem("Button FAB", typeof(MudFab))
