85754d43df2549317adee0bbe4346c4305d27138
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-05-24 14:23:28 +0200

    Call eventHandler in IsOpen setter

--- src/MudBlazor/Components/Autocomplete/MudAutocomplete.razor.cs
+++ src/MudBlazor/Components/Autocomplete/MudAutocomplete.razor.cs
@@ -123,10 +123,23 @@ namespace MudBlazor
         /// </summary>
         [Parameter] public bool CoerceValue { get; set; }
 
-        internal bool IsOpen { get; set; }
-        protected bool _isOpen { get; set; }
+        protected bool _isOpen;
 
-        public bool IsOpen { get => _isOpen; }
+        /// <summary>
+        /// Returns the open state of the drop-down.
+        /// Note, setting IsOpen will not open or close it. Use ToggleMenu() for that
+        /// </summary>
+        public bool IsOpen
+        {
+            get => _isOpen;
+            protected set
+            {
+                if (value == _isOpen)
+                    return;
+                _isOpen = value;
+                IsOpenChanged.InvokeAsync(_isOpen).AndForget();
+            }
+        }
 
         /// <summary>
         /// An event triggered when the state of IsOpen has changed
