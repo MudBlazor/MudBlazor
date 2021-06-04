45b65cf464a73beefcf671182e88a84831ccc8d6
Aronne Gandini <ii_arrows@hotmail.it>
2021-06-04 02:04:08 +0200

    Implement Format

--- src/MudBlazor/Utilities/BindingConverters/NumericBoundariesConverter.cs
+++ src/MudBlazor/Utilities/BindingConverters/NumericBoundariesConverter.cs
@@ -36,76 +36,76 @@ namespace MudBlazor
                 if (typeof(T) == typeof(sbyte) || typeof(T) == typeof(sbyte?))
                 {
                     if (sbyte.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((sbyte)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                 }
                 // byte
                 else if (typeof(T) == typeof(byte) || typeof(T) == typeof(byte?))
                 {
                     if (byte.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((byte)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // short
                 else if (typeof(T) == typeof(short) || typeof(T) == typeof(short?))
                 {
                     if (short.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((short)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // ushort
                 else if (typeof(T) == typeof(ushort) || typeof(T) == typeof(ushort?))
                 {
                     if (ushort.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((ushort)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // int
                 else if (typeof(T) == typeof(int) || typeof(T) == typeof(int?))
                 {
                     if (int.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((int)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // uint
                 else if (typeof(T) == typeof(uint) || typeof(T) == typeof(uint?))
                 {
                     if (uint.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((uint)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // long
                 else if (typeof(T) == typeof(long) || typeof(T) == typeof(long?))
                 {
                     if (long.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((long)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // ulong
                 else if (typeof(T) == typeof(ulong) || typeof(T) == typeof(ulong?))
                 {
                     if (ulong.TryParse(value, NumberStyles.Integer, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((ulong)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // float
                 else if (typeof(T) == typeof(float) || typeof(T) == typeof(float?))
                 {
                     if (float.TryParse(value, NumberStyles.Any, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((float)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // double
                 else if (typeof(T) == typeof(double) || typeof(T) == typeof(double?))
                 {
                     if (double.TryParse(value, NumberStyles.Any, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((double)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 // decimal
                 else if (typeof(T) == typeof(decimal) || typeof(T) == typeof(decimal?))
                 {
                     if (decimal.TryParse(value, NumberStyles.Any, Culture, out var parsedValue))
-                        return CheckBoundaries((T)(object)parsedValue);
+                        return ((decimal)(object)CheckBoundaries((T)(object)parsedValue)).ToString(Format, Culture);
                     UpdateGetError("Not a valid number");
                 }
                 else
@@ -121,6 +121,6 @@ namespace MudBlazor
             return null;
         }
 
-        private string CheckBoundaries(T value) => Convert.ToString(EvaluationFunc.Invoke(value), Culture);
+        private T CheckBoundaries(T value) => EvaluationFunc.Invoke(value);
     }
 }
