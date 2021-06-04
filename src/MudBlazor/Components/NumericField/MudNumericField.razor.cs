bb895c7b6e6fa8c627d3187d7f52531a9f34176d
Aronne Gandini <II_ARROWS@hotmail.it>
2021-06-04 14:45:24 +0200

    NumericInput: fix manual input boundaries check (#1745)
    * NumericBoundariesConverter
    
    * Fix empty string and remove invalid tests
    
    * Moved Converter in Utilities/BindingConverters
    
    * Tests for NumericBoundariesConverter
    
    * Implement Format

--- src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
+++ src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
@@ -16,6 +16,7 @@ namespace MudBlazor
             SetConverter(new DefaultConverter<T> { Culture = CultureInfo.InvariantCulture });
 
             _validateInstance = new Func<T, Task<bool>>(ValidateInput);
+            _inputConverter = new NumericBoundariesConverter<T>((val) => ConstrainBoundaries(val).value) { Culture = CultureInfo.InvariantCulture };
 
             #region parameters default depending on T
             //sbyte
@@ -107,7 +108,7 @@ namespace MudBlazor
 
         private MudInput<string> _elementReference;
 
-        private static Converter<string> s_inputConverter = new DefaultConverter<string> { Culture = CultureInfo.InvariantCulture };
+        private Converter<string> _inputConverter;
 
         public override ValueTask FocusAsync()
         {
@@ -128,7 +129,7 @@ namespace MudBlazor
         {
             bool valueChanged;
             (value, valueChanged) = ConstrainBoundaries(value);
-            await base.SetValueAsync(ConstrainBoundaries(value).value, valueChanged || updateText);
+            await base.SetValueAsync(value, valueChanged || updateText);
         }
 
         protected async Task<bool> ValidateInput(T value)
