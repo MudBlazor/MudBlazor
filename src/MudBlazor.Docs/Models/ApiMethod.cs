1f52844f90e20988174935c465b7d194263dc6a6
boukenka <mbogais@free.fr>
2021-06-08 20:34:27 +0200

    Addition of methods for the documentation

---
+++ src/MudBlazor.Docs/Models/ApiMethod.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+
+namespace MudBlazor.Docs.Models
+{
+    public class ApiMethod
+    {
+        public ParameterInfo Return { get; set; }
+        public string Documentation { get; set; }
+        public MethodInfo MethodInfo { get; set; }
+        public ParameterInfo[] Parameters { get; set; }
+    }
+}
