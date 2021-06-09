ff06037fd8fd0d44ce1b3ccc0b7ee203a75f8d0e
Henrique Clausing <hclausing@hotmail.com>
2021-06-09 17:03:43 -0300

    Initial work on Table Grouping

---
+++ src/MudBlazor/Components/Table/TableGroupDefinition.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazor
+{
+    public class TableGroupDefinition<T>
+    {
+        public TableGroupDefinition(Func<T, object> selector, TableGroupDefinition<T> innerGroup = null)
+        {
+            Selector = selector;
+            InnerGroup = innerGroup;
+        }
+
+        public Func<T, object> Selector { get; private set; }
+        public TableGroupDefinition<T> InnerGroup { get; private set; }
+
+    }
+}
