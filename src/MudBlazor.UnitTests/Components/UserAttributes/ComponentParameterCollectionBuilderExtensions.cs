9c885a386e8139c18dd9ff6224cc940af44e92bf
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2021-06-10 23:49:56 +0200

    Use UserAttributes in all components + enforce with test

---
+++ src/MudBlazor.UnitTests/Components/UserAttributes/ComponentParameterCollectionBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Bunit;
+
+namespace MudBlazor.UnitTests.UserAttributes
+{
+    internal static class ComponentParameterCollectionBuilderExtensions
+    {
+        public static ComponentParameterCollectionBuilder<TComponent> AddTestUserAttributes<TComponent>(
+            this ComponentParameterCollectionBuilder<TComponent> builder)
+            where TComponent : MudComponentBase
+            => builder.Add(x => x.UserAttributes, new Dictionary<string, object> { { "data-testid", "test-123" } });
+    }
+}
---
+++ src/MudBlazor.UnitTests/Components/UserAttributes/UserAttributesTests.cs
@@ -0,0 +1,179 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Bunit;
+using FluentAssertions;
+using MudBlazor.Charts;
+using NUnit.Framework;
+using TestContext = Bunit.TestContext;
+
+namespace MudBlazor.UnitTests.UserAttributes
+{
+    [TestFixture]
+    public sealed class UserAttributesTests
+    {
+        private static ConcurrentDictionary<Type, Func<TestContext, IRenderedFragment>> componentFactories = new ConcurrentDictionary<Type, Func<TestContext, IRenderedFragment>>();
+        private static ConcurrentBag<Type> excludedComponents = new ConcurrentBag<Type>();
+
+        static UserAttributesTests()
+        {
+            // Add a custom create function for components that cannot be created automatically.
+            // These include generic components or components that require certain attributes to be set before rendering anything.
+            componentFactories.TryAdd(typeof(MudAutocomplete<>), CreateGeneric<MudAutocomplete<string>>);
+            componentFactories.TryAdd(typeof(MudBreadcrumbs), Create_MudBreadcrumbs);
+            componentFactories.TryAdd(typeof(MudCarousel<>), CreateGeneric<MudCarousel<string>>);
+            componentFactories.TryAdd(typeof(MudCarouselItem), Create_MudCarouselItem);
+            componentFactories.TryAdd(typeof(MudCheckBox<>), CreateGeneric<MudCheckBox<bool>>);
+            //componentFactories.TryAdd(typeof(MudDialog), Create_MudDialog);
+            componentFactories.TryAdd(typeof(MudInput<>), CreateGeneric<MudInput<string>>);
+            componentFactories.TryAdd(typeof(MudNumericField<>), CreateGeneric<MudNumericField<int>>);
+            componentFactories.TryAdd(typeof(MudRangeInput<>), CreateGeneric<MudRangeInput<string>>);
+            componentFactories.TryAdd(typeof(MudOverlay), Create_MudOverlay);
+            componentFactories.TryAdd(typeof(MudRadio<>), CreateGeneric<MudRadio<string>>);
+            componentFactories.TryAdd(typeof(MudSelect<>), CreateGeneric<MudSelect<string>>);
+            componentFactories.TryAdd(typeof(MudSlider<>), CreateGeneric<MudSlider<int>>);
+            componentFactories.TryAdd(typeof(MudSwitch<>), CreateGeneric<MudSwitch<bool>>);
+            componentFactories.TryAdd(typeof(MudTable<>), CreateGeneric<MudTable<string>>);
+            componentFactories.TryAdd(typeof(MudTableSortLabel<>), CreateGeneric<MudTableSortLabel<string>>);
+            componentFactories.TryAdd(typeof(MudTextField<>), CreateGeneric<MudTextField<string>>);
+            componentFactories.TryAdd(typeof(MudTreeView<>), CreateGeneric<MudTreeView<string>>);
+            componentFactories.TryAdd(typeof(MudTreeViewItem<>), CreateGeneric<MudTreeViewItem<string>>);
+            componentFactories.TryAdd(typeof(MudHighlighter), Create_MudHighlighter);
+
+            excludedComponents.Add(typeof(LegendBase));
+            excludedComponents.Add(typeof(MudBooleanInput<>)); // This is an API only base class that is safe to skip
+            excludedComponents.Add(typeof(MudDialog)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudElement)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudHidden)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudMessageBox)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudPicker<>)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudRadioGroup<>)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudSelectItem<>));
+            excludedComponents.Add(typeof(MudTabs)); // TODO Can we make this work?
+            excludedComponents.Add(typeof(MudTabPanel)); // TODO Can we make this work?
+        }
+
+        [Test]
+        public void AllMudComponents_ShouldForwardUserAttributes()
+        {
+            using var testContext = new TestContext();
+            testContext.AddTestServices();
+
+            var componentTypes = typeof(MudElement).Assembly
+                .GetTypes()
+                .Where(type => type.IsAssignableTo(typeof(MudComponentBase)) && !type.IsAbstract);
+
+            foreach (var componentType in componentTypes)
+            {
+                var type = componentType.IsGenericType ? componentType.GetGenericTypeDefinition() : componentType;
+
+                if (excludedComponents.Contains(type))
+                    continue;
+
+                var componentName = componentType.Name;
+                var componentFactory = componentFactories.GetOrAdd(type, BuildComponentFactory)
+                    ?? throw new InvalidOperationException($"Could not build component factory for {componentName}");
+
+                var component = componentFactory(testContext);
+                TestComponent(component, componentName);
+            }
+        }
+
+        private static void TestComponent(IRenderedFragment component, string componentName)
+        {
+            component.Markup.Should()
+                .NotBeEmpty(because: $"the component {componentName} should at least contain one element");
+
+            var elementsWithUserAttributes = component.FindAll("[data-testid='test-123']");
+            elementsWithUserAttributes.Should()
+                .NotBeEmpty(because: $"UserAttributes should be forwarded by component {componentName}");
+        }
+
+        private static Func<TestContext, IRenderedFragment> BuildComponentFactory(Type componentType)
+        {
+            var componentName = componentType.Name;
+            var createMethod = typeof(UserAttributesTests)
+                .GetMethod(nameof(Create), BindingFlags.Static | BindingFlags.NonPublic)
+                ?.MakeGenericMethod(componentType);
+
+            if (createMethod == null) return null;
+
+            return testContext =>
+            {
+                try
+                {
+                    return createMethod.Invoke(null, new object[] { testContext }) as IRenderedFragment;
+                }
+                catch (Exception ex)
+                {
+                    throw new AssertionException($"Failed to create component {componentName}", ex);
+                }
+            };
+        }
+
+        private static IRenderedFragment Create<TComponent>(TestContext testContext)
+            where TComponent : MudComponentBase
+            => testContext.RenderComponent<TComponent>(attributes => attributes.AddTestUserAttributes());
+
+        private static IRenderedFragment CreateGeneric<TComponent>(TestContext testContext)
+            where TComponent : MudComponentBase
+        {
+            return testContext
+                .RenderComponent<TComponent>(attributes => attributes
+                    .AddTestUserAttributes());
+        }
+
+        private static IRenderedFragment Create_MudBreadcrumbs(TestContext testContext)
+        {
+            return testContext
+                .RenderComponent<MudBreadcrumbs>(attributes => attributes
+                    .AddTestUserAttributes()
+                    .Add(x => x.Items, new List<BreadcrumbItem> {new BreadcrumbItem("text", "href")}));
+        }
+
+        private static IRenderedFragment Create_MudCarouselItem(TestContext testContext)
+        {
+            var parent = testContext.RenderComponent<MudCarousel<string>>(attributes => attributes
+                .Add(x => x.SelectedIndex, 0))
+                .Instance;
+
+            return testContext
+                .RenderComponent<MudCarouselItem>(attributes => attributes
+                    .AddTestUserAttributes()
+                    .Add(x => x.Parent, parent));
+        }
+
+        private static IRenderedFragment Create_MudDialog(TestContext testContext)
+        {
+            var dialogInstance = testContext.RenderComponent<MudDialogInstance>();
+
+            return testContext
+                .RenderComponent<MudDialog>(attributes => attributes
+                    .AddTestUserAttributes()
+                    .AddCascadingValue("DialogInstance", dialogInstance.Instance));
+        }
+
+        private static IRenderedFragment Create_MudOverlay(TestContext testContext)
+        {
+            return testContext
+                .RenderComponent<MudOverlay>(attributes => attributes
+                    .AddTestUserAttributes()
+                    .Add(x => x.Visible, true));
+        }
+
+        private static IRenderedFragment Create_MudHighlighter(TestContext testContext)
+        {
+            return testContext
+                .RenderComponent<MudHighlighter>(attributes => attributes
+                    .AddTestUserAttributes()
+                    .Add(x => x.Text, "Hello world")
+                    .Add(x => x.HighlightedText, "Hello"));
+        }
+    }
+}
