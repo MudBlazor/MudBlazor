05dcce6be50b8315eb133becc99cc95b54c441fe
Porkopek <porkopek@users.noreply.github.com>
2021-06-16 22:31:53 +0100

    Add new mouse EventHandlers to Blazor

---
+++ src/MudBlazor/Interop/EventHandlers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MudBlazor
+{
+    /// <summary>
+    /// see https://github.com/dotnet/aspnetcore/issues/13104
+    /// </summary>
+    [EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+    [EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+    public static class EventHandlers
+    {
+
+    }
+}
