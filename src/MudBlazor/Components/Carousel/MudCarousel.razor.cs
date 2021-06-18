728093d048344f7048ff4ca75b72ef2b61017414
Jeffrey Jangli <shogun.net@outlook.com>
2021-06-18 13:14:37 +0000

    Carousel: Changed Timer and fixed bug in MoveTo  (#1913)
    * -Replaced System.Timers.Timer with System.Threading.Timer;
    -Replaced sync methods with async methods;
    
    * Some code refinements
    
    * Fixed bug in MoveTo()

--- src/MudBlazor/Components/Carousel/MudCarousel.razor.cs
+++ src/MudBlazor/Components/Carousel/MudCarousel.razor.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.Extensions;
 using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public partial class MudCarousel<TData> : MudBaseBindableItemsControl<MudCarouselItem, TData>, IDisposable
+    public partial class MudCarousel<TData> : MudBaseBindableItemsControl<MudCarouselItem, TData>, IAsyncDisposable
     {
-
         protected string Classname =>
                     new CssBuilder("mud-carousel")
                          .AddClass($"mud-carousel-{_currentColor.ToDescriptionString()}")
@@ -20,153 +21,176 @@ namespace MudBlazor
                         .Build();
 
 
-        private System.Timers.Timer _autoCycleTimer;
+        private Timer _timer;
+        private bool _autoCycle = true;
+        private Color _currentColor = Color.Inherit;
+        private TimeSpan _cycleTimeout = TimeSpan.FromSeconds(5);
+        private void _timerElapsed(object stateInfo) => InvokeAsync(async () => await TimerTickAsync());
 
-        internal Color _currentColor = Color.Inherit;
 
         [CascadingParameter] public bool RightToLeft { get; set; }
+
 
         /// <summary>
         /// Gets or Sets if 'Next' and 'Previous' arrows must be visible
         /// </summary>
         [Parameter] public bool ShowArrows { get; set; } = true;
 
+
         /// <summary>
         /// Gets or Sets if bottom bar with Delimiters musb be visible
         /// </summary>
         [Parameter] public bool ShowDelimiters { get; set; } = true;
+        
 
-        private bool _autoCycleField = true;
         /// <summary>
         /// Gets or Sets automatic cycle on item collection
         /// </summary>
         [Parameter] public bool AutoCycle
         {
-            get => _autoCycleField;
+            get => _autoCycle;
             set
             {
-                _autoCycleField = value;
-                if (AutoCycle == true)
-                    Timer_Reset();
+                _autoCycle = value;
+
+                if (_autoCycle)
+                    InvokeAsync(async () => await StartTimerAsync());
+
                 else
-                    Timer_Stop();
+                    InvokeAsync(async () => await StopTimerAsync());
             }
         }
+                
 
-        public TimeSpan _autoCycleTimeField = TimeSpan.FromSeconds(5);
         /// <summary>
         /// Gets or Sets the Auto Cycle time
         /// </summary>
         [Parameter] public TimeSpan AutoCycleTime
         {
-            get => _autoCycleTimeField;
+            get => _cycleTimeout;
             set
             {
-                _autoCycleTimeField = value;
-                if (AutoCycle == true)
-                    Timer_Reset();
+                _cycleTimeout = value;
+
+                if (_autoCycle == true)
+                    InvokeAsync(async () => await ResetTimerAsync());
+
+                else
+                    InvokeAsync(async () => await StopTimerAsync());
             }
         }
+
 
         /// <summary>
         /// Gets or Sets the Template for the Left Arrow
         /// </summary>
         [Parameter] public RenderFragment NextButtonTemplate { get; set; }
 
+
         /// <summary>
         /// Gets or Sets the Template for the Right Arrow
         /// </summary>
         [Parameter] public RenderFragment PreviousButtonTemplate { get; set; }
+
 
         /// <summary>
         /// Gets or Sets the Template for Delimiters
         /// </summary>
         [Parameter]  public RenderFragment<bool> DelimiterTemplate { get; set; }
 
+
         /// <summary>
         /// Fires when selected Index changed on base class
         /// </summary>
-        private void Selection_Changed()
+        private void SelectionChanged()
         {
-            Timer_Reset();
-            _currentColor = (SelectedContainer != null ? SelectedContainer.Color : Color.Inherit);
+            InvokeAsync(async () => await ResetTimerAsync());
+
+            _currentColor = SelectedContainer != null ? SelectedContainer.Color : Color.Inherit;
         }
+
 
         /// <summary>
         /// Provides Selection changes by horizontal swipe gesture
         /// </summary>
         private void OnSwipe(SwipeDirection direction)
         {
-            if (direction == SwipeDirection.LeftToRight)
-                Previous();
-            else if (direction == SwipeDirection.RightToLeft)
-                Next();
-        }
-
-        /// <summary>
-        /// Prepares the AutoCycle's timer for start its ticks, based on AutoCycleTime value
-        /// </summary>
-        private void Timer_Start()
-        {
-            if (AutoCycle == false)
-                return;
-            _autoCycleTimer = new System.Timers.Timer(AutoCycleTime.TotalMilliseconds)
+            switch (direction)
             {
-                Enabled = true
-            };
-            _autoCycleTimer.Elapsed += Timer_Tick;
-        }
+                case SwipeDirection.LeftToRight:
+                    Previous();
+                    break;
 
-        /// <summary>
-        /// Stops the AutoCycle's timer with immediate effect
-        /// </summary>
-        private void Timer_Stop()
-        {
-            if (_autoCycleTimer != null)
-            {
-                _autoCycleTimer.Elapsed -= Timer_Tick;
-                _autoCycleTimer.Dispose();
+                case SwipeDirection.RightToLeft:
+                    Next();
+                    break;
             }
         }
 
         /// <summary>
-        /// Resets the AutoCycle's timer (ex: when user mannualy change the selected index)
+        /// Immediately starts the AutoCycle timer
         /// </summary>
-        private void Timer_Reset()
+        private async ValueTask StartTimerAsync()
         {
-            Timer_Stop();
-            Timer_Start();
+            await Task.CompletedTask;
+
+            if (null != _timer && AutoCycle)
+                _timer.Change(AutoCycleTime, TimeSpan.Zero);
         }
+
+        /// <summary>
+        /// Immediately stops the AutoCycle timer
+        /// </summary>
+        private async ValueTask StopTimerAsync()
+        {
+            await Task.CompletedTask;
+
+            _timer?.Change(Timeout.Infinite, 0);
+        }
+
+        /// <summary>
+        /// Stops and restart the AutoCycle timer
+        /// </summary>
+        private async ValueTask ResetTimerAsync()
+        {
+            await StopTimerAsync();
+            await StartTimerAsync();
+        }
+
 
         /// <summary>
         /// Changes the SelectedIndex to a next one (or restart on 0)
         /// </summary>
-        private void Timer_Tick(object source, System.Timers.ElapsedEventArgs e)
+        private async ValueTask TimerTickAsync()
         {
-            InvokeAsync(Next);
+            await InvokeAsync(Next);
         }
 
 
-        protected override void OnInitialized()
+        protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            base.OnInitialized();
-            SelectedIndexChanged = new EventCallback<int>(this, (Action)Selection_Changed);
-        }
+            await Task.CompletedTask;
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
+            if (firstRender)
             {
-                Timer_Stop();
+                SelectedIndexChanged = new EventCallback<int>(this, (Action) SelectionChanged);
+
+                _timer = new Timer(_timerElapsed, null, TimeSpan.Zero, AutoCycleTime);
             }
         }
 
 
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsync(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        protected virtual async ValueTask DisposeAsync(bool disposing)
+        {
+            if (disposing)
+                await StopTimerAsync();
+        }
     }
 }
