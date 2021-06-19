MudBlazor/MudBlazor

commit a94aacbc28d71f8a08f26a59f1094dc360c50e6a

MudAutoComplete: close on Blur (#1761, #1807)
* Fix for #1761

* Fix for #1761 (PR comments)

Co-authored-by: Ricardo Francisco <ricardo.francisco@focus-bc.com>

src/MudBlazor.UnitTests/Components/AutocompleteTests.cs
@@ -171,7 +171,7 @@ namespace MudBlazor.UnitTests.Components
             autocomplete.Value.Should().Be("Alabama");
             autocomplete.Text.Should().Be("Alabama");
             // set a value the search won't find
-            autocompletecomp.SetParam(p=> p.Text, "Austria"); // not part of the U.S.
+            autocompletecomp.SetParam(p => p.Text, "Austria"); // not part of the U.S.
 
             // now trigger the coercion by toggling the the menu (it won't even open for invalid values, but it will coerce)
             await comp.InvokeAsync(() => autocomplete.ToggleMenu());
@@ -318,10 +318,35 @@ namespace MudBlazor.UnitTests.Components
 
             // Button shows again after entering text
             comp.Find("input").Input("text");
-            comp.WaitForAssertion(()=> comp.Find("button").Should().NotBeNull());
+            comp.WaitForAssertion(() => comp.Find("button").Should().NotBeNull());
             // Button removed after clearing text
             comp.Find("input").Input(string.Empty);
             comp.WaitForAssertion(() => comp.FindAll("button").Should().BeEmpty());
+        }
+
+        /// <summary>
+        /// Test for <seealso cref="https://github.com/Garderoben/MudBlazor/issues/1761"/>
+        /// </summary>
+        [Test]
+        public async Task Autocomplete_Should_Close_OnBlur()
+        {
+            var comp = ctx.RenderComponent<AutocompleteTest1>();
+            Console.WriteLine(comp.Markup);
+            // select elements needed for the test
+            var autocompletecomp = comp.FindComponent<MudAutocomplete<string>>();
+            var autocomplete = autocompletecomp.Instance;
+
+            // Should be closed
+            comp.WaitForAssertion(() => autocomplete.IsOpen.Should().BeFalse());
+
+            // Lets type something to cause it to open
+            autocompletecomp.Find("input").Input("Calif");
+            await Task.Delay(100);
+            comp.WaitForAssertion(() => autocomplete.IsOpen.Should().BeTrue());
+
+            // Lets call blur on the input and confirm that it closed
+            autocompletecomp.Find("input").Blur();
+            comp.WaitForAssertion(() => autocomplete.IsOpen.Should().BeFalse());
         }
 
         #region DataAttribute validation

src/MudBlazor/Components/Autocomplete/MudAutocomplete.razor.cs
@@ -367,6 +367,8 @@ namespace MudBlazor
         {
             //return !IsOpen ? CoerceTextToValue() : Task.CompletedTask;
             OnBlur.InvokeAsync(args);
+            IsOpen = false;
+            BeginValidate();
             return Task.CompletedTask;
             // we should not validate on blur in autocomplete, because the user needs to click out of the input to select a value,
             // resulting in a premature validation. thus, don't call base

commit efad2b93fc10b3689c4e3b49c943589ee8177b5f

Autocomplete: added a test case to prevent regressions like caused by #1807 (#1761)

src/MudBlazor.UnitTests/Components/AutocompleteTests.cs
@@ -171,7 +171,7 @@ namespace MudBlazor.UnitTests.Components
             autocomplete.Value.Should().Be("Alabama");
             autocomplete.Text.Should().Be("Alabama");
             // set a value the search won't find
-            autocompletecomp.SetParam(p=> p.Text, "Austria"); // not part of the U.S.
+            autocompletecomp.SetParam(p => p.Text, "Austria"); // not part of the U.S.
 
             // now trigger the coercion by toggling the the menu (it won't even open for invalid values, but it will coerce)
             await comp.InvokeAsync(() => autocomplete.ToggleMenu());
@@ -324,7 +324,6 @@ namespace MudBlazor.UnitTests.Components
             comp.WaitForAssertion(() => comp.FindAll("button").Should().BeEmpty());
         }
 
-        #region DataAttribute validation
         [Test]
         public async Task Autocomplete_Should_Validate_Data_Attribute_Fail()
         {
@@ -362,7 +361,6 @@ namespace MudBlazor.UnitTests.Components
             await comp.InvokeAsync(() => autocomplete.Validate());
             autocomplete.ValidationErrors.Should().BeEmpty();
         }
-        #endregion
 
 
         /// <summary>
@@ -380,6 +378,71 @@ namespace MudBlazor.UnitTests.Components
             await comp.InvokeAsync(() => autocomplete.Validate());
 
             autocomplete.ValidationErrors.First().Should().Be("Required");
+        }
+
+        /// <summary>
+        /// Test for <seealso cref="https://github.com/Garderoben/MudBlazor/issues/1761"/>
+        /// </summary>
+        [Ignore("This test is disabled for now because PR #1807 was reverted for introducing a bug")]
+        [Test]
+        public async Task Autocomplete_Should_Close_OnBlur()
+        {
+            var comp = ctx.RenderComponent<AutocompleteTest1>();
+            Console.WriteLine(comp.Markup);
+            // select elements needed for the test
+            var autocompletecomp = comp.FindComponent<MudAutocomplete<string>>();
+            var autocomplete = autocompletecomp.Instance;
+
+            // Should be closed
+            comp.WaitForAssertion(() => autocomplete.IsOpen.Should().BeFalse());
+
+            // Lets type something to cause it to open
+            autocompletecomp.Find("input").Input("Calif");
+            await Task.Delay(100);
+            comp.WaitForAssertion(() => autocomplete.IsOpen.Should().BeTrue());
+
+            // Lets call blur on the input and confirm that it closed
+            autocompletecomp.Find("input").Blur();
+            comp.WaitForAssertion(() => autocomplete.IsOpen.Should().BeFalse());
+        }
+        
+        /// <summary>
+        /// When selecting a value by clicking on it in the list the input will blur. However, this
+        /// must not cause the dropdown to close or else the click on the item will not be possible!
+        ///
+        /// If this test fails it means the dropdown has closed before we can even click any value in the list.
+        /// Such a regression happened and caused PR #1807 to be reverted
+        /// </summary>
+        [Test]
+        public async Task Autocomplete_Should_NotCloseDropdownOnInputBlur()
+        {
+            var comp = ctx.RenderComponent<AutocompleteTest1>();
+            Console.WriteLine(comp.Markup);
+            // select elements needed for the test
+            var autocompletecomp = comp.FindComponent<MudAutocomplete<string>>();
+            var autocomplete = autocompletecomp.Instance;
+
+            //No popover, due it's closed
+            comp.Markup.Should().NotContain("mud-popover");
+
+            // check initial state
+            autocomplete.Value.Should().Be("Alabama");
+            autocomplete.Text.Should().Be("Alabama");
+            await Task.Delay(100);
+
+
+            // now let's type a different state to see the popup open
+            autocompletecomp.Find("input").Input("Calif");
+            await Task.Delay(100);
+            comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
+            var items = comp.FindComponents<MudListItem>().ToArray();
+            items.Length.Should().Be(1);
+            items.First().Markup.Should().Contain("California");
+
+            // now, we blur the input and assert that the popover is still open.
+            autocompletecomp.Find("input").Blur();
+            await Task.Delay(100);
+            comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
         }
     }
 }