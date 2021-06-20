MudBlazor/MudBlazor

commit 86bc257dd0485d2a7cef29766416596dd2f1b08d

MudForm: make sure @bind-IsValid works, always. (#1868)

src/MudBlazor.UnitTests/Components/FormTests.cs
@@ -91,6 +91,42 @@ namespace MudBlazor.UnitTests.Components
         }
 
         /// <summary>
+        /// Form should update the bound variables valid and touched whenever they change.
+        /// </summary>
+        [Test]
+        public async Task FormIsValidTest3()
+        {
+            var comp = ctx.RenderComponent<FormIsValidTest3>();
+            Console.WriteLine(comp.Markup);
+            var form = comp.FindComponent<MudForm>().Instance;
+            var textFields = comp.FindComponents<MudTextField<string>>();
+            // check initial state: form should be invalid due to having a required field that is not filled
+            form.IsValid.Should().Be(false);
+            form.IsTouched.Should().Be(false);
+            comp.FindComponents<MudSwitch<bool>>()[0].Instance.Checked.Should().Be(false);
+            comp.FindComponents<MudSwitch<bool>>()[1].Instance.Checked.Should().Be(false);
+            // filling in the required field
+            textFields[1].Find("input").Change("Fill in the required field to make this form valid");
+            form.IsValid.Should().Be(true);
+            comp.FindComponents<MudSwitch<bool>>()[0].Instance.Checked.Should().Be(true);
+            comp.FindComponents<MudSwitch<bool>>()[1].Instance.Checked.Should().Be(true);
+        }
+
+        /// <summary>
+        /// Form should update the bound variable valid to true even though it is set false upon first render because there is no required field.
+        /// </summary>
+        [Test]
+        public async Task FormIsValidTest4()
+        {
+            var comp = ctx.RenderComponent<FormIsValidTest4>();
+            Console.WriteLine(comp.Markup);
+            var form = comp.FindComponent<MudForm>().Instance;
+            // check initial state: form should be valid due to having no required field, but the user's two-way binding did override that value to false
+            comp.WaitForAssertion(() => form.IsValid.Should().Be(true));
+            comp.WaitForAssertion(()=> comp.FindComponent<MudSwitch<bool>>().Instance.Checked.Should().Be(true));
+        }
+
+        /// <summary>
         /// Changing a fields value should set IsTouched to true
         /// </summary>
         [Test]
@@ -382,32 +418,32 @@ namespace MudBlazor.UnitTests.Components
             var comp = ctx.RenderComponent<MudFormExample>();
             Console.WriteLine(comp.Markup);
             var form = comp.FindComponent<MudForm>().Instance;
-            form.IsValid.Should().BeFalse(because: "it contains required fields that are not filled out");
+            comp.WaitForAssertion(()=>form.IsValid.Should().BeFalse(because: "it contains required fields that are not filled out"));
             var buttons = comp.FindComponents<MudButton>();
             // click validate button
             var validateButton = buttons[1];
             validateButton.Find("button").Click();
             var textfields = comp.FindComponents<MudTextField<string>>();
-            textfields[0].Instance.HasErrors.Should().BeTrue();
+            comp.WaitForAssertion(() => textfields[0].Instance.HasErrors.Should().BeTrue());
             textfields[0].Instance.ErrorText.Should().Be("User name is required!");
-            textfields[1].Instance.HasErrors.Should().BeTrue();
+            comp.WaitForAssertion(() => textfields[1].Instance.HasErrors.Should().BeTrue());
             textfields[1].Instance.ErrorText.Should().Be("Email is required!");
-            textfields[2].Instance.HasErrors.Should().BeTrue();
+            comp.WaitForAssertion(() => textfields[2].Instance.HasErrors.Should().BeTrue());
             textfields[2].Instance.ErrorText.Should().Be("Password is required!");
             var checkbox = comp.FindComponent<MudCheckBox<bool>>();
-            checkbox.Instance.HasErrors.Should().BeTrue();
+            comp.WaitForAssertion(() => checkbox.Instance.HasErrors.Should().BeTrue());
             checkbox.Instance.ErrorText.Should().Be("You must agree");
             // click reset validation
             var resetValidationButton = buttons[3];
             resetValidationButton.Find("button").Click();
             comp.WaitForState(() => form.Errors.Length == 0);
-            textfields[0].Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => textfields[0].Instance.HasErrors.Should().BeFalse());
             textfields[0].Instance.ErrorText.Should().BeNullOrEmpty();
-            textfields[1].Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => textfields[1].Instance.HasErrors.Should().BeFalse());
             textfields[1].Instance.ErrorText.Should().BeNullOrEmpty();
-            textfields[2].Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => textfields[2].Instance.HasErrors.Should().BeFalse());
             textfields[2].Instance.ErrorText.Should().BeNullOrEmpty();
-            checkbox.Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => checkbox.Instance.HasErrors.Should().BeFalse());
             checkbox.Instance.ErrorText.Should().BeNullOrEmpty();
             // fill in the form to make it valid
             textfields[0].Find("input").Change("Rick Sanchez");
@@ -421,19 +457,19 @@ namespace MudBlazor.UnitTests.Components
             var resetButton = buttons[2];
             resetButton.Find("button").Click();
             comp.WaitForState(() => form.Errors.Length == 0);
-            textfields[0].Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => textfields[0].Instance.HasErrors.Should().BeFalse());
             textfields[0].Instance.ErrorText.Should().BeNullOrEmpty();
             textfields[0].Instance.Text.Should().BeNullOrEmpty();
-            textfields[1].Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => textfields[1].Instance.HasErrors.Should().BeFalse());
             textfields[1].Instance.ErrorText.Should().BeNullOrEmpty();
             textfields[1].Instance.Text.Should().BeNullOrEmpty();
-            textfields[2].Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => textfields[2].Instance.HasErrors.Should().BeFalse());
             textfields[2].Instance.ErrorText.Should().BeNullOrEmpty();
             textfields[2].Instance.Text.Should().BeNullOrEmpty();
-            checkbox.Instance.HasErrors.Should().BeFalse();
+            comp.WaitForAssertion(() => checkbox.Instance.HasErrors.Should().BeFalse());
             checkbox.Instance.ErrorText.Should().BeNullOrEmpty();
-            checkbox.Instance.Checked.Should().BeFalse();
-            // TODO: fill out the form with errors, field after field, check how fields get validation erros after blur
+            comp.WaitForAssertion(() => checkbox.Instance.Checked.Should().BeFalse());
+            // TODO: fill out the form with errors, field after field, check how fields get validation errors after blur
         }
 
         /// <summary>

src/MudBlazor.UnitTests.Viewer/TestComponents/Form/FormIsValidTest3.razor
@@ -1,0 +1,22 @@
+﻿<div style="max-width: 400px;">
+    <MudForm @ref="form1" @bind-IsValid="@valid" @bind-IsTouched="@touched" ValidationDelay="0" >
+        <MudTextField T="string" Label="First Name" />
+        <MudTextField T="string" Required="true" Label="Last Name" />
+    </MudForm>
+    <div class="d-flex mt-5">
+        <MudButton Variant="Variant.Filled" Color="Color.Primary" OnClick="@(()=>form1.Validate())">Validate</MudButton>
+        <MudButton Variant="Variant.Filled" Color="Color.Secondary" OnClick="@(()=>form1.Reset())" Class="mx-2">Reset</MudButton>
+        <MudButton Variant="Variant.Filled" OnClick="@(()=>form1.ResetValidation())">Reset Validation</MudButton>
+    </div>
+    <div class="d-flex mt-3">
+        <MudSwitch @bind-Checked="@valid" Label="IsValid " Color="Color.Info" />
+        <MudSwitch @bind-Checked="@touched" Label="IsTouched" Color="Color.Info" />
+    </div>
+</div>
+
+@code {
+    public static string __description__ = "Form should update the bound variables valid and touched whenever they change.";
+    bool valid=true;
+    bool touched=false;
+    MudForm form1;
+}

src/MudBlazor.UnitTests.Viewer/TestComponents/Form/FormIsValidTest4.razor
@@ -1,0 +1,14 @@
+﻿<div style="max-width: 400px;">
+    <MudForm @bind-IsValid="@valid" ValidationDelay="300" >
+        <MudTextField T="string" Label="First Name" />
+        <MudTextField T="string" Label="Last Name" />
+    </MudForm>
+    <div class="d-flex mt-3">
+        <MudSwitch @bind-Checked="@valid" Label="IsValid " Color="Color.Info" />
+    </div>
+</div>
+
+@code {
+    public static string __description__ = "Form should update the bound variable valid to true even though it is set false upon calling form.Validate().";
+    bool valid=false;
+}

src/MudBlazor/Components/Form/MudForm.razor.cs
@@ -23,14 +23,35 @@ namespace MudBlazor
         [Parameter] public RenderFragment ChildContent { get; set; }
 
         /// <summary>
-        /// Validation status. True if the form is valid and without errors. This parameter is readonly.
+        /// Validation status. True if the form is valid and without errors. This parameter is two-way bindable.
         /// </summary>
         [Parameter]
-        //public bool IForm.IsValid => _valid;
         public bool IsValid
         {
             get => _valid;
-            set { /* readonly parameter! */ }
+            set
+            {
+                if (_valid == value)
+                    return;
+                SetIsValid(value);
+                // the user might have bound bind a value that is contrary to the form's validity state. 
+                // thus, we must re-evaluate and push back to rectify the bound variable
+                ReEvaluateIsValid();
+            }
+        }
+
+        private async void ReEvaluateIsValid()
+        {
+            await Task.Delay(1);
+            Validate();
+        }
+
+        private void SetIsValid(bool value)
+        {
+            if (_valid == value)
+                return;
+            _valid = value;
+            IsValidChanged.InvokeAsync(_valid).AndForget();
         }
 
         // Note: w/o any children the form is automatically valid.
@@ -97,7 +118,7 @@ namespace MudBlazor
         void IForm.Add(IFormComponent formControl)
         {
             if (formControl.Required)
-                _valid = false;
+                SetIsValid( false);
             _formControls.Add(formControl);
         }
 
@@ -135,21 +156,19 @@ namespace MudBlazor
             _errors.Clear();
             foreach (var error in _formControls.SelectMany(control => control.ValidationErrors))
                 _errors.Add(error);
-            var old_valid = _valid;
             // form can only be valid if:
             // - none have an error
             // - all required fields have been touched (and thus validated)
             var no_errors = _formControls.All(x => x.HasErrors == false);
             var required_all_touched = _formControls.Where(x => x.Required).All(x => x.Touched);
-            _valid = no_errors && required_all_touched;
+            var valid = no_errors && required_all_touched;
 
             var old_touched = _touched;
             _touched = _formControls.Any(x => x.Touched);
             try
             {
                 _shouldRender = false;
-                if (old_valid != _valid)
-                    await IsValidChanged.InvokeAsync(_valid);
+                SetIsValid( valid);
                 await ErrorsChanged.InvokeAsync(Errors);
                 if(old_touched != _touched)
                     await IsTouchedChanged.InvokeAsync(_touched);

commit cafa8074ac88e7e5b70988465ab6bfa4f9745b84

Form: added a test case that will prevent regressions like #1912, caused by commit 86bc257d (#1868)

src/MudBlazor.UnitTests/Components/FormTests.cs
@@ -689,6 +689,18 @@ namespace MudBlazor.UnitTests.Components
                 vc.Should().NotBeNull();
             }
         }
+
+        /// <summary>
+        /// This test should prevent regressions like #1912, caused by commit 86bc257d (#1868)
+        /// </summary>
+        [Test]
+        public async Task MudForm_MustNot_ValidateOnInitialRender()
+        {
+            var comp = ctx.RenderComponent<MudFormExample>();
+            await Task.Delay(100);
+            var form=comp.FindComponent<MudForm>().Instance;
+            form.Errors.Should().BeEmpty();
+        }
     }
 }
 