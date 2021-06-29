d6d34184a30de95989bc1284abfff542a0f0fbee
Jonny Larsson <jonny@gardnet.nu>
2021-06-29 18:59:15 +0200

    Docs: Contents Scroll/Active functionality

--- src/MudBlazor.Docs/Components/DocsPage.razor.cs
+++ src/MudBlazor.Docs/Components/DocsPage.razor.cs
@@ -8,61 +8,141 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
 using MudBlazor.Docs.Models;
 using MudBlazor.Docs.Extensions;
+using MudBlazor.Services;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.JSInterop;
 
 namespace MudBlazor.Docs.Components
 {
-    public partial class DocsPage : IDisposable
+    public partial class DocsPage : ComponentBase, IDisposable
     {
+        public string test1 { get; set; }
+        public string test2 { get; set; }
+
         [Inject] IScrollListener ScrollListener { get; set; }
         [Inject] IScrollManager ScrollManager { get; set; }
+        [Inject] IResizeObserver ResizeObserver { get; set; }
+        [Inject] IJSRuntime JsRuntime { get; set; }
         [Inject] NavigationManager NavigationManager { get; set; }
 
         [Parameter] public MaxWidth MaxWidth { get; set; } = MaxWidth.Medium;
         [Parameter] public EventCallback<ScrollEventArgs> OnScroll { get; set; }
         [Parameter] public RenderFragment ChildContent { get; set; }
 
-        private List<DocsLink> _sections = new List<DocsLink>();
+        private List<DocsSectionLink> _sections = new List<DocsSectionLink>();
         private string _pageuri { get; set; }
+
+        private bool _scrolled = false;
+        private bool _eventScrolled = false;
 
         protected override void OnInitialized()
         {
-            _pageuri = $"{NavigationManagerExtensions.GetSection(NavigationManager)}/{NavigationManagerExtensions.GetComponentLink(NavigationManager)}";
             NavigationManager.LocationChanged += TryFragmentNavigation;
         }
 
-        internal void AddSection(string section)
+        protected override void OnAfterRender(bool firstRender)
         {
-            var newsection = new DocsLink()
+            if (firstRender)
             {
-                Href = $"{_pageuri}#{section.Replace(" ", "").ToLower()}",
-                Title = section
-            };
+                ScrollListener.OnScroll += ScrollListener_OnScroll;
+            }
+        }
 
-            _sections.Add(newsection);
-
+        internal void AddSection(DocsSectionLink section)
+        {
+            _sections.Add(section);
             StateHasChanged();
         }
 
         private async void ScrollListener_OnScroll(object sender, ScrollEventArgs e)
         {
             await OnScroll.InvokeAsync(e);
+
+            if(_scrolled == false)
+            {
+                await ObserveAllSectionsAsync();
+            }
+
+            if(_eventScrolled)
+            {
+                _eventScrolled = false;
+                return;
+            }
+
+            var activelink = _sections.OrderBy(item => Math.Abs((e.FirstChildBoundingClientRect.Top * -1) - item.Location.Top)).First();
+
+            _sections.ToList().ForEach(item => item.Active = false);
+            activelink.Active = true;
+
+            await ChangeUrl(activelink.Id);
+            StateHasChanged();
+
+            await InvokeAsync(() => StateHasChanged());
         }
 
         private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
         {
             string currenturl = NavigationManager.Uri;
 
-            if(currenturl.Contains("#"))
+            if (currenturl.Contains("#"))
             {
                 string id = currenturl.Substring(currenturl.IndexOf('#') + 1);
+                var activelink = _sections.FirstOrDefault(item => item.Id == id);
+                activelink.Active = true;
+                _eventScrolled = true;
                 await ScrollManager.ScrollToFragmentAsync(id, ScrollBehavior.Smooth);
             }
+            StateHasChanged();
+        }
+
+        private async void OnNavLinkClick(string id)
+        {
+            _sections.ToList().ForEach(item => item.Active = false);
+            var activelink = _sections.FirstOrDefault(item => item.Id == id);
+            activelink.Active = true;
+            _eventScrolled = true;
+
+            await ChangeUrl(activelink.Id);
+            await ScrollManager.ScrollToFragmentAsync(id, ScrollBehavior.Smooth);
+
+            StateHasChanged();
+        }
+
+        private async Task ChangeUrl(string id)
+        {
+            string currenturl = $"{NavigationManagerExtensions.GetSection(NavigationManager)}/{NavigationManagerExtensions.GetComponentLink(NavigationManager)}";
+            if (currenturl.Contains("#"))
+            {
+                currenturl = currenturl.Substring(0, currenturl.IndexOf("#") + 0);
+            }
+            _pageuri = currenturl;
+            await JsRuntime.InvokeVoidAsync("ChangeUrl", $"{_pageuri}#{id}");
+        }
+
+        private async Task ObserveAllSectionsAsync()
+        {
+            foreach (var section in _sections)
+            {
+                section.Location = await ResizeObserver.Observe(section.Reference);
+            }
+            _scrolled = true;
+            StateHasChanged();
         }
 
         public void Dispose()
         {
             ScrollListener.OnScroll -= ScrollListener_OnScroll;
             NavigationManager.LocationChanged -= TryFragmentNavigation;
+            ResizeObserver.Dispose();
+        }
+
+        private string GetNavLinkClass(bool active)
+        {
+            if (active)
+                return $"docs-contents-navlink active";
+            else
+                return $"docs-contents-navlink";
         }
     }
 }
