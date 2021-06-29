6c49ba0f187ae044529f931aa6dffa3a2342802f
boukenka <mbogais@free.fr>
2021-06-29 19:17:58 +0200

    Methods modification after review

--- src/MudBlazor.Docs/Models/ApiMethod.cs
+++ src/MudBlazor.Docs/Models/ApiMethod.cs
@@ -5,6 +5,7 @@ namespace MudBlazor.Docs.Models
 {
     public class ApiMethod
     {
+        public string Signature { get; set; }
         public ParameterInfo Return { get; set; }
         public string Documentation { get; set; }
         public MethodInfo MethodInfo { get; set; }
