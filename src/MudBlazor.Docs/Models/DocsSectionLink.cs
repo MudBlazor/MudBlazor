abe7c86f591cf918b185936e117d3a2dca191ee0
Jonny Larsson <jonny@gardnet.nu>
2021-06-29 18:58:25 +0200

    Docs: Added class for the contents sections

---
+++ src/MudBlazor.Docs/Models/DocsSectionLink.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Interop;
+
+namespace MudBlazor.Docs.Models
+{
+    public class DocsSectionLink
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public bool Active { get; set; }
+        public ElementReference Reference { get; set; }
+        public BoundingClientRect Location { get; set; }
+    }
+}
