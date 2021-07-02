0a861a015a859119230d1dcd8103ad1bd1fedb05
Jonny Larsson <jonny@gardnet.nu>
2021-07-02 11:23:19 +0200

    Docs: Contents/Section Navigation per component (#2069)
    * Docs: CarbonAds CSS
    
    * Docs: Adjusted margin for ads
    
    * Docs: Added ads to page header
    
    * Docs: Adjusted design for the ads
    
    * Docs: Removed Gitter links and added Github Dissuction
    
    * Docs: Fixed Ads showing double when refreshing the page
    
    * Docs: Changed the Gitter SVG to Github SVG
    
    * Docs: Added support for two code files directly in SectionSource
    
    * Docs: Updated DialogPage with the new Section Source
    
    * Docs: Added set max height for ads and animation
    
    * Docs: Made the ads a bit wider and lowered the height.
    
    * Docs: Added css for docs contents
    
    * Docs: Started work on docs contents
    
    * Docs: Added basic scroll to fragment for contents
    
    * Docs: testing dialog page with new contents menu
    
    * Docs: added higher z-index to scroll to top
    
    * Docs: Hide/Show the new contents menu depending on page  width
    
    * Docs: Made the contents drawer smaller
    
    * Docs: better Uri control
    
    * Docs: Added null check for contents
    
    * Docs: Changed SectionHeader Title from Renderfragment to String
    
    * Docs: added js to changeUrl
    
    * Docs: Changes to contents CSS
    
    * Docs: Added class for the contents sections
    
    * Docs: Updated SectionHeader with the new Contents class
    
    * Docs: Contents Scroll/Active functionality
    
    * Docs: SectionHeader added null checks and Id check
    
    * Docs: DocsPage removed InvokeAsync() and test strings
    
    * Docs: SectionHeader, removed Typo.H6 from Description
    
    * Docs: Section, changed how ResizeObserver is used
    
    Co-authored-by: Meinrad Recheis <meinrad.recheis@gmail.com>

---
+++ src/MudBlazor.Docs/Components/DocsPage.razor.cs
@@ -0,0 +1,146 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using MudBlazor.Docs.Models;
+using MudBlazor.Docs.Extensions;
+using MudBlazor.Services;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.JSInterop;
+
+namespace MudBlazor.Docs.Components
+{
+    public partial class DocsPage : ComponentBase, IDisposable
+    {
+        [Inject] IScrollListener ScrollListener { get; set; }
+        [Inject] IScrollManager ScrollManager { get; set; }
+        [Inject] IResizeObserver ResizeObserver { get; set; }
+        [Inject] IJSRuntime JsRuntime { get; set; }
+        [Inject] NavigationManager NavigationManager { get; set; }
+
+        [Parameter] public MaxWidth MaxWidth { get; set; } = MaxWidth.Medium;
+        [Parameter] public EventCallback<ScrollEventArgs> OnScroll { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        private List<DocsSectionLink> _sections = new List<DocsSectionLink>();
+        private string _pageuri { get; set; }
+
+        private bool _scrolled = false;
+        private bool _eventScrolled = false;
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.LocationChanged += TryFragmentNavigation;
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                ScrollListener.OnScroll += ScrollListener_OnScroll;
+            }
+        }
+
+        internal void AddSection(DocsSectionLink section)
+        {
+            _sections.Add(section);
+            StateHasChanged();
+        }
+
+        private async void ScrollListener_OnScroll(object sender, ScrollEventArgs e)
+        {
+            await OnScroll.InvokeAsync(e);
+
+            if(_scrolled == false)
+            {
+                await ObserveAllSectionsAsync();
+            }
+
+            if(_eventScrolled)
+            {
+                _eventScrolled = false;
+                return;
+            }
+
+            var activelink = _sections.OrderBy(item => Math.Abs((e.FirstChildBoundingClientRect.Top * -1) - item.Location.Top)).First();
+
+            _sections.ToList().ForEach(item => item.Active = false);
+            activelink.Active = true;
+
+            await ChangeUrl(activelink.Id);
+
+            StateHasChanged();
+        }
+
+        private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
+        {
+            string currenturl = NavigationManager.Uri;
+
+            if (currenturl.Contains("#"))
+            {
+                string id = currenturl.Substring(currenturl.IndexOf('#') + 1);
+                var activelink = _sections.FirstOrDefault(item => item.Id == id);
+                activelink.Active = true;
+                _eventScrolled = true;
+                await ScrollManager.ScrollToFragmentAsync(id, ScrollBehavior.Smooth);
+            }
+            StateHasChanged();
+        }
+
+        private async void OnNavLinkClick(string id)
+        {
+            _sections.ToList().ForEach(item => item.Active = false);
+            var activelink = _sections.FirstOrDefault(item => item.Id == id);
+            activelink.Active = true;
+            _eventScrolled = true;
+
+            await ChangeUrl(activelink.Id);
+            await ScrollManager.ScrollToFragmentAsync(id, ScrollBehavior.Smooth);
+
+            StateHasChanged();
+        }
+
+        private async Task ChangeUrl(string id)
+        {
+            string currenturl = $"{NavigationManagerExtensions.GetSection(NavigationManager)}/{NavigationManagerExtensions.GetComponentLink(NavigationManager)}";
+            if (currenturl.Contains("#"))
+            {
+                currenturl = currenturl.Substring(0, currenturl.IndexOf("#") + 0);
+            }
+            _pageuri = currenturl;
+            await JsRuntime.InvokeVoidAsync("ChangeUrl", $"{_pageuri}#{id}");
+        }
+
+        private async Task ObserveAllSectionsAsync()
+        {
+            var observeResult = (await ResizeObserver.Observe(_sections.Select(x => x.Reference).ToArray())).ToArray();
+
+            for (int i = 0; i < _sections.Count; i++)
+            {
+                _sections[i].Location = observeResult[i];
+            }
+            _scrolled = true;
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            ScrollListener.OnScroll -= ScrollListener_OnScroll;
+            NavigationManager.LocationChanged -= TryFragmentNavigation;
+            ResizeObserver.Dispose();
+        }
+
+        private string GetNavLinkClass(bool active)
+        {
+            if (active)
+                return $"docs-contents-navlink active";
+            else
+                return $"docs-contents-navlink";
+        }
+    }
+}
