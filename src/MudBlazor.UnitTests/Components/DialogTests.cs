b4af7c8b4a29b353a7b0c7ff03cea563622ef472
Lukas Klinger <lk@mim.gmbh>
2021-07-05 14:03:41 +0200

    Improvements to the dialog system (#2132)
    * Moved dismiss to interface, made it public and let it return a success bool
    
    * Dismiss instance now considers the success bool to proceed with removing it
    
    * Trying to make object hierachy consistent
    
    * DialogService interface now requires Close functions to be implemented.
    
    Internal stuff is bad!!!
    
    * Generalized everything to use interfaces internally instead of assuming it will be a DialogService instance
    
    This enables more customizability without destorying internal logics
    
    * Return always true to not change the behavior in any way
    
    * Increased timeouts for the inline dialog test
    
    * Added test for custom dialog services
    
    * Removed unecessary modifiers on properties in the interface
    
    Co-authored-by: Meinrad Recheis <meinrad.recheis@gmail.com>

--- src/MudBlazor.UnitTests/Components/DialogTests.cs
+++ src/MudBlazor.UnitTests/Components/DialogTests.cs
@@ -2,9 +2,11 @@
 #pragma warning disable IDE1006 // leading underscore
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Bunit;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.UnitTests.TestComponents;
 using MudBlazor.UnitTests.TestComponents.Dialog;
 using NUnit.Framework;
@@ -110,13 +112,13 @@ namespace MudBlazor.UnitTests.Components
             comp.Find("div.mud-dialog-container").Should().NotBe(null);
             // close by click outside
             comp.Find("div.mud-overlay").Click();
-            comp.WaitForAssertion(() => comp.Markup.Trim().Should().BeEmpty(), TimeSpan.FromSeconds(2));
+            comp.WaitForAssertion(() => comp.Markup.Trim().Should().BeEmpty(), TimeSpan.FromSeconds(5));
             // open again
             comp1.Find("button").Click();
-            comp.WaitForAssertion(() => comp.Find("div.mud-dialog-container").Should().NotBe(null), TimeSpan.FromSeconds(2));
+            comp.WaitForAssertion(() => comp.Find("div.mud-dialog-container").Should().NotBe(null), TimeSpan.FromSeconds(5));
             // close again by click outside
             comp.Find("div.mud-overlay").Click();
-            comp.WaitForAssertion(() => comp.Markup.Trim().Should().BeEmpty(), TimeSpan.FromSeconds(2));
+            comp.WaitForAssertion(() => comp.Markup.Trim().Should().BeEmpty(), TimeSpan.FromSeconds(5));
         }
 
         /// <summary>
@@ -225,6 +227,65 @@ namespace MudBlazor.UnitTests.Components
             testComp.WaitForAssertion(()=>
                 testComp.Find("p").TextContent.Trim().Should().Be("Search Text:  User input ...")
             );
+        }
+
+        [Test]
+        public async Task CustomDialogService()
+        {
+            //Remove default IDialogService so we can provide our custom implementation
+            //This is not necessary in normal cases, you would rather just not register all services in the beginning, but the test environment requires here to do so.
+            ctx.Services.Remove(ctx.Services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(IDialogService)));
+            //Register our custom dialog service implementation as the new service instance behind IDialogService
+            ctx.Services.AddScoped<IDialogService>(sp => new CustomDialogService());
+
+            //Render our dialog provider and make sure everything is fine
+            var comp = ctx.RenderComponent<MudDialogProvider>();
+            comp.Markup.Trim().Should().BeEmpty();
+
+            //Try to get the current service instance for the type IDialogService and make sure it is our custom implementation
+            var service = ctx.Services.GetService<IDialogService>();
+            service.Should().NotBe(null);
+            service.Should().BeAssignableTo(typeof(CustomDialogService));
+
+            //Show the dialog, create reference and make sure it is our custom dialog reference implementation
+            IDialogReference dialogReference = null;
+            //The type of the dialog does not really matter, for the sake of laziness I will re-use this existing one
+            await comp.InvokeAsync(() => dialogReference = service?.Show<DialogThatUpdatesItsTitle>());
+            dialogReference.Should().NotBe(null);
+            dialogReference.Should().BeAssignableTo(typeof(CustomDialogReference));
+
+            //After above checks have passed, we can safely cast the generic reference into our specific implementation  
+            var customDialogReference = (CustomDialogReference)dialogReference;
+            //The custom property should be false by default otherwise the rest of the test logic would be incorrect
+            customDialogReference.AllowDismiss.Should().BeFalse();
+
+            //Dialog should not be closable through backdrop click
+            comp.Find("div.mud-overlay").Click();
+            comp.WaitForAssertion(() => comp.Markup.Trim().Should().NotBeEmpty(), TimeSpan.FromSeconds(5));
+
+            //Allow dismission
+            customDialogReference.AllowDismiss = true;
+
+            //Dialog should now be closable through backdrop click
+            comp.Find("div.mud-overlay").Click();
+            comp.WaitForAssertion(() => comp.Markup.Trim().Should().BeEmpty(), TimeSpan.FromSeconds(5));
+        }
+    }
+
+    internal class CustomDialogService : DialogService
+    {
+        public override IDialogReference CreateReference() => new CustomDialogReference(Guid.NewGuid(), this);
+    }
+
+    internal class CustomDialogReference : DialogReference
+    {
+        public bool AllowDismiss { get; set; }
+
+        public CustomDialogReference(Guid dialogInstanceId, IDialogService dialogService) : base(dialogInstanceId, dialogService) { }
+
+        public override bool Dismiss(DialogResult result)
+        {
+            return AllowDismiss;
         }
     }
 }
