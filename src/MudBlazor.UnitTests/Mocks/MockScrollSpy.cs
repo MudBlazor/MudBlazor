8632435e27994012b90b32c41dda57391f6e5e15
Benjamin Kappel <b.kappel90@outlook.de>
2021-07-05 02:52:54 -0500

    added MockScrollSpy for unit testing

---
+++ src/MudBlazor.UnitTests/Mocks/MockScrollSpy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MudBlazor.UnitTests.Mocks
+{
+    /// <summary>
+    /// Mock for scroll spy
+    /// </summary>
+    public class MockScrollSpy : IScrollSpy
+    {
+        public string CenteredSection => "my-item";
+
+        public event EventHandler<ScrollSectionSectionCenteredEventArgs> ScrollSectionSectionCentered;
+
+        public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+        public Task ScrollToSection(string id) => Task.FromResult(true);
+        public Task ScrollToSection(Uri uri) => Task.FromResult(false);
+        public Task StartSpying(string elementsSelector) => Task.FromResult(false);
+
+        public void FireScrollSectionSectionCenteredEvent(string centeredElementId) => ScrollSectionSectionCentered?.Invoke(this, new(centeredElementId));
+    }
+}
