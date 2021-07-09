7526801f5bafafedae9eae3be2371cc25a6d496e
Meinrad Recheis <meinrad.recheis@gmail.com>
2021-07-09 19:17:49 +0200

    Docs: search bar shows list of most popular pages when clicked

--- src/MudBlazor.Docs/Shared/MainLayout.razor.cs
+++ src/MudBlazor.Docs/Shared/MainLayout.razor.cs
@@ -60,13 +60,28 @@ namespace MudBlazor.Docs.Shared
         private Task<IEnumerable<ApiLinkServiceEntry>> Search(string text)
         {
             if (string.IsNullOrWhiteSpace(text))
-                return Task.FromResult<IEnumerable<ApiLinkServiceEntry>>(Array.Empty<ApiLinkServiceEntry>());
+            {
+                // the user just clicked the autocomplete open, show the most popular pages as search result according to our analytics data
+                // ordered by popularity
+                return Task.FromResult<IEnumerable<ApiLinkServiceEntry>>(new[] {
+                    new ApiLinkServiceEntry{ Title = "Installation", Link="getting-started/installation", SubTitle="Getting started with MudBlazor fast and easy." },
+                    new ApiLinkServiceEntry{ Title = "Wireframes", Link="getting-started/wireframes", SubTitle="These small templates can be copied directly or just be used for inspiration." },
+                    new ApiLinkServiceEntry{ Title = "Table", Link ="components/table", ComponentType=typeof(MudTable<T>), SubTitle = "A sortable, filterable table with multiselection and pagination." },
+                    new ApiLinkServiceEntry{ Title = "Grid", Link ="components/grid", ComponentType=typeof(MudGrid), SubTitle = "The grid component helps keeping layout consistent across various screen resolutions and sizes." },
+                    new ApiLinkServiceEntry{ Title = "Button", Link ="components/button", ComponentType=typeof(MudGrid), SubTitle = "A Material Design button for triggering an action or navigating to a link." },
+                    new ApiLinkServiceEntry{ Title = "Card", Link ="components/card", ComponentType=typeof(MudCard), SubTitle = "Cards can contain actions, text, or media like images or graphics." },
+                    new ApiLinkServiceEntry{ Title = "Dialog", Link ="components/dialog", ComponentType=typeof(MudDialog), SubTitle = "A dialog will overlay your current app content, providing the user with either information, a choice, or other tasks." },
+                    new ApiLinkServiceEntry{ Title = "App Bar", Link ="components/appbar", ComponentType=typeof(MudAppBar), SubTitle = "App bar is used to display actions, branding, navigation and screen titles." },
+                    new ApiLinkServiceEntry{ Title = "Navigation Menu", Link ="components/navmenu", ComponentType=typeof(MudNavMenu), SubTitle = "Nav menu provides a tree-like menu linking to the content on your site." },
+                });
+            }
             return ApiLinkService.Search(text);
         }
 
-        private void OnSearchResult(ApiLinkServiceEntry entry)
+        private async void OnSearchResult(ApiLinkServiceEntry entry)
         {
             NavigationManager.NavigateTo(entry.Link);
+            await Task.Delay(1);
         }
 
         private void OnSwipe(SwipeDirection direction)
