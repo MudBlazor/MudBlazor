c6be989e9267632cea7af46f6d7aef9b0df20ea6
Jonny Larsson <jonny@gardnet.nu>
2021-07-09 17:59:33 +0200

    Added back new MudTimelineItem

---
+++ src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudTimelineItem : MudComponentBase
+    {
+        protected string Classnames =>
+            new CssBuilder("mud-timeline-item")
+            .AddClass(Class)
+            .Build();
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+    }
+}
