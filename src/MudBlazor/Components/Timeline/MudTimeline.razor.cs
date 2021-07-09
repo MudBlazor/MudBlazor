65ce3db300bc22efed8b8c306814863088d09b06
Henrique Clausing <hclausing@hotmail.com>
2021-07-09 15:44:41 -0300

    Added initial work on component.

--- src/MudBlazor/Components/Timeline/MudTimeline.razor.cs
+++ src/MudBlazor/Components/Timeline/MudTimeline.razor.cs
@@ -2,22 +2,62 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public partial class MudTimeline : MudComponentBase
+    public partial class MudTimeline<TData> : MudBaseBindableItemsControl<MudTimelineItem, TData>, IAsyncDisposable
+
     {
         protected string Classnames =>
             new CssBuilder("mud-timeline")
-            .AddClass(Class)
-            .Build();
+                .AddClass($"mud-timeline-rounded", Rounded)
+                .AddClass($"mud-border-right", Border)
+                .AddClass($"mud-paper-outlined", Outlined)
+                .AddClass($"mud-elevation-{Elevation}", Elevation != 0)
+                .AddClass(Class)
+                .Build();
+
+        [CascadingParameter] public bool RightToLeft { get; set; }
+
+
+        /// <summary>
+        /// If true, sets the border-radius to theme default.
+        /// </summary>
+        [Parameter] public bool Rounded { get; set; }
+
+        /// <summary>
+        /// If true, sets a border.
+        /// </summary>
+        [Parameter] public bool Border { get; set; }
+
+        /// <summary>
+        /// If true, toolbar will be outlined.
+        /// </summary>
+        [Parameter] public bool Outlined { get; set; }
 
         /// <summary>
         /// Child content of component.
         /// </summary>
-        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public int Elevation { set; get; } = 0;
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsync(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual async ValueTask DisposeAsync(bool disposing)
+        {
+            if (disposing)
+            {
+                //TODO
+                await Task.Delay(1);
+            }
+        }
 
     }
 }
--- src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
+++ src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
@@ -2,22 +2,54 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public partial class MudTimelineItem : MudComponentBase
+    public partial class MudTimelineItem : MudComponentBase, IDisposable
+
     {
         protected string Classnames =>
-            new CssBuilder("mud-timeline-item")
-            .AddClass(Class)
-            .Build();
+            new CssBuilder("mud-timeline")
+                .AddClass($"mud-elevation-{Elevation}", Elevation != 0)
+                .AddClass(Class)
+                .Build();
+
+        [CascadingParameter] protected internal MudBaseItemsControl<MudTimelineItem> Parent { get; set; }
+        [CascadingParameter] public bool RightToLeft { get; set; }
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
+        [Parameter] public int Elevation { set; get; } = 0;
+
+        /// <summary>
+        /// TimeLineItem's Alignment on main TimeLine component
+        /// </summary>
+        [Parameter] public Align Align { get; set; } = Align.Left;
+
+        /// <summary>
+        /// Icon for the TimeLineItem
+        /// </summary>
+        [Parameter] public string Icon { get; set; }
+
+        /// <summary>
+        /// Color for the TimeLineItem symbol
+        /// </summary>
+        [Parameter] public Color Color { get; set; } = Color.Primary;
 
         /// <summary>
         /// Child content of component.
         /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
+
+        public void Dispose()
+        {
+            Parent?.Items.Remove(this);
+        }
+
 
     }
 }
