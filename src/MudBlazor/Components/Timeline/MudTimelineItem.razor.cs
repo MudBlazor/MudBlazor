84a2587d3aa75594aead1550edf6e83046611fc3
Jonny Larsson <jonny@gardnet.nu>
2021-07-12 17:14:48 +0200

    TimelineItem: Added TimelineMode and OppositeContent

--- src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
+++ src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
@@ -25,7 +25,7 @@ namespace MudBlazor
 
         protected string DotInnerClassnames =>
         new CssBuilder("mud-timeline-item-dot-inner")
-          .AddClass($"mud-timeline-dot-fill", FillDot)
+          .AddClass($"mud-timeline-dot-fill", Variant == Variant.Filled)
           .AddClass($"mud-{Color.ToDescriptionString()}")
         .Build();
 
@@ -35,6 +35,11 @@ namespace MudBlazor
         /// Dot Icon
         /// </summary>
         [Parameter] public string Icon { get; set; }
+
+        /// <summary>
+        /// Variant of the dot.
+        /// </summary>
+        [Parameter] public Variant Variant { get; set; } = Variant.Outlined;
 
         /// <summary>
         /// Color of the dot.
@@ -52,17 +57,22 @@ namespace MudBlazor
         [Parameter] public int Elevation { set; get; } = 1;
 
         /// <summary>
-        /// If true, dot will be filled with one color.
-        /// </summary>
-        [Parameter] public bool FillDot { get; set; }
-
-        /// <summary>
         /// If true, dot will not be displayed.
         /// </summary>
         [Parameter] public bool HideDot { get; set; }
 
         /// <summary>
-        /// Child content of component.
+        /// If used renders child content of the TimelineOppositeContent.
+        /// </summary>
+        [Parameter] public RenderFragment TimelineOppositeContent { get; set; }
+
+        /// <summary>
+        /// If used renders child content of the TimelineItemContent.
+        /// </summary>
+        [Parameter] public RenderFragment TimelineItemContent { get; set; }
+
+        /// <summary>
+        /// Optional child content
         /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
 
