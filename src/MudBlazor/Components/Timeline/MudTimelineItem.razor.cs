82ea113a9189147e8e176cbd0ec5eec6019155c8
Jonny Larsson <jonny@gardnet.nu>
2021-07-19 18:34:30 +0200

    TimelineItem Align/Skip order

--- src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
+++ src/MudBlazor/Components/Timeline/MudTimelineItem.razor.cs
@@ -15,6 +15,7 @@ namespace MudBlazor
     {
         protected string Classnames =>
             new CssBuilder("mud-timeline-item")
+                .AddClass($"mud-timeline-item-skip-order", SkipOrder)
                 .AddClass(Class)
                 .Build();
 
@@ -61,6 +62,11 @@ namespace MudBlazor
         /// If true, dot will not be displayed.
         /// </summary>
         [Parameter] public bool HideDot { get; set; }
+
+        /// <summary>
+        /// If true, TimelineItem skips the sorting order from MudTimeline and is positioned on same side as the item before and after this item.
+        /// </summary>
+        [Parameter] public bool SkipOrder { get; set; }
 
         /// <summary>
         /// If used renders child content of the TimelineOppositeContent.
