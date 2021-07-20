2488f8a156bee06c40180f4dc5f15d0fdca6c85c
Mike Surcouf <mikes@surcouf.co.uk>
2021-07-20 09:46:02 +0100

    Enable CS1591 Missing XML Docs (exclude Colors and Icons)

--- src/MudBlazor/Icons/Custom/Brands.cs
+++ src/MudBlazor/Icons/Custom/Brands.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System.Diagnostics.CodeAnalysis;
 
 namespace MudBlazor
 {
