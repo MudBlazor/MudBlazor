90bcc5f3e807c9d873959eca228df1e937a29e0f
Jonny Larsson <jonny@gardnet.nu>
2021-07-21 18:27:21 +0200

    MudAvatar added functionality for AvatarGroup

--- src/MudBlazor/Components/Avatar/MudAvatar.razor.cs
+++ src/MudBlazor/Components/Avatar/MudAvatar.razor.cs
@@ -6,6 +6,7 @@ namespace MudBlazor
 {
     partial class MudAvatar : MudComponentBase
     {
+        [CascadingParameter] private MudAvatarGroup AvatarGroup { get; set; }
         protected string Classname =>
         new CssBuilder("mud-avatar")
           .AddClass($"mud-avatar-{Size.ToDescriptionString()}")
@@ -13,8 +14,16 @@ namespace MudBlazor
           .AddClass($"mud-avatar-square", Square)
           .AddClass($"mud-avatar-{Variant.ToDescriptionString()}")
           .AddClass($"mud-avatar-{Variant.ToDescriptionString()}-{Color.ToDescriptionString()}")
+          .AddClass($"ms-n{_groupSpacing}", AvatarGroup != null)
+          .AddClass($"z-{_groupPosition}", AvatarGroup != null)
+          .AddClass($"mud-elevation-{Elevation.ToString()}")
           .AddClass(Class)
         .Build();
+
+        /// <summary>
+        /// The higher the number, the heavier the drop-shadow.
+        /// </summary>
+        [Parameter] public int Elevation { set; get; } = 0;
 
         /// <summary>
         /// If true, border-radius is set to 0.
@@ -50,5 +59,42 @@ namespace MudBlazor
         /// Child content of the component.
         /// </summary>
         [Parameter] public RenderFragment ChildContent { get; set; }
+
+        internal bool GroupMaxReached { get; set; }
+
+        private int _groupSpacing;
+        private int _groupPosition;
+
+        private int GetGroupSpacing()
+        {
+            if(AvatarGroup != null)
+            {
+                return AvatarGroup.Spacing;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if (AvatarGroup != null)
+            {
+                _groupPosition = 99 - AvatarGroup._avatars.Count + 1;
+                _groupSpacing = AvatarGroup.Spacing;
+                AvatarGroup.AddAvatar(this);
+            }
+        }
+
+        protected void Dispose()
+        {
+            if (AvatarGroup != null)
+            {
+                AvatarGroup.RemoveAvatar(this);
+            }
+        }
     }
 }
