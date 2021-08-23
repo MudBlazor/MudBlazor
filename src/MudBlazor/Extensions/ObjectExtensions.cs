d5701439d013162813465e55a3cfcfe5f1db8317
Christophe <christophe.bude@uantwerpen.be>
2021-08-23 13:54:58 +0200

    simplify code

--- src/MudBlazor/Extensions/ObjectExtensions.cs
+++ src/MudBlazor/Extensions/ObjectExtensions.cs
@@ -2,12 +2,9 @@
 {
     public static class ObjectExtensions
     {
-
         public static T As<T>(this object self)
-        {
-            if (self == null || !(self is T))
-                return default;
-            return (T)self;
-        }
+            => self is not T selfT
+                ? default
+                : selfT;
     }
 }
