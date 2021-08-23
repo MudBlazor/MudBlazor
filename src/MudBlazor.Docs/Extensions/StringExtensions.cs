220abef940379d8a1716155b6cb644f58cbba417
Christophe <christophe.bude@uantwerpen.be>
2021-08-23 14:35:35 +0200

    simplify code

--- src/MudBlazor.Docs/Extensions/StringExtensions.cs
+++ src/MudBlazor.Docs/Extensions/StringExtensions.cs
@@ -71,18 +71,13 @@ namespace MudBlazor.Docs.Extensions
 
         public static string ToCompressedEncodedUrl(this string code)
         {
-            string urlEncodedBase64compressedCode;
-            byte[] bytes;
-
             using var uncompressed = new MemoryStream(Encoding.UTF8.GetBytes(code));
             using var compressed = new MemoryStream();
             using var compressor = new DeflateStream(compressed, CompressionMode.Compress);
             uncompressed.CopyTo(compressor);
             compressor.Close();
-            bytes = compressed.ToArray();
-            urlEncodedBase64compressedCode = WebEncoders.Base64UrlEncode(bytes);
 
-            return urlEncodedBase64compressedCode;
+            return WebEncoders.Base64UrlEncode(compressed.ToArray());
         }
     }
 }
