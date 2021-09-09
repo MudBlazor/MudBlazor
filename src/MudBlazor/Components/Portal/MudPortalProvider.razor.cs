f9b3a0a4fda7608db4f536358d8210e17e629161
Benjamin Kappel <51370361+just-the-benno@users.noreply.github.com>
2021-09-09 11:31:05 -0500

    first try of new portal (#2684)

--- src/MudBlazor/Components/Portal/MudPortalProvider.razor.cs
+++ src/MudBlazor/Components/Portal/MudPortalProvider.razor.cs
@@ -4,30 +4,30 @@ using MudBlazor.Services;
 
 namespace MudBlazor
 {
-    public partial class MudPortalProvider : IDisposable
+    public partial class MudPortalProvider
     {
-        [Inject] private IPortal Portal { get; set; }
+        //[Inject] private IPortal Portal { get; set; }
 
-        protected override void OnInitialized() => Portal.OnChange += HandleChange;
+        //protected override void OnInitialized() => Portal.OnChange += HandleChange;
 
-        /// <summary>
-        /// This is called when the portal adds or removes an item
-        /// </summary>
-        private void HandleChange(object _, PortalEventsArg e)
-        {
-            InvokeAsync(StateHasChanged);
-        }
+        ///// <summary>
+        ///// This is called when the portal adds or removes an item
+        ///// </summary>
+        //private void HandleChange(object _, PortalEventsArg e)
+        //{
+        //    InvokeAsync(StateHasChanged);
+        //}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposing) return;
-            Portal.OnChange -= HandleChange;
-        }
+        //protected virtual void Dispose(bool disposing)
+        //{
+        //    if (!disposing) return;
+        //    Portal.OnChange -= HandleChange;
+        //}
 
-        void IDisposable.Dispose()
-        {
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
+        //void IDisposable.Dispose()
+        //{
+        //    Dispose(disposing: true);
+        //    GC.SuppressFinalize(this);
+        //}
     }
 }
