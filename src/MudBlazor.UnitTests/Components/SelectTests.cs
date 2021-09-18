17b6bd156fc412ab1da2b0ab6515f7c6ba20e60e
mckaragoz <m.cankaragoz@outlook.com>
2021-09-18 04:30:20 +0300

    Try2

--- src/MudBlazor.UnitTests/Components/SelectTests.cs
+++ src/MudBlazor.UnitTests/Components/SelectTests.cs
@@ -670,26 +670,34 @@ namespace MudBlazor.UnitTests.Components
             comp.WaitForAssertion(() => select.Value.Should().Be(1));
             //Up key to be sure there is not out of index exception and select first item
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowUp", ShiftKey = true }));
+            await Task.Delay(1);
             comp.WaitForAssertion(() => select.Value.Should().Be(1));
             //Down key to select second item
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowDown", ShiftKey = true }));
+            await Task.Delay(1);
             comp.WaitForAssertion(() => select.Value.Should().Be(2));
             //Down key to select fourth item
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowDown", ShiftKey = true }));
+            await Task.Delay(1);
             comp.WaitForAssertion(() => select.Value.Should().Be(4));
             //Down key to be sure there is no out of index exception and select fourth item
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowDown", ShiftKey = true }));
-            comp.WaitForAssertion(() => select.Value.Should().Be(4));
+            await Task.Delay(1);
+            select.Value.Should().Be(4);
             //Up key to select second item
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowUp", ShiftKey = true }));
-            comp.WaitForAssertion(() => select.Value.Should().Be(2));
+            await Task.Delay(1);
+            select.Value.Should().Be(2);
             //Up key to select first item
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowUp", ShiftKey = true }));
+            await Task.Delay(1);
             comp.WaitForAssertion(() => select.Value.Should().Be(1));
             //Click non neighbour item to be sure up key recognizes the last item
             items[3].Click();
+            await Task.Delay(1);
             comp.WaitForAssertion(() => select.Value.Should().Be(4));
             comp.WaitForAssertion(() => comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "ArrowUp", ShiftKey = true }));
+            await Task.Delay(1);
             comp.WaitForAssertion(() => select.Value.Should().Be(2));
         }
     }
