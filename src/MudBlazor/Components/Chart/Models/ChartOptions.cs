d19c1b3a6a800391ff89dd8bda37bde10571090b
Bernardo Pinto <48964369+ritztm@users.noreply.github.com>
2021-09-29 11:38:38 +0100

    Revert "DonutChart inner circle Label"
    This reverts commit 2653642111c015e3fbf621083d526ca61fb32db8.

--- src/MudBlazor/Components/Chart/Models/ChartOptions.cs
+++ src/MudBlazor/Components/Chart/Models/ChartOptions.cs
@@ -22,17 +22,6 @@
         public bool DisableLegend { get; set; }
         public string[] ChartPalette { get; set; } = { Colors.Blue.Accent3, Colors.Teal.Accent3, Colors.Amber.Accent3, Colors.Orange.Accent3, Colors.Red.Accent3, Colors.DeepPurple.Accent3, Colors.Green.Accent3, Colors.LightBlue.Accent3, Colors.Teal.Lighten1, Colors.Amber.Lighten1, Colors.Orange.Lighten1, Colors.Red.Lighten1, Colors.DeepPurple.Lighten1, Colors.Green.Lighten1, Colors.LightBlue.Lighten1, Colors.Amber.Darken2, Colors.Orange.Darken2, Colors.Red.Darken2, Colors.DeepPurple.Darken2, Colors.Grey.Darken2 };
         public InterpolationOption InterpolationOption { get; set; } = InterpolationOption.Straight;
-
-        /// <summary>
-        /// Sets text in the Donut Chart inner circle.
-        /// </summary>
-        public string DonutInnerText { get; set; }
-
-        /// <summary>
-        /// Sets text font-size
-        /// </summary>
-        private int _fontSize = 12;
-        public int DonutInnerTextSize { get => _fontSize; set { if (_fontSize > 0) _fontSize = value; } }
     }
     public enum InterpolationOption
     {
