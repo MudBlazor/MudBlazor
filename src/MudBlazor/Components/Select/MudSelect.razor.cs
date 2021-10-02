642bcfc28b53588441b4f5219a05bf4123dafbfd
mckaragoz <m.cankaragoz@outlook.com>
2021-10-02 19:52:00 +0300

    S2

--- src/MudBlazor/Components/Select/MudSelect.razor.cs
+++ src/MudBlazor/Components/Select/MudSelect.razor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 using MudBlazor.Utilities;
 using MudBlazor.Utilities.Exceptions;
 
