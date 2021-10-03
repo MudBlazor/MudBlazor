f5b509779e9cecfc571cc0a388ed86da006ff89b
ClumsyPenguin <seppe318@gmail.com>
2021-10-03 12:38:21 +0200

    pie chart test

--- src/MudBlazor.UnitTests/Components/Charts/PieChartTests.cs
+++ src/MudBlazor.UnitTests/Components/Charts/PieChartTests.cs
@@ -2,6 +2,7 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
 using Bunit;
 using FluentAssertions;
 using MudBlazor.Charts;
@@ -38,9 +39,10 @@ namespace MudBlazor.UnitTests.Charts
         }
 
         [Test]
-        public void PieChartExampleData()
+        [TestCase(new double[]{77, 25, 20, 5})]
+        [TestCase(new double[]{77, 25, 20, 5, 8})]
+        public void PieChartExampleData(double[] data)
         {
-            double[] data = { 77, 25, 20, 5 };
             string[] labels = { "Uranium", "Plutonium", "Thorium", "Caesium", "Technetium", "Promethium",
                 "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Protactinium",
                 "Neptunium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Mudblaznium" };
@@ -52,8 +54,34 @@ namespace MudBlazor.UnitTests.Charts
                 .Add(p => p.Width, "300px")
                 .Add(p => p.InputData, data)
                 .Add(p => p.InputLabels,labels));
+            
+            comp.Markup.Should().Contain("class=\"mud-chart-pie\"");
+            comp.Markup.Should().Contain("class=\"mud-chart-serie\"");
+            comp.Markup.Should().Contain("mud-chart-legend-item");
+            
+            if (data.Length <= 4)
+            {
+                comp.Markup.Should().
+                    Contain("Uranium").And.Contain("Plutonium").And.Contain("Thorium").And.Contain("Caesium");
+            }
+            
+            if (data.Length <= 5)
+            {
+                comp.Markup.Should()
+                    .Contain("Technetium");
+            }
 
-            var t = comp;
+            if (data.Contains(77))
+            {
+                comp.Markup.Should()
+                    .Contain("M 1 0 A 1 1 0 1 1 -0.7851254621398548 -0.6193367490305087 L 0 0");
+            }
+
+            if (data.Contains(5))
+            {
+                comp.Markup.Should()
+                    .Contain("M 0.9695598647982466 -0.24485438238350116 A 1 1 0 0 1 1 -2.4492935982947064E-16 L 0 0");
+            }
         }
     }
 }
