4d8385342ef99bd1b8c280e678904618c6bb5ed5
mckaragoz <m.cankaragoz@outlook.com>
2021-10-06 20:48:21 +0300

    Start Commit

--- src/MudBlazor/Components/Switch/MudSwitch.razor.cs
+++ src/MudBlazor/Components/Switch/MudSwitch.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Extensions;
 using MudBlazor.Utilities;
 
@@ -9,6 +10,7 @@ namespace MudBlazor
         protected string Classname =>
         new CssBuilder("mud-switch")
             .AddClass($"mud-disabled", Disabled)
+            .AddClass($"mud-readonly", ReadOnly)
           .AddClass(Class)
         .Build();
         protected string SwitchClassname =>
@@ -16,7 +18,13 @@ namespace MudBlazor
             .AddClass($"mud-ripple mud-ripple-switch", !DisableRipple)
             .AddClass($"mud-switch-{Color.ToDescriptionString()}")
             .AddClass($"mud-switch-disabled", Disabled)
+            .AddClass($"mud-readonly", ReadOnly)
             .AddClass($"mud-checked", BoolValue)
+        .Build();
+
+        protected string SpanClassname =>
+        new CssBuilder("mud-switch-span mud-flip-x-rtl")
+            .AddClass($"mud-switch-span-{Size.ToDescriptionString()}")
         .Build();
 
         /// <summary>
@@ -29,10 +37,51 @@ namespace MudBlazor
         /// </summary>
         [Parameter] public string Label { get; set; }
 
+        [Parameter] public string SwitchIconOn { get; set; } = Icons.Material.Filled.Done;
+
+        [Parameter] public string SwitchIconOff { get; set; } = Icons.Material.Filled.Close;
+
+        [Parameter] public Size Size { get; set; } = Size.Large;
+
+        [Parameter] public bool Progress { get; set; } = false;
+
+        private string SwitchIcon = "";
+
         /// <summary>
         /// If true, disables ripple effect.
         /// </summary>
         [Parameter] public bool DisableRipple { get; set; }
 
+        protected void SwitchOnChange(ChangeEventArgs e)
+        {
+            OnChange(e);
+            if (BoolValue == true)
+            {
+                SwitchIcon = SwitchIconOn;
+            }
+            else
+            {
+                SwitchIcon = SwitchIconOff;
+            }
+        }
+
+        protected void HandleKeyDown(KeyboardEventArgs obj)
+        {
+            //Space key works by default, so we didn't write it again.
+            if (Disabled || ReadOnly)
+                return;
+            switch (obj.Key)
+            {
+                case "ArrowLeft":
+                case "Escape":
+                    SetBoolValueAsync(false);
+                    break;
+                case "ArrowRight":
+                case "Enter":
+                case "NumpadEnter":
+                    SetBoolValueAsync(true);
+                    break;
+            }
+        }
     }
 }
