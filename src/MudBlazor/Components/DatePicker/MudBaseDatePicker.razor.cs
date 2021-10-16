04de5c7499c793634bbc7687b7177c230ab70baa
Jonny Larsson <jonny@gardnet.nu>
2021-10-16 23:53:41 +0200

    DatePicker: Fixed selected month color

--- src/MudBlazor/Components/DatePicker/MudBaseDatePicker.razor.cs
+++ src/MudBlazor/Components/DatePicker/MudBaseDatePicker.razor.cs
@@ -392,7 +392,7 @@ namespace MudBlazor
         private string GetMonthClasses(DateTime month)
         {
             if (GetMonthStart(0) == month)
-                return $"mud-picker-month-selected mud-color-text-{Color.ToDescriptionString()}";
+                return $"mud-picker-month-selected mud-{Color.ToDescriptionString()}-text";
             return null;
         }
 
