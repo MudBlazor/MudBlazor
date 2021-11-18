36548fb5beba6b316b687947bb65c30c214488b5
Kasbolat Kumakhov <uhfath@gmail.com>
2021-11-18 11:49:48 +0300

    Table: Fix incorrect async code path for label sorting (#3352)

--- src/MudBlazor.UnitTests/Components/TableTests.cs
+++ src/MudBlazor.UnitTests/Components/TableTests.cs
@@ -834,6 +834,86 @@ namespace MudBlazor.UnitTests.Components
         }
 
         /// <summary>
+        /// The server-side load callback should be called only once per page change
+        /// </summary>
+        [Test]
+        public async Task TableServerSideDataTest5()
+        {
+            var comp = Context.RenderComponent<TableServerSideDataTest5>();
+            comp.Find("#counter").TextContent.Should().Be("1"); //initial counter
+
+            comp.FindAll("div.mud-table-pagination-actions button")[2].Click(); // next >
+            comp.Find("#counter").TextContent.Should().Be("2");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[2].Click(); // next >
+            comp.Find("#counter").TextContent.Should().Be("3");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[2].Click(); // next >
+            comp.Find("#counter").TextContent.Should().Be("4");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[1].Click(); // < previous
+            comp.Find("#counter").TextContent.Should().Be("5");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[1].Click(); // < previous
+            comp.Find("#counter").TextContent.Should().Be("6");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[1].Click(); // < previous
+            comp.Find("#counter").TextContent.Should().Be("7");
+
+            comp.Find("#reseter").Click(); //reset counter and test again
+            comp.Find("#counter").TextContent.Should().Be("0");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[2].Click(); // next >
+            comp.Find("#counter").TextContent.Should().Be("1");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[2].Click(); // next >
+            comp.Find("#counter").TextContent.Should().Be("2");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[2].Click(); // next >
+            comp.Find("#counter").TextContent.Should().Be("3");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[1].Click(); // < previous
+            comp.Find("#counter").TextContent.Should().Be("4");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[1].Click(); // < previous
+            comp.Find("#counter").TextContent.Should().Be("5");
+
+            comp.FindAll("div.mud-table-pagination-actions button")[1].Click(); // < previous
+            comp.Find("#counter").TextContent.Should().Be("6");
+        }
+
+        /// <summary>
+        /// The server-side load callback should be called only once per sort change
+        /// </summary>
+        [Test]
+        public async Task TableServerSideDataTest6()
+        {
+            var comp = Context.RenderComponent<TableServerSideDataTest5>();
+            comp.Find("#counter").TextContent.Should().Be("1"); //initial counter
+
+            comp.Find("span.mud-button-root.mud-table-sort-label").Click(); // sort
+            comp.Find("#counter").TextContent.Should().Be("2");
+
+            comp.Find("span.mud-button-root.mud-table-sort-label").Click(); // sort
+            comp.Find("#counter").TextContent.Should().Be("3");
+
+            comp.Find("span.mud-button-root.mud-table-sort-label").Click(); // sort
+            comp.Find("#counter").TextContent.Should().Be("4");
+
+            comp.Find("#reseter").Click(); //reset counter and test again
+            comp.Find("#counter").TextContent.Should().Be("0");
+
+            comp.Find("span.mud-button-root.mud-table-sort-label").Click(); // sort
+            comp.Find("#counter").TextContent.Should().Be("1");
+
+            comp.Find("span.mud-button-root.mud-table-sort-label").Click(); // sort
+            comp.Find("#counter").TextContent.Should().Be("2");
+
+            comp.Find("span.mud-button-root.mud-table-sort-label").Click(); // sort
+            comp.Find("#counter").TextContent.Should().Be("3");
+        }
+
+        /// <summary>
         /// The table should render the classes and style to the tr using the RowStyleFunc and RowClassFunc parameters
         /// </summary>
         [Test]
