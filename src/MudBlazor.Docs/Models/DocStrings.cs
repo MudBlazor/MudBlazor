f82c18d119d4d39f0c4bb6973e90a224d6ce8dd6
iwis <iwis@users.noreply.github.com>
2021-12-05 13:07:27 +0100

    API documentation: huge performance increase on wasm (#3489)
    * Optimize CheckIsTwoWayProperty method
    
    * Optimize GetPropertyDescription method
    
    * Remove unnecessary using directive
    
    * Rerun failed test

--- src/MudBlazor.Docs/Models/DocStrings.cs
+++ src/MudBlazor.Docs/Models/DocStrings.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace MudBlazor.Docs.Models
 {
-    // this is needed for the api docs
+    // this is needed for the api docs 
     public static partial class DocStrings
     {
         public static string GetPropertyDescription(Type t, string property)
         {
             var name = $"{GetSaveTypename(t).Replace("<T>", "").TrimEnd('_')}_{property}";
-            var field = typeof(DocStrings).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField)
-                .FirstOrDefault(f => f.Name == name);
+            var field = typeof(DocStrings).GetField(name, BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField);
             if (field == null)
                 return null;
             return (string)field.GetValue(null);
