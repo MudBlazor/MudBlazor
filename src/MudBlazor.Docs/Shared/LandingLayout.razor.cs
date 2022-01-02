9cee3585b7ceba7cc5125d66c6fc5209659864ba
Benjamin Kappel <b.kappel90@outlook.de>
2022-01-02 15:05:30 -0600

    Modifying theming a bit

--- src/MudBlazor.Docs/Shared/LandingLayout.razor.cs
+++ src/MudBlazor.Docs/Shared/LandingLayout.razor.cs
@@ -2,7 +2,6 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
 namespace MudBlazor.Docs.Shared
@@ -10,10 +9,9 @@ namespace MudBlazor.Docs.Shared
     public partial class LandingLayout : LayoutComponentBase
     {
         [CascadingParameter] private MainLayout MainData { get; set; }
-        
-        protected override async Task OnInitializedAsync()
+        protected override void OnInitialized()
         {
-            await MainData.SetBaseTheme(Theme.DocsTheme());
+            MainData.SetBaseTheme(Theme.LandingPageTheme());
         }
     }
 }
