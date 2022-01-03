0c1143a69c06192442d0381ad0c6d9dc78d3545a
Ambert Jean-Christophe <jc.ambert@free.fr>
2022-01-03 11:48:53 +0100

    Correct Readonly/Disable OnMouseWheel Handler

--- src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
+++ src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
@@ -264,7 +264,7 @@ namespace MudBlazor
 
         protected async Task OnMouseWheel(WheelEventArgs obj)
         {
-            if (!obj.ShiftKey)
+            if (!obj.ShiftKey || Disabled || ReadOnly)
                 return;
             if (obj.DeltaY < 0)
             {
