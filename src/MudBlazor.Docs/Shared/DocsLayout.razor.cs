6b55f2d7457739aa2763d9c1d2678791896152df
Jonny Larsson <jonny@gardnet.nu>
2022-01-06 17:14:44 +0100

    Changed back to layoutbased structure

--- src/MudBlazor.Docs/Shared/DocsLayout.razor.cs
+++ src/MudBlazor.Docs/Shared/DocsLayout.razor.cs
@@ -9,37 +9,11 @@ namespace MudBlazor.Docs.Shared;
 
 public partial class DocsLayout : LayoutComponentBase
 {
-    [CascadingParameter] private MainLayout MainData { get; set; }
-    
     private NavMenu _navMenuRef;
-    private bool _drawerOpen = true;
-    private bool _topMenuOpen = false;
-    
-    protected override void OnInitialized()
-    {
-         MainData.SetBaseTheme(Theme.DocsTheme());
-    }
-    
+
     protected override void OnAfterRender(bool firstRender)
     {
         //refresh nav menu because no parameters change in nav menu but internal data does
         _navMenuRef.Refresh();
-    }
-
-    private void ToggleDrawer()
-    {
-        _drawerOpen = !_drawerOpen;
-    }
-
-    private void OpenTopMenu()
-    {
-        _topMenuOpen = true;
-    }
-
-    private void OnDrawerOpenChanged(bool value)
-    {
-        _topMenuOpen = false;
-        _drawerOpen = value;
-        StateHasChanged();
     }
 }
---
+++ src/MudBlazor.Docs/Shared/DocsMainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MudBlazor.Docs.Shared;
+
+public partial class DocsMainLayout : LayoutComponentBase
+{
+    [CascadingParameter] private MainLayout MainData { get; set; }
+    
+    private bool _drawerOpen = true;
+
+    protected override void OnInitialized()
+    {
+         MainData.SetBaseTheme(Theme.DocsTheme());
+    }
+    
+    private void ToggleDrawer()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+}
