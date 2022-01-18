MudBlazor/MudBlazor

commit 2875394c2c3a07a6b198d4535b68545297461040

MudDialog: Fix Class, Style and Tag properties ignored in inline dialog (#3128) (#3252)

src/MudBlazor/Components/Dialog/MudDialog.razor.cs
@@ -93,13 +93,6 @@ namespace MudBlazor
                 if (_isVisible == value)
                     return;
                 _isVisible = value;
-                if (IsInline)
-                {
-                    if (_isVisible)
-                        Show();
-                    else
-                        Close();
-                }
                 IsVisibleChanged.InvokeAsync(value);
             }
         }
@@ -128,6 +121,9 @@ namespace MudBlazor
                 Close();
             var parameters = new DialogParameters()
             {
+                [nameof(Class)] = Class,
+                [nameof(Style)] = Style,
+                [nameof(Tag)] = Tag,
                 [nameof(TitleContent)] = TitleContent,
                 [nameof(DialogContent)] = DialogContent,
                 [nameof(DialogActions)] = DialogActions,
@@ -148,6 +144,13 @@ namespace MudBlazor
         {
             if (IsInline && _reference != null)
                 (_reference.Dialog as MudDialog)?.ForceUpdate(); // forward render update to instance
+            if (IsInline)
+            {
+                if (_isVisible)
+                    Show();
+                else
+                    Close();
+            }
             base.OnAfterRender(firstRender);
         }
 

src/MudBlazor.UnitTests/Components/DialogTests.cs
@@ -112,6 +112,30 @@ namespace MudBlazor.UnitTests.Components
         }
 
         /// <summary>
+        /// Based on bug report #3128
+        /// Dialog Class and Style parameters should be honored for inline dialog
+        /// </summary>
+        [Ignore("Sadly we can not get this test to work when it is run in bulk (local and CI). It passes when executed individually")]
+        [Test]
+        public async Task InlineDialogShouldHonorClassAndStyle()
+        {
+            var comp = Context.RenderComponent<MudDialogProvider>();
+            comp.Markup.Trim().Should().BeEmpty();
+            var service = Context.Services.GetService<IDialogService>() as DialogService;
+            service.Should().NotBe(null);
+            IDialogReference dialogReference = null;
+            // open simple test dialog
+            await comp.InvokeAsync(() => dialogReference = service?.Show<TestInlineDialog>());
+            dialogReference.Should().NotBe(null);
+            comp.Find("button").Click();
+            comp.Find("div.mud-dialog").ClassList.Should().Contain("test-class");
+            comp.Find("div.mud-dialog").Attributes["style"].Value.Should().Be("color: red;");
+            comp.Find("div.mud-dialog-content").Attributes["style"].Value.Should().Be("color: blue;");
+            comp.Find("div.mud-dialog-content").ClassList.Should().NotContain("test-class");
+            comp.Find("div.mud-dialog-content").ClassList.Should().Contain("content-class");
+        }
+
+        /// <summary>
         /// Based on bug report by Porkopek:
         /// Updating values that are referenced in TitleContent render fragment won't result in an update of the dialog title
         /// when they change. This is solved by allowing the user to call ForceRender() on DialogInstance

src/MudBlazor.UnitTests.Viewer/TestComponents/Dialog/TestInlineDialog.razor
@@ -2,7 +2,7 @@
 
 <MudButton Variant="Variant.Filled" OnClick="()=>visible=true">Open</MudButton>
 
-<MudDialog @bind-IsVisible="visible" Options="inlineOptions">
+<MudDialog @bind-IsVisible="visible" Options="inlineOptions" Class="test-class" ClassContent="content-class" Style="color: red;" ContentStyle="color: blue;">
     <DialogContent>
         <MudText>Wabalabadubdub!</MudText>
     </DialogContent>

commit 4b6a12f1ba600b8e73ff9b92ce23183c23b557d0

Dialog: reverting regression by #3252. Issue #3701

src/MudBlazor/Components/Dialog/MudDialog.razor.cs
@@ -93,6 +93,13 @@ namespace MudBlazor
                 if (_isVisible == value)
                     return;
                 _isVisible = value;
+                if (IsInline)
+                {
+                    if (_isVisible)
+                        Show();
+                    else
+                        Close();
+                }
                 IsVisibleChanged.InvokeAsync(value);
             }
         }
@@ -144,13 +151,14 @@ namespace MudBlazor
         {
             if (IsInline && _reference != null)
                 (_reference.Dialog as MudDialog)?.ForceUpdate(); // forward render update to instance
-            if (IsInline)
-            {
-                if (_isVisible)
-                    Show();
-                else
-                    Close();
-            }
+            // Note by Henon: this caused bug #3701
+            // if (IsInline)
+            // {
+            //     if (_isVisible)
+            //         Show();
+            //     else
+            //         Close();
+            // }
             base.OnAfterRender(firstRender);
         }
 

src/MudBlazor.UnitTests/Components/DialogTests.cs
@@ -73,7 +73,9 @@ namespace MudBlazor.UnitTests.Components
             // open the dialog
             comp1.Find("button").Click();
             //Console.WriteLine("\nOpened dialog: " + comp.Markup);
-            comp.Find("div.mud-dialog-container").Should().NotBe(null);
+            comp1.WaitForAssertion(()=>
+                comp.Find("div.mud-dialog-container").Should().NotBe(null)
+            );
             comp.Find("p.mud-typography").TrimmedText().Should().Be("Wabalabadubdub!");
             comp.Find("div.mud-dialog").GetAttribute("class").Should().Contain("mud-dialog-width-full");
             // close by click on ok button
@@ -85,7 +87,7 @@ namespace MudBlazor.UnitTests.Components
         /// Click outside the dialog (or any other method) must update the IsVisible parameter two-way binding on close
         /// </summary>
         /// <returns></returns>
-        [Ignore("Sadly we can not get this test to work when it is run in bulk (local and CI). It passes when executed individually")]
+        //[Ignore("Sadly we can not get this test to work when it is run in bulk (local and CI). It passes when executed individually")]
         [Test]
         public async Task InlineDialog_Should_UpdateIsVisibleOnClose()
         {
@@ -98,7 +100,7 @@ namespace MudBlazor.UnitTests.Components
             // open the dialog
             comp1.Find("button").Click();
             //Console.WriteLine("\nOpened dialog: " + comp.Markup);
-            comp.Find("div.mud-dialog-container").Should().NotBe(null);
+            comp.WaitForAssertion(()=> comp.Find("div.mud-dialog-container").Should().NotBe(null));
             // close by click outside
             comp.Find("div.mud-overlay").Click();
             comp.WaitForAssertion(() => comp.Markup.Trim().Should().BeEmpty(), TimeSpan.FromSeconds(5));
@@ -115,7 +117,7 @@ namespace MudBlazor.UnitTests.Components
         /// Based on bug report #3128
         /// Dialog Class and Style parameters should be honored for inline dialog
         /// </summary>
-        [Ignore("Sadly we can not get this test to work when it is run in bulk (local and CI). It passes when executed individually")]
+        //[Ignore("Sadly we can not get this test to work when it is run in bulk (local and CI). It passes when executed individually")]
         [Test]
         public async Task InlineDialogShouldHonorClassAndStyle()
         {
@@ -126,9 +128,9 @@ namespace MudBlazor.UnitTests.Components
             IDialogReference dialogReference = null;
             // open simple test dialog
             await comp.InvokeAsync(() => dialogReference = service?.Show<TestInlineDialog>());
-            dialogReference.Should().NotBe(null);
+            comp.WaitForAssertion(()=> dialogReference.Should().NotBe(null));
             comp.Find("button").Click();
-            comp.Find("div.mud-dialog").ClassList.Should().Contain("test-class");
+            comp.WaitForAssertion(()=> comp.Find("div.mud-dialog").ClassList.Should().Contain("test-class"));
             comp.Find("div.mud-dialog").Attributes["style"].Value.Should().Be("color: red;");
             comp.Find("div.mud-dialog-content").Attributes["style"].Value.Should().Be("color: blue;");
             comp.Find("div.mud-dialog-content").ClassList.Should().NotContain("test-class");