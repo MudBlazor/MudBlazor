6b68f5b09eac88550735043a3ea8d63aee22b950
Benjamin Kappel <51370361+just-the-benno@users.noreply.github.com>
2022-01-27 12:04:06 -0600

    Docs: Add prerendering for search engines (#3812)

--- src/MudBlazor.Docs.Server/Program.cs
+++ src/MudBlazor.Docs.Server/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Net.Http;
+using Blazor.Analytics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MudBlazor.Docs.Extensions;
+using MudBlazor.Docs.Server.Prerender;
 using MudBlazor.Docs.Services;
 using MudBlazor.Docs.Services.Notifications;
 using MudBlazor.Examples.Data;
@@ -17,6 +19,25 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IPeriodicTableService, PeriodicTableService>();
 builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddHttpContextAccessor();
+
+
+//adding client specific service for prerendering. This service are not used by the WASM app, but for prerending it. Thefore they are different
+builder.Services.AddScoped(sp =>
+{
+    var context = sp.GetRequiredService<IHttpContextAccessor>().HttpContext;
+    var client = new HttpClient { BaseAddress = new Uri($"{context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}") };
+
+    return client;
+});
+
+builder.Services.AddScoped<GitHubApiClient>();
+builder.Services.TryAddDocsViewServices();
+builder.Services.AddGoogleAnalytics("G-PRYNCB61NV");
+//set the capacity max so that content is not queue. Again this is for prerending to serve the entire page back to crawler
+builder.Services.AddSingleton<IRenderQueueService>(new RenderQueueService { Capacity = int.MaxValue });
+
+builder.Services.AddSingleton<ICrawlerIdentifier>(new FileBasedCrawlerIdentifier("CrawlerInfo.json"));
 
 var app = builder.Build();
 
@@ -38,14 +59,23 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.UsePrerenderMiddleware();
+
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
 
-var notificationService = app.Services.GetService<INotificationService>();
-if (notificationService is InMemoryNotificationService inmemoryService)
+app.MapFallbackToPage("/_Host");
+
+using (var scope = app.Services.CreateScope())
 {
-    inmemoryService.Preload();
+    var notificationService = scope.ServiceProvider.GetService<INotificationService>();
+    if (notificationService is InMemoryNotificationService inmemoryService)
+    {
+        inmemoryService.Preload();
+    }
+
+    var crawlerIdentifier = scope.ServiceProvider.GetRequiredService<ICrawlerIdentifier>();
+    await crawlerIdentifier.Initilize();
 }
 
 app.Run();
