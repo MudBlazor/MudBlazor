eb8d10f93ae632d60020e369ac585c5c4f683ba4
Benjamin Kappel <b.kappel90@outlook.de>
2022-01-29 17:05:26 -0600

    item based drop zone first try

---
+++ src/MudBlazor/Components/DropZone/MudItemDropContainer.razor.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public class DragAndDropItemTransaction<T>
+    {
+        private Action _commitCallback;
+        private Action _cancelCallback;
+
+        public T Item { get; set; }
+        public string DropGroup { get; set; }
+
+        public DragAndDropItemTransaction(T item, string dropGroup, Action commitCallback, Action cancelCallback)
+        {
+            Item = item;
+            DropGroup = dropGroup;
+
+            _commitCallback = commitCallback;
+            _cancelCallback = cancelCallback;
+        }
+
+        public void Cancel() => _cancelCallback.Invoke();
+        public void Commit() => _commitCallback.Invoke();
+    }
+
+    public class MudItemDropInfo<T>
+    {
+        public T Item { get; set; }
+        public string DropzoneIdentifier { get; set; }
+    }
+
+    public partial class MudItemDropContainer<T> : MudComponentBase
+    {
+        protected string Classname =>
+        new CssBuilder("mud-drop-cotainer")
+            .AddClass(Class)
+            .Build();
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// The items that can be drag and dropped within the container
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public IEnumerable<T> Items { get; set; }
+
+        /// <summary>
+        /// The items that can be drag and dropped within the container
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public RenderFragment<T> ItemRenderer { get; set; }
+
+        /// <summary>
+        /// The method is used to determinate if an item can be dropped within a drop zone
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public Func<T, string, bool> CanDrop { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public EventCallback<MudItemDropInfo<T>> ItemDropped { get; set; }
+
+        /// <summary>
+        /// The method is used to determinate if an item can be dropped within a drop zone
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public Func<T, string, bool> ItemsSelector { get; set; }
+
+        private DragAndDropItemTransaction<T> _transaction;
+
+        public void StartTransaction(T item, string dropGroup, Action commitCallback, Action cancelCallback)
+        {
+            _transaction = new DragAndDropItemTransaction<T>(item, dropGroup, commitCallback, cancelCallback);
+        }
+
+        public DragAndDropItemTransaction<T> GetContext() => _transaction;
+
+        public bool TransactionInProgress() => _transaction != null;
+
+    }
+}
---
+++ src/MudBlazor/Components/DropZone/MudItemDropZone.razor.cs
@@ -0,0 +1,159 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudItemDropZone<T> : MudComponentBase
+    {
+        [CascadingParameter]
+        protected MudItemDropContainer<T> Container { get; set; }
+
+        [Parameter]
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// The items that can be drag and dropped within the container
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public RenderFragment<T> ItemRenderer { get; set; }
+
+        /// <summary>
+        /// The method is used to determinate if an item can be dropped within a drop zone
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.Button.Behavior)]
+        public Func<T, bool> ItemsSelector { get; set; }
+
+        private IEnumerable<T> GetItems()
+        {
+            Func<T, bool> predicate = null;
+            if (ItemsSelector != null)
+            {
+                predicate = ItemsSelector;
+            }
+
+            predicate = (item) => Container.ItemsSelector(item, Identifier);
+
+            return Container.Items.Where(predicate).ToArray();
+        }
+
+
+        private RenderFragment<T> GetItemTemplate() => ItemRenderer ?? Container?.ItemRenderer;
+
+
+        private bool _canDrop = false;
+        private bool _itemOnDropZone = false;
+
+        protected string Classname =>
+            new CssBuilder("mud-drop-zone")
+                .AddClass(CanDropClass, _canDrop == true && _itemOnDropZone == true)
+                .AddClass(NoDropClass, _canDrop == false && _itemOnDropZone == true)
+                .AddClass(Class)
+                .Build();
+
+        /// <summary>
+        /// The CSS class to use if valid drop.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.DropZone.Appearance)]
+        public string CanDropClass { get; set; }
+
+        /// <summary>
+        /// The CSS class to use if not valid drop.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.DropZone.Appearance)]
+        public string NoDropClass { get; set; }
+
+        /// <summary>
+        /// The CSS class to use if an item from this zone is being dragged.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.DropZone.Appearance)]
+        public string DraggingClass { get; set; }
+
+
+
+        private (DragAndDropItemTransaction<T>, bool) ItemCanBeDropped()
+        {
+            if (Container == null || Container.TransactionInProgress() == false)
+            {
+                return (null, false);
+            }
+
+            var context = Container.GetContext();
+            //if (string.IsNullOrEmpty(context.DropGroup) == false)
+            //{
+            //    var groups = CanDropGroups ?? Array.Empty<string>();
+            //    if (groups.Any() == true)
+            //    {
+            //        if (groups.Any(x => x == context.DropGroup) == false)
+            //        {
+            //            return (context, false);
+            //        }
+            //    }
+            //}
+
+            var result = true;
+            if (Container.CanDrop != null)
+            {
+                result = Container.CanDrop(context.Item, Identifier);
+            }
+
+            return (context, result);
+        }
+
+        private void HandleDragEnter()
+        {
+            var (context, isValidZone) = ItemCanBeDropped();
+            if (context == null)
+            {
+                return;
+            }
+
+            _itemOnDropZone = true;
+            _canDrop = isValidZone;
+        }
+
+        private void HandleDragLeave()
+        {
+            var (context, _) = ItemCanBeDropped();
+            if (context == null)
+            {
+                return;
+            }
+
+            _itemOnDropZone = false;
+        }
+
+        private void HandleDrop()
+        {
+            var (context, isValidZone) = ItemCanBeDropped();
+            if (context == null)
+            {
+                return;
+            }
+
+            _itemOnDropZone = false;
+
+            if (isValidZone == false)
+            {
+                context.Cancel();
+                return;
+            }
+
+             context.Commit();
+        }
+
+    }
+}
