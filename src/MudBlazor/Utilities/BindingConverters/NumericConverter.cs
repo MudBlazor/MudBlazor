0fc8b233c63c601ad56d3f151f9c9958313f9403
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2022-03-02 14:19:44 +0300

    NumericField: Fix Value Out Of Bounds Exception (#4079)

--- src/MudBlazor/Utilities/BindingConverters/NumericConverter.cs
+++ src/MudBlazor/Utilities/BindingConverters/NumericConverter.cs
@@ -187,49 +187,49 @@ namespace MudBlazor
     {
         public static T To<T>(double d)
         {
-            if (typeof(T) == typeof(sbyte))
+            if (typeof(T) == typeof(sbyte) && d >= sbyte.MinValue && sbyte.MaxValue >= d)
                 return (T)(object)Convert.ToSByte(d);
-            if (typeof(T) == typeof(byte))
+            if (typeof(T) == typeof(byte) && d >= byte.MinValue && byte.MaxValue >= d)
                 return (T)(object)Convert.ToByte(d);
-            if (typeof(T) == typeof(short))
+            if (typeof(T) == typeof(short) && d >= short.MinValue && short.MaxValue >= d)
                 return (T)(object)Convert.ToInt16(d);
-            if (typeof(T) == typeof(ushort))
+            if (typeof(T) == typeof(ushort) && d >= ushort.MinValue && ushort.MaxValue >= d)
                 return (T)(object)Convert.ToUInt16(d);
-            if (typeof(T) == typeof(int))
+            if (typeof(T) == typeof(int) && d >= int.MinValue && int.MaxValue >= d)
                 return (T)(object)Convert.ToInt32(d);
-            if (typeof(T) == typeof(uint))
+            if (typeof(T) == typeof(uint) && d >= uint.MinValue && uint.MaxValue >= d)
                 return (T)(object)Convert.ToUInt32(d);
-            if (typeof(T) == typeof(long))
+            if (typeof(T) == typeof(long) && d >= long.MinValue && long.MaxValue >= d)
                 return (T)(object)Convert.ToInt64(d);
-            if (typeof(T) == typeof(ulong))
+            if (typeof(T) == typeof(ulong) && d >= ulong.MinValue && ulong.MaxValue >= d)
                 return (T)(object)Convert.ToUInt64(d);
-            if (typeof(T) == typeof(float))
+            if (typeof(T) == typeof(float) && d >= float.MinValue && float.MaxValue >= d)
                 return (T)(object)Convert.ToSingle(d);
-            if (typeof(T) == typeof(double))
+            if (typeof(T) == typeof(double) && d >= double.MinValue && double.MaxValue >= d)
                 return (T)(object)Convert.ToDouble(d);
-            if (typeof(T) == typeof(decimal))
+            if (typeof(T) == typeof(decimal) && (decimal)d >= decimal.MinValue && decimal.MaxValue >= (decimal)d)
                 return (T)(object)Convert.ToDecimal(d);
-            if (typeof(T) == typeof(sbyte?))
+            if (typeof(T) == typeof(sbyte?) && d >= sbyte.MinValue && sbyte.MaxValue >= d)
                 return (T)(object)Convert.ToSByte(d);
-            if (typeof(T) == typeof(byte?))
+            if (typeof(T) == typeof(byte?) && d >= byte.MinValue && byte.MaxValue >= d)
                 return (T)(object)Convert.ToByte(d);
-            if (typeof(T) == typeof(short?))
+            if (typeof(T) == typeof(short?) && d >= short.MinValue && short.MaxValue >= d)
                 return (T)(object)Convert.ToInt16(d);
-            if (typeof(T) == typeof(ushort?))
+            if (typeof(T) == typeof(ushort?) && d >= ushort.MinValue && ushort.MaxValue >= d)
                 return (T)(object)Convert.ToUInt16(d);
-            if (typeof(T) == typeof(int?))
+            if (typeof(T) == typeof(int?) && d >= int.MinValue && int.MaxValue >= d)
                 return (T)(object)Convert.ToInt32(d);
-            if (typeof(T) == typeof(uint?))
+            if (typeof(T) == typeof(uint?) && d >= uint.MinValue && uint.MaxValue >= d)
                 return (T)(object)Convert.ToUInt32(d);
-            if (typeof(T) == typeof(long?))
+            if (typeof(T) == typeof(long?) && d >= long.MinValue && long.MaxValue >= d)
                 return (T)(object)Convert.ToInt64(d);
-            if (typeof(T) == typeof(ulong?))
+            if (typeof(T) == typeof(ulong?) && d >= ulong.MinValue && ulong.MaxValue >= d)
                 return (T)(object)Convert.ToUInt64(d);
-            if (typeof(T) == typeof(float?))
+            if (typeof(T) == typeof(float?) && d >= float.MinValue && float.MaxValue >= d)
                 return (T)(object)Convert.ToSingle(d);
-            if (typeof(T) == typeof(double?))
+            if (typeof(T) == typeof(double?) && d >= double.MinValue && double.MaxValue >= d)
                 return (T)(object)Convert.ToDouble(d);
-            if (typeof(T) == typeof(decimal?))
+            if (typeof(T) == typeof(decimal?) && (decimal)d >= decimal.MinValue && decimal.MaxValue >= (decimal)d)
                 return (T)(object)Convert.ToDecimal(d);
             return default;
         }
