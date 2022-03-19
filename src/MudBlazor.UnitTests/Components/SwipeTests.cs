e3879469f3a4dc7f9bada0190c631e1030dfa89b
mckaragoz <m.cankaragoz@outlook.com>
2022-03-20 02:05:31 +0300

    Swipe

--- src/MudBlazor.UnitTests/Components/SwipeTests.cs
+++ src/MudBlazor.UnitTests/Components/SwipeTests.cs
@@ -17,6 +17,16 @@ namespace MudBlazor.UnitTests.Components
         {
             var comp = Context.RenderComponent<SwipeAreaTest>();
             var swipe = comp.FindComponent<MudSwipeArea>();
+            TouchPoint[] _point1 = { new() { ClientY = 0, ClientX = 150 } };
+            TouchPoint[] _point1reverse = { new() { ClientY = 0, ClientX = -150 } };
+            TouchPoint[] _point2 = { new() { ClientY = 20, ClientX = 0 } };
+            TouchPoint[] _point3 = { new() { ClientY = 150, ClientX = 0 } };
+            TouchPoint[] _point3reverse = { new() { ClientY = -150, ClientX = 0 } };
+            TouchEventArgs tea = new TouchEventArgs() { ChangedTouches = _point1 };
+            TouchEventArgs tea2 = new TouchEventArgs() { ChangedTouches = _point2 };
+            TouchEventArgs tea3 = new TouchEventArgs() { ChangedTouches = _point3 };
+            TouchEventArgs teaReverse = new TouchEventArgs() { ChangedTouches = _point1reverse };
+            TouchEventArgs tea3Reverse = new TouchEventArgs() { ChangedTouches = _point3reverse };
 
             await comp.InvokeAsync(() => swipe.Instance._yDown = 50);
             await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(new TouchEventArgs()));
@@ -26,6 +36,24 @@ namespace MudBlazor.UnitTests.Components
 
             await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(new TouchEventArgs()));
             comp.WaitForAssertion(() => swipe.Instance._xDown.Should().Be(null));
+
+            await comp.InvokeAsync(() => swipe.Instance._xDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance._yDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(tea));
+            await comp.InvokeAsync(() => swipe.Instance._xDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance._yDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(teaReverse));
+
+            await comp.InvokeAsync(() => swipe.Instance._xDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance._yDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(tea2));
+
+            await comp.InvokeAsync(() => swipe.Instance._xDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance._yDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(tea3));
+            await comp.InvokeAsync(() => swipe.Instance._xDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance._yDown = 50);
+            await comp.InvokeAsync(() => swipe.Instance.OnTouchEnd(tea3Reverse));
 
         }
     }
