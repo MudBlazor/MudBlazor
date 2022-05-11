372684513aafb72addd15dc589e616a5d460664b
snakex64 <pascx64@gmail.com>
2022-05-11 18:29:24 -0400

    Bugfix autocomplete when changing adornment icon

--- src/MudBlazor/Components/Autocomplete/MudAutocomplete.razor.cs
+++ src/MudBlazor/Components/Autocomplete/MudAutocomplete.razor.cs
@@ -221,7 +221,7 @@ namespace MudBlazor
                 if (value == _isOpen)
                     return;
                 _isOpen = value;
-                UpdateIcon();
+                
                 IsOpenChanged.InvokeAsync(_isOpen).AndForget();
             }
         }
@@ -250,7 +250,7 @@ namespace MudBlazor
         /// </summary>
         [Parameter] public EventCallback<MouseEventArgs> OnClearButtonClick { get; set; }
 
-        private string _currentIcon;
+        private string CurrentIcon => !string.IsNullOrWhiteSpace(AdornmentIcon) ? AdornmentIcon : _isOpen ? CloseIcon : OpenIcon;
 
         /// <summary>
         /// This boolean will keep track if the clear function is called too keep the set text function to be called.
@@ -310,14 +310,9 @@ namespace MudBlazor
             }
         }
 
-        private void UpdateIcon()
-        {
-            _currentIcon = !string.IsNullOrWhiteSpace(AdornmentIcon) ? AdornmentIcon : _isOpen ? CloseIcon : OpenIcon;
-        }
 
         protected override void OnInitialized()
         {
-            UpdateIcon();
             var text = GetItemString(Value);
             if (!string.IsNullOrWhiteSpace(text))
                 Text = text;
