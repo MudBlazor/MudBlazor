4f3c5abe8a664979c470fc16de348d2e38f96c7e
Enderlook <enderlook@gmail.com>
2022-05-16 20:19:05 -0300

    MudHighlighter: Add support for multiple highlighted texts (#4621)
    Add property HighlightedTexts to MudHighlighter component which accepts an enumerable of texts to highlight.
    Fix typo in documentation.

--- src/MudBlazor/Components/Highlighter/Splitter.cs
+++ src/MudBlazor/Components/Highlighter/Splitter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MudBlazor.Components.Highlighter
@@ -14,40 +15,84 @@ namespace MudBlazor.Components.Highlighter
         /// </summary>
         /// <param name="text">The whole text</param>
         /// <param name="highlightedText">The text to be highlighted</param>
+        /// <param name="highlightedTexts">The texts to be highlighted</param>
+        /// <param name="regex">Regex expression that was used to split fragments.</param>
         /// <param name="caseSensitive">Whether it's case sensitive or not</param>
         /// <param name="untilNextBoundary">If true, splits until the next regex boundary</param>
         /// <returns></returns>
         public static IEnumerable<string> GetFragments(string text,
                                                        string highlightedText,
+                                                       IEnumerable<string> highlightedTexts,
+                                                       out string regex,
                                                        bool caseSensitive = false,
                                                        bool untilNextBoundary = false)
         {
-            if (string.IsNullOrWhiteSpace(text))
+            if (string.IsNullOrEmpty(text))
             {
+                regex = "";
                 return new List<string>();
             }
 
-            if (string.IsNullOrWhiteSpace(highlightedText))
+            StringBuilder builder = new();
+            //the first brace in the pattern is to keep the patten when splitting,
+            //the `(?:` in the pattern is to accept multiple highlightedTexts but not capture them.
+            builder.Append("((?:");
+
+            //this becomes true if `AppendPattern` was called at least once.
+            bool hasAtLeastOnePattern = false;
+            if (!string.IsNullOrEmpty(highlightedText))
             {
-                return new List<string> { text };
+                AppendPattern(highlightedText);
+            }
+
+            if (highlightedTexts is not null)
+            {
+                foreach (var substring in highlightedTexts)
+                {
+                    if (string.IsNullOrEmpty(substring))
+                        continue;
+
+                    //split pattern if we already added an string to search.
+                    if (hasAtLeastOnePattern)
+                    {
+                        builder.Append(")|(?:");
+                    }
+
+                    AppendPattern(substring);
+                }
+            }
+
+            if (hasAtLeastOnePattern)
+            {
+                //close the last pattern group and the capture group.
+                builder.Append("))");
             }
             else
             {
-                //escapes the text for regex             
-                highlightedText = Regex.Escape(highlightedText);
+                //all patterns were empty or null.
+                regex = "";
+                return new List<string>() { text };
+            }
+
+            regex = builder.ToString();
+            return Regex
+                    .Split(text,
+                           regex,
+                           caseSensitive
+                             ? RegexOptions.None
+                             : RegexOptions.IgnoreCase)
+                    .Where(s => !string.IsNullOrEmpty(s));
+
+            void AppendPattern(string value)
+            {
+                hasAtLeastOnePattern = true;
+                //escapes the text for regex
+                value = Regex.Escape(value);
+                builder.Append(value);
                 if (untilNextBoundary)
                 {
-                    highlightedText += NextBoundary;
+                    builder.Append(NextBoundary);
                 }
-
-                //using braces in the pattern keeps the pattern when splitting
-                return Regex
-                     .Split(text,
-                            $"({highlightedText})",
-                            caseSensitive
-                              ? RegexOptions.None
-                              : RegexOptions.IgnoreCase)
-                     .Where(s => !string.IsNullOrEmpty(s));
             }
         }
     }
