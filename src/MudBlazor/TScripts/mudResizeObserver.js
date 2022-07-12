55db7f46e674a995b2efbaf180a18de4dbcfbe63
Bevan Weiss <bevanweiss@users.noreply.github.com>
2022-07-12 17:11:14 +1000

    Refactor: Fix typos of initialized (#4855, #4857)

--- src/MudBlazor/TScripts/mudResizeObserver.js
+++ src/MudBlazor/TScripts/mudResizeObserver.js
@@ -53,7 +53,7 @@ class MudResizeObserver {
         var observervedElements = [];
         this._observervedElements = observervedElements;
 
-        this.logger('[MudBlazor | ResizeObserver] Observer initilized');
+        this.logger('[MudBlazor | ResizeObserver] Observer initialized');
 
         this._resizeObserver = new ResizeObserver(entries => {
             var changes = [];
@@ -64,12 +64,12 @@ class MudResizeObserver {
                 if (affectedObservedElement) {
 
                     var size = entry.target.getBoundingClientRect();
-                    if (affectedObservedElement.isInitilized == true) {
+                    if (affectedObservedElement.isInitialized == true) {
 
                         changes.push({ id: affectedObservedElement.id, size: size });
                     }
                     else {
-                        affectedObservedElement.isInitilized = true;
+                        affectedObservedElement.isInitialized = true;
                     }
                 }
             }
@@ -102,7 +102,7 @@ class MudResizeObserver {
             var newEntry = {
                 element: elements[i],
                 id: ids[i],
-                isInitilized: false,
+                isInitialized: false,
             };
 
             this.logger("[MudBlazor | ResizeObserver] Start observing element:", { newEntry });
