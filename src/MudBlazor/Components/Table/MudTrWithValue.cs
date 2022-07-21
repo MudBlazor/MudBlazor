7b7448554398bb0aa10fed216fe4719c5ba058ea
Riley Nielsen <riley@nielsen.tech>
2022-07-21 09:27:23 -0500

    TableContext: Use HashSet instead of Dictionary to keep track of rows.

---
+++ src/MudBlazor/Components/Table/MudTrWithValue.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace MudBlazor
+{
+    public record MudTrWithValue<T>(T Value, MudTr Row);
+    //{
+    //    public MudTrWithValue(T value, MudTr row)
+    //    {
+    //        Value = value;
+    //        Row = row;
+    //    }
+
+    //    public T Value { get; set; }
+    //    public MudTr Row { get; set; }
+    //}
+}
