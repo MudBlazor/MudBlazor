2004d3b8182c54f731a4ecb902889ed69353ffd9
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2022-07-28 11:01:18 +0300

    Generic List Sub Header and fixed sticky header with disable padding

--- src/MudBlazor/Components/List/MudListSubheader.razor.cs
+++ src/MudBlazor/Components/List/MudListSubheader.razor.cs
@@ -4,15 +4,18 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public partial class MudListSubheader : MudComponentBase
+    public partial class MudListSubheader<T> : MudComponentBase
     {
         protected string Classname =>
         new CssBuilder("mud-list-subheader")
             .AddClass("mud-list-subheader-gutters", !DisableGutters)
             .AddClass("mud-list-subheader-inset", Inset)
             .AddClass("mud-list-subheader-sticky", Sticky)
+            .AddClass("mud-list-subheader-sticky-dense", Sticky && (MudList != null && MudList.DisablePadding))
             .AddClass(Class)
         .Build();
+
+        [CascadingParameter] protected MudList<T> MudList { get; set; }
 
         [Parameter]
         [Category(CategoryTypes.List.Behavior)]
