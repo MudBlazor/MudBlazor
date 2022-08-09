9d5989ab216caa94b3a1774288391e4fee426c89
WangYongjin <yongjin0125@live.hk>
2022-08-09 13:25:50 +0800

    Add a converter of type EnumConverter<T> to get the conversion from the enum's DescriptionAttribute.

---
+++ src/MudBlazor/Utilities/BindingConverters/EnumConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MudBlazor.Utilities.BindingConverters
+{
+    [ExcludeFromCodeCoverage]
+    public class EnumConverter<T> : Converter<T, string> where T : Enum
+    {
+        public EnumConverter()
+        {
+            SetFunc = OnSet;
+            GetFunc = OnGet;
+        }
+
+        private T OnGet(string value)
+        {
+            try
+            {
+                foreach (var fieldInfo in typeof(T).GetFields())
+                {
+                    if (Attribute.GetCustomAttribute(fieldInfo, typeof(DescriptionAttribute)) is DescriptionAttribute descAttr)
+                    {
+                        if (descAttr.Description == value)
+                        {
+                            return (T)fieldInfo.GetValue(null);
+                        }
+                    }
+                    else
+                    {
+                        if (fieldInfo.Name == value)
+                        {
+                            return (T)fieldInfo.GetValue(null);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                UpdateGetError("Conversion error: " + e.Message);
+                return default;
+            }
+
+            return default;
+        }
+
+        private string OnSet(T value)
+        {
+            try
+            {
+                var fi = value.GetType().GetField(value.ToString());
+                if (Attribute.GetCustomAttribute(fi, typeof(DescriptionAttribute)) is DescriptionAttribute descAttr && !string.IsNullOrEmpty(descAttr.Description))
+                {
+                    return descAttr.Description;
+                }
+                else
+                {
+                    return value.ToString();
+                }
+            }
+            catch (FormatException ex)
+            {
+                UpdateSetError("Conversion error: " + ex.Message);
+                return default;
+            }
+        }
+    }
+}
