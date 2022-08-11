37e927115c21c734926bf6a430e50ad640d26fd2
Johnny Everman <j.everman@scrubdaddy.com>
2022-08-11 11:22:00 -0400

    Add ability to disable keyboard input in MudCheckBox

--- src/MudBlazor/Components/CheckBox/MudCheckBox.razor.cs
+++ src/MudBlazor/Components/CheckBox/MudCheckBox.razor.cs
@@ -53,6 +53,13 @@ namespace MudBlazor
         public string Label { get; set; }
 
         /// <summary>
+        /// If true the keyboard controls will be disabled.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.FormComponent.Behavior)]
+        public bool DisableKeyboardControls { get; set; } = false;
+
+        /// <summary>
         /// If true, disables ripple effect.
         /// </summary>
         [Parameter]
@@ -148,7 +155,7 @@ namespace MudBlazor
 
         protected void HandleKeyDown(KeyboardEventArgs obj)
         {
-            if (Disabled || ReadOnly)
+            if (Disabled || ReadOnly || DisableKeyboardControls)
                 return;
             switch (obj.Key)
             {
