MudBlazor/MudBlazor

commit 9e837492eee366c4a5cde91f52a9b36b0f6da7be

NumericField: Fix decimal precision and overflows (#4971)

src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
@@ -177,16 +177,25 @@ namespace MudBlazor
         /// <param name="factor">Multiplication factor (1 or -1) will be applied to the step</param>
         private async Task Change(double factor = 1)
         {
-            var value = Num.To<T>(Num.From(Value) + Num.From(Step) * factor);
-            await SetValueAsync(ConstrainBoundaries(value).value);
-            _elementReference.SetText(Text).AndForget();
+            try
+            {
+                var nextValue = typeof(T) == typeof(decimal) || typeof(T) == typeof(decimal?)
+                    ? (T)(object)Convert.ToDecimal(FromDecimal(Value) + FromDecimal(Step) * (decimal)factor)
+                    : Num.To<T>(Num.From(Value) + Num.From(Step) * factor);
+                await SetValueAsync(ConstrainBoundaries(nextValue).value);
+                _elementReference.SetText(Text).AndForget();
+            }
+            catch (OverflowException)
+            {
+                // if next value overflows the primitive type, lets set it to Min or Max depending if factor is positive or negative
+                await SetValueAsync(factor > 0 ? Max : Min, true);
+            }
         }
 
         /// <summary>
         /// Adds a Step to the Value
         /// </summary>
         public Task Increment() => Change(factor: 1);
-
 
         /// <summary>
         /// Substracts a Step from the Value
@@ -200,21 +209,54 @@ namespace MudBlazor
         /// <returns>Returns a valid value and if it has been changed.</returns>
         protected (T value, bool changed) ConstrainBoundaries(T v)
         {
+            if (typeof(T) == typeof(decimal) || typeof(T) == typeof(decimal?))
+                return ConstrainBoundariesDecimal(v);
+
+            return ConstrainBoundariesDouble(v);
+        }
+
+        /// <summary>
+        /// Check boundaries for this instance with double primitive data type
+        /// </summary>
+        /// <param name="v">see <see cref="ConstrainBoundaries"/></param>
+        /// <returns>see <see cref="ConstrainBoundaries"/></returns>
+        private (T value, bool changed) ConstrainBoundariesDouble(T v)
+        {
             var value = Num.From(v);
             var max = Num.From(Max);
             var min = Num.From(Min);
+        
             //check if Max/Min has value, if not use MaxValue/MinValue for that data type
             if (value > max)
                 return (Max, true);
             else if (value < min)
                 return (Min, true);
-            //return T default (null) when there is no value
             else if (v == null)
-            {
                 return (default(T), true);
-            }
 
             return (Num.To<T>(value), false);
+        }
+
+        /// <summary>
+        /// Check boundaries for this instance with decimal primitive data type
+        /// </summary>
+        /// <param name="v">see <see cref="ConstrainBoundaries"/></param>
+        /// <returns>see <see cref="ConstrainBoundaries"/></returns>
+        private (T value, bool changed) ConstrainBoundariesDecimal(T v)
+        {
+            var value = FromDecimal(v);
+            var max = Convert.ToDecimal(Max);
+            var min = Convert.ToDecimal(Min);
+
+            //check if Max/Min has value, if not use MaxValue/MinValue for that data type
+            if (value > max)
+                return (Max, true);
+            else if (value < min)
+                return (Min, true);
+            else if (v == null)
+                return (default(T), true);
+
+            return ((T)(object)Convert.ToDecimal(value), false);
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -388,6 +430,15 @@ namespace MudBlazor
                 return f.ToString(TagFormat, CultureInfo.InvariantCulture.NumberFormat);
             else
                 return null;
+        }
+
+        private decimal FromDecimal(T v)
+        {
+            if (typeof(T) == typeof(decimal))
+                return Convert.ToDecimal((decimal)(object)v);
+            if (typeof(T) == typeof(decimal?))
+                return Convert.ToDecimal((decimal?)(object)v);
+            return default;
         }
 
         protected override void Dispose(bool disposing)

src/MudBlazor.UnitTests/Components/NumericFieldTests.cs
@@ -236,6 +236,26 @@ namespace MudBlazor.UnitTests.Components
             //Console.WriteLine("Error message: " + numericField.ErrorText);
             numericField.ErrorText.Should().BeNullOrEmpty();
         }
+        
+        /// <summary>
+        /// Validate handling of decimal support & precision kept
+        /// </summary>
+        [Test]
+        public async Task NumericField_HandleDecimalPrecisionAndValues()
+        {
+            var comp = Context.RenderComponent<MudNumericField<decimal>>();
+            var numericField = comp.Instance;
+
+            // first try set max decimal value
+            comp.Find("input").Change(decimal.MaxValue);
+            numericField.Value.Should().Be(decimal.MaxValue);
+            numericField.ErrorText.Should().BeNullOrEmpty();
+
+            // next try set minimum decimal value
+            comp.Find("input").Change(decimal.MinValue);
+            numericField.Value.Should().Be(decimal.MinValue);
+            numericField.ErrorText.Should().BeNullOrEmpty();
+        }
 
         /// <summary>
         /// An unstable converter should not cause an infinite update loop. This test must complete in under 1 sec!
@@ -594,6 +614,27 @@ namespace MudBlazor.UnitTests.Components
             await comp.InvokeAsync(() => comp.Instance.Increment().Wait());
             await comp.InvokeAsync(() => comp.Instance.Decrement().Wait());
             comp.Instance.Value.Should().Be(value);
+        }
+
+        [TestCaseSource(nameof(TypeCases))]
+        public async Task NumericField_Increment_Decrement_OverflowHandled<T>(T value)
+        {
+            var comp = Context.RenderComponent<MudNumericField<T>>();
+            var max = Convert.ChangeType(10, typeof(T));
+            var min = Convert.ChangeType(0, typeof(T));
+            comp.SetParam(x => x.Max, max);
+            comp.SetParam(x => x.Min, min);
+            comp.SetParam(x => x.Step, value);
+            
+            // test max overflow
+            comp.SetParam(x => x.Value, max);
+            await comp.InvokeAsync(() => comp.Instance.Increment().Wait());
+            comp.Instance.Value.Should().Be(max);
+
+            // test min overflow
+            comp.SetParam(x => x.Value, min);
+            await comp.InvokeAsync(() => comp.Instance.Decrement().Wait());
+            comp.Instance.Value.Should().Be(min);
         }
 
         /// <summary>

commit 33229b5c1b8e26c7bf577a05f707df9b214c7464

NumericField: Resolve nullable issue introduced by #4971 (#5077)

src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Text.RegularExpressions;
@@ -17,6 +18,7 @@ namespace MudBlazor
     public partial class MudNumericField<T> : MudDebouncedInput<T>
     {
         private IKeyInterceptor _keyInterceptor;
+        private Comparer _comparer = new(CultureInfo.InvariantCulture);
 
         public MudNumericField() : base()
         {
@@ -180,14 +182,16 @@ namespace MudBlazor
             try
             {
                 var nextValue = GetNextValue(factor);
-                if (nextValue is IComparable<T> comparable)
+
+                // validate that the data type is a value type before we compare them
+                if (typeof(T).IsValueType)
                 {
-                    if (factor > 0 && comparable.CompareTo(Value) < 0)
+                    if (factor > 0 && _comparer.Compare(nextValue, Value) < 0)
                         nextValue = Max;
-                    else if (factor < 0 && comparable.CompareTo(Value) > 0)
+                    else if (factor < 0 && (_comparer.Compare(nextValue, Value) > 0 || nextValue is null))
                         nextValue = Min;
                 }
-                
+
                 await SetValueAsync(ConstrainBoundaries(nextValue).value);
                 _elementReference.SetText(Text).AndForget();
             }
@@ -226,16 +230,20 @@ namespace MudBlazor
         /// <returns>Returns a valid value and if it has been changed.</returns>
         protected (T value, bool changed) ConstrainBoundaries(T value)
         {
-            // check if Max/Min has value, if not use MaxValue/MinValue for that data type
-            if (value is IComparable<T> comparable)
+            if (value == null)
+                return (default(T), false);
+
+            // validate that the data type is a value type before we compare them
+            if (typeof(T).IsValueType)
             {
-                if (comparable.CompareTo(Max) > 0)
+                // check if value is bigger than defined MAX, if so take the defined MAX value instead
+                if (_comparer.Compare(value, Max) > 0)
                     return (Max, true);
-                else if (comparable.CompareTo(Min) < 0)
+
+                // check if value is lower than defined MIN, if so take the defined MIN value instead
+                if (_comparer.Compare(value, Min) < 0)
                     return (Min, true);
-            }
-            else if (value == null)
-                return (default(T), true);
+            };
 
             return (value, false);
         }
@@ -419,7 +427,7 @@ namespace MudBlazor
             => Convert.ToInt64((long?)(object)v);
         private ulong FromUInt64(T v)
             => Convert.ToUInt64((ulong?)(object)v);
-    
+
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);

src/MudBlazor.UnitTests.Viewer/TestComponents/NumericField/NumericFieldTest.razor
@@ -39,7 +39,7 @@
 				 Variant="Variant.Filled"
 				 Margin="Margin.Dense"
 				 Max="9.9"
-				 Min="-5.0" 
+				 Min="-5.0"
 				 Immediate="true"/>
 <br/><br/>
 <MudNumericField @bind-Value="_nullable"
@@ -47,7 +47,7 @@
 				 Variant="Variant.Outlined"
 				 Margin="Margin.Dense"
 				 Max="10" />
-				 
+
 <br/>
 <MudNumericField T="double?"
 				 Label="double?"
@@ -55,9 +55,21 @@
 				 Margin="Margin.Dense" />
 <br/>
 <MudNumericField T="decimal?"
-				 Label="decimal?"
-				 Variant="Variant.Outlined"
-				 Margin="Margin.Dense" />
+                 Label="decimal?"
+                 Variant="Variant.Outlined"
+                 Margin="Margin.Dense" />
+<br/>
+<MudNumericField T="byte?"
+                 Label="byte?"
+                 Variant="Variant.Outlined"
+                 Margin="Margin.Dense" />
+ <br/>
+ <MudNumericField T="int?"
+                  Label="int? clearable with min/max"
+                  Variant="Variant.Outlined"
+                  Margin="Margin.Dense"
+                  Clearable="true"
+                  Min="1" Max="10" />
 
 @code {
 	public static string __description__ = "The textfield should be changed by up/down arrows";

src/MudBlazor.UnitTests/Components/NumericFieldTests.cs
@@ -25,7 +25,7 @@ namespace MudBlazor.UnitTests.Components
     public class NumericFieldTests : BunitTest
     {
         // TestCaseSource does not know about "Nullable<T>" so having values as Nullable<T> does not make sense here
-        static object[] TypeCases = 
+        static object[] TypeCases =
         {
             new object[] { (byte)5 },
             new object[] { (sbyte)5 },
@@ -50,7 +50,7 @@ namespace MudBlazor.UnitTests.Components
             var label = comp.FindAll(".mud-input-label");
             label[0].Attributes.GetNamedItem("for")?.Value.Should().Be("numericFieldLabelTest");
         }
-        
+
         /// <summary>
         /// Initial Text for double should be 0, with F1 format it should be 0.0
         /// </summary>
@@ -93,57 +93,12 @@ namespace MudBlazor.UnitTests.Components
         /// <summary>
         /// Setting the value to null should not cause a validation error
         /// </summary>
-        [Test]
-        public async Task IntNumericFieldWithNullableTypes()
+        [TestCaseSource(nameof(TypeCases))]
+        public async Task NumericField_WithNullableTypes_ShouldAllowNulls<T>(T value) where T : struct
         {
-            var comp = Context.RenderComponent<MudNumericField<int?>>(ComponentParameter.CreateParameter("Value", 17));
+            var comp = Context.RenderComponent<MudNumericField<T?>>(ComponentParameter.CreateParameter("Value", value));
             // print the generated html
             //Console.WriteLine(comp.Markup);
-            comp.SetParametersAndRender(ComponentParameter.CreateParameter("Value", null));
-            comp.Find("input").Blur();
-            comp.FindAll("div.mud-input-error").Count.Should().Be(0);
-            comp.Find("input").Change("");
-            comp.Find("input").Blur();
-            comp.FindAll("div.mud-input-error").Count.Should().Be(0);
-        }
-        
-        /// <summary>
-        /// Setting the value to null should not cause a validation error
-        /// </summary>
-        [Test]
-        public async Task DecimalNumericFieldWithNullableTypes()
-        {
-            var comp = Context.RenderComponent<MudNumericField<decimal?>>(ComponentParameter.CreateParameter("Value", 17M));
-            comp.SetParametersAndRender(ComponentParameter.CreateParameter("Value", null));
-            comp.Find("input").Blur();
-            comp.FindAll("div.mud-input-error").Count.Should().Be(0);
-            comp.Find("input").Change("");
-            comp.Find("input").Blur();
-            comp.FindAll("div.mud-input-error").Count.Should().Be(0);
-        }
-
-        /// <summary>
-        /// Setting the value to null should not cause a validation error
-        /// </summary>
-        [Test]
-        public async Task Int64NumericFieldWithNullableTypes()
-        {
-            var comp = Context.RenderComponent<MudNumericField<long?>>(ComponentParameter.CreateParameter("Value", 17L));
-            comp.SetParametersAndRender(ComponentParameter.CreateParameter("Value", null));
-            comp.Find("input").Blur();
-            comp.FindAll("div.mud-input-error").Count.Should().Be(0);
-            comp.Find("input").Change("");
-            comp.Find("input").Blur();
-            comp.FindAll("div.mud-input-error").Count.Should().Be(0);
-        }
-
-        /// <summary>
-        /// Setting the value to null should not cause a validation error
-        /// </summary>
-        [Test]
-        public async Task UInt64NumericFieldWithNullableTypes()
-        {
-            var comp = Context.RenderComponent<MudNumericField<ulong?>>(ComponentParameter.CreateParameter("Value", 17UL));
             comp.SetParametersAndRender(ComponentParameter.CreateParameter("Value", null));
             comp.Find("input").Blur();
             comp.FindAll("div.mud-input-error").Count.Should().Be(0);
@@ -280,7 +235,7 @@ namespace MudBlazor.UnitTests.Components
             //Console.WriteLine("Error message: " + numericField.ErrorText);
             numericField.ErrorText.Should().BeNullOrEmpty();
         }
-        
+
         /// <summary>
         /// Validate handling of decimal support & precision kept
         /// </summary>
@@ -431,7 +386,7 @@ namespace MudBlazor.UnitTests.Components
             comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "9", Type = "keyup", });
             comp.WaitForAssertion(() => numericField.Value.Should().Be(1234.56));
         }
-        
+
         /// <summary>
         /// Keydown disabled, should not do anything
         /// </summary>
@@ -450,7 +405,7 @@ namespace MudBlazor.UnitTests.Components
             comp.Find("input").KeyUp(new KeyboardEventArgs() { Key = "9", Type = "keyup", });
             comp.WaitForAssertion(() => comp.Instance.Value.Should().Be(1234.56));
         }
-        
+
         /// <summary>
         /// Keydown readonly, should not do anything
         /// </summary>
@@ -677,9 +632,70 @@ namespace MudBlazor.UnitTests.Components
         }
 
         [TestCaseSource(nameof(TypeCases))]
+        public async Task NumericFieldMinMax<T>(T value)
+        {
+            var min = (T)Convert.ChangeType(1, typeof(T));
+            var max = (T)Convert.ChangeType(10, typeof(T));
+            var comp = Context.RenderComponent<MudNumericField<T>>();
+            comp.SetParam(x => x.Min, min);
+            comp.SetParam(x => x.Max, max);
+
+            comp.Find("input").Change("15");
+            comp.Find("input").Blur();
+
+            comp.WaitForAssertion(() => comp.Instance.Value.Should().Be(max));
+
+            comp.Find("input").Change("0");
+            comp.Find("input").Blur();
+
+            comp.WaitForAssertion(() => comp.Instance.Value.Should().Be(min));
+        }
+
+        [TestCaseSource(nameof(TypeCases))]
+        public async Task NumericFieldMinMaxNullable<T>(T value) where T : struct
+        {
+            var min = (T)Convert.ChangeType(1, typeof(T));
+            var max = (T)Convert.ChangeType(10, typeof(T));
+            var comp = Context.RenderComponent<MudNumericField<T?>>();
+            comp.SetParam(x => x.Min, min);
+            comp.SetParam(x => x.Max, max);
+
+            comp.Find("input").Change("15");
+            comp.Find("input").Blur();
+
+            comp.WaitForAssertion(() => comp.Instance.Value.Should().Be(max));
+
+            comp.Find("input").Change("0");
+            comp.Find("input").Blur();
+
+            comp.WaitForAssertion(() => comp.Instance.Value.Should().Be(min));
+        }
+
+        [TestCaseSource(nameof(TypeCases))]
         public async Task NumericField_Increment_Decrement<T>(T value)
         {
             var comp = Context.RenderComponent<MudNumericField<T>>();
+            var max = Convert.ChangeType(10, typeof(T));
+            var min = Convert.ChangeType(0, typeof(T));
+            comp.SetParam(x => x.Max, max);
+            comp.SetParam(x => x.Min, min);
+            comp.SetParam(x => x.Step, value);
+            comp.SetParam(x => x.Value, value);
+            await comp.InvokeAsync(() => comp.Instance.Increment().Wait());
+            await comp.InvokeAsync(() => comp.Instance.Decrement().Wait());
+            comp.Instance.Value.Should().Be(value);
+            // setting min and max to value will cover the boundary checking code
+            comp.SetParam(x => x.Max, value);
+            comp.SetParam(x => x.Min, value);
+            await comp.InvokeAsync(() => comp.Instance.Increment().Wait());
+            await comp.InvokeAsync(() => comp.Instance.Decrement().Wait());
+            comp.Instance.Value.Should().Be(value);
+        }
+
+        [TestCaseSource(nameof(TypeCases))]
+        public async Task NumericFieldNullable_Increment_Decrement<T>(T value) where T : struct
+        {
+            var comp = Context.RenderComponent<MudNumericField<T?>>();
             var max = Convert.ChangeType(10, typeof(T));
             var min = Convert.ChangeType(0, typeof(T));
             comp.SetParam(x => x.Max, max);
@@ -712,6 +728,41 @@ namespace MudBlazor.UnitTests.Components
             comp.SetParam(x => x.Value, comp.Instance.Min);
             await comp.InvokeAsync(() => comp.Instance.Decrement().Wait());
             comp.Instance.Value.Should().Be(comp.Instance.Min);
+        }
+
+        [TestCaseSource(nameof(TypeCases))]
+        public async Task NumericFieldNullable_Increment_Decrement_OverflowHandled<T>(T value) where T : struct
+        {
+            var comp = Context.RenderComponent<MudNumericField<T?>>();
+            comp.SetParam(x => x.Step, value);
+
+            // test max overflow
+            comp.SetParam(x => x.Value, comp.Instance.Max);
+            await comp.InvokeAsync(() => comp.Instance.Increment().Wait());
+            comp.Instance.Value.Should().Be(comp.Instance.Max);
+
+            // test min overflow
+            comp.SetParam(x => x.Value, comp.Instance.Min);
+            await comp.InvokeAsync(() => comp.Instance.Decrement().Wait());
+            comp.Instance.Value.Should().Be(comp.Instance.Min);
+        }
+
+        /// <summary>
+        /// NumericField with min/max set and nullable int can be cleared
+        /// </summary>
+        [TestCase(10, 20, 15)]
+        [TestCase(-20, -10, -15)]
+        public async Task NumericFieldCanBeCleared(int min, int max, int value)
+        {
+            var comp = Context.RenderComponent<MudNumericField<int?>>();
+            comp.SetParam(x => x.Min, min);
+            comp.SetParam(x => x.Max, max);
+            comp.SetParam(x => x.Value, value);
+
+            comp.Find("input").Change("");
+            comp.Find("input").Blur();
+
+            comp.WaitForAssertion(() => comp.Instance.Value.Should().BeNull());
         }
 
         /// <summary>