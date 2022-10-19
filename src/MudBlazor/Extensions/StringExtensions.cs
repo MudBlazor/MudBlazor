2a701b15df7d705c550fd08bc7e445e6b3e044f2
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2022-10-20 09:52:14 +1000

    🔐 Fix XSS by escaping string input

--- src/MudBlazor/Extensions/StringExtensions.cs
+++ src/MudBlazor/Extensions/StringExtensions.cs
@@ -12,4 +12,5 @@ internal static class StringExtensions
     public static string ToPercentage(this decimal value) => value.ToString("0.##", CultureInfo.InvariantCulture);
     public static string ToJsString(this string value) => char.ToLower(value[0]) + value.Substring(1);
     public static byte GetByteValue(this string value, int index) => byte.Parse(new string(new char[] { value[index], value[index + 1] }), NumberStyles.HexNumber);
+    public static string EscapeHtml(this string value) => value.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;").Replace("'", "&#39;");
 }
