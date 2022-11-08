4acb6f8264dc7f613585fb71fee68b2a4ab758ec
TDroogers <34547552+TDroogers@users.noreply.github.com>
2022-11-08 17:50:08 +0100

    Theming: Easier customization of dark mode (#5676)

---
+++ src/MudBlazor/Themes/Models/PaletteDark.cs
@@ -0,0 +1,35 @@
+﻿using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public class PaletteDark : Palette
+    {
+        public override MudColor Black { get; set; } = "#27272f";
+        public override MudColor Primary { get; set; } = "#776be7";
+        public override MudColor Info { get; set; } = "#3299ff";
+        public override MudColor Success { get; set; } = "#0bba83";
+        public override MudColor Warning { get; set; } = "#ffa800";
+        public override MudColor Error { get; set; } = "#f64e62";
+        public override MudColor Dark { get; set; } = "#27272f";
+        public override MudColor TextPrimary { get; set; } = "rgba(255,255,255, 0.70)";
+        public override MudColor TextSecondary { get; set; } = "rgba(255,255,255, 0.50)";
+        public override MudColor TextDisabled { get; set; } = "rgba(255,255,255, 0.2)";
+        public override MudColor ActionDefault { get; set; } = "#adadb1";
+        public override MudColor ActionDisabled { get; set; } = "rgba(255,255,255, 0.26)";
+        public override MudColor ActionDisabledBackground { get; set; } = "rgba(255,255,255, 0.12)";
+        public override MudColor Background { get; set; } = "#32333d";
+        public override MudColor BackgroundGrey { get; set; } = "#27272f";
+        public override MudColor Surface { get; set; } = "#373740";
+        public override MudColor DrawerBackground { get; set; } = "#27272f";
+        public override MudColor DrawerText { get; set; } = "rgba(255,255,255, 0.50)";
+        public override MudColor DrawerIcon { get; set; } = "rgba(255,255,255, 0.50)";
+        public override MudColor AppbarBackground { get; set; } = "#27272f";
+        public override MudColor AppbarText { get; set; } = "rgba(255,255,255, 0.70)";
+        public override MudColor LinesDefault { get; set; } = "rgba(255,255,255, 0.12)";
+        public override MudColor LinesInputs { get; set; } = "rgba(255,255,255, 0.3)";
+        public override MudColor TableLines { get; set; } = "rgba(255,255,255, 0.12)";
+        public override MudColor TableStriped { get; set; } = "rgba(255,255,255, 0.2)";
+        public override MudColor Divider { get; set; } = "rgba(255,255,255, 0.12)";
+        public override MudColor DividerLight { get; set; } = "rgba(255,255,255, 0.06)";
+    }
+}
