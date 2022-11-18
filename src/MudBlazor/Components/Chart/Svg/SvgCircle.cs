df6e280fe9278cd768232be1080c29588d63cf3d
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2022-11-18 20:49:59 +0100

    Fix SvgCircle

--- src/MudBlazor/Components/Chart/Svg/SvgCircle.cs
+++ src/MudBlazor/Components/Chart/Svg/SvgCircle.cs
@@ -3,10 +3,10 @@ namespace MudBlazor.Charts.SVG.Models
     public class SvgCircle
     {
         public int Index { get; set; }
-        public decimal CX { get; set; }
-        public decimal CY { get; set; }
-        public decimal Radius { get; set; }
+        public double CX { get; set; }
+        public double CY { get; set; }
+        public double Radius { get; set; }
         public string StrokeDashArray { get; set; }
-        public decimal StrokeDashOffset { get; set; }
+        public double StrokeDashOffset { get; set; }
     }
 }
