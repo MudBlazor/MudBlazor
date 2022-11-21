cc385881ce50f18c2eba0dfac26332eb6fca7eef
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2022-11-21 09:55:01 +0100

    Fix PieChartTests

--- src/MudBlazor.UnitTests/Components/Charts/PieChartTests.cs
+++ src/MudBlazor.UnitTests/Components/Charts/PieChartTests.cs
@@ -2,6 +2,7 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
 using Bunit;
@@ -41,14 +42,14 @@ namespace MudBlazor.UnitTests.Charts
         }
 
         [Theory]
-        [TestCase(new decimal[]{77, 25, 20, 5})]
-        [TestCase(new decimal[]{77, 25, 20, 5, 8})]
+        [TestCase(new List<decimal>(4) { 77m, 25m, 20m, 5m })]
+        [TestCase(new List<decimal>(5) { 77m, 25m, 20m, 5m, 8m })]
         public void PieChartExampleData(decimal[] data)
         {
-            string[] labels = { "Uranium", "Plutonium", "Thorium", "Caesium", "Technetium", "Promethium",
-                "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Protactinium",
-                "Neptunium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Mudblaznium" };
-            
+            var labels = new List<string>(20) { "Uranium", "Plutonium", "Thorium", "Caesium", "Technetium", "Promethium",
+                "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Protactinium", "Neptunium",
+                "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Mudblaznium" };
+
             var comp = Context.RenderComponent<MudChart>(parameters => parameters
                 .Add(p => p.ChartType, ChartType.Pie)
                 .Add(p => p.ChartOptions, new ChartOptions {ChartPalette = _baseChartPalette})
@@ -56,27 +57,25 @@ namespace MudBlazor.UnitTests.Charts
                 .Add(p => p.Width, "300px")
                 .Add(p => p.InputData, data)
                 .Add(p => p.InputLabels,labels));
-            
+
             comp.Markup.Should().Contain("class=\"mud-chart-pie\"");
             comp.Markup.Should().Contain("class=\"mud-chart-serie\"");
             comp.Markup.Should().Contain("mud-chart-legend-item");
 
-            if (data.Length <= 4)
+            if (data.Count < 5)
             {
-                comp.Markup.Should().
-                    Contain("Uranium").And.Contain("Plutonium").And.Contain("Thorium").And.Contain("Caesium");
+                comp.Markup.Should()
+                    .Contain("Uranium").And.Contain("Plutonium").And.Contain("Thorium").And.Contain("Caesium");
             }
             
-            if (data.Length >= 5)
+            if (data.Count > 4)
             {
-                comp.Markup.Should()
-                    .Contain("Technetium");
+                comp.Markup.Should().Contain("Technetium");
             }
 
-            if (data.Length == 4 && data.Contains(77))
+            if (data.Count == 4 && data.Contains(77))
             {
-                comp.Markup.Should()
-                    .Contain("M 1 0 A 1 1 0 1 1 -0.7851254621398548 -0.6193367490305087 L 0 0");
+                comp.Markup.Should().Contain("M 1 0 A 1 1 0 1 1 -0.7851254621398548 -0.6193367490305087 L 0 0");
             }
 
             if (data.Length == 4 && data.Contains(5))
