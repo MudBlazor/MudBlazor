f20707b00bd229f486290bdf81bd2c1ade04dfdf
ScarletKuro <artem.melnikov@live.com>
2023-03-03 18:46:09 +0200

    Add test scenario when GetField returns null

--- src/MudBlazor.UnitTests/Extensions/EnumExtensionsTests.cs
+++ src/MudBlazor.UnitTests/Extensions/EnumExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using MudBlazor.UnitTests.Dummy;
 using NUnit.Framework;
 
 namespace MudBlazor.UnitTests.Extensions
@@ -19,9 +20,11 @@ namespace MudBlazor.UnitTests.Extensions
         [Test]
         public void ToDescriptionStringOld()
         {
+            DummyEnumEmpty? dummyNullEnum = 0;
             MudBlazor.Extensions.EnumExtensions.ToDescriptionString(Adornment.Start).Should().Be("start");
             MudBlazor.Extensions.EnumExtensions.ToDescriptionString(Align.Inherit).Should().Be("inherit");
             MudBlazor.Extensions.EnumExtensions.ToDescriptionString(Breakpoint.Sm).Should().Be("sm");
+            MudBlazor.Extensions.EnumExtensions.ToDescriptionString(dummyNullEnum).Should().Be("0");
         }
 #pragma warning restore CS0618
     }
