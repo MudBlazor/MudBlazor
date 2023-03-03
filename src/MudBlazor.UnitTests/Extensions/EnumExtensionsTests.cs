fd8371e10417352726f508362c1bd38836449e6f
ScarletKuro <artem.melnikov@live.com>
2023-03-03 18:33:29 +0200

    Fix Analyzer(Resharper) warning about null exception in old ToDescriptionString.
    Add comment that the old test should be removed.

--- src/MudBlazor.UnitTests/Extensions/EnumExtensionsTests.cs
+++ src/MudBlazor.UnitTests/Extensions/EnumExtensionsTests.cs
@@ -7,11 +7,22 @@ namespace MudBlazor.UnitTests.Extensions
     public class EnumExtensionsTests
     {
         [Test]
-        public void ToDescriptionString()
+        public void ToDescriptionStringNew()
         {
             Adornment.Start.ToDescriptionString().Should().Be("start");
             Align.Inherit.ToDescriptionString().Should().Be("inherit");
             Breakpoint.Sm.ToDescriptionString().Should().Be("sm");
         }
+
+#pragma warning disable CS0618
+        /// <remarks>Remove during The Big Break: Breaking Changes in v7</remarks>>
+        [Test]
+        public void ToDescriptionStringOld()
+        {
+            MudBlazor.Extensions.EnumExtensions.ToDescriptionString(Adornment.Start).Should().Be("start");
+            MudBlazor.Extensions.EnumExtensions.ToDescriptionString(Align.Inherit).Should().Be("inherit");
+            MudBlazor.Extensions.EnumExtensions.ToDescriptionString(Breakpoint.Sm).Should().Be("sm");
+        }
+#pragma warning restore CS0618
     }
 }
