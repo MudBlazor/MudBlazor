ce1b00e63e03c9b7e2f0968bdbad54f00f79689e
Jonny Larsson <jonny@gardnet.nu>
2023-03-19 23:21:46 +0100

    Nuget Api Service

---
+++ src/MudBlazor.Docs/Models/NugetPackage.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MudBlazor.Docs.Models;
+
+public class NugetRespons
+{
+    public int totalHits { get; set; }
+    public List<NugetPackage> data { get; set; }
+}
+public class NugetPackage
+{
+    [JsonPropertyName("@id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("@type")]
+    public string type { get; set; }
+    public string registration { get; set; }
+    public string id { get; set; }
+    public string version { get; set; }
+    public string description { get; set; }
+    public string summary { get; set; }
+    public string title { get; set; }
+    public string iconUrl { get; set; }
+    public string licenseUrl { get; set; }
+    public string projectUrl { get; set; }
+    public List<string> tags { get; set; }
+    public List<string> authors { get; set; }
+    public List<string> owners { get; set; }
+    public int totalDownloads { get; set; }
+    public bool verified { get; set; }
+    public List<PackageType> packageTypes { get; set; }
+    public List<Version> versions { get; set; }
+
+    public class Version
+    {
+        public string version { get; set; }
+        public int downloads { get; set; }
+
+        [JsonPropertyName("@id")]
+        public string id { get; set; }
+    }
+
+    public class PackageType
+    {
+        public string name { get; set; }
+    }
+}
+
---
+++ src/MudBlazor.Docs/Services/NugetApiClient.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using MudBlazor.Docs.Models;
+
+namespace MudBlazor.Docs.Services
+{
+    public class NugetApiClient
+    {
+        private readonly HttpClient _http;
+
+        public NugetApiClient(HttpClient http)
+        {
+            _http = http;
+            http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Mobile Safari/537.36");
+        }
+        public async Task<NugetPackage> GetPackageAsync(string packageName)
+        {
+            try
+            {
+                var result = await _http.GetFromJsonAsync<NugetRespons>($"https://azuresearch-usnc.nuget.org/query?q=packageid:{packageName}&take=1");
+                return result.data.FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
