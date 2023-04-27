d515b5fbf3fb25eaadaa40faae0b91a7c625c3a6
Artyom M <artem.melnikov@live.com>
2023-04-27 10:09:21 +0300

    MudCollapse: Add nullable annotation. (#6747)

--- src/MudBlazor/Components/Collapse/MudCollapse.razor.cs
+++ src/MudBlazor/Components/Collapse/MudCollapse.razor.cs
@@ -7,6 +7,7 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
+#nullable enable
     public partial class MudCollapse : MudComponentBase
     {
         internal enum CollapseState
@@ -21,20 +22,20 @@ namespace MudBlazor
 
         protected string Stylename =>
             new StyleBuilder()
-            .AddStyle("max-height", MaxHeight.ToPx(), MaxHeight != null)
-            .AddStyle("height", "auto", _state == CollapseState.Entered)
-            .AddStyle("height", _height.ToPx(), _state is CollapseState.Entering or CollapseState.Exiting)
-            .AddStyle("animation-duration", $"{CalculatedAnimationDuration.ToString("#.##", CultureInfo.InvariantCulture)}s", _state == CollapseState.Entering)
-            .AddStyle(Style)
-            .Build();
+                .AddStyle("max-height", MaxHeight.ToPx(), MaxHeight != null)
+                .AddStyle("height", "auto", _state == CollapseState.Entered)
+                .AddStyle("height", _height.ToPx(), _state is CollapseState.Entering or CollapseState.Exiting)
+                .AddStyle("animation-duration", $"{CalculatedAnimationDuration.ToString("#.##", CultureInfo.InvariantCulture)}s", _state == CollapseState.Entering)
+                .AddStyle(Style)
+                .Build();
 
         protected string Classname =>
             new CssBuilder("mud-collapse-container")
-            .AddClass($"mud-collapse-entering", _state == CollapseState.Entering)
-            .AddClass($"mud-collapse-entered", _state == CollapseState.Entered)
-            .AddClass($"mud-collapse-exiting", _state == CollapseState.Exiting)
-            .AddClass(Class)
-            .Build();
+                .AddClass($"mud-collapse-entering", _state == CollapseState.Entering)
+                .AddClass($"mud-collapse-entered", _state == CollapseState.Entered)
+                .AddClass($"mud-collapse-exiting", _state == CollapseState.Exiting)
+                .AddClass(Class)
+                .Build();
 
         /// <summary>
         /// If true, expands the panel, otherwise collapse it. Setting this prop enables control over the panel.
@@ -67,16 +68,20 @@ namespace MudBlazor
         /// <summary>
         /// Explicitly sets the height for the Collapse element to override the css default.
         /// </summary>
-        [Parameter] public int? MaxHeight { get; set; }
+        [Parameter]
+        public int? MaxHeight { get; set; }
 
         /// <summary>
         /// Child content of component.
         /// </summary>
-        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
 
-        [Parameter] public EventCallback OnAnimationEnd { get; set; }
+        [Parameter]
+        public EventCallback OnAnimationEnd { get; set; }
 
-        [Parameter] public EventCallback<bool> ExpandedChanged { get; set; }
+        [Parameter]
+        public EventCallback<bool> ExpandedChanged { get; set; }
 
         /// <summary>
         /// Modified Animation duration that scales with height parameter.
@@ -89,13 +94,12 @@ namespace MudBlazor
                 if (MaxHeight != null)
                 {
                     if (MaxHeight <= 200) return 0.2;
-                    else if (MaxHeight <= 600) return 0.4;
-                    else if (MaxHeight <= 1400) return 0.6;
+                    if (MaxHeight <= 600) return 0.4;
+                    if (MaxHeight <= 1400) return 0.6;
                     return 1;
                 }
                 return Math.Min(_height / 800.0, 1);
             }
-            set { }
         }
 
         internal async Task UpdateHeight()
@@ -104,12 +108,12 @@ namespace MudBlazor
             {
                 _height = (await _wrapper.MudGetBoundingClientRectAsync())?.Height ?? 0;
             }
-            catch (Exception ex) when (ex is JSDisconnectedException || ex is TaskCanceledException)
+            catch (Exception ex) when (ex is JSDisconnectedException or TaskCanceledException)
             {
                 _height = 0;
             }
 
-            if (MaxHeight != null && _height > MaxHeight)
+            if (_height > MaxHeight)
             {
                 _height = MaxHeight.Value;
             }
@@ -131,6 +135,7 @@ namespace MudBlazor
             await base.OnAfterRenderAsync(firstRender);
         }
 
+        [Obsolete($"Use {nameof(AnimationEndAsync)} instead. This will be removed in v7")]
         public void AnimationEnd()
         {
             if (_state == CollapseState.Entering)
@@ -144,6 +149,21 @@ namespace MudBlazor
                 StateHasChanged();
             }
             OnAnimationEnd.InvokeAsync(Expanded);
+        }
+
+        public Task AnimationEndAsync()
+        {
+            if (_state == CollapseState.Entering)
+            {
+                _state = CollapseState.Entered;
+                StateHasChanged();
+            }
+            else if (_state == CollapseState.Exiting)
+            {
+                _state = CollapseState.Exited;
+                StateHasChanged();
+            }
+            return OnAnimationEnd.InvokeAsync(Expanded);
         }
     }
 }
