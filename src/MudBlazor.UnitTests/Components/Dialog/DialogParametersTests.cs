9909f1876c48dcac2c141a1b7e7c2b8e910f7a5b
Simon Schulze <simonschulze2589@hotmail.de>
2023-06-17 20:24:12 +0200

    MudDialog: Add tests for generic DialogParameters

---
+++ src/MudBlazor.UnitTests/Components/Dialog/DialogParametersTests.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using MudBlazor.UnitTests.TestComponents;
+using NUnit.Framework;
+
+namespace MudBlazor.UnitTests.Components.Dialog;
+
+#nullable enable
+[TestFixture]
+public sealed class DialogParametersTests
+{
+    [Test]
+    public void DialogParametersGeneric_Add_ShouldAddParameter()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+        Assert.IsEmpty(dialogParameters._parameters);
+
+        dialogParameters.Add(x => x.TestValue, "Test");
+        Assert.Contains(new KeyValuePair<string, object>("TestValue", "Test"), dialogParameters._parameters);
+    }
+
+    [Test]
+    public void DialogParametersGeneric_Add_ShouldThrow_IfNotMemberExpression()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+        Assert.Throws<ArgumentException>(() => dialogParameters.Add(x => 1, 2));
+    }
+
+    [Test]
+    public void DialogParametersGeneric_Get_ShouldGetParameter()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+        dialogParameters._parameters = new() { { "TestValue", "Test" } };
+
+        var parameter = dialogParameters.Get(x => x.TestValue);
+        Assert.AreEqual("Test", parameter);
+    }
+
+    [Test]
+    public void DialogParametersGeneric_Get_ShouldThrow_IfNotMemberExpression()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+        Assert.Throws<ArgumentException>(() => dialogParameters.Get(x => 1));
+    }
+
+    [Test]
+    public void DialogParametersGeneric_TryGet_ShouldGetParameter()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+        dialogParameters._parameters = new() { { "TestValue", "Test" } };
+
+        var parameter = dialogParameters.TryGet(x => x.TestValue);
+        Assert.AreEqual("Test", parameter!);
+    }
+
+    [Test]
+    public void DialogParametersGeneric_TryGet_ShouldGetDefault_IfParameterDoesNotExist()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+
+        var parameter = dialogParameters.TryGet(x => x.TestValue);
+        Assert.AreEqual(default(string), parameter);
+    }
+
+    [Test]
+    public void DialogParametersGeneric_TryGet_ShouldThrow_IfNotMemberExpression()
+    {
+        var dialogParameters = new DialogParameters<DialogWithParameters>();
+        Assert.Throws<ArgumentException>(() => dialogParameters.TryGet(x => 1));
+    }
+}
