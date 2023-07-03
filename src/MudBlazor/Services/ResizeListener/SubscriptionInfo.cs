d8aa4c17b583161c83fd580434b44d602fc49326
Artyom M <artem.melnikov@live.com>
2023-07-04 02:47:00 +0300

    ResizeService & BreakpointService: Fix Obsolete (#7147)

--- src/MudBlazor/Services/ResizeListener/SubscriptionInfo.cs
+++ src/MudBlazor/Services/ResizeListener/SubscriptionInfo.cs
@@ -48,6 +48,7 @@ namespace MudBlazor.Services
         }
     }
 
+    [Obsolete("This will be removed in v7.")]
     public class ResizeServiceSubscriptionInfo : SubscriptionInfo<BrowserWindowSize,ResizeOptions>
     {
         public ResizeServiceSubscriptionInfo(ResizeOptions options) : base(options)
@@ -55,6 +56,7 @@ namespace MudBlazor.Services
         }
     }
 
+    [Obsolete("This will be removed in v7.")]
     public class BreakpointServiceSubscriptionInfo : SubscriptionInfo<Breakpoint, ResizeOptions>
     {
         public BreakpointServiceSubscriptionInfo(ResizeOptions options) : base(options)
