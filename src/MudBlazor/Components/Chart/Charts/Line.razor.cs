8dadb3b643b3f756d44cb4957679e8ea4cc91a3b
chausner <15180557+chausner@users.noreply.github.com>
2023-08-13 18:03:09 +0000

    Add missing usings

--- src/MudBlazor/Components/Chart/Charts/Line.razor.cs
+++ src/MudBlazor/Components/Chart/Charts/Line.razor.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.Charts.SVG.Models;
 using MudBlazor.Components.Chart;
