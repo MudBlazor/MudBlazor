bd66d67d285c5802d7f3661875fde13a38b13ef0
David Vondráček <david.vondracek@cube-it.net>
2023-08-23 22:17:02 +0200

    MudStepper: Laid basis for the new Stepper component (#881)

---
+++ src/MudBlazor/Components/Stepper/StepperInteractionEventArgs.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) MudBlazor 2023
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace MudBlazor;
+
+public class StepperInteractionEventArgs
+{
+    public int StepIndex { get; init; }
+    public StepInteractionType InteractionType { get; set; }
+    public bool Cancel { get; set; }
+}
