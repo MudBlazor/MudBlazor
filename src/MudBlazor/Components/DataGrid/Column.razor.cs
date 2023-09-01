c3c1f6ba2ffd9200db0547f05b2a33287389c1c8
XPhilipp <Philipp.Putiatin@gmail.com>
2023-09-01 16:00:30 +0300

    Column.razor.cs - add parameter RenderFragment<IFilterDefinition<T>> SimpleFilterTemplate MudDataGrid.razor - change RenderFragment Filter(IFilterDefinition<T> filterDefinition, Column<T> column) to support parameter.

--- src/MudBlazor/Components/DataGrid/Column.razor.cs
+++ src/MudBlazor/Components/DataGrid/Column.razor.cs
@@ -125,6 +125,7 @@ namespace MudBlazor
 
         [Parameter] public RenderFragment<FilterContext<T>> FilterTemplate { get; set; }
 
+        [Parameter] public RenderFragment<IFilterDefinition<T>> SimpleFilterTemplate { get; set; }
         public string Identifier { get; set; }
         
 
