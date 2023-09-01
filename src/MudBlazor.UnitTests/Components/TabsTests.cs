4b8719cecdbc387e8b9b243127e6c1ccf82e0f5c
Yomodo <shogun.net@outlook.com>
2023-09-01 19:46:18 +0200

    Removed unused using

--- src/MudBlazor.UnitTests/Components/TabsTests.cs
+++ src/MudBlazor.UnitTests/Components/TabsTests.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Bunit;
 using FluentAssertions;
