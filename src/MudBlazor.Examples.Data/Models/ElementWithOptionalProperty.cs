f1f36532961331f391547add40117ce379714266
Elton (Eddie) Hartman <eddiehartman11@gmail.com>
2023-09-28 11:00:23 -0400

    Removing example code.

--- src/MudBlazor.Examples.Data/Models/ElementWithOptionalProperty.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MudBlazor.Examples.Data.Models
-{
-    public class ElementWithOptionalProperty : Element
-    {
-        public string OptionalNote { get; set; }
-        public int? OptionalSecondaryNumber { get; set; }
-    }
-}
