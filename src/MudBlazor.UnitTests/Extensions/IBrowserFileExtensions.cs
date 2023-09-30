bb753fb41320ffc1428120efc891f7b1c51abdfb
Jason Rebelo Neves <igotinfected@gmail.com>
2023-09-30 11:46:20 +0200

    MudFileUpload: Fix FileUpload validation in MudForm (#7577)
    Fixes an issue where the `MudFileUpload` component would keep a
    `MudForm` invalid / untouched despite user interaction.
    
    Fixes: #7577

--- src/MudBlazor.UnitTests/Extensions/IBrowserFileExtensions.cs
+++ src/MudBlazor.UnitTests/Extensions/IBrowserFileExtensions.cs
@@ -3,10 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
 
@@ -25,5 +24,26 @@ namespace MudBlazor.UnitTests
             await file.OpenReadStream().CopyToAsync(memoryStream);
             return Encoding.ASCII.GetString(memoryStream.ToArray());
         }
+    }
+
+    public class DummyBrowserFile : IBrowserFile
+    {
+        public string Name { get; set;  }
+        public DateTimeOffset LastModified { get; set; }
+        public long Size { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+
+        public DummyBrowserFile(string name, DateTimeOffset lastModified, long size, string contentType, byte[] content)
+        {
+            Name = name;
+            LastModified = lastModified;
+            Size = size;
+            ContentType = contentType;
+            Content = content;
+        }
+
+        public Stream OpenReadStream(long maxAllowedSize = 512000, CancellationToken cancellationToken = new())
+            => new MemoryStream(Content);
     }
 }
---
+++ src/MudBlazor.UnitTests/Extensions/InputFileExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using Bunit;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace MudBlazor.UnitTests;
+
+public static class InputFileExtensions
+{
+    public static void ClearFiles(this IRenderedComponent<InputFile> inputFileComponent)
+    {
+        if (inputFileComponent == null)
+            throw new ArgumentNullException(nameof(inputFileComponent));
+
+        var args = new InputFileChangeEventArgs(Array.Empty<IBrowserFile>());
+        var uploadTask = inputFileComponent.InvokeAsync(() => inputFileComponent.Instance.OnChange.InvokeAsync(args));
+        if (!uploadTask.IsCompleted)
+        {
+            uploadTask.GetAwaiter().GetResult();
+        }
+    }
+}
