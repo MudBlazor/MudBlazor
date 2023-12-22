35ecbbf55a6defcdf7a6b0667358519254f14c84
Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>
2023-12-22 20:05:21 +0300

    New Component: MudToggleGroup (#7309)
    * New Component MudToggleGroup
    ---------
    
    Co-authored-by: Artyom M <artem.melnikov@live.com>

---
+++ src/MudBlazor.UnitTests/Components/ToggleTests.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq;
+using AngleSharp.Common;
+using Bunit;
+using FluentAssertions;
+using MudBlazor.UnitTests.TestComponents;
+using NUnit.Framework;
+
+namespace MudBlazor.UnitTests.Components
+{
+    [TestFixture]
+    public class ToggleTests : BunitTest
+    {
+        [Test]
+        public void ToggleBind_Test()
+        {
+            var comp = Context.RenderComponent<ToggleBindTest>();
+            var toggleFirst = comp.FindComponents<MudToggleGroup<string>>().First();
+            var toggleSecond = comp.FindComponents<MudToggleGroup<string>>().Last();
+            var toggleItem = comp.FindAll("div.mud-toggle-item").GetItemByIndex(1);
+
+            toggleFirst.Instance.Value.Should().BeNull();
+            toggleSecond.Instance.Value.Should().BeNull();
+            toggleItem.Click();
+            toggleFirst.Instance.Value.Should().Be("Item Two");
+            toggleSecond.Instance.Value.Should().Be("Item Two");
+        }
+
+        [Test]
+        public void ToggleCustomFragmentBind_Test()
+        {
+            var comp = Context.RenderComponent<ToggleCustomFragmentTest>();
+            var toggleFirst = comp.FindComponents<MudToggleGroup<string>>().First();
+            var toggleSecond = comp.FindComponents<MudToggleGroup<string>>().Last();
+            var toggleItem = comp.FindAll("div.mud-toggle-item").GetItemByIndex(1);
+
+            toggleFirst.Instance.Value.Should().BeNull();
+            toggleSecond.Instance.Value.Should().BeNull();
+            toggleItem.Click();
+            toggleFirst.Instance.Value.Should().Be("Item Two");
+            toggleSecond.Instance.Value.Should().Be("Item Two");
+        }
+
+        [Test]
+        public void ToggleBindMultiSelection_Test()
+        {
+            var comp = Context.RenderComponent<ToggleBindMultiSelectionTest>();
+            var toggleFirst = comp.FindComponents<MudToggleGroup<string>>().First();
+            var toggleSecond = comp.FindComponents<MudToggleGroup<string>>().Last();
+            var toggleItemSecond = comp.FindAll("div.mud-toggle-item").GetItemByIndex(1);
+            var toggleItemThird = comp.FindAll("div.mud-toggle-item").GetItemByIndex(2);
+
+            toggleFirst.Instance.SelectedValues.Should().BeNull();
+            toggleSecond.Instance.SelectedValues.Should().BeNull();
+            toggleItemSecond.Click();
+            toggleFirst.Instance.SelectedValues.Should().Contain("Item Two");
+            toggleSecond.Instance.SelectedValues.Should().Contain("Item Two");
+            toggleItemThird.Click();
+            toggleFirst.Instance.SelectedValues.Should().BeEquivalentTo("Item Two", "Item Three");
+            toggleSecond.Instance.SelectedValues.Should().Contain("Item Three");
+            toggleItemSecond.Click();
+            toggleFirst.Instance.SelectedValues.Should().BeEquivalentTo("Item Three");
+            toggleSecond.Instance.SelectedValues.Should().Contain("Item Three");
+        }
+
+        [Test]
+        public void ToggleInitialize_Test()
+        {
+            var comp = Context.RenderComponent<ToggleInitializeTest>();
+            var toggleFirst = comp.FindComponents<MudToggleGroup<string>>().First();
+            var toggleSecond = comp.FindComponents<MudToggleGroup<string>>().Last();
+            var buttonOne = comp.FindAll("button").First();
+            var buttonTwo = comp.FindAll("button").Last();
+
+            toggleFirst.Instance.Value.Should().Be("Item Two");
+            toggleSecond.Instance.SelectedValues.Should().BeEquivalentTo("Item One", "Item Three");
+
+            buttonOne.Click();
+            toggleFirst.Instance.Value.Should().Be("Item One");
+
+            buttonTwo.Click();
+            toggleSecond.Instance.SelectedValues.Should().BeEquivalentTo("Item Two", "Item Three");
+        }
+
+        [Test]
+        public void ToggleToggleSelection_Test()
+        {
+            var comp = Context.RenderComponent<ToggleToggleSelectionTest>();
+            var toggle = comp.FindComponent<MudToggleGroup<string>>();
+            var toggleItem = comp.FindAll("div.mud-toggle-item").GetItemByIndex(0);
+
+            toggle.Instance.Value.Should().BeNull();
+            toggleItem.Click();
+            toggle.Instance.Value.Should().Be("Item One");
+            toggleItem.Click();
+            toggle.Instance.Value.Should().BeNull();
+        }
+    }
+}
---
+++ src/MudBlazor/Components/Toggle/MudToggleGroup.razor.cs
@@ -0,0 +1,291 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Interfaces;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+#nullable enable
+    public partial class MudToggleGroup<T> : MudComponentBase
+    {
+        private T? _oldValue;
+        private Color _oldColor;
+        private IEnumerable<T?>? _oldValues;
+        private string? _oldSelectedClass;
+        private bool _oldBordered;
+        private List<MudToggleItem<T>> _items = new();
+
+        protected string Classname => new CssBuilder("mud-toggle-group")
+            .AddClass(Class)
+            .Build();
+
+        protected string Stylename => new StyleBuilder()
+            .AddStyle("grid-template-columns", $"repeat({_items.Count}, minmax(0, 1fr))", Vertical == false)
+            .AddStyle("grid-template-rows", $"repeat({_items.Count}, minmax(0, 1fr))", Vertical == true)
+            .AddStyle(Style)
+            .Build();
+
+        /// <summary>
+        /// The generic value for the component.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public T? Value { get; set; }
+
+        /// <summary>
+        /// Fires when value changed.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public EventCallback<T?> ValueChanged { get; set; }
+
+        /// <summary>
+        /// Selected values that stored for multiselection mode.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public IEnumerable<T?>? SelectedValues { get; set; }
+
+        /// <summary>
+        /// Fires when SelectedValues changed.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public EventCallback<IEnumerable<T?>> SelectedValuesChanged { get; set; }
+
+        /// <summary>
+        /// Classnames only applied selected item, sepereated by space.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public string? SelectedClass { get; set; }
+
+        /// <summary>
+        /// Class for toggle item text.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public string? TextClass { get; set; }
+
+        /// <summary>
+        /// If true, items ordered vertically.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public bool Vertical { get; set; }
+
+        /// <summary>
+        /// if true, first and last item will be rounded.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public bool Rounded { get; set; }
+
+        /// <summary>
+        /// If true, items will be bordered. Default is true.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public bool Bordered { get; set; } = true;
+
+        /// <summary>
+        /// If true, disables the ripple effect.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public bool DisableRipple { get; set; }
+
+        /// <summary>
+        /// If true, component's margin and padding will reduce.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public bool Dense { get; set; }
+
+        /// <summary>
+        /// If true, multiselection is available.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public bool MultiSelection { get; set; }
+
+        /// <summary>
+        /// If true, user can deselect items in single selection mode.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public bool ToggleSelection { get; set; }
+
+        /// <summary>
+        /// Shows the icon when item is selected. Default is true and default icon is tickmark.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public bool ShowIconWhenSelected { get; set; } = true;
+
+        /// <summary>
+        /// The color of the component. Affect borders and selection color. Default is primary.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Appearance)]
+        public Color Color { get; set; } = Color.Primary;
+
+        /// <summary>
+        /// If true, only shows the icon.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public bool IconOnly { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.List.Behavior)]
+        public RenderFragment? ChildContent { get; set; }
+
+        protected internal void Register(MudToggleItem<T> item)
+        {
+            if (_items.Select(x => x.Value).Contains(item.Value))
+            {
+                return;
+            }
+
+            _items.Add(item);
+        }
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            // Handle single selection mode
+            if (((_oldValue is null && Value is not null) || (_oldValue is not null && Value is null) || (_oldValue is not null && !_oldValue.Equals(Value))) && !MultiSelection)
+            {
+                DeselectAllItems();
+
+                if (Value is not null)
+                {
+                    var selectedItem = _items.FirstOrDefault(x => Value.Equals(x.Value));
+                    selectedItem?.SetSelected(true);
+                }
+
+                _oldValue = Value;
+            }
+
+            // Handle multi-selection mode
+            if (((_oldValues is null && SelectedValues is not null) || (_oldValues is not null && !_oldValues.Equals(SelectedValues))) && MultiSelection)
+            {
+                DeselectAllItems();
+
+                if (SelectedValues is not null)
+                {
+                    var selectedItems = _items.Where(x => SelectedValues.Contains(x.Value)).ToList();
+                    selectedItems.ForEach(x => x.SetSelected(true));
+                }
+
+                _oldValues = SelectedValues;
+            }
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                // Handle single selection mode
+                if (Value is not null && !MultiSelection)
+                {
+                    var selectedItem = _items.FirstOrDefault(x => Value.Equals(x.Value));
+                    selectedItem?.SetSelected(true);
+                }
+
+                // Handle multi-selection mode
+                if (SelectedValues is not null && MultiSelection)
+                {
+                    var selectedItems = _items.Where(x => SelectedValues.Contains(x.Value)).ToList();
+                    selectedItems.ForEach(x => x.SetSelected(true));
+                }
+
+                StateHasChanged();
+            }
+
+            if (Color != _oldColor ||
+                SelectedClass != _oldSelectedClass ||
+                Bordered != _oldBordered)
+            {
+                _oldColor = Color;
+                _oldSelectedClass = SelectedClass;
+                _oldBordered = Bordered;
+                foreach (IMudStateHasChanged mudComponent in _items)
+                {
+                    mudComponent.StateHasChanged();
+                }
+
+                StateHasChanged();
+            }
+        }
+
+        protected internal async Task ToggleItemAsync(MudToggleItem<T> item)
+        {
+            if (MultiSelection)
+            {
+                if (item.IsSelected())
+                {
+                    SelectedValues = SelectedValues?.Where(x => !Equals(x, item.Value));
+                    await SelectedValuesChanged.InvokeAsync(SelectedValues);
+                }
+                else
+                {
+                    SelectedValues ??= new HashSet<T>();
+                    SelectedValues = SelectedValues.Append(item.Value);
+                    await SelectedValuesChanged.InvokeAsync(SelectedValues);
+                }
+                item.SetSelected(!item.IsSelected());
+                return;
+            }
+
+            if (ToggleSelection)
+            {
+                var selected = item.IsSelected();
+                if (!selected)
+                {
+                    DeselectAllItems();
+                    item.SetSelected(true);
+                    Value = item.Value;
+                    await ValueChanged.InvokeAsync(Value);
+                }
+                else
+                {
+                    item.SetSelected(false);
+                    Value = default(T);
+                    await ValueChanged.InvokeAsync(Value);
+                }
+            }
+            else
+            {
+                DeselectAllItems();
+                item.SetSelected(true);
+                Value = item.Value;
+                await ValueChanged.InvokeAsync(Value);
+            }
+        }
+
+        protected void DeselectAllItems()
+        {
+            foreach (var item in _items)
+            {
+                item.SetSelected(false);
+            }
+        }
+
+        protected internal IEnumerable<MudToggleItem<T>> GetItems() => _items;
+
+        protected internal bool IsFirstItem(MudToggleItem<T> item) => item.Equals(_items.FirstOrDefault());
+
+        protected internal bool IsLastItem(MudToggleItem<T> item) => item.Equals(_items.LastOrDefault());
+
+        protected internal double GetItemWidth() => 100 / (_items.Count == 0 ? 1 : _items.Count);
+    }
+}
