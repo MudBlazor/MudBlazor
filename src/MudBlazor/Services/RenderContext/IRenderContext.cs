841866efee61876361739af018ad051a2d2a71c7
Mike Surcouf <mike@surcouf.je>
2024-01-19 20:31:54 +0000

    Add Doc comments

--- src/MudBlazor/Services/RenderContext/IRenderContext.cs
+++ src/MudBlazor/Services/RenderContext/IRenderContext.cs
@@ -2,12 +2,29 @@
 
 namespace MudBlazor.Services;
 
+/// <summary>
+/// Service to return the current render mode
+/// </summary>
 public interface IRenderContext
 {
+
+    /// <summary>
+    /// WebAssembly interctive mode
+    /// </summary>
+    /// <returns>true if the mode is active else false</returns>
     bool IsInteractiveWebAssembly();
 
+    /// <summary>
+    /// Server Side interctive mode
+    /// </summary>
+    /// <returns>true if the mode is active else false</returns>
     bool IsInteractiveServer();
 
+    /// <summary>
+    /// The server is statically rendering the page for SSR or PreRendering.
+    /// No interactivity is available.
+    /// </summary>
+    /// <returns>true if the mode is active else false</returns>
     bool IsStaticServer();
 }
 
