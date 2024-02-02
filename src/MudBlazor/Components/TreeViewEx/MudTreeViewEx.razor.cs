098a16cd178e043be985819ba50be0cec45c71d0
Marcin Blichowski <74534746+mblichowski@users.noreply.github.com>
2024-02-02 09:25:35 +0100

    TreeViewEx: items filtering (#25)
    * items filtering
    
    * refactoring + cleaning
    
    * v6.8.0.1 update

---
+++ src/MudBlazor/Components/TreeViewEx/MudTreeViewEx.razor.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudTreeViewEx<T> : MudComponentBase
+    {
+        private MudTreeViewItemEx<T> _selectedValue;
+        private HashSet<MudTreeViewItemEx<T>> _selectedValues;
+        private List<MudTreeViewItemEx<T>> _childItems = new();
+
+        private HashSet<T> FilteredItems => Items.Where(ItemsFilter).ToHashSet();
+
+        protected string Classname =>
+        new CssBuilder("mud-treeview")
+          .AddClass("mud-treeview-dense", Dense)
+          .AddClass("mud-treeview-hover", Hover)
+          .AddClass($"mud-treeview-selected-{Color.ToDescriptionString()}")
+           .AddClass($"mud-treeview-checked-{CheckBoxColor.ToDescriptionString()}")
+          .AddClass(Class)
+        .Build();
+        protected string Stylename =>
+        new StyleBuilder()
+            .AddStyle($"width", Width, !string.IsNullOrWhiteSpace(Width))
+            .AddStyle($"height", Height, !string.IsNullOrWhiteSpace(Height))
+            .AddStyle($"max-height", MaxHeight, !string.IsNullOrWhiteSpace(MaxHeight))
+            .AddStyle(Style)
+        .Build();
+
+        /// <summary>
+        /// The color of the selected treeviewitem.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public Color Color { get; set; } = Color.Primary;
+
+        /// <summary>
+        /// Check box color if multiselection is used.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public Color CheckBoxColor { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public int? VisibleItemsLimit { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public int VisibleItemsIncrement { get; set; } = 10;
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public string VisibleItemsExtendText { get; set; } = "...and {0} more items";
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public bool EnableShowAllItemsButton { get; set; }
+
+        /// <summary>
+        /// if true, multiple values can be selected via checkboxes which are automatically shown in the tree view.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public bool MultiSelection { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public bool CanSelectDisabledItem { get; set; } = true;
+
+        /// <summary>
+        /// if true, multiple values can be selected via checkboxes which are automatically shown in the tree view.
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [Obsolete("Use MultiSelection instead.", true)]
+        [Parameter]
+        public bool CanSelect
+        {
+            get => MultiSelection;
+            set => MultiSelection = value;
+        }
+
+        [ExcludeFromCodeCoverage]
+        [Obsolete("MudTreeView now automaticly activates when using SelectedValue.", true)]
+        [Parameter] public bool CanActivate { get; set; }
+
+        /// <summary>
+        /// If true, clicking anywhere on the item will expand it, if it has childs.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.ClickAction)]
+        public bool ExpandOnClick { get; set; }
+
+        /// <summary>
+        /// If true, double clicking anywhere on the item will expand it, if it has childs.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.ClickAction)]
+        public bool ExpandOnDoubleClick { get; set; }
+
+        /// <summary>
+        /// Hover effect for item's on mouse-over.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public bool Hover { get; set; }
+
+        /// <summary>
+        /// Hover effect for item's on mouse-over.
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [Obsolete("Use Hover instead.", true)]
+        [Parameter]
+        public bool CanHover
+        {
+            get => Hover;
+            set => Hover = value;
+        }
+
+        /// <summary>
+        /// If true, compact vertical padding will be applied to all treeview items.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public bool Dense { get; set; }
+
+        /// <summary>
+        /// Setting a height will allow to scroll the treeview. If not set, it will try to grow in height.
+        /// You can set this to any CSS value that the attribute 'height' accepts, i.e. 500px.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public string Height { get; set; }
+
+        /// <summary>
+        /// Setting a maximum height will allow to scroll the treeview. If not set, it will try to grow in height.
+        /// You can set this to any CSS value that the attribute 'height' accepts, i.e. 500px.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public string MaxHeight { get; set; }
+
+        /// <summary>
+        /// Setting a width the treeview. You can set this to any CSS value that the attribute 'height' accepts, i.e. 500px.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public string Width { get; set; }
+
+        /// <summary>
+        /// If true, treeview will be disabled and all its childitems.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public Func<T, bool> ItemsFilter { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public HashSet<T> Items { get; set; }
+
+        [ExcludeFromCodeCoverage]
+        [Obsolete("Use SelectedValueChanged instead.", true)]
+        [Parameter]
+        public EventCallback<T> ActivatedValueChanged
+        {
+            get => SelectedValueChanged;
+            set => SelectedValueChanged = value;
+        }
+
+        /// <summary>
+        /// Called whenever the selected value changed.
+        /// </summary>
+        [Parameter] public EventCallback<T> SelectedValueChanged { get; set; }
+
+        /// <summary>
+        /// Called whenever the selectedvalues changed.
+        /// </summary>
+        [Parameter] public EventCallback<HashSet<T>> SelectedValuesChanged { get; set; }
+
+        /// <summary>
+        /// Child content of component.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// ItemTemplate for rendering children.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public RenderFragment<T> ItemTemplate { get; set; }
+
+        [CascadingParameter] MudTreeViewEx<T> MudTreeRoot { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public Func<T, Task<HashSet<T>>> ServerData { get; set; }
+
+        public MudTreeViewEx()
+        {
+            MudTreeRoot = this;
+        }
+
+        internal bool IsSelectable { get; private set; }
+
+        protected override void OnInitialized()
+        {
+            IsSelectable = SelectedValueChanged.HasDelegate;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && MudTreeRoot == this)
+            {
+                await UpdateSelectedItems();
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        internal async Task UpdateSelected(MudTreeViewItemEx<T> item, bool requestedValue)
+        {
+            if (!CanSelectDisabledItem && item.Disabled)
+                return;
+
+            if ((_selectedValue == item && requestedValue) ||
+                (_selectedValue != item && !requestedValue))
+                return;
+
+            if (_selectedValue == item && !requestedValue)
+            {
+                _selectedValue = default;
+                await item.Select(requestedValue);
+                await SelectedValueChanged.InvokeAsync(default);
+                return;
+            }
+
+            if (_selectedValue != null)
+            {
+                await _selectedValue.Select(false);
+            }
+
+            _selectedValue = item;
+            await item.Select(requestedValue);
+            await SelectedValueChanged.InvokeAsync(item.Value);
+        }
+
+        internal Task UpdateSelectedItems()
+        {
+            _selectedValues ??= new HashSet<MudTreeViewItemEx<T>>();
+
+            //collect selected items
+            _selectedValues.Clear();
+            foreach (var item in _childItems)
+            {
+                foreach (var selectedItem in item.GetSelectedItems())
+                {
+                    _selectedValues.Add(selectedItem);
+                }
+            }
+
+            return SelectedValuesChanged.InvokeAsync(new HashSet<T>(_selectedValues.Select(i => i.Value)));
+        }
+
+        public async Task Select(MudTreeViewItemEx<T> item, bool isSelected = true)
+        {
+            await UpdateSelected(item, isSelected);
+        }
+
+        internal void AddChild(MudTreeViewItemEx<T> item) => _childItems.Add(item);
+
+    }
+}
---
+++ src/MudBlazor/Components/TreeViewEx/MudTreeViewItemEx.razor.cs
@@ -0,0 +1,488 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Interfaces;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudTreeViewItemEx<T> : MudComponentBase
+    {
+        private string _text;
+        private bool _disabled;
+        private bool _canExpand = true;
+        private bool _isChecked, _isSelected, _isServerLoaded;
+        private Converter<T> _converter = new DefaultConverter<T>();
+        private readonly List<MudTreeViewItemEx<T>> _childItems = new();
+
+        private HashSet<T> FilteredItems =>
+            Items is not null && MudTreeRoot.ItemsFilter is not null ?
+            Items.Where(MudTreeRoot.ItemsFilter).ToHashSet() :
+            Items;
+
+        protected string Classname =>
+        new CssBuilder("mud-treeview-item")
+            .AddClass("mud-treeview-select-none", MudTreeRoot?.ExpandOnDoubleClick == true)
+          .AddClass(Class)
+        .Build();
+
+        protected string ContentClassname =>
+        new CssBuilder("mud-treeview-item-content")
+          .AddClass("cursor-pointer",
+                (MudTreeRoot?.IsSelectable == true && (!Disabled || MudTreeRoot?.CanSelectDisabledItem == true)) ||
+                (MudTreeRoot?.ExpandOnClick == true && HasChild))
+          .AddClass($"mud-treeview-item-selected", _isSelected)
+        .Build();
+
+        public string TextClassname =>
+        new CssBuilder("mud-treeview-item-label")
+            .AddClass(TextClass)
+        .Build();
+
+
+        [CascadingParameter] MudTreeViewEx<T> MudTreeRoot { get; set; }
+
+        [CascadingParameter] MudTreeViewItemEx<T> Parent { get; set; }
+
+        /// <summary>
+        /// Custom checked icon, leave null for default.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public string CheckedIcon { get; set; } = Icons.Material.Filled.CheckBox;
+
+        /// <summary>
+        /// Custom unchecked icon, leave null for default.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public string UncheckedIcon { get; set; } = Icons.Material.Filled.CheckBoxOutlineBlank;
+
+        /// <summary>
+        /// Value of the treeviewitem. Acts as the displayed text if no text is set.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public T Value { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public CultureInfo Culture { get; set; } = CultureInfo.CurrentCulture;
+
+        /// <summary>
+        /// The text to display
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public string Text
+        {
+            get => string.IsNullOrEmpty(_text) ? _converter.Set(Value) : _text;
+            set => _text = value;
+        }
+
+        /// <summary>
+        /// Tyopography for the text.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public Typo TextTypo { get; set; } = Typo.body1;
+
+        /// <summary>
+        /// User class names for the text, separated by space.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public string TextClass { get; set; }
+
+        /// <summary>
+        /// The text at the end of the item.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public string EndText { get; set; }
+
+        /// <summary>
+        /// Tyopography for the endtext.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public Typo EndTextTypo { get; set; } = Typo.body1;
+
+        /// <summary>
+        /// User class names for the endtext, separated by space.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public string EndTextClass { get; set; }
+
+        /// <summary>
+        /// If true, treeviewitem will be disabled.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public bool Disabled
+        {
+            get => _disabled || (MudTreeRoot?.Disabled ?? false);
+            set => _disabled = value;
+        }
+
+        /// <summary>
+        /// If false, TreeViewItem will not be able to expand.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public bool CanExpand
+        {
+            get => _canExpand;
+            set => _canExpand = value;
+        }
+
+        /// <summary>
+        /// Child content of component used to create sub levels.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public RenderFragment ChildContent { get; set; }
+
+        /// <summary>
+        /// Content of the item, if used completly replaced the default rendering.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public RenderFragment Content { get; set; }
+
+        /// <summary>
+        /// Content of the item body, if used replaced the text, end text and end icon rendering.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public RenderFragment<MudTreeViewItemEx<T>> BodyContent { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Data)]
+        public HashSet<T> Items { get; set; }
+
+        /// <summary>
+        /// Command executed when the user clicks on the CommitEdit Button.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.ClickAction)]
+        [Obsolete($"Use {nameof(OnClick)} instead. This will be removed in v7.")]
+        public ICommand Command { get; set; }
+
+        /// <summary>
+        /// Expand or collapse treeview item when it has children. Two-way bindable. Note: if you directly set this to
+        /// true or false (instead of using two-way binding) it will force the item's expansion state.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Expanding)]
+        public bool Expanded { get; set; }
+
+        /// <summary>
+        /// Called whenever expanded changed.
+        /// </summary>
+        [Parameter] public EventCallback<bool> ExpandedChanged { get; set; }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public bool Activated
+        {
+            get => _isSelected;
+            set
+            {
+                _ = MudTreeRoot?.UpdateSelected(this, value);
+            }
+        }
+
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public bool Selected
+        {
+            get => _isChecked;
+            set
+            {
+                if (_isChecked == value)
+                    return;
+
+                _isChecked = value;
+                MudTreeRoot?.UpdateSelectedItems();
+                SelectedChanged.InvokeAsync(_isChecked);
+            }
+        }
+
+        /// <summary>
+        /// Icon placed before the text if set.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// The color of the icon. It supports the theme colors.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public Color IconColor { get; set; } = Color.Default;
+
+        /// <summary>
+        /// Icon placed after the text if set.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Behavior)]
+        public string EndIcon { get; set; }
+
+        /// <summary>
+        /// The color of the icon. It supports the theme colors.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public Color EndIconColor { get; set; } = Color.Default;
+
+        /// <summary>
+        /// The expand/collapse icon.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Expanding)]
+        public string ExpandedIcon { get; set; } = Icons.Material.Filled.ChevronRight;
+
+        /// <summary>
+        /// The color of the expand/collapse button. It supports the theme colors.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Expanding)]
+        public Color ExpandedIconColor { get; set; } = Color.Default;
+
+        /// <summary>
+        /// The loading icon.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public string LoadingIcon { get; set; } = Icons.Material.Filled.Loop;
+
+        /// <summary>
+        /// The color of the loading. It supports the theme colors.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Appearance)]
+        public Color LoadingIconColor { get; set; } = Color.Default;
+
+        /// <summary>
+        /// Called whenever the activated value changed.
+        /// </summary>
+        [Parameter] public EventCallback<bool> ActivatedChanged { get; set; }
+
+        /// <summary>
+        /// Called whenever the selected value changed.
+        /// </summary>
+        [Parameter] public EventCallback<bool> SelectedChanged { get; set; }
+
+        /// <summary>
+        /// Tree item click event.
+        /// </summary>
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        /// <summary>
+        /// Tree item double click event.
+        /// </summary>
+        [Parameter] public EventCallback<MouseEventArgs> OnDoubleClick { get; set; }
+
+        public bool Loading { get; set; }
+
+        bool HasChild => ChildContent != null ||
+             (MudTreeRoot != null && FilteredItems != null && FilteredItems.Count != 0) ||
+             (MudTreeRoot?.ServerData != null && _canExpand && !_isServerLoaded && (Items == null || Items.Count == 0));
+
+        protected bool IsChecked
+        {
+            get => Selected;
+            set { _ = SelectItem(value, this); }
+        }
+
+        protected internal bool ArrowExpanded
+        {
+            get => Expanded;
+            set
+            {
+                if (value == Expanded)
+                    return;
+
+                Expanded = value;
+                ExpandedChanged.InvokeAsync(value);
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            if (Parent != null)
+            {
+                Parent.AddChild(this);
+            }
+            else
+            {
+                MudTreeRoot?.AddChild(this);
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && _isSelected)
+            {
+                await MudTreeRoot.UpdateSelected(this, _isSelected);
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        protected async Task OnItemClicked(MouseEventArgs ev)
+        {
+            if (HasChild && (MudTreeRoot?.ExpandOnClick ?? false))
+            {
+                Expanded = !Expanded;
+                await TryInvokeServerLoadFunc();
+                await ExpandedChanged.InvokeAsync(Expanded);
+            }
+
+            if (Disabled)
+            {
+                return;
+            }
+
+            if (MudTreeRoot?.IsSelectable ?? false)
+            {
+                await MudTreeRoot.UpdateSelected(this, !_isSelected);
+            }
+
+            await OnClick.InvokeAsync(ev);
+#pragma warning disable CS0618
+            if (Command?.CanExecute(Value) ?? false)
+            {
+                Command.Execute(Value);
+            }
+#pragma warning restore CS0618
+        }
+
+        protected async Task OnItemDoubleClicked(MouseEventArgs ev)
+        {
+            if (HasChild && (MudTreeRoot?.ExpandOnDoubleClick ?? false))
+            {
+                Expanded = !Expanded;
+                await TryInvokeServerLoadFunc();
+                await ExpandedChanged.InvokeAsync(Expanded);
+            }
+
+            if (Disabled)
+            {
+                return;
+            }
+
+            if (MudTreeRoot?.IsSelectable ?? false)
+            {
+                await MudTreeRoot.UpdateSelected(this, !_isSelected);
+            }
+
+            await OnDoubleClick.InvokeAsync(ev);
+        }
+
+        protected internal async Task OnItemExpanded(bool expanded)
+        {
+            if (Expanded != expanded)
+            {
+                Expanded = expanded;
+                await TryInvokeServerLoadFunc();
+                await ExpandedChanged.InvokeAsync(expanded);
+            }
+        }
+
+        /// <summary>
+        /// Clear the tree items, and try to reload from server.
+        /// </summary>
+        public async Task ReloadAsync()
+        {
+            if (Items != null)
+            {
+                Items.Clear();
+            }
+            await TryInvokeServerLoadFunc();
+
+            if (Parent != null)
+            {
+                Parent.StateHasChanged();
+            }
+            else if (MudTreeRoot != null)
+            {
+                ((IMudStateHasChanged)MudTreeRoot).StateHasChanged();
+            }
+        }
+
+        internal Task Select(bool value)
+        {
+            if (_isSelected == value)
+                return Task.CompletedTask;
+
+            _isSelected = value;
+
+            StateHasChanged();
+
+            return ActivatedChanged.InvokeAsync(_isSelected);
+        }
+
+        internal async Task SelectItem(bool value, MudTreeViewItemEx<T> source = null)
+        {
+            if (value == _isChecked)
+                return;
+
+            _isChecked = value;
+            _childItems.ForEach(async c => await c.SelectItem(value, source));
+
+            StateHasChanged();
+
+            await SelectedChanged.InvokeAsync(_isChecked);
+
+            if (source == this)
+            {
+                if (MudTreeRoot != null)
+                {
+                    await MudTreeRoot.UpdateSelectedItems();
+                }
+            }
+        }
+
+        private void AddChild(MudTreeViewItemEx<T> item) => _childItems.Add(item);
+
+        internal IEnumerable<MudTreeViewItemEx<T>> GetSelectedItems()
+        {
+            if (_isChecked)
+                yield return this;
+
+            foreach (var treeItem in _childItems)
+            {
+                foreach (var selected in treeItem.GetSelectedItems())
+                {
+                    yield return selected;
+                }
+            }
+        }
+
+        internal async Task TryInvokeServerLoadFunc()
+        {
+            if (Expanded && (Items == null || Items.Count == 0) && _canExpand && MudTreeRoot?.ServerData != null)
+            {
+                Loading = true;
+                StateHasChanged();
+
+                Items = await MudTreeRoot.ServerData(Value);
+
+                Loading = false;
+                _isServerLoaded = true;
+
+                StateHasChanged();
+            }
+        }
+
+    }
+}
