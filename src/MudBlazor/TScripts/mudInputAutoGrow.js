892a1de395f976f8b654c2b9de504344f079f57b
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-02-19 03:30:14 -0600

    MudTextField: Preserve scroll position while typing with AutoGrow and update when window is resized (#8193)
    * AutoGrow: Fix scrollbar flashing
    * AutoGrow: Preserve scroll position better Fixes #8152
    * AutoGrow: Update when window is resized #8091
    * AutoGrow: Leave scrollbars alone

--- src/MudBlazor/TScripts/mudInputAutoGrow.js
+++ src/MudBlazor/TScripts/mudInputAutoGrow.js
@@ -5,18 +5,29 @@
 window.mudInputAutoGrow = {
 
     initAutoGrow: (elem, maxLines) => {
-        const compStyles = getComputedStyle(elem);
-        const lineHeight = parseFloat(compStyles.getPropertyValue('line-height'));
+        const compStyle = getComputedStyle(elem);
+        const lineHeight = parseFloat(compStyle.getPropertyValue('line-height'));
 
         let minHeight = lineHeight * elem.rows;
 
         let maxHeight = 0;
         if (maxLines > 0) {
+            // Cap the height to the max number of lines.
             maxHeight = lineHeight * maxLines;
         }
 
         // Capture min and max height in closure to trigger height adjustment on element in MudTextField.
         elem.adjustAutoGrowHeight = function () {
+            // Save scroll positions https://github.com/MudBlazor/MudBlazor/issues/8152.
+            const scrollTops = [];
+            let curElem = elem;
+            while (curElem && curElem.parentNode && curElem.parentNode instanceof Element) {
+                if (curElem.parentNode.scrollTop) {
+                    scrollTops.push([curElem.parentNode, curElem.parentNode.scrollTop]);
+                }
+                curElem = curElem.parentNode;
+            }
+
             elem.style.height = 0;
 
             let newHeight = Math.max(minHeight, elem.scrollHeight);
@@ -25,12 +36,26 @@ window.mudInputAutoGrow = {
             }
 
             elem.style.height = newHeight + "px";
+
+            // Restore scroll positions.
+            scrollTops.forEach(([node, scrollTop]) => {
+                node.style.scrollBehavior = 'auto';
+                node.scrollTop = scrollTop;
+                node.style.scrollBehavior = null;
+            });
         }
 
+        // Adjust height when input happens.
         elem.addEventListener('input', () => {
             elem.adjustAutoGrowHeight();
         });
 
+        // Adjust height when the window resizes.
+        window.addEventListener('resize', () => {
+            elem.adjustAutoGrowHeight();
+        });
+
+        // Initial height adjustment.
         elem.adjustAutoGrowHeight();
     },
     adjustHeight: (elem) => {
