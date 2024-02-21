5fd65f1481c6b9cb47f1b1f11f99e5a2b9e7e3b0
Artyom M <artem.melnikov@live.com>
2024-02-21 15:38:13 +0200

    PopoverService: Improve performance

---
+++ src/MudBlazor/Utilities/AsyncKeyedLocker/AsyncKeyedLockDictionary.cs
@@ -0,0 +1,214 @@
+﻿// Copyright (c) 2023 Mark Cilia Vincenti
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+// ReSharper disable LocalizableElement
+namespace MudBlazor.Utilities.AsyncKeyedLocker
+{
+    internal sealed class AsyncKeyedLockDictionary<TKey> : ConcurrentDictionary<TKey, AsyncKeyedLockReleaser<TKey>>, IDisposable
+    {
+        private readonly AsyncKeyedLockPool<TKey> _pool;
+
+        public int MaxCount { get; } = 1;
+
+        internal bool PoolingEnabled { get; }
+
+        public AsyncKeyedLockDictionary()
+        {
+        }
+
+        public AsyncKeyedLockDictionary(AsyncKeyedLockOptions options)
+        {
+            if (options.MaxCount < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(options), options.MaxCount, $"{nameof(options.MaxCount)} should be greater than or equal to 1.");
+            }
+
+            MaxCount = options.MaxCount;
+            if (options.PoolSize > 0)
+            {
+                PoolingEnabled = true;
+                _pool = new AsyncKeyedLockPool<TKey>(this, options.PoolSize, options.PoolInitialFill);
+            }
+        }
+
+        public AsyncKeyedLockDictionary(IEqualityComparer<TKey> comparer) : base(comparer)
+        {
+        }
+
+        public AsyncKeyedLockDictionary(AsyncKeyedLockOptions options, IEqualityComparer<TKey> comparer) : base(comparer)
+        {
+            if (options.MaxCount < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(options), options.MaxCount, $"{nameof(options.MaxCount)} should be greater than or equal to 1.");
+            }
+
+            MaxCount = options.MaxCount;
+            if (options.PoolSize > 0)
+            {
+                PoolingEnabled = true;
+                _pool = new AsyncKeyedLockPool<TKey>(this, options.PoolSize, options.PoolInitialFill);
+            }
+        }
+
+        public AsyncKeyedLockDictionary(int concurrencyLevel, int capacity) : base(concurrencyLevel, capacity)
+        {
+        }
+
+        public AsyncKeyedLockDictionary(AsyncKeyedLockOptions options, int concurrencyLevel, int capacity) : base(concurrencyLevel, capacity)
+        {
+            if (options.MaxCount < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(options), options.MaxCount, $"{nameof(options.MaxCount)} should be greater than or equal to 1.");
+            }
+
+            MaxCount = options.MaxCount;
+            if (options.PoolSize > 0)
+            {
+                PoolingEnabled = true;
+                _pool = new AsyncKeyedLockPool<TKey>(this, options.PoolSize, options.PoolInitialFill);
+            }
+        }
+
+        public AsyncKeyedLockDictionary(int concurrencyLevel, int capacity, IEqualityComparer<TKey> comparer) : base(concurrencyLevel, capacity, comparer)
+        {
+        }
+
+        public AsyncKeyedLockDictionary(AsyncKeyedLockOptions options, int concurrencyLevel, int capacity, IEqualityComparer<TKey> comparer) : base(concurrencyLevel, capacity, comparer)
+        {
+            if (options.MaxCount < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(options), options.MaxCount, $"{nameof(options.MaxCount)} should be greater than or equal to 1.");
+            }
+
+            MaxCount = options.MaxCount;
+            if (options.PoolSize > 0)
+            {
+                PoolingEnabled = true;
+                _pool = new AsyncKeyedLockPool<TKey>(this, options.PoolSize, options.PoolInitialFill);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public AsyncKeyedLockReleaser<TKey> GetOrAdd(TKey key)
+        {
+            if (PoolingEnabled)
+            {
+                if (TryGetValue(key, out var releaser) && releaser.TryIncrement(key))
+                {
+                    return releaser;
+                }
+
+                var releaserToAdd = _pool.GetObject(key);
+                if (TryAdd(key, releaserToAdd))
+                {
+                    return releaserToAdd;
+                }
+
+                while (true)
+                {
+                    releaser = GetOrAdd(key, releaserToAdd);
+                    if (ReferenceEquals(releaser, releaserToAdd))
+                    {
+                        return releaser;
+                    }
+                    if (releaser.TryIncrement(key))
+                    {
+                        releaserToAdd.IsNotInUse = true;
+                        _pool.PutObject(releaserToAdd);
+                        return releaser;
+                    }
+                }
+            }
+
+            if (TryGetValue(key, out var releaserNoPooling) && releaserNoPooling.TryIncrementNoPooling())
+            {
+                return releaserNoPooling;
+            }
+
+            var releaserToAddNoPooling = new AsyncKeyedLockReleaser<TKey>(key, new SemaphoreSlim(MaxCount, MaxCount), this);
+            if (TryAdd(key, releaserToAddNoPooling))
+            {
+                return releaserToAddNoPooling;
+            }
+
+            while (true)
+            {
+                releaserNoPooling = GetOrAdd(key, releaserToAddNoPooling);
+                if (ReferenceEquals(releaserNoPooling, releaserToAddNoPooling) || releaserNoPooling.TryIncrementNoPooling())
+                {
+                    return releaserNoPooling;
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Release(AsyncKeyedLockReleaser<TKey> releaser)
+        {
+            Monitor.Enter(releaser);
+
+            if (releaser.ReferenceCount == 1)
+            {
+                TryRemove(releaser.Key, out _);
+                releaser.IsNotInUse = true;
+                Monitor.Exit(releaser);
+                if (PoolingEnabled)
+                {
+                    _pool.PutObject(releaser);
+                }
+                releaser.SemaphoreSlim.Release();
+                return;
+            }
+
+            --releaser.ReferenceCount;
+            Monitor.Exit(releaser);
+            releaser.SemaphoreSlim.Release();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void ReleaseWithoutSemaphoreRelease(AsyncKeyedLockReleaser<TKey> releaser)
+        {
+            Monitor.Enter(releaser);
+
+            if (releaser.ReferenceCount == 1)
+            {
+                TryRemove(releaser.Key, out _);
+                releaser.IsNotInUse = true;
+                Monitor.Exit(releaser);
+                if (PoolingEnabled)
+                {
+                    _pool.PutObject(releaser);
+                }
+                return;
+            }
+            --releaser.ReferenceCount;
+            Monitor.Exit(releaser);
+        }
+
+        public void Dispose()
+        {
+            foreach (var semaphore in Values)
+            {
+                try
+                {
+                    semaphore.Dispose();
+                }
+                catch
+                {
+                    // do nothing
+                }
+            }
+            Clear();
+            if (PoolingEnabled)
+            {
+                _pool.Dispose();
+            }
+        }
+    }
+}
