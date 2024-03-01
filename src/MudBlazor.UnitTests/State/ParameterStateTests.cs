bb0574fd98c78e5be3c2bb5a62516bc9be279b65
Artyom M <artem.melnikov@live.com>
2024-03-01 03:02:18 +0200

    WIP unit tests

--- src/MudBlazor.UnitTests/State/ParameterStateTests.cs
+++ src/MudBlazor.UnitTests/State/ParameterStateTests.cs
@@ -2,6 +2,7 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Components;
@@ -11,6 +12,7 @@ using NUnit.Framework;
 
 namespace MudBlazor.UnitTests.State;
 
+#nullable enable
 [TestFixture]
 public class ParameterStateTests
 {
@@ -61,5 +63,46 @@ public class ParameterStateTests
 
         // Assert
         parameterChangedHandlerMock.FireCount.Should().Be(1);
+    }
+
+    [Test]
+    public void HasParameterChanged_ReturnsTrueIfParameterChanged()
+    {
+        // Arrange
+        const int InitialValue = 5;
+        const int NewValue = 10;
+        var parameterName = nameof(InitialValue);
+        var parameterState = ParameterState<int>.Attach(parameterName, () => InitialValue, () => default);
+        var parametersDictionary = new Dictionary<string, object?>
+        {
+            { parameterName, NewValue }
+        };
+        var parameters = ParameterView.FromDictionary(parametersDictionary);
+
+        // Act
+        var changed = parameterState.HasParameterChanged(parameters);
+
+        // Assert
+        changed.Should().BeTrue();
+    }
+
+    [Test]
+    public void HasParameterChanged_ReturnsFalseIfParameterNotChanged()
+    {
+        // Arrange
+        const int InitialValue = 5;
+        var parameterName = nameof(InitialValue);
+        var parameterState = ParameterState<int>.Attach(parameterName, () => InitialValue, () => default);
+        var parametersDictionary = new Dictionary<string, object?>
+        {
+            { parameterName, InitialValue }
+        };
+        var parameters = ParameterView.FromDictionary(parametersDictionary);
+
+        // Act
+        var changed = parameterState.HasParameterChanged(parameters);
+
+        // Assert
+        changed.Should().BeFalse();
     }
 }
