5b3757a4c881e4b69b4f8eead488e22c1280d905
Artyom M <artem.melnikov@live.com>
2024-03-05 13:10:57 +0200

    Introduce ParameterState framework to facilitate parameter change logic and avoid BL0007 (#8258)
    Co-authored-by: peterthorpe81 <peterthorpe81@gmail.com>

---
+++ src/MudBlazor.UnitTests/State/Mocks/ParameterChangedHandlerMock.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using MudBlazor.State;
+
+namespace MudBlazor.UnitTests.State.Mocks;
+
+#nullable enable
+public class ParameterChangedHandlerMock : IParameterChangedHandler
+{
+    public int FireCount { get; private set; }
+
+    public Task HandleAsync()
+    {
+        FireCount++;
+
+        return Task.CompletedTask;
+    }
+}
