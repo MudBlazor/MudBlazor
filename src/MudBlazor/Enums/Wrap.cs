9ae1ae75b7fe898cbb623c05eadb266d428d472c
n.wallura@live-icom.com <n.wallura@live-icom.com>
2024-03-05 17:31:50 +0100

    Stack: add wrap property

---
+++ src/MudBlazor/Enums/Wrap.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazor
+{
+    public enum Wrap
+    {
+        [Description("nowrap")]
+        NoWrap,
+        [Description("wrap")]
+        Wrap,
+        [Description("wrap-reverse")]
+        WrapReverse
+    }
+}
