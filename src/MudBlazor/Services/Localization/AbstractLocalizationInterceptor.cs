f89c963a61efbefeb6993c33f31d2d2828defdb4
Artyom M <artem.melnikov@live.com>
2024-03-07 13:53:45 +0200

    Localization: Extend AbstractLocalizationInterceptor to be less specific. (#8306)

--- src/MudBlazor/Services/Localization/AbstractLocalizationInterceptor.cs
+++ src/MudBlazor/Services/Localization/AbstractLocalizationInterceptor.cs
@@ -23,17 +23,37 @@ public abstract class AbstractLocalizationInterceptor : ILocalizationInterceptor
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AbstractLocalizationInterceptor"/> class.
+    /// This creates an ResX reader for builtin <see cref="LanguageResource"/> with the default <see cref="LocalizationOptions"/>.
     /// </summary>
     /// <param name="loggerFactory">The logger factory.</param>
     /// <param name="mudLocalizer">The optional custom MudLocalizer.</param>
+    /// <remarks>
+    /// For more custom options use <see cref="AbstractLocalizationInterceptor(IStringLocalizer,MudBlazor.MudLocalizer)"/> constuctor.
+    /// </remarks>
     protected AbstractLocalizationInterceptor(ILoggerFactory loggerFactory, MudLocalizer? mudLocalizer = null)
+        : this(DefaultLanguageResourceReader(loggerFactory), mudLocalizer)
     {
-        var options = Options.Create(new LocalizationOptions());
-        var factory = new ResourceManagerStringLocalizerFactory(options, loggerFactory);
-        Localizer = factory.Create(typeof(LanguageResource));
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AbstractLocalizationInterceptor"/> class.
+    /// </summary>
+    /// <param name="localizer">The instance of <see cref="IStringLocalizer"/>.</param>
+    /// <param name="mudLocalizer">The optional custom MudLocalizer.</param>
+    protected AbstractLocalizationInterceptor(IStringLocalizer localizer, MudLocalizer? mudLocalizer = null)
+    {
+        Localizer = localizer;
         MudLocalizer = mudLocalizer;
     }
 
     /// <inheritdoc />
     public abstract LocalizedString Handle(string key);
+
+    private static IStringLocalizer DefaultLanguageResourceReader(ILoggerFactory loggerFactory)
+    {
+        var options = Options.Create(new LocalizationOptions());
+        var factory = new ResourceManagerStringLocalizerFactory(options, loggerFactory);
+
+        return factory.Create(typeof(LanguageResource));
+    }
 }
