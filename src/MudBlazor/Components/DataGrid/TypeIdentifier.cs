cf6883c5df867a9e5a633dd0289d2510bb807668
Artyom M <artem.melnikov@live.com>
2024-03-13 21:20:04 +0200

    MudDataGrid: Cleanup (#8358)

--- src/MudBlazor/Components/DataGrid/TypeIdentifier.cs
+++ src/MudBlazor/Components/DataGrid/TypeIdentifier.cs
@@ -39,15 +39,14 @@ namespace MudBlazor
             typeof(BigInteger?),
         };
 
-        internal static bool IsString(Type? type)
+        public static bool IsString(Type? type)
         {
             if (type is null)
+            {
                 return false;
+            }
 
-            if (type == typeof(string))
-                return true;
-
-            return false;
+            return type == typeof(string);
         }
 
         public static bool IsNumber(Type? type)
@@ -58,48 +57,68 @@ namespace MudBlazor
         public static bool IsEnum(Type? type)
         {
             if (type is null)
+            {
                 return false;
+            }
 
             if (type.IsEnum)
+            {
                 return true;
+            }
 
             var underlyingType = Nullable.GetUnderlyingType(type);
+
             return underlyingType is { IsEnum: true };
         }
 
         public static bool IsDateTime(Type? type)
         {
             if (type is null)
+            {
                 return false;
+            }
 
             if (type == typeof(DateTime))
+            {
                 return true;
+            }
 
             var underlyingType = Nullable.GetUnderlyingType(type);
+
             return underlyingType is not null && underlyingType == typeof(DateTime);
         }
 
         public static bool IsBoolean(Type? type)
         {
             if (type is null)
+            {
                 return false;
+            }
 
             if (type == typeof(bool))
+            {
                 return true;
+            }
 
             var underlyingType = Nullable.GetUnderlyingType(type);
+
             return underlyingType is not null && underlyingType == typeof(bool);
         }
 
         public static bool IsGuid(Type? type)
         {
             if (type is null)
+            {
                 return false;
+            }
 
             if (type == typeof(Guid))
+            {
                 return true;
+            }
 
             var underlyingType = Nullable.GetUnderlyingType(type);
+
             return underlyingType is not null && underlyingType == typeof(Guid);
         }
     }
