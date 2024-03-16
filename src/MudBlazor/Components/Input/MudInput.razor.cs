6723d16f7e9b65bb5cbc14ac68850398c62926c5
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-03-16 18:31:36 -0500

    Input: Responsive to parameters & Dispose events

--- src/MudBlazor/Components/Input/MudInput.razor.cs
+++ src/MudBlazor/Components/Input/MudInput.razor.cs
@@ -8,7 +8,7 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
-    public partial class MudInput<T> : MudBaseInput<T>
+    public partial class MudInput<T> : MudBaseInput<T>, IAsyncDisposable
     {
         protected string Classname =>
            new CssBuilder(
@@ -179,10 +179,16 @@ namespace MudBlazor
         }
 
         private string _internalText;
+        private bool _shouldInitAutoGrow;
 
         public override async Task SetParametersAsync(ParameterView parameters)
         {
+            var oldLines = Lines;
+            var oldMaxLines = MaxLines;
+            var oldAutoGrow = AutoGrow;
+
             await base.SetParametersAsync(parameters);
+
             //if (!_isFocused || _forceTextUpdate)
             //    _internalText = Text;
             if (RuntimeLocation.IsServerSide && TextUpdateSuppression)
@@ -197,6 +203,27 @@ namespace MudBlazor
                 // in WASM (or in BSS with TextUpdateSuppression==false) we always update
                 _internalText = Text;
             }
+
+            // AutoGrow:
+            if (!oldAutoGrow && AutoGrow)
+            {
+                // Enable it.
+                _shouldInitAutoGrow = true;
+            }
+            else if (oldAutoGrow && !AutoGrow)
+            {
+                // Disable it.
+                _shouldInitAutoGrow = false;
+                await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.destroy", ElementReference);
+            }
+            else if (!_shouldInitAutoGrow && (oldLines != Lines || oldMaxLines != MaxLines))
+            {
+                if (AutoGrow && !_shouldInitAutoGrow)
+                {
+                    // Update it (if it was already enabled).
+                    await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.updateParams", ElementReference, MaxLines);
+                }
+            }
         }
 
         [Inject] private IJSRuntime JsRuntime { get; set; } = null!;
@@ -205,15 +232,19 @@ namespace MudBlazor
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            if (AutoGrow && firstRender)
+            if (AutoGrow)
             {
-                await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.initAutoGrow", ElementReference, MaxLines);
-                _oldText = _internalText;
-            }
-            else if(AutoGrow && _oldText != _internalText)
-            {
-                await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.adjustHeight", ElementReference);
-                _oldText = _internalText;
+                if (firstRender || _shouldInitAutoGrow)
+                {
+                    _shouldInitAutoGrow = false;
+                    await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.initAutoGrow", ElementReference, MaxLines);
+                    _oldText = _internalText;
+                }
+                else if (_oldText != _internalText)
+                {
+                    await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.adjustHeight", ElementReference);
+                    _oldText = _internalText;
+                }
             }
 
             await base.OnAfterRenderAsync(firstRender);
@@ -235,6 +266,14 @@ namespace MudBlazor
         {
             return GetInputType() is InputType.Color or InputType.Date or InputType.DateTimeLocal or InputType.Month
                 or InputType.Time or InputType.Week;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (AutoGrow)
+            {
+                await JsRuntime.InvokeVoidAsyncWithErrorHandling("mudInputAutoGrow.destroy", ElementReference);
+            }
         }
     }
 
--- src/MudBlazor/TScripts/mudInputAutoGrow.js
+++ src/MudBlazor/TScripts/mudInputAutoGrow.js
@@ -1,21 +1,26 @@
 ﻿// Copyright (c) MudBlazor 2023
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
 window.mudInputAutoGrow = {
     initAutoGrow: (elem, maxLines) => {
         const compStyle = getComputedStyle(elem);
         const lineHeight = parseFloat(compStyle.getPropertyValue('line-height'));
-
-        let minHeight = lineHeight * elem.rows;
+        const initialOverflowY = elem.style.overflowY;
+        const initialHeight = elem.style.height;
 
         let maxHeight = 0;
-        if (maxLines > 0) {
-            // Cap the height to the number of lines specified in MudTextField.
-            maxHeight = lineHeight * maxLines;
+
+        // Update parameters that effect the functionality and visuals of the auto-growing input.
+        elem.updateParameters = function (newMaxLines) {
+            if (newMaxLines > 0) {
+                // Cap the height to the number of lines specified in the input.
+                maxHeight = lineHeight * newMaxLines;
+            } else {
+                maxHeight = 0;
+            }
         }
 
-        // Capture min and max height in closure to trigger height adjustment on element in MudTextField.
+        // Capture min and max height in closure to trigger height adjustment on element in the input.
         elem.adjustAutoGrowHeight = function () {
             // Save scroll positions https://github.com/MudBlazor/MudBlazor/issues/8152.
             const scrollTops = [];
@@ -29,6 +34,7 @@ window.mudInputAutoGrow = {
 
             elem.style.height = 0;
 
+            let minHeight = lineHeight * elem.rows;
             let newHeight = Math.max(minHeight, elem.scrollHeight);
             if (maxHeight > 0 && newHeight > maxHeight) {
                 // Content height exceeds the max height so we'll see a scrollbar.
@@ -50,22 +56,40 @@ window.mudInputAutoGrow = {
             });
         }
 
+        // Terminate the ability to auto-grow and restore the input element back to its original state.
+        elem.restoreToInitialState = function () {
+            elem.removeEventListener('input', elem.adjustAutoGrowHeight);
+            elem.style.overflowY = initialOverflowY;
+            elem.style.height = initialHeight;
+        }
+
         // Adjust height when input happens.
-        elem.addEventListener('input', () => {
-            elem.adjustAutoGrowHeight();
-        });
+        elem.addEventListener('input', elem.adjustAutoGrowHeight);
 
         // Adjust height when the window resizes.
-        window.addEventListener('resize', () => {
-            elem.adjustAutoGrowHeight();
-        });
+        window.addEventListener('resize', elem.adjustAutoGrowHeight);
 
-        // Initial height adjustment.
+        // Initial parameters and height adjustment.
+        elem.updateParameters(maxLines);
         elem.adjustAutoGrowHeight();
     },
     adjustHeight: (elem) => {
         if (typeof elem.adjustAutoGrowHeight === 'function') {
             elem.adjustAutoGrowHeight();
+        }
+    },
+    updateParams: (elem, maxLines) => {
+        if (typeof elem.updateParameters === 'function') {
+            elem.updateParameters(maxLines);
+        }
+        if (typeof elem.adjustAutoGrowHeight === 'function') {
+            elem.adjustAutoGrowHeight();
+        }
+    },
+    destroy: (elem) => {
+        window.removeEventListener('resize', elem.adjustAutoGrowHeight);
+        if (typeof elem.restoreToInitialState === 'function') {
+            elem.restoreToInitialState();
         }
     }
 }
