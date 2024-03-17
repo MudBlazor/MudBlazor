f12a85a2689b296c48f22d46f4acc6ca7a3ac62b
Meinrad Recheis <meinrad.recheis@gmail.com>
2024-03-17 13:20:04 +0100

    IntegrationTest with bUnit

---
+++ src/MudBlazor.UnitTests.Viewer/TestComponents/Utilities/AbcXyzTestComp.razor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.State;
+
+namespace MudBlazor.UnitTests;
+
+public partial class AbcXyzTestComp : MudComponentBase
+{
+    public AbcXyzTestComp() : base() {
+        // abc shared handler group
+        _a=RegisterParameter(nameof(A), ()=>A, OnAbcChanged);
+        _b=RegisterParameter(nameof(B), ()=>B, OnAbcChanged);
+        _c=RegisterParameter(nameof(C), ()=>C, OnAbcChanged);
+        // xyz shared handler group
+        _x=RegisterParameter(nameof(X), ()=>X, OnXyzChanged);
+        _y=RegisterParameter(nameof(Y), ()=>Y, OnXyzChanged);
+        _z=RegisterParameter(nameof(Z), ()=>Z, OnXyzChanged);
+    }
+
+    private ParameterState<int> _a;
+    private ParameterState<int> _b;
+    private ParameterState<int> _c;
+    private ParameterState<int> _x;
+    private ParameterState<int> _y;
+    private ParameterState<int> _z;
+
+    private void OnAbcChanged()
+    {
+        AbcHandlerCallCount++;
+    }
+
+    private void OnXyzChanged()
+    {
+        XyzHandlerCallCount++;
+    }
+
+    public int AbcHandlerCallCount { get; private set; }
+    public int XyzHandlerCallCount { get; private set; }
+    
+    [Parameter]
+    public int A { get; set; }
+
+    [Parameter]
+    public int B { get; set; }
+
+    [Parameter]
+    public int C { get; set; }
+    
+    [Parameter]
+    public int X { get; set; }
+
+    [Parameter]
+    public int Y { get; set; }
+
+    [Parameter]
+    public int Z { get; set; }
+
+}
---
+++ src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Bunit;
+using FluentAssertions;
+using MudBlazor.UnitTests.Components;
+using NUnit.Framework;
+
+namespace MudBlazor.UnitTests.State;
+
+[TestFixture]
+public class ParameterStateUsageTests : BunitTest
+{
+    [Test]
+    public void SharedHandlerIntegrationTest()
+    {
+        var comp = Context.RenderComponent<ParameterStateSharedHandlerTestComp>();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("button.abc").Click();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("button.xyz").Click();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("2");
+    }
+
+}
