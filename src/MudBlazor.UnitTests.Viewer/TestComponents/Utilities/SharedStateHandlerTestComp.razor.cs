9768b890181e7d1d9105912d79b3acaa79e5ca7e
Meinrad Recheis <meinrad.recheis@gmail.com>
2024-03-17 15:08:43 +0100

    Update the bUnit test to verify the lambda exclusion

--- src/MudBlazor.UnitTests.Viewer/TestComponents/Utilities/SharedStateHandlerTestComp.razor.cs
+++ src/MudBlazor.UnitTests.Viewer/TestComponents/Utilities/SharedStateHandlerTestComp.razor.cs
@@ -3,14 +3,17 @@ using MudBlazor.State;
 
 namespace MudBlazor.UnitTests;
 
-public partial class AbcXyzTestComp : MudComponentBase
+public partial class SharedStateHandlerTestComp : MudComponentBase
 {
-    public AbcXyzTestComp()
+    public SharedStateHandlerTestComp() : base()
     {
         // abc shared handler group
         _a = RegisterParameter(nameof(A), () => A, OnAbcChanged);
         _b = RegisterParameter(nameof(B), () => B, OnAbcChanged);
         _c = RegisterParameter(nameof(C), () => C, OnAbcChanged);
+        // o and p are not sharing their handler because lambdas are excluded, even if they contain the same code
+        _o = RegisterParameter(nameof(O), () => O, () => OpHandlerCallCount++);
+        _p = RegisterParameter(nameof(P), () => P, () => OpHandlerCallCount++);
         // xyz shared handler group
         _x = RegisterParameter(nameof(X), () => X, OnXyzChanged);
         _y = RegisterParameter(nameof(Y), () => Y, OnXyzChanged);
@@ -20,6 +23,8 @@ public partial class AbcXyzTestComp : MudComponentBase
     private ParameterState<int> _a;
     private ParameterState<int> _b;
     private ParameterState<int> _c;
+    private ParameterState<int> _o;
+    private ParameterState<int> _p;
     private ParameterState<int> _x;
     private ParameterState<int> _y;
     private ParameterState<int> _z;
@@ -35,24 +40,22 @@ public partial class AbcXyzTestComp : MudComponentBase
     }
 
     public int AbcHandlerCallCount { get; private set; }
-
+    public int OpHandlerCallCount { get; private set; }
     public int XyzHandlerCallCount { get; private set; }
 
-    [Parameter]
-    public int A { get; set; }
+    [Parameter] public int A { get; set; }
 
-    [Parameter]
-    public int B { get; set; }
+    [Parameter] public int B { get; set; }
 
-    [Parameter]
-    public int C { get; set; }
+    [Parameter] public int C { get; set; }
 
-    [Parameter]
-    public int X { get; set; }
+    [Parameter] public int O { get; set; }
 
-    [Parameter]
-    public int Y { get; set; }
+    [Parameter] public int P { get; set; }
 
-    [Parameter]
-    public int Z { get; set; }
+    [Parameter] public int X { get; set; }
+
+    [Parameter] public int Y { get; set; }
+
+    [Parameter] public int Z { get; set; }
 }
--- src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
+++ src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
@@ -13,14 +13,23 @@ public class ParameterStateUsageTests : BunitTest
     {
         var comp = Context.RenderComponent<ParameterStateSharedHandlerTestComp>();
 
+        // note: the handler for abc and the one for xyz are each called once per click
+        // the handlers for o and p are lambdas which are excluded from this optimization so they
+        // are each called per click resulting in an increment of 2 per click for op 
         comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("2");
         comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("1");
         comp.Find("button.abc").Click();
         comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("2");
         comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("1");
         comp.Find("button.xyz").Click();
         comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("button.op").Click();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("4");
         comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("2");
     }
-
 }
