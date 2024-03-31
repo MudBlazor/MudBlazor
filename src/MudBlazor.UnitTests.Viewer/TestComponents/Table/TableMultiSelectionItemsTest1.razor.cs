df3c9531544013a98d7649af541b8a4d89fe14aa
Román Ezequiel Alvarez <ralvarez.goyeneche@gmail.com>
2024-03-30 21:20:53 -0300

    fix order

--- src/MudBlazor.UnitTests.Viewer/TestComponents/Table/TableMultiSelectionItemsTest1.razor.cs
+++ src/MudBlazor.UnitTests.Viewer/TestComponents/Table/TableMultiSelectionItemsTest1.razor.cs
@@ -2,10 +2,10 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using Microsoft.AspNetCore.Components;
 
 namespace MudBlazor.UnitTests.TestComponents
 {
