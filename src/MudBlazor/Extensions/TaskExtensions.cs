2d427e16776aeb947995b780d6e46b2dff343e60
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-04-01 10:25:05 -0500

    Remove obsolete Extensions, Services, Utilities (#8531)
    Co-authored-by: Mike Surcouf <mike@surcouf.je>

--- src/MudBlazor/Extensions/TaskExtensions.cs
+++ src/MudBlazor/Extensions/TaskExtensions.cs
@@ -1,22 +1,16 @@
-﻿using System;
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Threading.Tasks;
 
 namespace MudBlazor
 {
 #nullable enable
 #pragma warning disable CS1998
-    [Obsolete("This will be removed in v7.")]
-    public enum TaskOption
-    {
-        None,
-        Safe
-    }
-
     public static class TaskExtensions
     {
-        [Obsolete("Use the bool parameter version. This will be removed in v7.")]
-        public static void AndForget(this Task task, TaskOption option) => AndForget(task);
-
         /// <summary>
         /// Task will be awaited and exceptions will be forwarded to MudBlazorGlobal.UnhandledExceptionHandler.
         /// </summary>
@@ -29,12 +23,11 @@ namespace MudBlazor
             catch (Exception ex)
             {
                 if (!ignoreExceptions)
+                {
                     MudGlobal.UnhandledExceptionHandler?.Invoke(ex);
+                }
             }
         }
-
-        [Obsolete("Use the bool parameter version. This will be removed in v7.")]
-        public static async void AndForget(this ValueTask task, TaskOption option) => AndForget(task);
 
         /// <summary>
         /// ValueTask will be awaited and exceptions will be forwarded to MudBlazorGlobal.UnhandledExceptionHandler.
@@ -48,12 +41,11 @@ namespace MudBlazor
             catch (Exception ex)
             {
                 if (!ignoreExceptions)
+                {
                     MudGlobal.UnhandledExceptionHandler?.Invoke(ex);
+                }
             }
         }
-
-        [Obsolete("Use the bool parameter version. This will be removed in v7.")]
-        public static async void AndForget<T>(this ValueTask<T> task, TaskOption option) => AndForget(task, option);
 
         /// <summary>
         /// ValueTask(bool) will be awaited and exceptions will be forwarded to MudBlazorGlobal.UnhandledExceptionHandler.
@@ -67,7 +59,9 @@ namespace MudBlazor
             catch (Exception ex)
             {
                 if (!ignoreExceptions)
+                {
                     MudGlobal.UnhandledExceptionHandler?.Invoke(ex);
+                }
             }
         }
     }
