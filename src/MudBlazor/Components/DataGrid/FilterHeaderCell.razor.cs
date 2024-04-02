130bf9e53e378973a5d87454698a646ef79e43ef
andrewMa <andrew.ma@tuvsud.com>
2024-04-02 09:18:58 +0100

    Testing tjscience's changes

--- src/MudBlazor/Components/DataGrid/FilterHeaderCell.razor.cs
+++ src/MudBlazor/Components/DataGrid/FilterHeaderCell.razor.cs
@@ -18,7 +18,6 @@ namespace MudBlazor
 
         [Parameter] public Column<T> Column { get; set; }
         [Parameter] public RenderFragment ChildContent { get; set; }
-        [Parameter] public bool IsReordered { get; set; }
 
         private string _classname =>
             new CssBuilder(Column?.HeaderClass)
@@ -33,13 +32,6 @@ namespace MudBlazor
                 .AddStyle(Style)
                 .Build();
 
-        private string _valueString;
-        private double? _valueNumber;
-        private Enum _valueEnum = null;
-        private bool? _valueBool;
-        private DateTime? _valueDate;
-        private TimeSpan? _valueTime;
-
         #region Computed Properties and Functions
 
         private Type dataType
@@ -50,6 +42,8 @@ namespace MudBlazor
             }
         }
 
+        private FieldType fieldType => FieldType.Identify(dataType);
+
         private string[] operators
         {
             get
@@ -58,119 +52,124 @@ namespace MudBlazor
             }
         }
 
-        private IParameterState<bool> _isReorderedState;
-
-        private string _operator;
+        private string valueString => fieldType.IsString && Column.FilterContext.FilterDefinition.Value is not null ? (string)Column.FilterContext.FilterDefinition.Value : default;
+        private double? valueNumber => fieldType.IsNumber ? (double?)Column.FilterContext.FilterDefinition.Value : default;
+        private bool? valueBool => fieldType.IsBoolean && Column.FilterContext.FilterDefinition.Value is not null ? (bool?)Column.FilterContext.FilterDefinition.Value : default;
+        private Enum valueEnum => fieldType.IsEnum && Column.FilterContext.FilterDefinition.Value is not null ? (Enum)Column.FilterContext.FilterDefinition.Value : default;
+        private DateTime? valueDate => fieldType.IsDateTime ? (DateTime?)Column.FilterContext.FilterDefinition.Value : default;
+        private TimeSpan? valueTime => fieldType.IsDateTime && Column.FilterContext.FilterDefinition.Value is not null ? ((DateTime?)Column.FilterContext.FilterDefinition.Value).Value.TimeOfDay : null;
+        private string @operator => Column.FilterContext.FilterDefinition.Operator ?? operators.FirstOrDefault();
 
         public FilterHeaderCell()
         {
             // Update _operator if underlying column had changed position
-            _isReorderedState = RegisterParameter(
-                nameof(IsReordered),
-                () => IsReordered,
-                IsReorderedChangedHandlerAsync
-            );
+            //_columnState = RegisterParameter(
+            //    nameof(Column),
+            //    () => Column,
+            //    IsColumnChangedHandlerAsync
+            //);
         }
 
-        private async Task IsReorderedChangedHandlerAsync(ParameterChangedEventArgs<bool> args)
-        {
-            if (args.Value)
-            {
-                _operator = Column.FilterContext.FilterDefinition.Operator;
-                await _isReorderedState.SetValueAsync(false);
-            }
-        }
+        //private async Task IsColumnChangedHandlerAsync(ParameterChangedEventArgs<Column<T>> args)
+        //{
+        //    if (Column.FilterContext.FilterDefinition.Operator != _operator)
+        //    {
+        //        _operator = Column.FilterContext.FilterDefinition.Operator;
+        //        await _columnState.SetValueAsync(args.Value);
+        //        StateHasChanged();
+        //    }
+        //}
 
         private string chosenOperatorStyle(string o)
         {
-            return o == _operator ? "color:var(--mud-palette-primary-text);background-color:var(--mud-palette-primary)" : "";
+            return o == @operator ? "color:var(--mud-palette-primary-text);background-color:var(--mud-palette-primary)" : "";
         }
 
-        private bool isNumber
-        {
-            get
-            {
-                return TypeIdentifier.IsNumber(dataType);
-            }
-        }
+        //private bool isNumber
+        //{
+        //    get
+        //    {
+        //        return TypeIdentifier.IsNumber(dataType);
+        //    }
+        //}
 
-        private bool isEnum
-        {
-            get
-            {
-                return TypeIdentifier.IsEnum(dataType);
-            }
-        }
+        //private bool isEnum
+        //{
+        //    get
+        //    {
+        //        return TypeIdentifier.IsEnum(dataType);
+        //    }
+        //}
 
         #endregion
 
-        protected override void OnInitialized()
-        {
-            _operator = operators.FirstOrDefault();
-        }
+        //protected override void OnInitialized()
+        //{
+        //    _operator = operators.FirstOrDefault();
+        //}
 
         #region Events
 
         private async Task ChangeOperatorAsync(string o)
         {
-            _operator = o;
-            Column.FilterContext.FilterDefinition.Operator = _operator;
+            //_operator = o;
+            Column.FilterContext.FilterDefinition.Operator = o;
             await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
         }
 
         internal async Task StringValueChangedAsync(string value)
         {
-            _valueString = value;
-            Column.FilterContext.FilterDefinition.Operator = _operator;
+            //_valueString = value;
+            //Column.FilterContext.FilterDefinition.Operator = _operator;
             Column.FilterContext.FilterDefinition.Value = value;
             await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
         }
 
         internal async Task NumberValueChangedAsync(double? value)
         {
-            _valueNumber = value;
-            Column.FilterContext.FilterDefinition.Operator = _operator;
+            //_valueNumber = value;
+            //Column.FilterContext.FilterDefinition.Operator = _operator;
             Column.FilterContext.FilterDefinition.Value = value;
             await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
         }
 
         internal async Task EnumValueChangedAsync(Enum value)
         {
-            _valueEnum = value;
-            Column.FilterContext.FilterDefinition.Operator = _operator;
+            //_valueEnum = value;
+            //Column.FilterContext.FilterDefinition.Operator = _operator;
             Column.FilterContext.FilterDefinition.Value = value;
             await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
         }
 
         internal async Task BoolValueChangedAsync(bool? value)
         {
-            _valueBool = value;
-            Column.FilterContext.FilterDefinition.Operator = _operator;
+            //_valueBool = value;
+            //Column.FilterContext.FilterDefinition.Operator = _operator;
             Column.FilterContext.FilterDefinition.Value = value;
             await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
         }
 
         internal async Task DateValueChangedAsync(DateTime? value)
         {
-            _valueDate = value;
+            //_valueDate = value;
 
             if (value != null)
             {
                 var date = value.Value.Date;
 
                 // get the time component and add it to the date.
-                if (_valueTime != null)
+                if (valueTime != null)
                 {
-                    date.Add(_valueTime.Value);
+                    date.Add(valueTime.Value);
                 }
 
-                Column.FilterContext.FilterDefinition.Operator = _operator;
+                //Column.FilterContext.FilterDefinition.Operator = _operator;
                 Column.FilterContext.FilterDefinition.Value = date;
                 await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
             }
             else
             {
-                Column.FilterContext.FilterDefinition.Operator = _operator;
+                //Column.FilterContext.FilterDefinition.Operator = _operator;
                 Column.FilterContext.FilterDefinition.Value = value;
                 await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
             }
@@ -178,19 +177,19 @@ namespace MudBlazor
 
         internal async Task TimeValueChangedAsync(TimeSpan? value)
         {
-            _valueTime = value;
+            //_valueTime = value;
 
-            if (_valueDate != null)
+            if (valueDate != null)
             {
-                var date = _valueDate.Value.Date;
+                var date = valueDate.Value.Date;
 
                 // get the time component and add it to the date.
-                if (_valueTime != null)
+                if (valueTime != null)
                 {
-                    date = date.Add(_valueTime.Value);
+                    date = date.Add(valueTime.Value);
                 }
 
-                Column.FilterContext.FilterDefinition.Operator = _operator;
+                //Column.FilterContext.FilterDefinition.Operator = _operator;
                 Column.FilterContext.FilterDefinition.Value = date;
                 await ApplyFilterAsync(Column.FilterContext.FilterDefinition);
             }
@@ -210,26 +209,26 @@ namespace MudBlazor
         {
             await ClearFilterAsync(Column.FilterContext.FilterDefinition);
 
-            if (dataType == typeof(string))
-                _valueString = null;
-            else if (isNumber)
-                _valueNumber = null;
-            else if (isEnum)
-                _valueEnum = null;
-            else if (dataType == typeof(bool))
-                _valueBool = null;
-            else if (dataType == typeof(DateTime) || dataType == typeof(DateTime?))
-            {
-                _valueDate = null;
-                _valueTime = null;
-            }
+            //if (dataType == typeof(string))
+            //_valueString = null;
+            //if (isNumber)
+            //    _valueNumber = null;
+            //else if (isEnum)
+            //    _valueEnum = null;
+            //else if (dataType == typeof(bool))
+            //    _valueBool = null;
+            //else if (dataType == typeof(DateTime) || dataType == typeof(DateTime?))
+            //{
+            //    _valueDate = null;
+            //    _valueTime = null;
+            //}
         }
 
         internal async Task ClearFilterAsync(IFilterDefinition<T> filterDefinition)
         {
             await DataGrid.RemoveFilterAsync(filterDefinition.Id);
         }
-    
+
         #endregion
     }
 }
