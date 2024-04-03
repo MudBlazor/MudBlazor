591a61d32a211a9f00b8a24016ef9aed94088c2a
Artyom M <artem.melnikov@live.com>
2024-04-03 15:07:52 +0300

    Replace reflection with simple substring

--- src/MudBlazor/Extensions/MudComponentExtensions.cs
+++ src/MudBlazor/Extensions/MudComponentExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 using MudBlazor.State;
 
 namespace MudBlazor.Extensions;
@@ -19,12 +18,15 @@ internal static class MudComponentExtensions
     /// <typeparam name="T">The type of the parameter.</typeparam>
     /// <param name="component">The MudBlazor component instance.</param>
     /// <param name="propertyExpression">An expression representing the property whose parameter state needs to be accessed.</param>
-    /// <returns>The read-only parameter state of the specified property.</returns>
+    /// <param name="propertyNameCallerArgumentExpression">The property name. Do not set this value as it's set at compile-time through <see cref="CallerArgumentExpressionAttribute"/>.</param>
+    /// <returns>The <see cref="IParameterState{T}.Value"/> of the specified property.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="propertyExpression"/> is null.</exception>
     /// <exception cref="ArgumentException">Thrown when <paramref name="propertyExpression"/> does not represent a property.</exception>
-    public static T? GetSate<TComponent, T>(this TComponent component, Expression<Func<TComponent, T>> propertyExpression) where TComponent : MudComponentBase
+    /// <exception cref="KeyNotFoundException">Thrown when the parameter state with <paramref name="propertyExpression"/> is not found.</exception>
+    public static T? GetSate<TComponent, T>(this TComponent component, Func<TComponent, T> propertyExpression, [CallerArgumentExpression(nameof(propertyExpression))] string? propertyNameCallerArgumentExpression = null) where TComponent : MudComponentBase
     {
-        var propertyName = GetPropertyName(propertyExpression);
+        ArgumentNullException.ThrowIfNull(propertyExpression);
+        var propertyName = GetPropertyFromFuncLambda(propertyNameCallerArgumentExpression);
 
         return GetSate<TComponent, T>(component, propertyName);
     }
@@ -35,8 +37,8 @@ internal static class MudComponentExtensions
     /// <typeparam name="TComponent">The type of the MudBlazor component.</typeparam>
     /// <typeparam name="T">The type of the parameter.</typeparam>
     /// <param name="component">The MudBlazor component instance.</param>
-    /// <param name="propertyName">The name of the property whose parameter state needs to be accessed.</param>
-    /// <returns>The read-only parameter state of the specified property.</returns>
+    /// <param name="propertyName">The name of the property whose parameter state needs to be accessed. Use nameof(...) to get the property name.</param>
+    /// <returns>The <see cref="IParameterState{T}.Value"/> of the specified property.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the parameter state with <paramref name="propertyName"/> is not found.</exception>
     public static T? GetSate<TComponent, T>(this TComponent component, string propertyName) where TComponent : MudComponentBase
     {
@@ -51,20 +53,19 @@ internal static class MudComponentExtensions
         throw new KeyNotFoundException($"ParameterState<{typeof(T).Name}> with {propertyName} was not found!");
     }
 
-    private static string GetPropertyName<TComponent, T>(Expression<Func<TComponent, T>> propertyExpression) where TComponent : MudComponentBase
+    private static string GetPropertyFromFuncLambda(string? propertyNameExpression)
     {
-        ArgumentNullException.ThrowIfNull(nameof(propertyExpression));
+        ArgumentNullException.ThrowIfNull(propertyNameExpression);
 
-        if (propertyExpression.Body is not MemberExpression body)
+        var lastDotIndex = propertyNameExpression.LastIndexOf('.');
+
+        if (lastDotIndex != -1)
         {
-            throw new ArgumentException(@"Invalid argument", nameof(propertyExpression));
+            var propertyName = propertyNameExpression[(lastDotIndex + 1)..];
+
+            return propertyName[(lastDotIndex + 1)..];
         }
 
-        if (body.Member is not PropertyInfo property)
-        {
-            throw new ArgumentException(@"Argument is not a property", nameof(propertyExpression));
-        }
-
-        return property.Name;
+        throw new ArgumentException("Invalid property expression ()!");
     }
 }
