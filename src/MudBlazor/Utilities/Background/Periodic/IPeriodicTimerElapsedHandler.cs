c09e1118ba40165b32c469483d011764ffdd6ab1
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-04-05 17:17:00 -0500

    order

--- src/MudBlazor/Utilities/Background/Periodic/IPeriodicTimerElapsedHandler.cs
+++ src/MudBlazor/Utilities/Background/Periodic/IPeriodicTimerElapsedHandler.cs
@@ -2,8 +2,8 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MudBlazor.Utilities.Background.Periodic;
 
