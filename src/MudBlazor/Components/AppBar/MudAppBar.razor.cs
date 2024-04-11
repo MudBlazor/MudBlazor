516eaa93f0184e20acc2d03746bc5d16da0f51eb
Jon Person <person.jon@gmail.com>
2024-04-10 21:03:40 -0600

    MudAppBar: Added <c> tags to colorize literals in docs.  Removed docs on protected members

--- src/MudBlazor/Components/AppBar/MudAppBar.razor.cs
+++ src/MudBlazor/Components/AppBar/MudAppBar.razor.cs
@@ -12,12 +12,6 @@ namespace MudBlazor
     public partial class MudAppBar : MudComponentBase
     {
 #nullable enable
-        /// <summary>
-        /// Gets the CSS classes to apply to this component.
-        /// </summary>
-        /// <remarks>
-        /// The set of classes returned depends on the values of the <see cref="Dense"/>, <see cref="Fixed"/>, <see cref="Bottom"/>, <see cref="Elevation"/>, and <see cref="Color"/> properties.
-        /// </remarks>
         protected string Classname =>
             new CssBuilder("mud-appbar")
                 .AddClass($"mud-appbar-dense", Dense)
@@ -28,9 +22,6 @@ namespace MudBlazor
                 .AddClass(Class)
                 .Build();
 
-        /// <summary>
-        /// Gets the CSS class to apply to the toolbar.
-        /// </summary>
         protected string ToolBarClassname =>
             new CssBuilder("mud-toolbar-appbar")
                 .AddClass(ToolBarClass)
@@ -47,7 +38,7 @@ namespace MudBlazor
         /// Gets or sets the size of the drop shadow.
         /// </summary>
         /// <remarks>
-        /// Defaults to 4.  A higher number creates a heavier drop shadow.  Use a value of 0 for no shadow.
+        /// Defaults to <c>4</c>.  A higher number creates a heavier drop shadow.  Use a value of <c>0</c> for no shadow.
         /// </remarks>
         [Parameter]
         [Category(CategoryTypes.AppBar.Appearance)]
@@ -57,7 +48,7 @@ namespace MudBlazor
         /// Gets or sets a value indicating whether compact padding will be used.
         /// </summary>
         /// <remarks>
-        /// Defaults to false.
+        /// Defaults to <c>false</c>.
         /// </remarks>
         [Parameter]
         [Category(CategoryTypes.AppBar.Appearance)]
@@ -67,7 +58,7 @@ namespace MudBlazor
         /// Gets or sets a value indicating whether left and right padding is added to the appbar.
         /// </summary>
         /// <remarks>
-        /// Defaults to true.
+        /// Defaults to <c>true</c>.
         /// </remarks>
         [Parameter]
         [Category(CategoryTypes.AppBar.Appearance)]
@@ -87,7 +78,7 @@ namespace MudBlazor
         /// Gets or sets a value indicating whether the appbar remains in the same place when the page is scrolled.
         /// </summary>
         /// <remarks>
-        /// Defaults to true.
+        /// Defaults to <c>true</c>.
         /// </remarks>
         [Parameter]
         [Category(CategoryTypes.AppBar.Behavior)]
@@ -97,7 +88,7 @@ namespace MudBlazor
         /// Gets or sets a value indicating whether the appbar content is allowed to wrap.
         /// </summary>
         /// <remarks>
-        /// Defaults to false.
+        /// Defaults to <c>false</c>.
         /// </remarks>
         [Parameter]
         [Category(CategoryTypes.AppBar.Behavior)]
@@ -107,7 +98,7 @@ namespace MudBlazor
         /// Gets or sets CSS classes applied to the nested toolbar.
         /// </summary>
         /// <remarks>
-        /// Defaults to null.  Use spaces to separate multiple classes.
+        /// Defaults to <c>null</c>.  You can use spaces to separate multiple classes.
         /// </remarks>
         [Parameter]
         [Category(CategoryTypes.AppBar.Appearance)]
