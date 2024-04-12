2b37591ee77f7d81cd93956c6476e7897fc19fb4
Artyom M <artem.melnikov@live.com>
2024-04-12 14:46:17 +0300

    Fix formatting

--- src/MudBlazor.UnitTests/Components/TreeViewTest.cs
+++ src/MudBlazor.UnitTests/Components/TreeViewTest.cs
@@ -149,31 +149,30 @@ namespace MudBlazor.UnitTests.Components
             comp.FindAll("div.mud-treeview-item-content")[2].Click();
             comp.FindAll("li.mud-treeview-item").Count.Should().Be(8);
         }
-
-//        [Test]
-//        public async Task TreeView_OtherTest()
-//        {
-//            var comp = Context.RenderComponent<TreeViewTest2>();
-//            var item = comp.FindComponent<MudTreeViewItem<string>>();
 #pragma warning disable BL0005
-//            await comp.InvokeAsync(() => item.Instance.Disabled = true);
-//            comp.WaitForAssertion(() => item.Instance.Selected.Should().BeFalse());
-//            await comp.InvokeAsync(() => item.Instance.Selected = true);
-//            comp.WaitForAssertion(() => item.Instance.Selected.Should().BeTrue());
-//            await comp.InvokeAsync(() => item.Instance.SelectItem(true));
-//            comp.WaitForAssertion(() => item.Instance.Selected.Should().BeTrue());
-//            comp.WaitForAssertion(() => item.Instance.ArrowExpanded.Should().BeFalse());
-//            await comp.InvokeAsync(() => item.Instance.ArrowExpanded = true);
-//            comp.WaitForAssertion(() => item.Instance.ArrowExpanded.Should().BeTrue());
-//            await comp.InvokeAsync(() => item.Instance.ArrowExpanded = true);
-//            comp.WaitForAssertion(() => item.Instance.ArrowExpanded.Should().BeTrue());
+        //        [Test]
+        //        public async Task TreeView_OtherTest()
+        //        {
+        //            var comp = Context.RenderComponent<TreeViewTest2>();
+        //            var item = comp.FindComponent<MudTreeViewItem<string>>();
+        //            await comp.InvokeAsync(() => item.Instance.Disabled = true);
+        //            comp.WaitForAssertion(() => item.Instance.Selected.Should().BeFalse());
+        //            await comp.InvokeAsync(() => item.Instance.Selected = true);
+        //            comp.WaitForAssertion(() => item.Instance.Selected.Should().BeTrue());
+        //            await comp.InvokeAsync(() => item.Instance.SelectItem(true));
+        //            comp.WaitForAssertion(() => item.Instance.Selected.Should().BeTrue());
+        //            comp.WaitForAssertion(() => item.Instance.ArrowExpanded.Should().BeFalse());
+        //            await comp.InvokeAsync(() => item.Instance.ArrowExpanded = true);
+        //            comp.WaitForAssertion(() => item.Instance.ArrowExpanded.Should().BeTrue());
+        //            await comp.InvokeAsync(() => item.Instance.ArrowExpanded = true);
+        //            comp.WaitForAssertion(() => item.Instance.ArrowExpanded.Should().BeTrue());
 
-//            comp.WaitForAssertion(() => item.Instance.Expanded.Should().BeTrue());
-//            await comp.InvokeAsync(() => item.Instance.OnItemExpanded(true));
-//            comp.WaitForAssertion(() => item.Instance.Expanded.Should().BeTrue());
+        //            comp.WaitForAssertion(() => item.Instance.Expanded.Should().BeTrue());
+        //            await comp.InvokeAsync(() => item.Instance.OnItemExpanded(true));
+        //            comp.WaitForAssertion(() => item.Instance.Expanded.Should().BeTrue());
 
-//            await comp.InvokeAsync(() => item.Instance.Select(false));
-//        }
+        //            await comp.InvokeAsync(() => item.Instance.Select(false));
+        //        }
 
         [Test]
         public async Task TreeViewItem_DoubleClick_CheckExpanded()
