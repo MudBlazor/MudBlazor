0c89ce930bd8f3f66ec19cdbb199a32b57a1fabe
Meinrad Recheis <meinrad.recheis@gmail.com>
2024-04-27 17:49:13 +0200

    ChartOptions: rename DisableLegend to ShowLegend

--- src/MudBlazor/Components/Chart/Models/ChartOptions.cs
+++ src/MudBlazor/Components/Chart/Models/ChartOptions.cs
@@ -17,9 +17,9 @@
         public bool XAxisLines { get; set; }
 
         /// <summary>
-        /// If true, legend will not be displayed.
+        /// If false, legend will not be displayed.
         /// </summary>
-        public bool DisableLegend { get; set; }
+        public bool ShowLegend { get; set; } = true;
         public string[] ChartPalette { get; set; } = { Colors.Blue.Accent3, Colors.Teal.Accent3, Colors.Amber.Accent3, Colors.Orange.Accent3, Colors.Red.Accent3, Colors.DeepPurple.Accent3, Colors.Green.Accent3, Colors.LightBlue.Accent3, Colors.Teal.Lighten1, Colors.Amber.Lighten1, Colors.Orange.Lighten1, Colors.Red.Lighten1, Colors.DeepPurple.Lighten1, Colors.Green.Lighten1, Colors.LightBlue.Lighten1, Colors.Amber.Darken2, Colors.Orange.Darken2, Colors.Red.Darken2, Colors.DeepPurple.Darken2, Colors.Gray.Darken2 };
         public InterpolationOption InterpolationOption { get; set; } = InterpolationOption.Straight;
 
