655d2e071032a0ce97149f3efa3077589fd9ecf9
Artyom M <artem.melnikov@live.com>
2024-05-08 23:09:04 +0300

    MudTable: Add nullable annotation. (#8926)

--- src/MudBlazor/Components/Table/MudSortableColumn.razor.cs
+++ src/MudBlazor/Components/Table/MudSortableColumn.razor.cs
@@ -4,12 +4,16 @@ using Microsoft.AspNetCore.Components;
 
 namespace MudBlazor
 {
+#nullable enable
     /// <summary>
     /// Binds an object's property to a column by its property name 
     /// </summary>
     public partial class MudSortableColumn<T, ModelType> : MudBaseColumn
     {
-        T InternalValue
+        private T? _footerValue;
+        private bool _footerValueAvailable;
+
+        private T? InternalValue
         {
             get => Value;
             set
@@ -25,49 +29,54 @@ namespace MudBlazor
         /// <summary>
         /// Specifies the name of the object's property bound to the column
         /// </summary>
-        [Parameter] public T Value { get; set; }
-        [Parameter] public EventCallback<T> ValueChanged { get; set; }
+        [Parameter]
+        public T? Value { get; set; }
+
+        [Parameter]
+        public EventCallback<T> ValueChanged { get; set; }
 
         /// <summary>
         /// Specifies the name of the object's property bound to the footer
         /// </summary>
         [Parameter]
-        public T FooterValue
+        public T? FooterValue
         {
             get { return _footerValue; }
             set { _footerValue = value; _footerValueAvailable = true; }
         }
-        private T _footerValue;
-        private bool _footerValueAvailable = false;
 
         /// <summary>
         /// Used if no FooterValue is available
         /// </summary>
-        [Parameter] public string FooterText { get; set; }
+        [Parameter]
+        public string? FooterText { get; set; }
 
         /// <summary>
         /// Specifies which string format should be used.
         /// </summary>
-        [Parameter] public string DataFormatString { get; set; }
+        [Parameter]
+        public string? DataFormatString { get; set; }
 
         /// <summary>
         /// Specifies if the column should be readonly even if the DataTable is in editmode.
         /// </summary>
-        [Parameter] public bool ReadOnly { get; set; }
-        [Parameter] public string SortLabel { get; set; }
+        [Parameter]
+        public bool ReadOnly { get; set; }
 
-        [Parameter] public Func<ModelType, object> SortBy { get; set; } = default;
+        [Parameter]
+        public string? SortLabel { get; set; }
 
-        private string GetFormattedString(T item)
+        [Parameter]
+        public Func<ModelType, object>? SortBy { get; set; }
+
+        private string? GetFormattedString(T? item)
         {
             if (DataFormatString != null)
             {
                 return string.Format(DataFormatString, item);
             }
-            else
-            {
-                return item?.ToString();
-            }
+
+            return item?.ToString();
         }
     }
 }
--- src/MudBlazor/Components/Table/MudTd.razor.cs
+++ src/MudBlazor/Components/Table/MudTd.razor.cs
@@ -3,22 +3,25 @@ using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
+#nullable enable
     public partial class MudTd : MudComponentBase
     {
-
         protected string Classname =>
-        new CssBuilder("mud-table-cell")
-            .AddClass("mud-table-cell-hide", HideSmall)
-            .AddClass(Class)
-        .Build();
+            new CssBuilder("mud-table-cell")
+                .AddClass("mud-table-cell-hide", HideSmall)
+                .AddClass(Class)
+                .Build();
 
-        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
 
-        [Parameter] public string DataLabel { get; set; }
+        [Parameter]
+        public string? DataLabel { get; set; }
 
         /// <summary>
         /// Hide cell when breakpoint is smaller than the defined value in table.
         /// </summary>
-        [Parameter] public bool HideSmall { get; set; }
+        [Parameter]
+        public bool HideSmall { get; set; }
     }
 }
