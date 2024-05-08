b69d7c52f3d62b668bd52752701f350c82a471ee
PALIZ-DEVOPS\ajr <ajr@palizct.com>
2024-05-08 10:52:20 +0330

    Removed not used using;

--- src/MudBlazor/Components/DatePicker/MudDatePicker.cs
+++ src/MudBlazor/Components/DatePicker/MudDatePicker.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Extensions;
 using MudBlazor.Utilities;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MudBlazor
 {
