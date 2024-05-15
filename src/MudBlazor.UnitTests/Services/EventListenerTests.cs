8d81a024d15b4fc2ded630fda3be1f04cd90404e
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-05-15 14:37:26 -0500

    revert

--- src/MudBlazor.UnitTests/Services/EventListenerTests.cs
+++ src/MudBlazor.UnitTests/Services/EventListenerTests.cs
@@ -26,10 +26,17 @@ namespace MudBlazor.UnitTests.Services
         {
             _runtimeMock = new Mock<IJSRuntime>(MockBehavior.Strict);
             _service = new EventListener(_runtimeMock.Object);
-            _expectedProperties = [
+#if NET7_0_OR_GREATER
+            _expectedProperties = new[] {
              "detail", "screenX", "screenY", "clientX", "clientY", "offsetX", "offsetY", "pageX", "pageY",
              "movementX", "movementY", "button", "buttons", "ctrlKey", "shiftKey", "altKey", "metaKey", "type"
-            ];
+            };
+#else
+            _expectedProperties  = new[] {
+            "detail", "screenX", "screenY", "clientX", "clientY", "offsetX", "offsetY", "pageX", "pageY",
+            "button", "buttons", "ctrlKey", "shiftKey", "altKey", "metaKey", "type"
+            };
+#endif
         }
 
         private bool ContainsEqual(IEnumerable<string> firstColl, IEnumerable<string> secondColl)
@@ -255,7 +262,7 @@ namespace MudBlazor.UnitTests.Services
             // a second time shouldn't change something
             await _service.DisposeAsync();
 
-            // a normal dispose shouldn't change something
+            // a normal dispose shouldnt' change something
             _service.Dispose();
 
             _runtimeMock.Verify(x => x.InvokeAsync<IJSVoidResult>("mudThrottledEventManager.subscribe", It.Is<object[]>(z =>
