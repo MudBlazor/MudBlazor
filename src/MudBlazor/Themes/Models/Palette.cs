1df0e9a760886413ef08bda9cd422fb9180001dd
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-05-16 22:54:57 -0500

    Add file headers

--- src/MudBlazor/Themes/Models/Palette.cs
+++ src/MudBlazor/Themes/Models/Palette.cs
@@ -1,4 +1,8 @@
-﻿using MudBlazor.Utilities;
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using MudBlazor.Utilities;
 
 namespace MudBlazor
 {
