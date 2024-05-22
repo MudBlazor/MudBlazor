ac13ba0b26bc6f72efe14b3e31f43d91e97ac41e
BenMcLean <mclean.ben@gmail.com>
2024-05-22 14:58:49 -0500

    RGBA uint format support for Utilities.MudColor

--- src/MudBlazor/Utilities/MudColor.cs
+++ src/MudBlazor/Utilities/MudColor.cs
@@ -50,6 +50,21 @@ namespace MudBlazor.Utilities
         [JsonIgnore]
         public string Value => $"#{R:x2}{G:x2}{B:x2}{A:x2}";
 
+        [JsonIgnore]
+        public uint UInt32
+        {
+            get => (uint)(R << 24 | G << 16 | B << 8 | A);
+            set
+            {
+                _valuesAsByte[0] = (byte)(value >> 24);
+                _valuesAsByte[1] = (byte)(value >> 16);
+                _valuesAsByte[2] = (byte)(value >> 8);
+                _valuesAsByte[3] = (byte)value;
+            }
+        }
+
+        public static explicit operator uint(MudColor mudColor) => mudColor.UInt32;
+
         /// <summary>
         /// Gets the red component value of the color.
         /// </summary>
@@ -220,6 +235,12 @@ namespace MudBlazor.Utilities
             S = s;
             L = l;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MudColor"/> class with the specified color.
+        /// </summary>
+        /// <param name="rgba">the four bytes of this 32-bit unsigned integer contain the red, green, blue and alpha components</param>
+        public MudColor(uint rgba) : this(r: (byte)(rgba >> 24), g: (byte)(rgba >> 16), b: (byte)(rgba >> 8), a: (byte)rgba) { }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MudColor"/> class with the specified red, green, blue, and alpha values, copying the hue value from the provided color.
