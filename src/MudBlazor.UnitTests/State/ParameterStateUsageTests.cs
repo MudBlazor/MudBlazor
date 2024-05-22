91608800d8833bd887147ba63e8ee4be9cc814d9
Artyom M <artem.melnikov@live.com>
2024-05-22 16:20:42 +0300

    Finish integration tests

--- src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
+++ src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
@@ -58,6 +58,31 @@ public class ParameterStateUsageTests : BunitTest
     }
 
     [Test]
+    public void InheritanceIntegrationTest()
+    {
+        var comp = Context.RenderComponent<ParameterStateSharedInheritanceHandlerTestComp>();
+
+        // note: the handler for abc and the one for xyz are each called once per click
+        // the handlers for o and p are lambdas which are excluded from this optimization, so they
+        // are each called per click resulting in an increment of 2 per click for op 
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("button.abc").Click();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("1");
+        comp.Find("button.xyz").Click();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("button.op").Click();
+        comp.Find("span.abc").InnerHtml.Trimmed().Should().Be("2");
+        comp.Find("span.op").InnerHtml.Trimmed().Should().Be("4");
+        comp.Find("span.xyz").InnerHtml.Trimmed().Should().Be("2");
+    }
+
+    [Test]
     public void EventArgsIntegrationTest()
     {
         var comp = Context.RenderComponent<ParameterStateEventArgsTestComp>();
