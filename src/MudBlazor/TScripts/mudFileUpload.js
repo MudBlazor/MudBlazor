7e3add2eef5a27a469d9e676b402e895abd8f9cc
Jason Rebelo Neves <igotinfected@gmail.com>
2024-06-10 18:23:40 +0200

    Disable UserActivation checks for Safari 16.0 -> 16.4

--- src/MudBlazor/TScripts/mudFileUpload.js
+++ src/MudBlazor/TScripts/mudFileUpload.js
@@ -1,3 +1,5 @@
+// noinspection JSUnusedGlobalSymbols
+
 class MudFileUpload {
     openFilePicker (id) {
         const element = document.getElementById(id);
@@ -6,14 +8,15 @@ class MudFileUpload {
             return;
         }
 
-        // checking for user activation because Safari won't do anything without it
-        if (!navigator.userActivation.isActive)
-        {
-            return;
-        }
-
         try {
-            // showPicker is more reliable than click() and works in Safari
+            // only supported starting with Safari 16.4+
+            // // checking for user activation because browsers won't execute showPicker() without it
+            // if (!navigator.userActivation.isActive)
+            // {
+            //     return;
+            // }
+
+            // more reliable than click() and works in Safari
             element.showPicker();
         } catch {
             // fallback
