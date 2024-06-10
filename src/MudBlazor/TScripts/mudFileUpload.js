1077fd8719543c1b0f8c0117397a45cb3e472d72
Jason Rebelo Neves <igotinfected@gmail.com>
2024-06-10 18:31:58 +0200

    Fix eslint error

--- src/MudBlazor/TScripts/mudFileUpload.js
+++ src/MudBlazor/TScripts/mudFileUpload.js
@@ -18,7 +18,7 @@ class MudFileUpload {
 
             // more reliable than click() and works in Safari
             element.showPicker();
-        } catch {
+        } catch (error) {
             // fallback
             element.click();
         }
