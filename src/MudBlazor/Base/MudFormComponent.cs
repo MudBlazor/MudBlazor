832a4615933df6a5c99f63d7e9e8a9407910cb36
Laszlo Lieszkovszky <Laszlo.Mana@Lieszkovszky.com>
2024-06-14 13:39:46 +0200

    Add handling for validation expressions that refer to non-nullable properties (#8931)

--- src/MudBlazor/Base/MudFormComponent.cs
+++ src/MudBlazor/Base/MudFormComponent.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.SqlTypes;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
