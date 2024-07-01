988f96b7272d9c1833b1c35d9a9433bc7c4fb4cb
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-07-01 02:33:42 -0500

    Docs: Tweaks to API Documentation (#9279)

--- src/MudBlazor.Docs/Services/ApiLink/IApiLinkService.cs
+++ src/MudBlazor.Docs/Services/ApiLink/IApiLinkService.cs
@@ -7,7 +7,18 @@ namespace MudBlazor.Docs.Services;
 #nullable enable
 public interface IApiLinkService
 {
+    /// <summary>
+    /// Registers a new page to the search index for a component.
+    /// </summary>
+    /// <param name="title">The title of the page</param>
+    /// <param name="subtitle">The subtitle of the page</param>
+    /// <param name="componentType">The type of the component</param>
+    /// <param name="link">The link to the page</param>
     void RegisterPage(string title, string? subtitle, Type? componentType, string? link);
 
+    /// <summary>
+    /// Returns the search results for the specified text using a fuzzy algorithm.
+    /// </summary>
+    /// <param name="text">The search query</param>
     Task<IReadOnlyCollection<ApiLinkServiceEntry>> Search(string text);
 }
