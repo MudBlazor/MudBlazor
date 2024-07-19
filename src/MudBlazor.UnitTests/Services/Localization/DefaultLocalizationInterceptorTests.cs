809c9f2b2822e89808f4f8391c35f6d51fda5728
Samuel Meenzen <samuel@meenzen.net>
2024-07-19 21:15:48 +0200

    Localization: Correctly handle templates (#9438) (#9451)
    Co-authored-by: Artyom M <artem.melnikov@live.com>

--- src/MudBlazor.UnitTests/Services/Localization/DefaultLocalizationInterceptorTests.cs
+++ src/MudBlazor.UnitTests/Services/Localization/DefaultLocalizationInterceptorTests.cs
@@ -16,7 +16,7 @@ namespace MudBlazor.UnitTests.Services.Localization;
 public class DefaultLocalizationInterceptorTests
 {
     [Test]
-    public void IgnoreDefaultEnglishTrue_NullMudLocalizer_ReturnsDefaultEnglish()
+    public void NullMudLocalizer_IgnoreDefaultEnglishTrue_ReturnsDefaultEnglish()
     {
         // Even though IgnoreDefaultEnglish is "true", we still should get default English localization
         // Because we didn't provide custom MudLocalizer, so it will fall back.
@@ -44,7 +44,7 @@ public class DefaultLocalizationInterceptorTests
 
     [Test]
     [SetUICulture("en-US")]
-    public void IgnoreDefaultEnglishFalse_CustomMudLocalizer_EnglishUICulture_ReturnsDefaultEnglish()
+    public void CustomMudLocalizer_IgnoreDefaultEnglishFalse_EnglishUICulture_ReturnsDefaultEnglish()
     {
         // When IgnoreDefaultEnglish is "false" and CurrentUICulture is "en", we return default English localization
         // Even when custom MudLocalizer is provided
@@ -67,7 +67,7 @@ public class DefaultLocalizationInterceptorTests
 
     [Test]
     [SetUICulture("fr-FR")]
-    public void IgnoreDefaultEnglishFalse_CustomMudLocalizer_NonEnglishUICulture_ReturnsDefaultEnglish()
+    public void CustomMudLocalizer_IgnoreDefaultEnglishFalse_NonEnglishUICulture_ReturnsCustomLocalization()
     {
         // When IgnoreDefaultEnglish is "false" and CurrentUICulture is NOT "en", we return localization provided from custom MudLocalizer
 
@@ -85,5 +85,114 @@ public class DefaultLocalizationInterceptorTests
 
         // Assert
         result.Value.Should().Be("XXX", "The UICulture is not English therefore the value is the one from the Mock.");
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    [SetUICulture("fr-FR")]
+    public void CustomMudLocalizer_IgnoreDefaultEnglishFalse_TemplateTranslation_ReturnsFormattedString(bool ignoreDefaultEnglish)
+    {
+        // Regardless of the IgnoreDefaultEnglish setting, the custom localization should be returned.
+        // This is because custom MudLocalizer was provided, and we have an existing key.
+
+        // Arrange
+        var mudLocalizer = new Mock<MudLocalizer> { CallBase = true };
+        mudLocalizer.Setup(mock => mock["TemplateString"]).Returns(new LocalizedString("TemplateString", "Bonjour {0}!", resourceNotFound: false));
+        var defaultLocalizationIInterceptor = new DefaultLocalizationInterceptor(NullLoggerFactory.Instance, mudLocalizer.Object)
+        {
+            IgnoreDefaultEnglish = ignoreDefaultEnglish
+        };
+        var internalMudLocalizer = new InternalMudLocalizer(defaultLocalizationIInterceptor);
+
+        // Act
+        var result = internalMudLocalizer["TemplateString", "le monde"];
+
+        // Assert
+        result.Value.Should().Be("Bonjour le monde!", "The value should be the template string with the provided parameter.");
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    [SetUICulture("en-US")]
+    public void CustomMudLocalizer_DefaultLocalizationFallback_TemplateTranslation_ReturnsFormattedString(bool ignoreDefaultEnglish)
+    {
+        // Regardless of the IgnoreDefaultEnglish setting, the default English localization should be returned.
+        // Although a custom MudLocalizer is provided, the key does not exist, resulting in resourceNotFound being "true".
+        // Consequently, this causes a fallback to the default English localization.
+
+        // Arrange
+        var mudLocalizer = new Mock<MudLocalizer> { CallBase = true };
+        var defaultLocalizationIInterceptor = new DefaultLocalizationInterceptor(NullLoggerFactory.Instance, mudLocalizer.Object)
+        {
+            IgnoreDefaultEnglish = ignoreDefaultEnglish
+        };
+        var internalMudLocalizer = new InternalMudLocalizer(defaultLocalizationIInterceptor);
+
+        // Act
+
+        var result = internalMudLocalizer[LanguageResource.MudBaseDatePicker_PrevMonth, "2024"];
+
+        // Assert
+        var expectedValue = GetResourceString(LanguageResource.MudBaseDatePicker_PrevMonth, "2024");
+        result.Value.Should().Be(expectedValue);
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    [SetUICulture("en-US")]
+    public void CustomMudLocalizer_DefaultLocalizationFallback_KeyTranslation_ReturnsString(bool ignoreDefaultEnglish)
+    {
+        // Regardless of the IgnoreDefaultEnglish setting, the default English localization should be returned.
+        // Although a custom MudLocalizer is provided, the key does not exist, resulting in resourceNotFound being "true".
+        // Consequently, this causes a fallback to the default English localization.
+
+        // Arrange
+        var mudLocalizer = new Mock<MudLocalizer> { CallBase = true };
+        var defaultLocalizationIInterceptor = new DefaultLocalizationInterceptor(NullLoggerFactory.Instance, mudLocalizer.Object)
+        {
+            IgnoreDefaultEnglish = ignoreDefaultEnglish
+        };
+        var internalMudLocalizer = new InternalMudLocalizer(defaultLocalizationIInterceptor);
+
+        // Act
+        var result = internalMudLocalizer[LanguageResource.MudColorPicker_ModeSwitch];
+
+        // Assert
+        var expectedValue = GetResourceString(LanguageResource.MudColorPicker_ModeSwitch);
+        result.Value.Should().Be(expectedValue);
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    [SetUICulture("en-US")]
+    public void NullMudLocalizer_DefaultLocalizationFallback_TemplateTranslation_ReturnsFormattedString(bool ignoreDefaultEnglish)
+    {
+        // Regardless of the IgnoreDefaultEnglish setting, the default English localization should be returned.
+        // This is because no custom MudLocalizer was provided, causing a fallback to the default English localization even when IgnoreDefaultEnglish is "true".
+
+        // Arrange
+        var defaultLocalizationIInterceptor = new DefaultLocalizationInterceptor(NullLoggerFactory.Instance, mudLocalizer: null)
+        {
+            IgnoreDefaultEnglish = ignoreDefaultEnglish
+        };
+        var internalMudLocalizer = new InternalMudLocalizer(defaultLocalizationIInterceptor);
+
+        // Act
+        var result = internalMudLocalizer[LanguageResource.MudBaseDatePicker_PrevMonth, "2024"];
+
+        // Assert
+        var expectedValue = GetResourceString(LanguageResource.MudBaseDatePicker_PrevMonth, "2024");
+        result.Value.Should().Be(expectedValue, "The value should be the template string with the provided parameter.");
+    }
+
+    private static string GetResourceString(string key, params object[] parameters)
+    {
+        var resourceString = LanguageResource.GetResourceString(key) ?? string.Empty;
+
+        return string.Format(CultureInfo.CurrentUICulture, resourceString, parameters);
     }
 }
