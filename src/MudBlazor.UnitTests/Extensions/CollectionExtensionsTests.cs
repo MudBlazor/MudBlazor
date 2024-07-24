f6d88b65a068b4707d740cdf944abf0a770917b9
Justin Lampe <xC0dex@users.noreply.github.com>
2024-07-24 19:25:27 +0200

    refactor: dotnet format

--- src/MudBlazor.UnitTests/Extensions/CollectionExtensionsTests.cs
+++ src/MudBlazor.UnitTests/Extensions/CollectionExtensionsTests.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using NUnit.Framework;
 using FluentAssertions;
+using NUnit.Framework;
 
 namespace MudBlazor.UnitTests;
 
