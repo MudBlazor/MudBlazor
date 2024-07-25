5764017aad653feb1169bbae3d2c6182f53734fb
Artyom M <artem.melnikov@live.com>
2024-07-26 02:55:46 +0300

    Clean usings

--- src/MudBlazor/Extensions/EnumExtensions.cs
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Reflection;
 
 namespace MudBlazor.Extensions
 {
