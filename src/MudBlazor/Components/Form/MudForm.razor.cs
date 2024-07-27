abfe1d711a3cc83f987f1015d8c7f2d65bc22939
Meinrad Recheis <meinrad.recheis@gmail.com>
2024-07-27 17:37:37 +0200

    Update MudForm.razor.cs

--- src/MudBlazor/Components/Form/MudForm.razor.cs
+++ src/MudBlazor/Components/Form/MudForm.razor.cs
@@ -10,7 +10,8 @@ using MudBlazor.Utilities;
 namespace MudBlazor
 {
     /// <summary>
-    /// A component for collecting and validating input data.
+    /// A component for collecting and validating user input. Every MudFormComponent 
+    // withing it is monitored and validated.
     /// </summary>
     public partial class MudForm : MudComponentBase, IDisposable, IForm
     {
