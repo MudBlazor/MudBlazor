9c0bfcea57ca08e392ea8c5504580be46565fea8
Román Ezequiel Alvarez <ralvarez.goyeneche@gmail.com>
2024-07-28 01:31:06 -0300

    change margins

--- src/MudBlazor/Components/Field/MudField.razor.cs
+++ src/MudBlazor/Components/Field/MudField.razor.cs
@@ -43,6 +43,7 @@ namespace MudBlazor
 
         protected string InputControlClassname =>
             new CssBuilder("mud-field")
+                .AddClass($"mud-input-control-with-label", !string.IsNullOrEmpty(Label))
                 .AddClass(Class)
                 .Build();
 
