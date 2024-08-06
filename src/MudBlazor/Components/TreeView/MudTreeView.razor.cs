a76434a26be35e6a063e70da8de1e8dc65552a82
Julien Rousselle <rousselle@trasis.com>
2024-08-06 10:07:32 +0200

    MudTreeView: Allow for parent auto selection configuration

--- src/MudBlazor/Components/TreeView/MudTreeView.razor.cs
+++ src/MudBlazor/Components/TreeView/MudTreeView.razor.cs
@@ -106,6 +106,14 @@ namespace MudBlazor
         public bool TriState { get; set; } = true;
 
         /// <summary>
+        /// If true, selecting all children will result in the parent being automatically selected.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.TreeView.Selecting)]
+        public bool AutoSelectParent { get; set; } = true;
+
+        /// <summary>
+        /// <summary>
         /// If true, clicking anywhere on the item will expand it, if it has children.
         /// </summary>
         [Parameter]
@@ -358,7 +366,10 @@ namespace MudBlazor
                         _selection.Add(item.GetValue()!);
                     }
                 }
-                UpdateParentItem(clickedItem.Parent);
+                if (AutoSelectParent)
+                {
+                    UpdateParentItem(clickedItem.Parent);
+                }
                 await _selectedValuesState.SetValueAsync(_selection.ToList()); // note: .ToList() is essential here!
                 await UpdateItemsAsync();
                 return;
