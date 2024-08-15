45e88497b12454bc714c09149b3985cdef2f25a1
raphael.renaud <raphael.renaud@streamlinedata.ch>
2024-08-15 03:34:14 +0200

    Remove duplicated description in StringBuilderCache

--- src/MudBlazor/Utilities/StringBuilderCache.cs
+++ src/MudBlazor/Utilities/StringBuilderCache.cs
@@ -31,36 +31,6 @@
 **            cache and return the resulting string
 **
 ===========================================================*/
-/*============================================================
-**
-** Class:  StringBuilderCache
-**
-** Purpose: provide a cached reusable instance of stringbuilder
-**          per thread  it's an optimisation that reduces the 
-**          number of instances constructed and collected.
-**
-**  Acquire - is used to get a string builder to use of a 
-**            particular size.  It can be called any number of 
-**            times, if a stringbuilder is in the cache then
-**            it will be returned and the cache emptied.
-**            subsequent calls will return a new stringbuilder.
-**
-**            A StringBuilder instance is cached in 
-**            Thread Local Storage and so there is one per thread
-**
-**  Release - Place the specified builder in the cache if it is 
-**            not too big.
-**            The stringbuilder should not be used after it has 
-**            been released.
-**            Unbalanced Releases are perfectly acceptable.  It
-**            will merely cause the runtime to create a new 
-**            stringbuilder next time Acquire is called.
-**
-**  GetStringAndRelease
-**          - ToString() the stringbuilder, Release it to the 
-**            cache and return the resulting string
-**
-===========================================================*/
 
 namespace System.Text
 {
