aa335f0e77b802430bc8d33c3de908e460220359
Artyom M <artem.melnikov@live.com>
2024-08-20 21:15:33 +0300

    Move SnackbarStateExtensions to it's own file

--- src/MudBlazor/Components/Snackbar/SnackbarState.cs
+++ src/MudBlazor/Components/Snackbar/SnackbarState.cs
@@ -1,20 +1,12 @@
 ﻿// Copyright (c) A//Copyright(c) Alessandro Ghidini.All rights reserved.
 //Changes and improvements Copyright (c) The MudBlazor Team.
 
-namespace MudBlazor
+namespace MudBlazor;
+#nullable enable
+internal enum SnackbarState
 {
-    internal enum SnackbarState
-    {
-        Init,
-        Showing,
-        Hiding,
-        Visible
-    }
-
-    internal static class SnackbarStateExtensions
-    {
-        public static bool IsShowing(this SnackbarState state) => state == SnackbarState.Showing;
-        public static bool IsVisible(this SnackbarState state) => state == SnackbarState.Visible;
-        public static bool IsHiding(this SnackbarState state) => state == SnackbarState.Hiding;
-    }
+    Init,
+    Showing,
+    Hiding,
+    Visible
 }
