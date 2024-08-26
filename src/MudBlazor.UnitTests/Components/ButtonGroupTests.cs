69b43f7d7995891969939dc08683efd4517cb195
Cedric VERNOU <cedric.vernou@hotmail.com>
2024-08-27 00:23:09 +0200

    Add tests

---
+++ src/MudBlazor.UnitTests/Components/ButtonGroupTests.cs
@@ -0,0 +1,38 @@
+﻿using Bunit;
+using FluentAssertions;
+using MudBlazor.UnitTests.TestComponents.ButtonGroup;
+using NUnit.Framework;
+using static Bunit.ComponentParameterFactory;
+
+namespace MudBlazor.UnitTests.Components
+{
+    [TestFixture]
+    public class ButtonGroupTests : BunitTest
+    {
+        [Test]
+        public void WithFullWidth_ThenAllButtonsStreched()
+        {
+            // Arrange
+
+            var comp = Context.RenderComponent<ButtonGroupFullWidth>();
+
+            // Assert
+
+            comp.FindAll(".mud-button-group-root.mud-width-full").Count.Should().Be(1);
+            comp.FindAll(".mud-button-root.mud-width-full").Count.Should().Be(3);
+        }
+
+        [Test]
+        public void WithFullWidthAndHasOneButtonFullWidth_ThenOtherButtonsNotStreched()
+        {
+            // Arrange
+
+            var comp = Context.RenderComponent<ButtonGroupFullWidthWithButtonFullWidth>();
+
+            // Assert
+
+            comp.FindAll(".mud-button-group-root.mud-width-full").Count.Should().Be(1);
+            comp.FindAll(".mud-button-root.mud-width-full").Count.Should().Be(1);
+        }
+    }
+}
