6584a21e371befcab926ecad7744e7c1446067d5
Artyom M. <artem.melnikov@live.com>
2024-09-05 11:28:30 +0300

    Snackbar: Revert ReaderWriterLockSlim  dispose in SnackbarService

--- src/MudBlazor.UnitTests/Components/SnackbarTests.cs
+++ src/MudBlazor.UnitTests/Components/SnackbarTests.cs
@@ -313,6 +313,15 @@ namespace MudBlazor.UnitTests.Components
         }
 
         [Test]
+        public async Task DisposeTest1()
+        {
+            await _provider.InvokeAsync(() =>  _service.Add("Boom, big reveal. Im a pickle!"));
+            _service.Dispose();
+
+            _service.ShownSnackbars.Count().Should().Be(0);
+        }
+
+        [Test]
         public async Task TestSnackBarRemoveByKey()
         {
             const string TestText = "Boom, big reveal. Im a pickle!";
