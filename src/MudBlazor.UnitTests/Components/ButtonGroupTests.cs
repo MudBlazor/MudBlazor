52c288624c8d3863846e09f02c38f72e8187f54c
Cedric VERNOU <cedric.vernou@hotmail.com>
2024-09-18 00:56:55 +0200

    Add test to reproduce the bug

--- src/MudBlazor.UnitTests/Components/ButtonGroupTests.cs
+++ src/MudBlazor.UnitTests/Components/ButtonGroupTests.cs
@@ -37,5 +37,29 @@ namespace MudBlazor.UnitTests.Components
             buttonComps[1].ClassList.Should().NotContain("mud-width-full");
             buttonComps[2].ClassList.Should().NotContain("mud-width-full");
         }
+
+        [Test]
+        public void WhenButtonWithFullWidthIsRemoved_ThenOtherButtonsAreStreched()
+        {
+            // Arrange
+
+            var comp = Context.RenderComponent<ButtonGroupWithThreeButtons>(
+                parameters => parameters
+                .Add(c => c.ButtonGroupFullWidth, true)
+                .Add(c => c.Button1FullWidth, true)
+            );
+
+            // Act
+
+            comp.SetParam(c => c.Button1Displayed, false);
+
+            // Assert
+
+            comp.FindAll(".mud-button-group-root.mud-width-full").Count.Should().Be(1);
+            var buttonComps = comp.FindAll(".mud-button-root");
+            buttonComps.Count.Should().Be(2);
+            buttonComps[0].ClassList.Should().Contain("mud-width-full");
+            buttonComps[1].ClassList.Should().Contain("mud-width-full");
+        }
     }
 }
