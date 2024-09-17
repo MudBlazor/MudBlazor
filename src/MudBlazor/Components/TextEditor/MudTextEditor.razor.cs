6300db9849232e439dba5f194dd7da5a4400a788
kwise990 <105219438+kwise990@users.noreply.github.com>
2024-09-17 11:18:00 -0400

    added a texteditor

---
+++ src/MudBlazor/Components/TextEditor/MudTextEditor.razor.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor.Interop;
+using MudBlazor.Utilities;
+
+namespace MudBlazor
+{
+    public partial class MudTextEditor : MudBaseInput<string>
+    {
+        [Inject] protected IJSRuntime jsRuntime { get; set; } = default!;
+        [Parameter]
+        public RenderFragment EditorContent { get; set; }
+
+        [Parameter]
+        public RenderFragment ToolbarContent { get; set; }
+
+        //[Parameter]
+        //public bool ReadOnly { get; set; }
+        //    = false;
+
+        //[Parameter]
+        //public string Placeholder { get; set; }
+        //    = "Compose an epic...";
+
+        protected string Classname =>
+           new CssBuilder("mud-input-control mud-input-input-control")
+               .AddClass($"mud-input-{Variant.ToDescriptionString()}-with-label", !string.IsNullOrEmpty(Label))
+               .AddClass(Class)
+               .Build();
+
+        protected string InputClassname =>
+           new CssBuilder(
+               MudInputCssHelper.GetClassname(this,
+                   () =>  !string.IsNullOrEmpty(Text) || Adornment == Adornment.Start || !string.IsNullOrWhiteSpace(Placeholder) || ShrinkLabel))
+            .Build();
+
+
+        [Parameter]
+        public string Theme { get; set; }
+            = "snow";
+
+        [Parameter]
+        public string[] Formats { get; set; }
+            = null;
+
+        [Parameter]
+        public string DebugLevel { get; set; }
+            = "info";
+
+        /// <summary>
+        /// Support for normal css classes
+        /// </summary>
+        [Parameter]
+        public string EditorCssClass { get; set; }
+            = string.Empty;
+
+        /// <summary>
+        /// Support for normal css styles
+        /// </summary>
+        [Parameter]
+        public string EditorCssStyle { get; set; }
+            = string.Empty;
+
+        /// <summary>
+        /// Support for normal css classes
+        /// </summary>
+        [Parameter]
+        public string ToolbarCSSClass { get; set; }
+            = string.Empty;
+
+        /// <summary>
+        /// Support for normal css styles
+        /// </summary>
+        [Parameter]
+        public string ToolbarCssStyle { get; set; }
+            = string.Empty;
+
+        [Parameter]
+        public bool BottomToolbar { get; set; }
+
+        private ElementReference QuillElement;
+        private ElementReference ToolBar;
+        
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await TextEditorInterop.CreateQuill(
+                    jsRuntime,
+                    QuillElement,
+                    ToolBar,
+                    ReadOnly,
+                    Placeholder,
+                    Theme,
+                    Formats,
+                    DebugLevel);
+            }
+        }
+
+        public async Task<string> GetText()
+        {
+            return await TextEditorInterop.GetText(
+                jsRuntime, QuillElement);
+        }
+
+        public async Task<string> GetHTML()
+        {
+            return await TextEditorInterop.GetHTML(
+                jsRuntime, QuillElement);
+        }
+
+        public async Task<string> GetContent()
+        {
+            return await TextEditorInterop.GetContent(
+                jsRuntime, QuillElement);
+        }
+
+        public async Task LoadContent(string Content)
+        {
+            var QuillDelta =
+                await TextEditorInterop.LoadQuillContent(
+                    jsRuntime, QuillElement, Content);
+        }
+
+        public async Task LoadHTMLContent(string quillHTMLContent)
+        {
+            var QuillDelta =
+                await TextEditorInterop.LoadQuillHTMLContent(
+                    jsRuntime, QuillElement, quillHTMLContent);
+        }
+
+        public async Task InsertImage(string ImageURL)
+        {
+            var QuillDelta =
+                await TextEditorInterop.InsertQuillImage(
+                    jsRuntime, QuillElement, ImageURL);
+        }
+
+        public async Task InsertText(string text)
+        {
+            var QuillDelta =
+                await TextEditorInterop.InsertQuillText(
+                    jsRuntime, QuillElement, text);
+        }
+
+        public async Task EnableEditor(bool mode)
+        {
+            var QuillDelta =
+                await TextEditorInterop.EnableQuillEditor(
+                    jsRuntime, QuillElement, mode);
+        }
+
+    }
+}
---
+++ src/MudBlazor/Interop/TextEditorInterop.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MudBlazor.Interop
+{
+    public static class TextEditorInterop
+    {
+        
+        internal static ValueTask<object> CreateQuill(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement,
+            ElementReference toolbar,
+            bool readOnly,
+            string placeholder,
+            string theme,
+            string[] formats,
+            string debugLevel)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "QuillFunctions.createQuill",
+                quillElement, toolbar, readOnly,
+                placeholder, theme, formats, debugLevel);
+        }
+
+        internal static ValueTask<string> GetText(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement)
+        {
+            return jsRuntime.InvokeAsync<string>(
+                "QuillFunctions.getQuillText",
+                quillElement);
+        }
+
+        internal static ValueTask<string> GetHTML(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement)
+        {
+            return jsRuntime.InvokeAsync<string>(
+                "QuillFunctions.getQuillHTML",
+                quillElement);
+        }
+
+        internal static ValueTask<string> GetContent(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement)
+        {
+            return jsRuntime.InvokeAsync<string>(
+                "QuillFunctions.getQuillContent",
+                quillElement);
+        }
+
+        internal static ValueTask<object> LoadQuillContent(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement,
+            string Content)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "QuillFunctions.loadQuillContent",
+                quillElement, Content);
+        }
+
+        internal static ValueTask<object> LoadQuillHTMLContent(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement,
+            string quillHTMLContent)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "QuillFunctions.loadQuillHTMLContent",
+                quillElement, quillHTMLContent);
+        }
+
+        internal static ValueTask<object> EnableQuillEditor(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement,
+            bool mode)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "QuillFunctions.enableQuillEditor",
+                quillElement, mode);
+        }
+
+        internal static ValueTask<object> InsertQuillImage(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement,
+            string imageURL)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "QuillFunctions.insertQuillImage",
+                quillElement, imageURL);
+        }
+
+        internal static ValueTask<object> InsertQuillText(
+            IJSRuntime jsRuntime,
+            ElementReference quillElement,
+            string text)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "QuillFunctions.insertQuillText",
+                quillElement, text);
+        }
+    }
+}
+
