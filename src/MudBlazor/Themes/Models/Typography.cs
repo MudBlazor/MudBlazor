ec7ad24ab3a4f93923ca30fb1240fac79136f5c2
Artyom M <artem.melnikov@live.com>
2024-09-19 17:14:21 +0300

    Typography: Fix cannot resolve symbol in the xmldoc

--- src/MudBlazor/Themes/Models/Typography.cs
+++ src/MudBlazor/Themes/Models/Typography.cs
@@ -10,7 +10,7 @@
         /// Gets or sets the typography settings for the default typo.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Default.Default()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Default"/> constructor.
         /// </remarks>
         public Default Default { get; set; } = new();
 
@@ -18,7 +18,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.h1"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="H1.H1()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.H1"/> constructor.
         /// </remarks>
         public H1 H1 { get; set; } = new();
 
@@ -26,7 +26,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.h2"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="H2.H2()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.H2"/> constructor.
         /// </remarks>
         public H2 H2 { get; set; } = new();
 
@@ -34,7 +34,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.h3"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="H3.H3()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.H3"/> constructor.
         /// </remarks>
         public H3 H3 { get; set; } = new();
 
@@ -42,7 +42,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.h4"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="H4.H4()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.H4"/> constructor.
         /// </remarks>
         public H4 H4 { get; set; } = new();
 
@@ -50,7 +50,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.h5"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="H5.H5()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.H5"/> constructor.
         /// </remarks>
         public H5 H5 { get; set; } = new();
 
@@ -58,7 +58,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.h6"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="H6.H6()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.H6"/> constructor.
         /// </remarks>
         public H6 H6 { get; set; } = new();
 
@@ -66,7 +66,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.subtitle1"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Subtitle1.Subtitle1()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Subtitle1"/> constructor.
         /// </remarks>
         public Subtitle1 Subtitle1 { get; set; } = new();
 
@@ -74,7 +74,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.subtitle2"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Subtitle2.Subtitle2()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Subtitle2"/> constructor.
         /// </remarks>
         public Subtitle2 Subtitle2 { get; set; } = new();
 
@@ -82,7 +82,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.body1"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Body1.Body1()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Body1"/> constructor.
         /// </remarks>
         public Body1 Body1 { get; set; } = new();
 
@@ -90,7 +90,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.body2"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Body2.Body2()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Body2"/> constructor.
         /// </remarks>
         public Body2 Body2 { get; set; } = new();
 
@@ -98,7 +98,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.input"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Input.Input()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Input"/> constructor.
         /// </remarks>
         public Input Input { get; set; } = new();
 
@@ -106,7 +106,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.button"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Button.Button()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Button"/> constructor.
         /// </remarks>
         public Button Button { get; set; } = new();
 
@@ -114,7 +114,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.caption"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Caption.Caption()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Caption"/> constructor.
         /// </remarks>
         public Caption Caption { get; set; } = new();
 
@@ -122,7 +122,7 @@
         /// Gets or sets the typography settings for <see cref="Typo.overline"/>.
         /// </summary>
         /// <remarks>
-        /// Defaults to the values from the <see cref="Overline.Overline()"/> constructor.
+        /// Defaults to the values from the <see cref="MudBlazor.Overline"/> constructor.
         /// </remarks>
         public Overline Overline { get; set; } = new();
     }
