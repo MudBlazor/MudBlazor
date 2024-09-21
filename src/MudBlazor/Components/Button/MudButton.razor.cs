722c500cc0e4a0ff31f76634b568fa006460ce09
Artyom M. <artem.melnikov@live.com>
2024-09-21 14:19:17 +0300

    MudButton: Fix sonar warning about Dispose implementation (#9844)

--- src/MudBlazor/Components/Button/MudButton.razor.cs
+++ src/MudBlazor/Components/Button/MudButton.razor.cs
@@ -143,12 +143,22 @@ namespace MudBlazor
             ButtonGroup?.AddButton(this);
         }
 
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
         /// <summary>
         /// Releases resources used by this button.
         /// </summary>
-        public virtual void Dispose()
+        protected virtual void Dispose(bool disposing)
         {
-            ButtonGroup?.RemoveButton(this);
+            if (disposing)
+            {
+                ButtonGroup?.RemoveButton(this);
+            }
         }
 
         internal bool GetRealFullWith()
