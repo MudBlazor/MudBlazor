40a62177cdd8789a1c29b493e26ce53f9e84548f
Jakob Sorgeloos <sorgeloosjakob@gmail.com>
2024-10-09 20:46:22 +0200

    dotnet format

--- src/MudBlazor.UnitTests/Components/Charts/LineChartTests.cs
+++ src/MudBlazor.UnitTests/Components/Charts/LineChartTests.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) MudBlazor 2021
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-using MudBlazor.Charts;
 using AngleSharp.Dom;
 using Bunit;
 using FluentAssertions;
+using MudBlazor.Charts;
 using MudBlazor.UnitTests.Components;
 using NUnit.Framework;
 
