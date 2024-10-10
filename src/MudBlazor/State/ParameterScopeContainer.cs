07368755d10db59d4f159278223e6e4c78859cf0
Artyom M. <artem.melnikov@live.com>
2024-10-10 18:51:58 +0300

    ParameterState: Small optimization & using cleanup (#9955)

--- src/MudBlazor/State/ParameterScopeContainer.cs
+++ src/MudBlazor/State/ParameterScopeContainer.cs
@@ -2,13 +2,9 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections;
 using System.Collections.Frozen;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.State.Comparer;
 
@@ -117,7 +113,7 @@ internal class ParameterScopeContainer : IParameterScopeContainer
     {
         var parametersHandlerShouldFire = _parameters.Value.Values
             .Where(parameter => parameter.HasHandler && parameter.HasParameterChanged(parameters))
-            .ToFrozenSet(ParameterHandlerUniquenessComparer.Default);
+            .ToHashSet(ParameterHandlerUniquenessComparer.Default);
 
         await baseSetParametersAsync(parameters);
 
