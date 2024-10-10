16509743712ea3ba1cd47d011a339aaeb329bcbe
Artyom M <artem.melnikov@live.com>
2024-10-10 18:06:25 +0300

    Remove old comment

--- src/MudBlazor/Components/Table/EditButtonContext.cs
+++ src/MudBlazor/Components/Table/EditButtonContext.cs
@@ -2,7 +2,7 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace MudBlazor; // A bug in Blazor requires a different namespace in some scenarios, see: https://github.com/dotnet/aspnetcore/issues/36326 (fixed in .NET 7)
+namespace MudBlazor;
 
 #nullable enable
 /// <summary>
