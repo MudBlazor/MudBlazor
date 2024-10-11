MudBlazor/MudBlazor

commit 589f4b3b5f9440a03671ae34aa5028866076f2df

MudTabs: Add nullable annotation (#9889)

src/MudBlazor/Components/Tabs/MudTabPanel.razor
@@ -17,12 +17,15 @@ else
 }
 
 @code {
-
+#nullable enable
     private Boolean _disposed;
 
-    [CascadingParameter] private MudTabs Parent { get; set; }
+    [CascadingParameter] private MudTabs? Parent { get; set; }
 
-    public ElementReference PanelRef;
+    /// <summary>
+    /// Reference to the underlying panel element.
+    /// </summary>
+    public ElementReference? PanelRef;
 
 
     /// <summary>
@@ -31,14 +34,14 @@ else
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public string Text { get; set; }
+    public string? Text { get; set; }
 
     /// <summary>
     /// Icon placed before the text if set.
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public string Icon { get; set; }
+    public string? Icon { get; set; }
 
     /// <summary>
     /// The color of the <see cref="Icon" />.
@@ -69,7 +72,7 @@ else
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public object BadgeData { get; set; }
+    public object? BadgeData { get; set; }
 
     /// <summary>
     /// Optional information to show the badge as a dot.
@@ -90,7 +93,7 @@ else
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public object ID { get; set; }
+    public object? ID { get; set; }
 
     /// <summary>
     /// Raised when tab is clicked
@@ -102,14 +105,14 @@ else
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public RenderFragment ChildContent { get; set; }
+    public RenderFragment? ChildContent { get; set; }
 
     /// <summary>
     /// Tab content of component.
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public RenderFragment TabContent { get; set; }
+    public RenderFragment? TabContent { get; set; }
 
     /// <summary>
     /// Tab content wrapper of component. It is used to wrap the content of a tab heading in a user supplied div or component. 
@@ -118,24 +121,26 @@ else
     /// </summary>
     [Parameter] 
     [Category(CategoryTypes.Tabs.Behavior)]
-    public RenderFragment<RenderFragment> TabWrapperContent { get; set; }
+    public RenderFragment<RenderFragment>? TabWrapperContent { get; set; }
 
     /// <summary>
     /// TabPanel Tooltip. It will be ignored if TabContent is provided.
     /// </summary>
     [Parameter]
     [Category(CategoryTypes.Tabs.Behavior)]
-    public string ToolTip { get; set; }
+    public string? ToolTip { get; set; }
 
+    /// <inheritdoc/>
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
-        if(firstRender == true && Parent != null)
+        if(firstRender == true && Parent is not null)
         {
-            await Parent.SetPanelRef(PanelRef);
+            await Parent.SetPanelRef(PanelRef!.Value);
         }
     }
 
+    /// <inheritdoc/>
     protected override void OnInitialized()
     {
         // NOTE: we must not throw here because we need the component to be able to live for the API docs to be able to infer default values
@@ -146,6 +151,7 @@ else
         Parent?.AddPanel(this);
     }
 
+    /// <inheritdoc/>
     public async ValueTask DisposeAsync()
     {
         if(_disposed == true) { return;  }

src/MudBlazor/Components/Tabs/MudTabs.razor.cs
@@ -10,6 +10,7 @@ using MudBlazor.Interop;
 using MudBlazor.Services;
 using MudBlazor.Utilities;
 
+#nullable enable
 namespace MudBlazor
 {
     public partial class MudTabs : MudComponentBase, IAsyncDisposable
@@ -30,11 +31,13 @@ namespace MudBlazor
         private double _allTabsSize;
         private double _scrollPosition;
 
-        private IResizeObserver _resizeObserver;
+        private IResizeObserver? _resizeObserver = null;
 
-        [CascadingParameter(Name = "RightToLeft")] public bool RightToLeft { get; set; }
+        [CascadingParameter(Name = "RightToLeft")]
+        public bool RightToLeft { get; set; }
 
-        [Inject] private IResizeObserverFactory _resizeObserverFactory { get; set; }
+        [Inject]
+        private IResizeObserverFactory _resizeObserverFactory { get; set; } = null!;
 
         /// <summary>
         /// If true, render all tabs and hide (display:none) every non-active.
@@ -181,7 +184,7 @@ namespace MudBlazor
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Behavior)]
-        public RenderFragment ChildContent { get; set; }
+        public RenderFragment? ChildContent { get; set; }
 
         /// <summary>
         /// This fragment is placed between tabHeader and panels. 
@@ -190,37 +193,37 @@ namespace MudBlazor
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Behavior)]
-        public RenderFragment<MudTabPanel> PrePanelContent { get; set; }
+        public RenderFragment<MudTabPanel>? PrePanelContent { get; set; }
 
         /// <summary>
         /// Custom class/classes for TabPanel
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Appearance)]
-        public string TabPanelClass { get; set; }
+        public string? TabPanelClass { get; set; }
 
         /// <summary>
         /// Custom class/classes for TabHeader
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Appearance)]
-        public string TabHeaderClass { get; set; }
+        public string? TabHeaderClass { get; set; }
 
         /// <summary>
         /// Custom class/classes for the active tab
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Appearance)]
-        public string ActiveTabClass { get; set; }
+        public string? ActiveTabClass { get; set; }
 
         /// <summary>
         /// Custom class/classes for Selected Content Panel
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Appearance)]
-        public string PanelClass { get; set; }
+        public string? PanelClass { get; set; }
 
-        public MudTabPanel ActivePanel { get; private set; }
+        public MudTabPanel? ActivePanel { get; private set; }
 
         /// <summary>
         /// The current active panel index. Also with Bidirectional Binding
@@ -266,7 +269,7 @@ namespace MudBlazor
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Behavior)]
-        public RenderFragment<MudTabs> Header { get; set; }
+        public RenderFragment<MudTabs>? Header { get; set; }
 
         /// <summary>
         /// Additional content specified by Header is placed either before the tabs, after or not at all
@@ -280,7 +283,7 @@ namespace MudBlazor
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Behavior)]
-        public RenderFragment<MudTabPanel> TabPanelHeader { get; set; }
+        public RenderFragment<MudTabPanel>? TabPanelHeader { get; set; }
 
         /// <summary>
         /// Additional content specified by Header is placed either before the tabs, after or not at all
@@ -294,16 +297,16 @@ namespace MudBlazor
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Tabs.Behavior)]
-        public Func<TabInteractionEventArgs, Task> OnPreviewInteraction { get; set; }
+        public Func<TabInteractionEventArgs, Task>? OnPreviewInteraction { get; set; }
 
         /// <summary>
         /// Can be used in derived class to add a class to the main container. If not overwritten return an empty string
         /// </summary>
         protected virtual string InternalClassName { get; } = string.Empty;
 
-        private string _prevIcon;
+        private string? _prevIcon;
 
-        private string _nextIcon;
+        private string? _nextIcon;
 
         #region Life cycle management
 
@@ -323,10 +326,7 @@ namespace MudBlazor
         {
             base.OnParametersSet();
 
-            if (_resizeObserver == null)
-            {
-                _resizeObserver = _resizeObserverFactory.Create();
-            }
+            _resizeObserver ??= _resizeObserverFactory.Create();
 
             Rerender();
         }
@@ -335,13 +335,13 @@ namespace MudBlazor
         {
             if (firstRender)
             {
-                var items = _panels.Select(x => x.PanelRef).ToList();
+                var items = _panels.Select(x => x.PanelRef!.Value).ToList();
                 items.Add(_tabsContentSize);
 
                 if (_activePanelIndex != -1 && _panels.Count > 0)
                     ActivePanel = _panels[_activePanelIndex];
 
-                await _resizeObserver.Observe(items);
+                await _resizeObserver!.Observe(items);
 
                 _resizeObserver.OnResized += OnResized;
 
@@ -357,7 +357,7 @@ namespace MudBlazor
             if (_isDisposed)
                 return;
             _isDisposed = true;
-            _resizeObserver.OnResized -= OnResized;
+            _resizeObserver!.OnResized -= OnResized;
             if (IsJSRuntimeAvailable)
             {
                 await _resizeObserver.DisposeAsync();
@@ -378,7 +378,7 @@ namespace MudBlazor
 
         internal async Task SetPanelRef(ElementReference reference)
         {
-            if (_isRendered && _resizeObserver.IsElementObserved(reference) == false)
+            if (_isRendered && _resizeObserver!.IsElementObserved(reference) == false)
             {
                 await _resizeObserver.Observe(reference);
                 Rerender();
@@ -412,12 +412,12 @@ namespace MudBlazor
             }
 
             _panels.Remove(tabPanel);
-            await _resizeObserver.Unobserve(tabPanel.PanelRef);
+            await _resizeObserver!.Unobserve(tabPanel.PanelRef!.Value);
             Rerender();
             StateHasChanged();
         }
 
-        public void ActivatePanel(MudTabPanel panel, bool ignoreDisabledState = false)
+        public void ActivatePanel(MudTabPanel? panel, bool ignoreDisabledState = false)
         {
             if (panel is not null && _panels.IndexOf(panel) > -1)
                 ActivatePanel(panel, null, ignoreDisabledState);
@@ -431,17 +431,21 @@ namespace MudBlazor
 
         public void ActivatePanel(object id, bool ignoreDisabledState = false)
         {
-            var panel = _panels.Where((p) => Equals(p.ID, id)).FirstOrDefault();
+            var panel = _panels.FirstOrDefault(p => Equals(p.ID, id));
             if (panel != null)
                 ActivatePanel(panel, null, ignoreDisabledState);
         }
 
-        private async void ActivatePanel(MudTabPanel panel, MouseEventArgs ev, bool ignoreDisabledState = false)
+        private async void ActivatePanel(MudTabPanel panel, MouseEventArgs? ev, bool ignoreDisabledState = false)
         {
             if (!panel.Disabled || ignoreDisabledState)
             {
                 var index = _panels.IndexOf(panel);
-                var previewArgs = new TabInteractionEventArgs { PanelIndex = index, InteractionType = TabInteractionType.Activate };
+                var previewArgs = new TabInteractionEventArgs
+                {
+                    PanelIndex = index,
+                    InteractionType = TabInteractionType.Activate
+                };
 
                 if (OnPreviewInteraction != null)
                     await OnPreviewInteraction.Invoke(previewArgs);
@@ -449,7 +453,10 @@ namespace MudBlazor
                 if (previewArgs.Cancel) return;
 
                 ActivePanelIndex = previewArgs.PanelIndex;
-                await ActivePanel?.OnClick.InvokeAsync(ev);
+                if (ActivePanel is not null)
+                {
+                    await ActivePanel.OnClick.InvokeAsync(ev);
+                }
 
                 CenterScrollPositionAroundSelectedItem();
                 SetScrollabilityStates();
@@ -617,7 +624,7 @@ namespace MudBlazor
 
         private void SetSliderState()
         {
-            if (ActivePanel == null) { return; }
+            if (ActivePanel?.PanelRef is null) { return; }
 
             _sliderPosition = GetLengthOfPanelItems(ActivePanel);
             _sliderSize = GetRelevantSize(ActivePanel.PanelRef);
@@ -640,11 +647,12 @@ namespace MudBlazor
             _allTabsSize = totalTabsSize;
         }
 
-        private double GetRelevantSize(ElementReference reference) => Position switch
-        {
-            Position.Top or Position.Bottom => _resizeObserver.GetWidth(reference),
-            _ => _resizeObserver.GetHeight(reference)
-        };
+        private double GetRelevantSize(ElementReference? reference) =>
+            Position switch
+            {
+                Position.Top or Position.Bottom => _resizeObserver!.GetWidth(reference!.Value),
+                _ => _resizeObserver!.GetHeight(reference!.Value)
+            };
 
         private double GetLengthOfPanelItems(MudTabPanel panel, bool inclusive = false)
         {
@@ -667,11 +675,11 @@ namespace MudBlazor
             return value;
         }
 
-        private double GetPanelLength(MudTabPanel panel) => panel == null ? 0.0 : GetRelevantSize(panel.PanelRef);
+        private double GetPanelLength(MudTabPanel? panel) => panel == null ? 0.0 : GetRelevantSize(panel.PanelRef);
 
         #endregion
 
-        #region scrolling 
+        #region scrolling
 
         private void SetScrollButtonVisibility()
         {
@@ -749,13 +757,18 @@ namespace MudBlazor
         {
             if (_showScrollButtons && ActivePanelIndex + 1 == _panels.Count)
             {
-                var lastPannel = _panels.Last();
-                var isScrolled = ScrollToItem(lastPannel, true);
+                var lastPanel = _panels.Last();
+                var isScrolled = ScrollToItem(lastPanel, true);
                 if (isScrolled)
                 {
-                    _scrollIndex = _panels.IndexOf(lastPannel);
+                    _scrollIndex = _panels.IndexOf(lastPanel);
                     return;
                 }
+            }
+
+            if (ActivePanel is null)
+            {
+                return;
             }
 
             var panelToStart = ActivePanel;

src/MudBlazor/Components/Tabs/MudDynamicTabs.razor
@@ -6,7 +6,7 @@
 @Render
 
 @code {
-
+#nullable enable
     /// <inheritdoc />
     protected override void OnInitialized()
     {

src/MudBlazor/Components/Tabs/MudDynamicTabs.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 
+#nullable enable
 namespace MudBlazor
 {
     public partial class MudDynamicTabs : MudTabs

src/MudBlazor/Components/Tabs/MudTabs.razor
@@ -72,6 +72,7 @@
 </div>
 
 @code {
+#nullable enable
     RenderFragment RenderTab(MudTabPanel panel) => @<div @ref="panel.PanelRef" class="@GetTabClass(panel)" style="@GetTabStyle(panel)" @onclick=@( e => ActivatePanel(panel, e, false) )>
         @if (TabPanelHeaderPosition == TabHeaderPosition.Before && TabPanelHeader != null)
             {

src/MudBlazor/Components/Tabs/TabHeaderPosition.cs
@@ -2,6 +2,7 @@
 
 using System.ComponentModel;
 
+#nullable enable
 namespace MudBlazor
 {
     public enum TabHeaderPosition

src/MudBlazor/Components/Tabs/TabInteractionEventArgs.cs
@@ -4,6 +4,7 @@
 
 namespace MudBlazor;
 
+#nullable enable
 public class TabInteractionEventArgs
 {
     public int PanelIndex { get; set; }

src/MudBlazor/Components/Tabs/TabInteractionType.cs
@@ -4,6 +4,7 @@
 
 namespace MudBlazor;
 
+#nullable enable
 public enum TabInteractionType
 {
     Activate

commit 9d6dc5600da2ecc8257efa04e11826b96dd28ef0

MudTabs: Fix ElementReference regression in #9889

src/MudBlazor/Components/Tabs/MudTabPanel.razor
@@ -25,7 +25,7 @@ else
     /// <summary>
     /// Reference to the underlying panel element.
     /// </summary>
-    public ElementReference? PanelRef;
+    public ElementReference PanelRef;
 
 
     /// <summary>
@@ -134,9 +134,9 @@ else
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
-        if(firstRender == true && Parent is not null)
+        if(firstRender && Parent is not null)
         {
-            await Parent.SetPanelRef(PanelRef!.Value);
+            await Parent.SetPanelRef(PanelRef);
         }
     }
 
@@ -154,7 +154,7 @@ else
     /// <inheritdoc/>
     public async ValueTask DisposeAsync()
     {
-        if(_disposed == true) { return;  }
+        if(_disposed) { return;  }
 
         _disposed = true;
         if (Parent is not null)

src/MudBlazor/Components/Tabs/MudTabs.razor.cs
@@ -335,7 +335,7 @@ namespace MudBlazor
         {
             if (firstRender)
             {
-                var items = _panels.Select(x => x.PanelRef!.Value).ToList();
+                var items = _panels.Select(x => x.PanelRef).ToList();
                 items.Add(_tabsContentSize);
 
                 if (_activePanelIndex != -1 && _panels.Count > 0)
@@ -412,7 +412,7 @@ namespace MudBlazor
             }
 
             _panels.Remove(tabPanel);
-            await _resizeObserver!.Unobserve(tabPanel.PanelRef!.Value);
+            await _resizeObserver!.Unobserve(tabPanel.PanelRef);
             Rerender();
             StateHasChanged();
         }
@@ -624,7 +624,10 @@ namespace MudBlazor
 
         private void SetSliderState()
         {
-            if (ActivePanel?.PanelRef is null) { return; }
+            if (ActivePanel is null)
+            {
+                return;
+            }
 
             _sliderPosition = GetLengthOfPanelItems(ActivePanel);
             _sliderSize = GetRelevantSize(ActivePanel.PanelRef);
@@ -647,11 +650,11 @@ namespace MudBlazor
             _allTabsSize = totalTabsSize;
         }
 
-        private double GetRelevantSize(ElementReference? reference) =>
+        private double GetRelevantSize(ElementReference reference) =>
             Position switch
             {
-                Position.Top or Position.Bottom => _resizeObserver!.GetWidth(reference!.Value),
-                _ => _resizeObserver!.GetHeight(reference!.Value)
+                Position.Top or Position.Bottom => _resizeObserver!.GetWidth(reference),
+                _ => _resizeObserver!.GetHeight(reference)
             };
 
         private double GetLengthOfPanelItems(MudTabPanel panel, bool inclusive = false)