MudBlazor/MudBlazor

commit d37788f39c9e59aa418d10dc37d0786c4b42c547

Introduce KeyInterceptorService withs sync/async support to replace KeyInterceptor  (#9896)

src/MudBlazor/Services/KeyInterceptor/KeyInterceptorOptions.cs
@@ -2,84 +2,59 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
+namespace MudBlazor.Services;
 
-namespace MudBlazor.Services
+#nullable enable
+/// <summary>
+/// Configuration options for key interception.
+/// </summary>
+public class KeyInterceptorOptions
 {
-    public class KeyInterceptorOptions
+    /// <summary>
+    /// The CSS class of the target HTML element that should be observed for keyboard events.
+    /// </summary>
+    /// <remarks>
+    /// Note: This must be a single class name.
+    /// </remarks>
+    public string? TargetClass { get; init; }
+
+    /// <summary>
+    /// Specifies whether resize events should be logged in the browser's console.
+    /// </summary>
+    public bool EnableLogging { get; init; }
+
+    /// <summary>
+    /// A list of key options that define the keys to intercept and their respective configurations.
+    /// </summary>
+    public IReadOnlyList<KeyOptions> Keys { get; init; } = [];
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KeyInterceptorOptions"/> class.
+    /// </summary>
+    public KeyInterceptorOptions()
     {
-        /// <summary>
-        /// <para>Class of the target node which should be observed for keyboard events</para>
-        /// <para>Note: this must be a single class</para>
-        /// </summary>
-        public string TargetClass { get; set; }
-
-        /// <summary>
-        /// Report resize events in the browser's console.
-        /// </summary>
-        public bool EnableLogging { get; set; } = false;
-
-        /// <summary>
-        /// Intercept configuration for keys of interest
-        /// </summary>
-        public List<KeyOptions> Keys { get; set; } = new List<KeyOptions>();
     }
 
     /// <summary>
-    /// <para>Configuration for preventDefault() and stopPropagation() control</para>
-    /// <para>
-    /// For PreventDown, PreventUp, StopDown and StopUp the configuration which key combinations should match
-    /// is a Javascript boolean expression.
-    /// </para>
-    /// <para>
-    /// Examples:
-    /// For the examples, let's assume the Tab key was pressed.
-    /// Note: for combinations of more than one modifier the following order of modifiers must be followed strictly: shift+ctrl+alt+meta
-    /// </para>
-    /// <para>
-    ///  * Don't prevent key down:
-    ///          PreventDown=null or PreventDown="none"
-    ///  * Prevent key down of unmodified keystrokes such as "Tab":
-    ///          PreventDown="key+none"
-    ///  * Prevent key down of Tab and Ctrl+Tab
-    ///          PreventDown="key+none|key+ctrl"
-    ///  * Prevent key down of just Ctrl+Tab
-    ///          PreventDown="key+ctrl"
-    ///  * Prevent key down of Ctrl+Tab and Shift+Tab but not Shift+Ctrl+Tab:
-    ///          PreventDown="key+shift|key+ctrl"
-    ///  * Prevent key down of Shift+Ctrl+Tab and Ctrl+Tab but not Shift+Tab:
-    ///          PreventDown="key+shift+ctrl|key+ctrl"
-    ///  * Prevent any combination of key and modifiers, but not the unmodified key:
-    ///          PreventDown="key+any"
-    ///  * Prevent any combination of key and modifiers, even the unmodified key:
-    ///          PreventDown="any"
-    /// </para>
+    /// Initializes a new instance of the <see cref="KeyInterceptorOptions"/> class with the specified target class and key options.
     /// </summary>
-    public class KeyOptions
+    /// <param name="targetClass">The CSS class of the target HTML element.</param>
+    /// <param name="keys">The key options to intercept.</param>
+    public KeyInterceptorOptions(string targetClass, params KeyOptions[] keys)
     {
-        /// <summary>
-        /// <para>Javascript keyboard event.key</para>
-        /// <para>
-        /// Examples: " " for space, "Tab" for tab, "a" for lowercase A-key.
-        /// Also allowed: JS regex such as "/[a-z]/" or "/a|b/" but NOT "/[a-z]/g" or "/[a-z]/i"
-        ///      regex must be enclosed in two forward slashes!
-        /// </para>
-        /// </summary>
-        public string Key { get; set; }
+        TargetClass = targetClass;
+        Keys = keys;
+    }
 
-        /// <summary>
-        /// Subscribe down key and invoke event KeyDown on c# side
-        /// </summary>
-        public bool SubscribeDown { get; set; }
-
-        /// <summary>
-        /// Subscribe up key and invoke event KeyUp on c# side
-        /// </summary>
-        public bool SubscribeUp { get; set; }
-
-        public string PreventDown { get; set; } = "none";
-        public string PreventUp { get; set; } = "none";
-        public string StopDown { get; set; } = "none";
-        public string StopUp { get; set; } = "none";
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KeyInterceptorOptions"/> class with the specified target class, key options, and logging option.
+    /// </summary>
+    /// <param name="targetClass">The CSS class of the target HTML element.</param>
+    /// <param name="keys">The key options to intercept.</param>
+    /// <param name="enableLogging">Specifies whether resize events should be logged in the browser's console.</param>
+    public KeyInterceptorOptions(string targetClass, bool enableLogging = false, params KeyOptions[] keys)
+        : this(targetClass, keys)
+    {
+        EnableLogging = enableLogging;
     }
 }

src/MudBlazor.UnitTests/Components/SelectTests.cs
@@ -24,7 +24,7 @@ namespace MudBlazor.UnitTests.Components
         {
             var comp = Context.RenderComponent<SelectRequiredTest>();
             var select = comp.FindComponent<MudSelect<string>>();
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter" }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter" }));
             await comp.InvokeAsync(() => select.SetParam("ListClass", "my-list-class"));
             comp.WaitForAssertion(() => comp.Markup.Should().Contain("my-list-class"));
         }
@@ -912,62 +912,62 @@ namespace MudBlazor.UnitTests.Components
             // select elements needed for the test
             var select = comp.FindComponent<MudSelect<string>>();
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Escape", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Escape", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
             //If we didn't select an item with mouse or arrow keys yet, value should remains null.
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be(null));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", AltKey = true, Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", AltKey = true, Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowUp", AltKey = true, Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowUp", AltKey = true, Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
             //If dropdown is closed, arrow key should not set a value.
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be(null));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "NumpadEnter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "NumpadEnter", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowUp", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowUp", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("1"));
             //End key should not select the last disabled item
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("3"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowUp", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowUp", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("2"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Home", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Home", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("1"));
             //Arrow up should select still the first item
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowUp", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowUp", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("1"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("3"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "2", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "2", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("2"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "2", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "2", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("2"));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().HaveCount(1));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
             comp.Render(); // <-- this is necessary for reliable passing of the test
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
         }
@@ -980,54 +980,54 @@ namespace MudBlazor.UnitTests.Components
             // select elements needed for the test
             var select = comp.FindComponent<MudSelect<string>>();
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "a", CtrlKey = true, Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "a", CtrlKey = true, Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("0 feline has been selected"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "A", CtrlKey = true, Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "A", CtrlKey = true, Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("7 felines have been selected"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("6 felines have been selected"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "A", CtrlKey = true, Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "A", CtrlKey = true, Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.Value.Should().Be("7 felines have been selected"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Escape", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Escape", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().Contain("mud-popover-open"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().Contain("Jaguar"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Home", Type = "keydown", }));
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "NumpadEnter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Home", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "NumpadEnter", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().NotContain("Jaguar"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().Contain("Leopard"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().NotContain("Tiger"));
 
             select.SetParam("Disabled", true);
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().NotContain("Tiger"));
 
             select.SetParam("Disabled", false);
             //Test the keyup event
-            await comp.InvokeAsync(() => select.Instance.HandleKeyUp(new KeyboardEventArgs() { Key = "Enter", Type = "keyup", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyUpAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keyup", }));
             comp.WaitForAssertion(() => select.Instance.SelectedValues.Should().NotContain("Tiger"));
 
-            await comp.InvokeAsync(() => select.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Tab", Type = "keydown", }));
+            await comp.InvokeAsync(() => select.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Tab", Type = "keydown", }));
             await comp.InvokeAsync(() => select.Instance.OnKeyUp.InvokeAsync(new KeyboardEventArgs() { Key = "Tab" }));
             comp.Render(); // <-- this is necessary for reliable passing of the test
             comp.WaitForAssertion(() => comp.Find("div.mud-popover").ClassList.Should().NotContain("mud-popover-open"));
@@ -1058,11 +1058,11 @@ namespace MudBlazor.UnitTests.Components
 
             // print the generated html
 
-            await comp.InvokeAsync(() => comp.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
-            await comp.InvokeAsync(() => comp.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
-            await comp.InvokeAsync(() => comp.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Home", Type = "keydown", }));
-            await comp.InvokeAsync(() => comp.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
-            await comp.InvokeAsync(() => comp.Instance.HandleKeyDown(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
+            await comp.InvokeAsync(() => comp.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = " ", Type = "keydown", }));
+            await comp.InvokeAsync(() => comp.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "ArrowDown", Type = "keydown", }));
+            await comp.InvokeAsync(() => comp.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Home", Type = "keydown", }));
+            await comp.InvokeAsync(() => comp.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "End", Type = "keydown", }));
+            await comp.InvokeAsync(() => comp.Instance.HandleKeyDownAsync(new KeyboardEventArgs() { Key = "Enter", Type = "keydown", }));
             comp.WaitForAssertion(() => comp.Instance.SelectedValues.Should().HaveCount(0));
             comp.WaitForAssertion(() => comp.Instance.Value.Should().Be(null));
         }

src/MudBlazor.UnitTests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -216,17 +216,22 @@ public class ServiceCollectionExtensionsTests
     {
         // Arrange
         var services = new ServiceCollection()
+            .AddLogging()
             .AddSingleton<IJSRuntime, MockJsRuntime>();
 
         // Act
         services.AddMudBlazorKeyInterceptor();
         var serviceProvider = services.BuildServiceProvider();
+#pragma warning disable CS0618 // Type or member is obsolete
         var keyInterceptor = serviceProvider.GetService<IKeyInterceptor>();
         var keyInterceptorFactory = serviceProvider.GetService<IKeyInterceptorFactory>();
+#pragma warning restore CS0618 // Type or member is obsolete
+        var keyInterceptorService = serviceProvider.GetService<IKeyInterceptorService>();
 
         // Assert
         keyInterceptor.Should().NotBeNull();
         keyInterceptorFactory.Should().NotBeNull();
+        keyInterceptorService.Should().NotBeNull();
     }
 
     [Test]
@@ -426,8 +431,11 @@ public class ServiceCollectionExtensionsTests
         var browserViewportService = serviceProvider.GetService<IBrowserViewportService>();
         var resizeObserver = serviceProvider.GetService<IResizeObserver>();
         var resizeObserverFactory = serviceProvider.GetService<IResizeObserverFactory>();
+#pragma warning disable CS0618 // Type or member is obsolete
         var keyInterceptor = serviceProvider.GetService<IKeyInterceptor>();
         var keyInterceptorFactory = serviceProvider.GetService<IKeyInterceptorFactory>();
+#pragma warning restore CS0618 // Type or member is obsolete
+        var keyInterceptorService = serviceProvider.GetService<IKeyInterceptorService>();
         var jsEvent = serviceProvider.GetService<IJsEvent>();
         var jsEventFactory = serviceProvider.GetService<IJsEventFactory>();
         var scrollManager = serviceProvider.GetService<IScrollManager>();
@@ -453,6 +461,7 @@ public class ServiceCollectionExtensionsTests
         resizeObserverFactory.Should().NotBeNull();
         keyInterceptor.Should().NotBeNull();
         keyInterceptorFactory.Should().NotBeNull();
+        keyInterceptorService.Should().NotBeNull();
         jsEvent.Should().NotBeNull();
         jsEventFactory.Should().NotBeNull();
         scrollManager.Should().NotBeNull();
@@ -528,8 +537,11 @@ public class ServiceCollectionExtensionsTests
         var browserViewportService = serviceProvider.GetService<IBrowserViewportService>();
         var resizeObserver = serviceProvider.GetService<IResizeObserver>();
         var resizeObserverFactory = serviceProvider.GetService<IResizeObserverFactory>();
+#pragma warning disable CS0618 // Type or member is obsolete
         var keyInterceptor = serviceProvider.GetService<IKeyInterceptor>();
         var keyInterceptorFactory = serviceProvider.GetService<IKeyInterceptorFactory>();
+#pragma warning restore CS0618 // Type or member is obsolete
+        var keyInterceptorService = serviceProvider.GetService<IKeyInterceptorService>();
         var jsEvent = serviceProvider.GetService<IJsEvent>();
         var jsEventFactory = serviceProvider.GetService<IJsEventFactory>();
         var scrollManager = serviceProvider.GetService<IScrollManager>();
@@ -563,6 +575,7 @@ public class ServiceCollectionExtensionsTests
         resizeObserverFactory.Should().NotBeNull();
         keyInterceptor.Should().NotBeNull();
         keyInterceptorFactory.Should().NotBeNull();
+        keyInterceptorService.Should().NotBeNull();
         jsEvent.Should().NotBeNull();
         jsEventFactory.Should().NotBeNull();
         scrollManager.Should().NotBeNull();

src/MudBlazor.UnitTests/Generated/ApiDocsTests.cs
@@ -41,7 +41,7 @@ namespace MudBlazor.UnitTests.Components
             ctx.Services.AddSingleton<IMudPopoverService, MockPopoverService>();
 #pragma warning restore CS0618
             ctx.Services.AddSingleton<IPopoverService, MockPopoverServiceV2>();
-            ctx.Services.AddTransient<IKeyInterceptorFactory, MockKeyInterceptorServiceFactory>();
+            ctx.Services.AddSingleton<IKeyInterceptorService, MockKeyInterceptorService>();
             ctx.Services.AddTransient<IJsEventFactory, MockJsEventFactory>();
             ctx.Services.AddScoped<IRenderQueueService, RenderQueueService>();
             ctx.Services.AddTransient<InternalMudLocalizer>();

src/MudBlazor.UnitTests/Generated/ExampleDocsTests.cs
@@ -31,7 +31,7 @@ namespace MudBlazor.UnitTests.Components
             ctx.Services.AddTransient<IResizeObserverFactory, MockResizeObserverFactory>();
             ctx.Services.AddTransient<IEventListenerFactory, MockEventListenerFactory>();
             ctx.Services.AddTransient<IEventListener, MockEventListener>();
-            ctx.Services.AddTransient<IKeyInterceptorFactory, MockKeyInterceptorServiceFactory>();
+            ctx.Services.AddSingleton<IKeyInterceptorService, MockKeyInterceptorService>();
             ctx.Services.AddTransient<IJsEventFactory, MockJsEventFactory>();
 #pragma warning disable CS0618
             //TODO: Remove in v7

commit 6e75dea4f430fa1f7503e0a6fb86068d4cf469ee

KeyInterceptorOptions: Fix breaking change introduced in #9896

src/MudBlazor/Services/KeyInterceptor/KeyInterceptorOptions.cs
@@ -16,17 +16,17 @@ public class KeyInterceptorOptions
     /// <remarks>
     /// Note: This must be a single class name.
     /// </remarks>
-    public string? TargetClass { get; init; }
+    public string? TargetClass { get; set; }
 
     /// <summary>
     /// Specifies whether resize events should be logged in the browser's console.
     /// </summary>
-    public bool EnableLogging { get; init; }
+    public bool EnableLogging { get; set; }
 
     /// <summary>
     /// A list of key options that define the keys to intercept and their respective configurations.
     /// </summary>
-    public IReadOnlyList<KeyOptions> Keys { get; init; } = [];
+    public List<KeyOptions> Keys { get; set; } = [];
 
     /// <summary>
     /// Initializes a new instance of the <see cref="KeyInterceptorOptions"/> class.
@@ -43,7 +43,7 @@ public class KeyInterceptorOptions
     public KeyInterceptorOptions(string targetClass, params KeyOptions[] keys)
     {
         TargetClass = targetClass;
-        Keys = keys;
+        Keys = keys.ToList();
     }
 
     /// <summary>