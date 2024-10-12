fcf7451be13834d31e6fd360dafc099d3be6cac7
Artyom M. <artem.melnikov@live.com>
2024-10-12 17:19:42 +0300

    DateTimeExtensions: Add unit tests. (#9988)

--- src/MudBlazor.UnitTests/Extensions/DateTimeExtensionTests.cs
+++ src/MudBlazor.UnitTests/Extensions/DateTimeExtensionTests.cs
@@ -1,31 +1,124 @@
-﻿// Copyright (c) 2021 - MudBlazor
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-using System;
 using System.Globalization;
 using FluentAssertions;
 using MudBlazor.Extensions;
 using NUnit.Framework;
 
+namespace MudBlazor.UnitTests.Extensions;
 
-
-namespace MudBlazor.UnitTests.Extensions
+#nullable enable
+[TestFixture]
+public class DateTimeExtensionTests
 {
-    [TestFixture]
-    public class DateTimeExtensionTests
+    [Test]
+    public void ToIsoDateString_ShouldReturnFormattedString_WhenDateTimeIsProvided()
     {
-        [Test]
-        public void StartOfMonth_EndOfMonth_Test()
-        {
-            var date = new DateTime(2021, 02, 14);
-            var gregorian_start = date.StartOfMonth(CultureInfo.InvariantCulture);
-            gregorian_start.ToIsoDateString().Should().Be("2021-02-01");
-            var gregorian_end = date.EndOfMonth(CultureInfo.InvariantCulture);
-            gregorian_end.ToIsoDateString().Should().Be("2021-02-28");
-            var persian = CultureInfo.GetCultureInfo("fa-IR");
-            var persian_start = date.StartOfMonth(persian);
-            persian_start.ToIsoDateString().Should().Be("2021-01-20"); // 1399-11-01
-            var persian_end = date.EndOfMonth(persian);
-            persian_end.ToIsoDateString().Should().Be("2021-02-18");  // 1399-11-30
-        }
+        // Arrange
+        var dateTime = new DateTime(2023, 10, 5);
+
+        // Act
+        var result = dateTime.ToIsoDateString();
+
+        // Assert
+        result.Should().Be("2023-10-05");
+    }
+
+    [Test]
+    public void ToIsoDateString_ShouldReturnFormattedString_WhenNullableDateTimeIsProvided()
+    {
+        // Arrange
+        DateTime? dateTime = new DateTime(2023, 10, 5);
+
+        // Act
+        var result = dateTime.ToIsoDateString();
+
+        // Assert
+        result.Should().Be("2023-10-05");
+    }
+
+    [Test]
+    public void ToIsoDateString_ShouldReturnNull_WhenNullableDateTimeIsNull()
+    {
+        // Arrange
+        DateTime? dateTime = null;
+
+        // Act
+        var result = dateTime.ToIsoDateString();
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Test]
+    public void StartOfMonth_ShouldReturnFirstDayOfMonth()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 10, 15);
+        var culture = CultureInfo.InvariantCulture;
+
+        // Act
+        var result = dateTime.StartOfMonth(culture);
+
+        // Assert
+        result.Should().Be(new DateTime(2023, 10, 1));
+    }
+
+    [Test]
+    public void EndOfMonth_ShouldReturnLastDayOfMonth()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 10, 15);
+        var culture = CultureInfo.InvariantCulture;
+
+        // Act
+        var result = dateTime.EndOfMonth(culture);
+
+        // Assert
+        result.Should().Be(new DateTime(2023, 10, 31));
+    }
+
+    [Test]
+    public void StartOfWeek_ShouldReturnFirstDayOfWeek()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 10, 5); // Thursday
+        const DayOfWeek FirstDayOfWeek = DayOfWeek.Monday;
+
+        // Act
+        var result = dateTime.StartOfWeek(FirstDayOfWeek);
+
+        // Assert
+        result.Should().Be(new DateTime(2023, 10, 2)); // Monday
+    }
+
+    [Test]
+    public void StartOfWeek_ShouldReturnSameDate_WhenDateIsFirstDayOfWeek()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 10, 2); // Monday
+        const DayOfWeek FirstDayOfWeek = DayOfWeek.Monday;
+
+        // Act
+        var result = dateTime.StartOfWeek(FirstDayOfWeek);
+
+        // Assert
+        result.Should().Be(new DateTime(2023, 10, 2)); // Monday
+    }
+
+    [Test]
+    public void StartOfWeek_ShouldHandleEdgeCase_WhenDateIsNearStartOfYear()
+    {
+        // Arrange
+        var dateTime = new DateTime(1, 1, 3); // Wednesday
+        const DayOfWeek FirstDayOfWeek = DayOfWeek.Monday;
+
+        // Act
+        var result = dateTime.StartOfWeek(FirstDayOfWeek);
+
+        // Assert
+        result.Should().Be(new DateTime(1, 1, 1)); // Monday
     }
 }
