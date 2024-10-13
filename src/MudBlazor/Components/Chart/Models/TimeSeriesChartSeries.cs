c904a7f76425b470ff5f27b8d62b24e5bfa0182a
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-10-13 08:34:54 -0500

    TimeSeries: Rename `TimeSeriesDiplayType` to fix typo (#9581)

--- src/MudBlazor/Components/Chart/Models/TimeSeriesChartSeries.cs
+++ src/MudBlazor/Components/Chart/Models/TimeSeriesChartSeries.cs
@@ -13,14 +13,13 @@ namespace MudBlazor.Components.Chart.Models
 
         public int Index { get; set; }
 
-        public TimeSeriesDiplayType Type { get; set; } = TimeSeriesDiplayType.Line;
+        public TimeSeriesDisplayType Type { get; set; } = TimeSeriesDisplayType.Line;
 
         public double FillOpacity { get; set; } = 0.4;
         public double StrokeOpacity { get; set; } = 1;
     }
 
-    // TODO: v8 rename to TimeSeriesDisplayType (typo)
-    public enum TimeSeriesDiplayType
+    public enum TimeSeriesDisplayType
     {
         Line,
         Area,
