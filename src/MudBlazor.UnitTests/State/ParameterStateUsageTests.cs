32cc4c6b4216b2be822230cb9b2da795835c6eff
Artyom M. <artem.melnikov@live.com>
2024-10-14 15:22:22 +0300

    ParameterState: Add more tests. (#10014)

--- src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
+++ src/MudBlazor.UnitTests/State/ParameterStateUsageTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using AngleSharp.Dom;
+﻿using AngleSharp.Dom;
 using Bunit;
 using Bunit.Rendering;
 using FluentAssertions;
