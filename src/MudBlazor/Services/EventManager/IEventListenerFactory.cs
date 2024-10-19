50abb0480c1761fa590ab48542fa2833e3502c38
Artyom M <artem.melnikov@live.com>
2024-10-19 12:43:00 +0300

    EventListener: Clean up, make implementations internal, add xmodoc

--- src/MudBlazor/Services/EventManager/IEventListenerFactory.cs
+++ src/MudBlazor/Services/EventManager/IEventListenerFactory.cs
@@ -4,7 +4,15 @@
 
 namespace MudBlazor;
 
+#nullable enable
+/// <summary>
+/// Factory interface for creating instances of <see cref="IEventListener"/>.
+/// </summary>
 public interface IEventListenerFactory
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="IEventListener"/>.
+    /// </summary>
+    /// <returns>A new instance of <see cref="IEventListener"/>.</returns>
     IEventListener Create();
 }
