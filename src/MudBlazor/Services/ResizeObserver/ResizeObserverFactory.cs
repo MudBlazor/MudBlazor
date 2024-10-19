95f69e9b4fe13e737286becd58512c171fac2844
Artyom M <artem.melnikov@live.com>
2024-10-19 18:14:22 +0300

    ResizeObserver: Clean up, make implementations internal, add xmodoc

--- src/MudBlazor/Services/ResizeObserver/ResizeObserverFactory.cs
+++ src/MudBlazor/Services/ResizeObserver/ResizeObserverFactory.cs
@@ -1,26 +1,39 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
 
-namespace MudBlazor.Services
+namespace MudBlazor.Services;
+
+#nullable enable
+/// <summary>
+/// Factory for creating instances of <see cref="IResizeObserver"/>.
+/// </summary>
+internal sealed class ResizeObserverFactory : IResizeObserverFactory
 {
-    public class ResizeObserverFactory : IResizeObserverFactory
+    private readonly IServiceProvider _provider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResizeObserverFactory"/> class.
+    /// </summary>
+    /// <param name="provider">The service provider.</param>
+    public ResizeObserverFactory(IServiceProvider provider)
     {
-        private readonly IServiceProvider _provider;
+        _provider = provider;
+    }
 
-        public ResizeObserverFactory(IServiceProvider provider)
-        {
-            _provider = provider;
-        }
+    /// <inheritdoc />
+    public IResizeObserver Create()
+    {
+        var options = _provider.GetService<IOptions<ResizeObserverOptions>>();
 
-        public IResizeObserver Create(ResizeObserverOptions options) =>
-            new ResizeObserver(_provider.GetRequiredService<IJSRuntime>(), new OptionsWrapper<ResizeObserverOptions>(options));
+        return Create(options?.Value ?? new ResizeObserverOptions());
+    }
 
-        public IResizeObserver Create()
-        {
-            var options = _provider.GetService<IOptions<ResizeObserverOptions>>();
-            return Create(options?.Value ?? new ResizeObserverOptions());
-        }
+    /// <inheritdoc />
+    public IResizeObserver Create(ResizeObserverOptions options)
+    {
+        var jsRuntime = _provider.GetRequiredService<IJSRuntime>();
+
+        return new ResizeObserver(jsRuntime, new OptionsWrapper<ResizeObserverOptions>(options));
     }
 }
