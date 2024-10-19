b6b1e9dc05dd060154d3a94d67f1e004088b4329
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-10-19 15:59:37 -0500

    part 1

--- src/MudBlazor/Components/Collapse/MudCollapse.razor.cs
+++ src/MudBlazor/Components/Collapse/MudCollapse.razor.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using MudBlazor.State;
@@ -19,26 +17,19 @@ namespace MudBlazor
             Entering, Entered, Exiting, Exited
         }
 
-        internal double _height;
         private readonly ParameterState<bool> _expandedState;
-        private bool _isRendered;
-        private bool _updateHeight;
-        private ElementReference _wrapper;
         internal CollapseState _state = CollapseState.Exited;
-
-        protected string Stylename => new StyleBuilder()
-            .AddStyle("max-height", MaxHeight.ToPx(), MaxHeight != null)
-            .AddStyle("height", "auto", _state == CollapseState.Entered)
-            .AddStyle("height", _height.ToPx(), _state is CollapseState.Entering or CollapseState.Exiting)
-            .AddStyle("animation-duration", $"{CalculatedAnimationDuration.ToString("#.##", CultureInfo.InvariantCulture)}s", _state == CollapseState.Entering)
-            .AddStyle(Style)
-            .Build();
 
         protected string Classname => new CssBuilder("mud-collapse-container")
             .AddClass($"mud-collapse-entering", _state == CollapseState.Entering)
             .AddClass($"mud-collapse-entered", _state == CollapseState.Entered)
             .AddClass($"mud-collapse-exiting", _state == CollapseState.Exiting)
             .AddClass(Class)
+            .Build();
+
+        protected string Stylename => new StyleBuilder()
+            .AddStyle("max-height", MaxHeight.ToPx(), MaxHeight != null)
+            .AddStyle(Style)
             .Build();
 
         /// <summary>
@@ -88,70 +79,9 @@ namespace MudBlazor
 
         private async Task OnExpandedParameterChangedAsync()
         {
-            if (_isRendered)
-            {
-                _state = _expandedState.Value ? CollapseState.Entering : CollapseState.Exiting;
-                await UpdateHeightAsync();
-                _updateHeight = true;
-            }
-            else if (_expandedState.Value)
-            {
-                _state = CollapseState.Entered;
-            }
+            _state = _expandedState.Value ? CollapseState.Entering : CollapseState.Exiting;
 
             await ExpandedChanged.InvokeAsync(_expandedState.Value);
-        }
-
-        /// <summary>
-        /// Modified Animation duration that scales with height parameter.
-        /// Basic implementation for now but should be a math formula to allow it to scale between 0.1s and 1s for the effect to be consistently smooth.
-        /// </summary>
-        private double CalculatedAnimationDuration
-        {
-            get
-            {
-                return MaxHeight switch
-                {
-                    null => Math.Min(_height / 800.0, 1),
-                    <= 200 => 0.2,
-                    <= 600 => 0.4,
-                    <= 1400 => 0.6,
-                    _ => 1
-                };
-            }
-        }
-
-        internal async Task UpdateHeightAsync()
-        {
-            try
-            {
-                _height = (await _wrapper.MudGetBoundingClientRectAsync())?.Height ?? 0;
-            }
-            catch (Exception ex) when (ex is JSDisconnectedException or TaskCanceledException)
-            {
-                _height = 0;
-            }
-
-            if (_height > MaxHeight)
-            {
-                _height = MaxHeight.Value;
-            }
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                _isRendered = true;
-                await UpdateHeightAsync();
-            }
-            else if (_updateHeight && _state is CollapseState.Entering or CollapseState.Exiting)
-            {
-                _updateHeight = false;
-                await UpdateHeightAsync();
-                StateHasChanged();
-            }
-            await base.OnAfterRenderAsync(firstRender);
         }
 
         /// <summary>
