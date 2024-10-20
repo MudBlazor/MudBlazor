65c064beea31cf8d21ecf23699a44e0638a65702
Artyom M. <artem.melnikov@live.com>
2024-10-20 13:32:49 +0300

    ResizeObserver: Clean up, make implementations internal, add xmodoc (#10055)

--- src/MudBlazor/Services/ResizeObserver/ResizeObserverOptions.cs
+++ src/MudBlazor/Services/ResizeObserver/ResizeObserverOptions.cs
@@ -1,16 +1,21 @@
-﻿namespace MudBlazor.Services
-{
-    public class ResizeObserverOptions
-    {
-        /// <summary>
-        /// Timepsan in milliseconds after the browser detect the last chance and notify the interop service.
-        /// Setting this value too low can cause poor application performance.
-        /// </summary>
-        public int ReportRate { get; set; } = 200;
+﻿namespace MudBlazor.Services;
 
-        /// <summary>
-        /// Report resize events in the browser's console.
-        /// </summary>
-        public bool EnableLogging { get; set; } = false;
-    }
+#nullable enable
+/// <summary>
+/// Options for configuring the <see cref="IResizeObserver"/>.
+/// </summary>
+public class ResizeObserverOptions
+{
+    /// <summary>
+    /// Timespan in milliseconds after the browser detects the last change and notifies the interop service.
+    /// </summary>
+    /// <remarks>
+    /// Defaults to <c>200</c>.  Setting this value too low can cause poor application performance.
+    /// </remarks>
+    public int ReportRate { get; set; } = 200;
+
+    /// <summary>
+    /// Report resize events in the browser's console.
+    /// </summary>
+    public bool EnableLogging { get; set; }
 }
