894ec1d283b8be22bb32b9cdeab9ea3991ee725a
Artyom M. <artem.melnikov@live.com>
2024-10-20 14:11:37 +0300

    JsEvent: Use IAsyncDisposable (#10061)

--- src/MudBlazor.UnitTests/Mocks/MockJsEvent.cs
+++ src/MudBlazor.UnitTests/Mocks/MockJsEvent.cs
@@ -2,52 +2,38 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
 using MudBlazor.Services;
 
-namespace MudBlazor.UnitTests.Mocks
+namespace MudBlazor.UnitTests.Mocks;
+
+#nullable enable
+public class MockJsEventFactory : IJsEventFactory
 {
-#pragma warning disable CS1998
+    private readonly IJsEvent? _jsEvent;
+
+    public MockJsEventFactory() : this(null)
+    {
+    }
+
+    public MockJsEventFactory(IJsEvent? jsEvent)
+    {
+        _jsEvent = jsEvent;
+    }
+
+    public IJsEvent Create() => _jsEvent ?? new MockJsEvent();
+}
+
+public class MockJsEvent : IJsEvent
+{
 #pragma warning disable CS0067
-    public class MockJsEventFactory : IJsEventFactory
-    {
-        private readonly MockJsEvent _jsEvent;
+    public event Action<int>? CaretPositionChanged;
+    public event Action<string>? Paste;
+    public event Action<int, int>? Select;
+#pragma warning restore CS0067
 
-        public MockJsEventFactory(MockJsEvent jsEvent)
-        {
-            _jsEvent = jsEvent;
-        }
+    public Task Connect(string element, JsEventOptions options) => Task.CompletedTask;
 
-        public MockJsEventFactory()
-        {
+    public Task Disconnect() => Task.CompletedTask;
 
-        }
-
-        public IJsEvent Create() => _jsEvent ?? new MockJsEvent();
-    }
-
-    public class MockJsEvent : IJsEvent
-    {
-        public void Dispose()
-        {
-
-        }
-
-        public Task Connect(string element, JsEventOptions options)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task Disconnect()
-        {
-            return Task.CompletedTask;
-        }
-
-        public event Action<int> CaretPositionChanged;
-        public event Action<string> Paste;
-        public event Action Copy;
-        public event Action<int, int> Select;
-    }
+    public ValueTask DisposeAsync() => ValueTask.CompletedTask;
 }
