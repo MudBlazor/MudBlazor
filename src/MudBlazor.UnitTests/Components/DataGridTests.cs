a387c03169d1ab8ded2fc298432213a45a0c825e
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-10-28 23:29:05 -0500

    tests

--- src/MudBlazor.UnitTests/Components/DataGridTests.cs
+++ src/MudBlazor.UnitTests/Components/DataGridTests.cs
@@ -3182,7 +3182,7 @@ namespace MudBlazor.UnitTests.Components
             comp.Find(".filter-button").Click();
             comp.FindAll(".filters-panel").Count.Should().Be(1);
 
-            comp.FindAll("div.mud-input-control")[0].Click();
+            comp.FindAll("div.mud-input-control")[0].PointerDown();
             comp.FindAll("div.mud-list-item").Count.Should().Be(3);
         }
 
@@ -4635,7 +4635,7 @@ namespace MudBlazor.UnitTests.Components
             FilterButton().Click();
 
             IElement SelectElement() => comp.Find("div.mud-select.filter-input");
-            SelectElement().Click();
+            SelectElement().PointerDown();
 
             var items = comp.FindAll("div.mud-list-item").ToArray();
 
