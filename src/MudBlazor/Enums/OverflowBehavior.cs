aeb78f507284af50b530a7c169313de916f776bd
Jon Person <person.jon@gmail.com>
2024-11-02 22:08:20 -0600

    MudPopover: Added XML Documentation for Public Members

--- src/MudBlazor/Enums/OverflowBehavior.cs
+++ src/MudBlazor/Enums/OverflowBehavior.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.ComponentModel;
 
-namespace MudBlazor
+namespace MudBlazor;
+
+/// <summary>
+/// Controls how a component behaves when the browser is scrolled.
+/// </summary>
+public enum OverflowBehavior
 {
-    public enum OverflowBehavior
-    {
-        [Description("flip-never")]
-        FlipNever,
-        [Description("flip-onopen")]
-        FlipOnOpen,
-        [Description("flip-always")]
-        FlipAlways,
-    }
+    /// <summary>
+    /// No special behavior will occur as the browser is scrolled.
+    /// </summary>
+    [Description("flip-never")]
+    FlipNever,
+
+    /// <summary>
+    /// The component will display on-screen when opened.
+    /// </summary>
+    [Description("flip-onopen")]
+    FlipOnOpen,
+
+    /// <summary>
+    /// The component will remain visible even when the browser is scrolled.
+    /// </summary>
+    [Description("flip-always")]
+    FlipAlways,
 }
