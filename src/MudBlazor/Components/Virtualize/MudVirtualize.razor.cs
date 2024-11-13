9aa6250e8aa1f96ab335e244a70b8f49c212ef4f
Ptipoi-fj <petipoipigme@hotmail.fr>
2024-11-14 00:40:01 +0100

    Add `NoRecordsContent` to `MudVirtualize` component (`EmptyContent`)

--- src/MudBlazor/Components/Virtualize/MudVirtualize.razor.cs
+++ src/MudBlazor/Components/Virtualize/MudVirtualize.razor.cs
@@ -2,8 +2,6 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
@@ -34,6 +32,12 @@ namespace MudBlazor
         /// </summary>
         [Parameter]
         public RenderFragment? Placeholder { get; set; }
+
+        /// <summary>
+        /// The content shown when there are no rows to display.
+        /// </summary>
+        [Parameter]
+        public RenderFragment? NoRecordsContent { get; set; }
 
         /// <summary>
         /// Gets or sets the fixed item source.
