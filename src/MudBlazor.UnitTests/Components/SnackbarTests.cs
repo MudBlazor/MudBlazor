0a387e0f0a53c13fa1c84592c0b687cc62b5821f
Anu6is <curtis.mayers@gmail.com>
2024-11-20 07:37:25 -0400

    whitespace

--- src/MudBlazor.UnitTests/Components/SnackbarTests.cs
+++ src/MudBlazor.UnitTests/Components/SnackbarTests.cs
@@ -251,7 +251,6 @@ namespace MudBlazor.UnitTests.Components
             );
 
             var svg = _provider.Find("#mud-snackbar-container .mud-snackbar").FirstElementChild.FirstElementChild;
-            
             svg.ClassName.Should().Contain("mud-icon-size-large");
             svg.InnerHtml.Should().Contain("M15.73,3H8.27L3,8.27v7.46L8.27,21h7.46L21,15.73V8.27L15.73,3z M19,14.9L14.9,19H9.1L5,14.9V9.1L9.1,5h5.8L19,9.1V14.9z");
         }
