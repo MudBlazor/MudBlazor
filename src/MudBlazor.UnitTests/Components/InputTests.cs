7ba37c24dc8c30fc4478f2ed2e219677f7b0a8bb
Cedric VERNOU <cedric.vernou@hotmail.com>
2024-11-30 13:39:58 +0100

    Add tests

--- src/MudBlazor.UnitTests/Components/InputTests.cs
+++ src/MudBlazor.UnitTests/Components/InputTests.cs
@@ -1,5 +1,7 @@
 ﻿using Bunit;
 using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.UnitTests.TestComponents.Input;
 using NUnit.Framework;
 
 namespace MudBlazor.UnitTests.Components;
@@ -21,5 +23,49 @@ public class InputTests : BunitTest
         comp.SetParametersAndRender(p => p.Add(x => x.ReadOnly, true)); //no clear button when readonly
         comp.FindAll(".mud-input-clear-button").Count.Should().Be(0);
     }
-#nullable disable
+
+    [Test]
+    public async Task WithImmediate_BindValueModifiedOnChange_SoTextUpdated()
+    {
+        // Arrange
+
+        var comp = Context.RenderComponent<InputToUpperTest>(parameters => parameters
+            .Add(i => i.Immediate, true)
+        );
+        var stu = comp.Instance;
+        var input = comp.FindComponent<MudInput<string>>().Instance;
+
+        // Act
+
+        await comp.Find("input").InputAsync(new ChangeEventArgs { Value = "AbCdEfGhI" });
+
+        // Assert
+
+        stu.Value.Should().Be("ABCDEFGHI");
+        input.Value.Should().Be("ABCDEFGHI");
+        input.Text.Should().Be("ABCDEFGHI");
+    }
+
+    [Test]
+    public async Task WithoutImmediate_BindValueModifiedOnChange_SoTextUpdated()
+    {
+        // Arrange
+
+        var comp = Context.RenderComponent<InputToUpperTest>(parameters => parameters
+            .Add(i => i.Immediate, false)
+        );
+        var stu = comp.Instance;
+        var input = comp.FindComponent<MudInput<string>>().Instance;
+
+        // Act
+
+        comp.Find("input").KeyDown("a"); // Trick to force the focus on input
+        await comp.Find("input").ChangeAsync(new ChangeEventArgs { Value = "AbCdEfGhI" });
+
+        // Assert
+
+        stu.Value.Should().Be("ABCDEFGHI");
+        input.Value.Should().Be("ABCDEFGHI");
+        input.Text.Should().Be("ABCDEFGHI");
+    }
 }
