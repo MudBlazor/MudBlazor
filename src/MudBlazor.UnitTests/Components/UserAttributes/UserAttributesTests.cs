8a6058cd92010066585adac3130289b1da4d2ff2
Versile Johnson II <versilej@gmail.com>
2024-12-01 09:42:05 -0600

    fix component testing

--- src/MudBlazor.UnitTests/Components/UserAttributes/UserAttributesTests.cs
+++ src/MudBlazor.UnitTests/Components/UserAttributes/UserAttributesTests.cs
@@ -45,7 +45,7 @@ namespace MudBlazor.UnitTests.UserAttributes
             // these components do not need to have user attributes
             var excludedComponents = new HashSet<string>()
             {
-                nameof(MudPopover), nameof(MudStep), nameof(MudContextualActionBar),
+                nameof(MudPopover), nameof(MudStep), nameof(MudContextualActionBar), nameof(MudHeatMapCell),
                 "Column`1", "FooterCell`1", "HeaderCell`1", "FilterHeaderCell`1", "SelectColumn`1",
                 "HierarchyColumn`1", "PropertyColumn`2", "TemplateColumn`1",
             };
@@ -74,7 +74,7 @@ namespace MudBlazor.UnitTests.UserAttributes
                 .ToArray();
         }
 
-        private static ConcurrentBag<Type> _excludedComponents = new();
+        private static ConcurrentBag<Type> _excludedComponents = [];
         private static void Exclude(Type componentType) => _excludedComponents.Add(componentType);
     }
 }
