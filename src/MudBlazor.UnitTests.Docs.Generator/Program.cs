3788331327ab1dee51bd05a912af214598c610ec
Mike Surcouf <mike@surcouf.je>
2024-12-11 23:49:09 +0000

    Build: Split test generation

---
+++ src/MudBlazor.UnitTests.Docs.Generator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace MudBlazor.UnitTests.Docs.Generator;
+
+#nullable enable 
+public class Program
+{
+    public static int Main()
+    {
+        var stopWatch = Stopwatch.StartNew();
+        var success =
+            new TestsForExamples().Execute()
+            && new TestsForApiPages().Execute();
+
+        Console.WriteLine(@$"MudBlazor.UnitTests.Docs.Generator completed in {stopWatch.ElapsedMilliseconds} milliseconds.");
+        return success ? 0 : 1;
+    }
+}
---
+++ src/MudBlazor.UnitTests.Docs.Generator/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace MudBlazor.UnitTests.Docs.Generator;
+
+#nullable enable
+public static partial class StringExtensions
+{
+    [return: NotNullIfNotNull(nameof(self))]
+    public static string? ToLfLineEndings(this string? self)
+    {
+        return self is null
+            ? null
+            : NewLineRegularExpression().Replace(self, "\n");
+    }
+
+    [GeneratedRegex(@"\r?\n")]
+    private static partial Regex NewLineRegularExpression();
+}
