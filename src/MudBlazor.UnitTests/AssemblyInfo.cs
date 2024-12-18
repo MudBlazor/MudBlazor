36809a9f0398f43ff46a794ea00413b1baef003c
Jon Person <person.jon@gmail.com>
2024-12-17 23:53:44 -0700

    Tests: Enabled Parallelization of NUnit Test Fixtures

---
+++ src/MudBlazor.UnitTests/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using NUnit.Framework;
+
+// Make each test fixture in the test assembly run in parallel
+[assembly: Parallelizable(ParallelScope.Fixtures)]
