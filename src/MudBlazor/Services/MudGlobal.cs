f5b332bedfad178a07ec1a41748098221bad21a3
Daniel Chalmers <daniel.chalmers@outlook.com>
2025-01-07 18:17:21 -0600

    link to component

--- src/MudBlazor/Services/MudGlobal.cs
+++ src/MudBlazor/Services/MudGlobal.cs
@@ -12,12 +12,12 @@ public static class MudGlobal
     public static class ButtonDefaults
     {
         /// <summary>
-        /// The color of the button.
+        /// The color of the <see cref="MudButton"/>.
         /// </summary>
         public static Color Color { get; set; } = Color.Default;
 
         /// <summary>
-        /// The display variation to use.
+        /// The display variation to use for <see cref="MudButton"/>.
         /// </summary>
         public static Variant Variant { get; set; } = Variant.Text;
     }
@@ -25,7 +25,7 @@ public static class MudGlobal
     public static class DialogDefaults
     {
         /// <summary>
-        /// The element which will receive focus when this dialog is shown.
+        /// The element which will receive focus when this <see cref="MudDialog"/> is shown.
         /// </summary>
         public static DefaultFocus DefaultFocus { get; set; } = DefaultFocus.Element;
     }
@@ -33,7 +33,7 @@ public static class MudGlobal
     public static class GridDefaults
     {
         /// <summary>
-        /// The gap between items, measured in increments of <c>4px</c>.
+        /// The gap between items in <see cref="MudGrid"/>, measured in increments of <c>4px</c>.
         /// </summary>
         public static int Spacing { set; get; } = 6;
     }
@@ -41,17 +41,17 @@ public static class MudGlobal
     public static class InputDefaults
     {
         /// <summary>
-        /// Shows the label inside the input if no <c>Value</c> is specified.
+        /// Shows the label inside the <see cref="MudInput"/> if no <c>Value</c> is specified.
         /// </summary>
         public static bool ShrinkLabel { get; set; }
 
         /// <summary>
-        /// The appearance variation to use.
+        /// The appearance variation to use for <see cref="MudInput"/>.
         /// </summary>
         public static Variant Variant { get; set; } = Variant.Text;
 
         /// <summary>
-        /// The amount of vertical spacing for this input.
+        /// The amount of vertical spacing for this <see cref="MudInput"/>.
         /// </summary>
         public static Margin Margin { get; set; } = Margin.None;
     }
@@ -59,17 +59,17 @@ public static class MudGlobal
     public static class LinkDefaults
     {
         /// <summary>
-        /// The color of the link.
+        /// The color of the <see cref="MudLink"/>.
         /// </summary>
         public static Color Color { get; set; } = Color.Primary;
 
         /// <summary>
-        /// The typography variant to use.
+        /// The typography variant to use for <see cref="MudLink"/>.
         /// </summary>
         public static Typo Typo { get; set; } = Typo.body1;
 
         /// <summary>
-        /// Applies an underline to the link.
+        /// Applies an underline to the <see cref="MudLink"/>.
         /// </summary>
         public static Underline Underline { get; set; } = Underline.Hover;
     }
@@ -77,7 +77,7 @@ public static class MudGlobal
     public static class MenuDefaults
     {
         /// <summary>
-        /// The time in milliseconds before a menu is activated by the cursor hovering over it
+        /// The time in milliseconds before a <see cref="MudMenu"/> is activated by the cursor hovering over it
         /// or before it is hidden after the cursor leaves the menu.
         /// </summary>
         public static int HoverDelay { get; set; } = 300;
@@ -86,7 +86,7 @@ public static class MudGlobal
     public static class PopoverDefaults
     {
         /// <summary>
-        /// The amount of drop shadow to apply.
+        /// The amount of drop shadow to apply to <see cref="MudPopover"/>.
         /// </summary>
         public static int Elevation { get; set; } = 8;
     }
@@ -94,7 +94,7 @@ public static class MudGlobal
     public static class StackDefaults
     {
         /// <summary>
-        /// The gap between items, measured in increments of <c>4px</c>.
+        /// The gap between items in <see cref="MudStack"/>, measured in increments of <c>4px</c>.
         /// </summary>
         public static int Spacing { get; set; } = 3;
     }
@@ -102,12 +102,12 @@ public static class MudGlobal
     public static class TooltipDefaults
     {
         /// <summary>
-        /// The amount of time in milliseconds to wait from opening the popover before beginning to perform the transition. 
+        /// The amount of time in milliseconds to wait from opening the <see cref="MudTooltip"/> before beginning to perform the transition. 
         /// </summary>
         public static TimeSpan Delay { get; set; } = TimeSpan.Zero;
 
         /// <summary>
-        /// The length of time that the opening transition takes to complete.
+        /// The length of time that the opening transition for <see cref="MudTooltip"/> takes to complete.
         /// </summary>
         public static TimeSpan Duration { get; set; } = TimeSpan.FromMilliseconds(251);
     }
@@ -115,18 +115,18 @@ public static class MudGlobal
     public static class TransitionDefaults
     {
         /// <summary>
-        /// The length of time that the opening transition takes to complete.
+        /// The length of time that the opening transition for <see cref="MudTransition"/> takes to complete.
         /// </summary>
         public static TimeSpan Delay { get; set; } = TimeSpan.Zero;
 
         /// <summary>
-        /// The amount of time in milliseconds to wait from opening the popover before beginning to perform the transition. 
+        /// The amount of time in milliseconds to wait from opening the <see cref="MudTransition"/> before beginning to perform the transition. 
         /// </summary>
         public static TimeSpan Duration { get; set; } = TimeSpan.FromMilliseconds(251);
     }
 
     /// <summary>
-    /// Applies regular rounding by default; additional rounding if set to true; or squares them if set to false.
+    /// Applies regular rounding by default; additional rounding if set to true; or squares them if set to false for MudBlazor components.
     /// </summary>
     public static bool? Rounded { get; set; }
 
