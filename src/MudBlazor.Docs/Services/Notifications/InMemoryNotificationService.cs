4c7090e9978b578163abf27a020362901b670974
Daniel Chalmers <daniel.chalmers@outlook.com>
2025-01-17 11:10:33 -0600

    Update InMemoryNotificationService.cs

--- src/MudBlazor.Docs/Services/Notifications/InMemoryNotificationService.cs
+++ src/MudBlazor.Docs/Services/Notifications/InMemoryNotificationService.cs
@@ -92,7 +92,7 @@ public class InMemoryNotificationService : INotificationService
             "v8: Riding the Momentum of v7",
             "Learn about the new major version",
             "Announcement",
-            new DateTime(2025, 01, 15),
+            new DateTime(2025, 01, 19),
             "https://github.com/MudBlazor/MudBlazor/blob/f979c2c84e3ddd5f01a20ebc1102838d32a4b01b/content/Nuget.png",
             [
                 new NotificationAuthor("The MudBlazor Team", "https://mudblazor.com/_content/MudBlazor.Docs/images/logo.png")
