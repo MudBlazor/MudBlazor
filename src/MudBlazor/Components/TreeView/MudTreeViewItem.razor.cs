73840cd0f2cd37fdb84c36a813ef638350ec8891
René Paw Christensen <spam1@rpc-scandinavia.dk>
2025-02-12 09:05:19 +0100

    ExpandToSelectedAsync
    Changed after being insulted by "SonarQube".

--- src/MudBlazor/Components/TreeView/MudTreeViewItem.razor.cs
+++ src/MudBlazor/Components/TreeView/MudTreeViewItem.razor.cs
@@ -600,9 +600,12 @@ namespace MudBlazor
             return _selectedState ? CheckedIcon : UncheckedIcon;
         }
 
-		internal async Task ExpandToSelectedAsync(Int32 level) {
-			if ((this.MudTreeRoot != null) && (this.MudTreeRoot.IsInExpandedPath != null) && (this.Value is not null)) {
-				if (this.Selected == true) {
+		internal async Task ExpandToSelectedAsync(Int32 level)
+        {
+			if ((this.MudTreeRoot != null) && (this.MudTreeRoot.IsInExpandedPath != null) && (this.Value is not null))
+            {
+				if (this.Selected)
+                {
 					HashSet<T> selectedValues = new HashSet<T>();
 					selectedValues.Add(this.Value);
 					await UpdateSelectionStateAsync(selectedValues);
@@ -614,14 +617,17 @@ namespace MudBlazor
 				}
 
 				// Try to expand this item, if it is in the path to a selected item.
-				if (this.MudTreeRoot.IsInExpandedPath(this.Value, level) == true) {
+				if (this.MudTreeRoot.IsInExpandedPath(this.Value, level))
+                {
 					await _expandedState.SetValueAsync(true);
 					await this.TryInvokeServerLoadFunc();
 				}
 
 				// Iterate through all the child items.
-				if (this._childItems != null) {
-					foreach (MudTreeViewItem<T> childItem in this._childItems) {
+				if (this._childItems != null)
+                {
+					foreach (MudTreeViewItem<T> childItem in this._childItems)
+                    {
 						await childItem.ExpandToSelectedAsync(level + 1);
 					}
 				}
