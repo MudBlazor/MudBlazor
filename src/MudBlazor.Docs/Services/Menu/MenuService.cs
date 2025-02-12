788d9f65dbc41833060491b6457cfe417132d658
Anu6is <curtis.mayers@gmail.com>
2025-02-12 15:33:29 -0400

    new doc page for parameter state

--- src/MudBlazor.Docs/Services/Menu/MenuService.cs
+++ src/MudBlazor.Docs/Services/Menu/MenuService.cs
@@ -139,6 +139,7 @@ namespace MudBlazor.Docs.Services
                 new DocsLink {Title = "Elevation", Href = "features/elevation"},
                 new DocsLink {Title = "Converters", Href = "features/converters"},
                 new DocsLink {Title = "Icon Reference", Href = "features/icons"}, // <-- note: title changed from "Icons" to "Icon Reference" to avoid confusion in Search box with the MudIcon page which is also called "Icons"
+                new DocsLink {Title = "Parameter State", Href = "features/parameterstate"},
                 new DocsLink {Title = "Masking", Href = "features/masking"},
                 new DocsLink {Title = "RTL Languages", Href = "features/rtl-languages"},
                 new DocsLink {Title = "Localization", Href = "features/localization"},
