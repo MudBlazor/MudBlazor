2ac1f6b7e195f7978762946828852c15dd419e94
Mathieu Aubin <Mathdx@hotmail.com>
2025-05-29 15:14:16 -0400

    check if js is readybefor calling it

--- src/MudBlazor/Base/MudBaseInput.cs
+++ src/MudBlazor/Base/MudBaseInput.cs
@@ -791,7 +791,7 @@ namespace MudBlazor
 
         protected async Task HandleContainerClick()
         {
-            if (_isFocused == false)
+            if (_isFocused == false && IsJSRuntimeAvailable)
             {
                 await JsRuntime.InvokeVoidAsync("mudInput.focusInput", InputElementId);
             }
