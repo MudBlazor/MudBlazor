65b4665f244a775db64421d094def46cd208da3c
Versile Johnson II <versilej@gmail.com>
2025-06-28 15:44:35 -0500

    updates for invokeasync and docs

--- src/MudBlazor/Components/Tabs/MudTabs.razor.cs
+++ src/MudBlazor/Components/Tabs/MudTabs.razor.cs
@@ -654,7 +654,7 @@ namespace MudBlazor
                 SetScrollButtonVisibility();
                 SetScrollabilityStates();
                 Rerender();
-                StateHasChanged();
+                await InvokeAsync(StateHasChanged);
             }
         }
 
