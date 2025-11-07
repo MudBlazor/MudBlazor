41af70f9f50f46c55516fba7e6038ac30872f29b
Kyle Clemente <KClemente@4feldco.com>
2025-11-07 17:15:04 -0600

    Issue #12063 resolved

--- src/MudBlazor/Components/DatePicker/MudBaseDatePicker.razor.cs
+++ src/MudBlazor/Components/DatePicker/MudBaseDatePicker.razor.cs
@@ -476,6 +476,7 @@ namespace MudBlazor
             if (nextView != null)
             {
                 CurrentView = (OpenTo)nextView;
+                HighlightedDate = new DateTime(year,1,1);
             }
 
             return Task.CompletedTask;
