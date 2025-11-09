248a5aa66198d492673c96097a79491af9519e58
Daniel Chalmers <daniel.chalmers@outlook.com>
2025-11-09 14:04:14 -0600

    MudTreeView: Add ITreeItemData and make Children IReadOnlyCollection

--- src/MudBlazor/Components/TreeView/TreeItemData.cs
+++ src/MudBlazor/Components/TreeView/TreeItemData.cs
@@ -9,10 +9,63 @@ namespace MudBlazor;
 #nullable enable
 
 /// <summary>
+/// Provides the contract for data that can be rendered inside a <see cref="MudTreeView{T}"/>.
+/// </summary>
+/// <typeparam name="T">The type of value associated with each item.</typeparam>
+public interface ITreeItemData<T>
+{
+    /// <summary>
+    /// The text of this item.
+    /// </summary>
+    string? Text { get; set; }
+
+    /// <summary>
+    /// The icon for this item.
+    /// </summary>
+    string? Icon { get; set; }
+
+    /// <summary>
+    /// The value associated with this item.
+    /// </summary>
+    T? Value { get; }
+
+    /// <summary>
+    /// Whether this item is expanded.
+    /// </summary>
+    bool Expanded { get; set; }
+
+    /// <summary>
+    /// Whether this item can be expanded.
+    /// </summary>
+    bool Expandable { get; set; }
+
+    /// <summary>
+    /// Whether this item is selected.
+    /// </summary>
+    bool Selected { get; set; }
+
+    /// <summary>
+    /// Whether this item is displayed.
+    /// </summary>
+    bool Visible { get; set; }
+
+    /// <summary>
+    /// The child items underneath this item.
+    /// </summary>
+    IReadOnlyCollection<ITreeItemData<T>>? Children { get; set; }
+
+    /// <summary>
+    /// Whether this item contains child items.
+    /// </summary>
+    [MemberNotNullWhen(true, nameof(Children))]
+    bool HasChildren { get; }
+}
+
+/// <summary>
 /// The current state of a <see cref="MudTreeViewItem{T}"/>.
 /// </summary>
 /// <typeparam name="T">The type of item to display.</typeparam>
-public class TreeItemData<T> : IEquatable<TreeItemData<T>>
+public class TreeItemData<T> : ITreeItemData<T>, IEquatable<TreeItemData<T>>
 {
     public TreeItemData() : this(default) { }
 
@@ -80,7 +133,7 @@ public class TreeItemData<T> : IEquatable<TreeItemData<T>>
     /// <summary>
     /// The child items underneath this item.
     /// </summary>
-    public virtual List<TreeItemData<T>>? Children { get; set; }
+    public virtual IReadOnlyCollection<ITreeItemData<T>>? Children { get; set; }
 
     /// <summary>
     /// Whether this item contains child items.
