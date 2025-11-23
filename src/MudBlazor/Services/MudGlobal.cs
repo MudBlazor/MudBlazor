04ad09a955e918d2c8aedcb99a119460db72504c
Daniel Chalmers <daniel.chalmers@outlook.com>
2025-11-23 11:30:21 -0600

    Update src/MudBlazor/Services/MudGlobal.cs
    Co-authored-by: Versile Johnson II <148913404+versile2@users.noreply.github.com>

--- src/MudBlazor/Services/MudGlobal.cs
+++ src/MudBlazor/Services/MudGlobal.cs
@@ -147,7 +147,7 @@ public static class MudGlobal
         /// Only applies to components that use a <see cref="MudPopover"/> in conjunction with a <see cref="MudOverlay"/>
         /// to close the popover when a user clicks outside, such as <see cref="MudSelect{T}"/>.
         /// </remarks>
-        public static bool ModalOverlay { get; set; } = false;
+        public static bool ModalOverlay { get; set; }
     }
 
     /// <summary>
