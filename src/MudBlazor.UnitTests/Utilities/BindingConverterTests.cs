b8e37a20a5029411b26247d29d4c75bb048fe63d
ScarletKuro <19953225+ScarletKuro@users.noreply.github.com>
2025-12-03 18:29:38 +0200

    Remove those that don't require port

--- src/MudBlazor.UnitTests/Utilities/BindingConverterTests.cs
+++ src/MudBlazor.UnitTests/Utilities/BindingConverterTests.cs
@@ -2,80 +2,16 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Drawing;
 using System.Globalization;
-using System.Text.Json;
 using FluentAssertions;
-using MudBlazor.Resources;
 using NUnit.Framework;
 
+#nullable enable
 namespace MudBlazor.UnitTests.Utilities
 {
     [TestFixture]
     public class BindingConverterTests
     {
-        //[Test]
-        //public void BaseConverter_ErrorCases()
-        //{
-        //    string errorMessage = LanguageResource.Converter_ConversionFailed;
-
-        //    // Test null
-        //    var c1 = new Converter<object, string>
-        //    {
-        //        GetFunc = null,
-        //        SetFunc = null
-        //    };
-        //    c1.Get("a-z").Should().Be(default(object));
-        //    c1.Set("a-z").Should().Be(default(string));
-
-        //    // Test invalid format
-        //    var exception = "Conversion failed.";
-        //    var c2 = new Converter<object, string>
-        //    {
-        //        GetFunc = (value) => { throw new FormatException(exception); },
-        //        SetFunc = (value) => { throw new FormatException(exception); }
-        //    };
-        //    c2.Get("a-z").Should().Be(null);
-        //    c2.GetError.Should().BeTrue();
-        //    c2.GetErrorMessage.HasValue.Should().BeTrue();
-        //    c2.GetErrorMessage.Value.Item1.Should().Be(errorMessage);
-        //    c2.GetErrorMessage.Value.Item2.Should().BeEquivalentTo([typeof(string).Name, typeof(object).Name, exception]);
-
-        //    c2.Set("a-z").Should().Be(null);
-        //    c2.SetError.Should().BeTrue();
-        //    c2.SetErrorMessage.HasValue.Should().BeTrue();
-        //    c2.SetErrorMessage.Value.Item1.Should().Be(errorMessage);
-        //    c2.SetErrorMessage.Value.Item2.Should().BeEquivalentTo([typeof(object).Name, typeof(string).Name, exception]);
-        //}
-
-        //[Test]
-        //public void DefaultConverter_GlobalFunc_ValidCases()
-        //{
-        //    var c1 = new DefaultConverter<Point>();
-        //    DefaultConverter<Point>.GlobalGetFunc = x => $"[{x.X},{x.Y}]";
-        //    DefaultConverter<Point>.GlobalSetFunc = x => { var tmp = JsonSerializer.Deserialize<int[]>(x); return new Point(tmp[0], tmp[1]); };
-
-        //    c1.Set(new Point(1, 2)).Should().Be("[1,2]");
-        //    c1.Get("[1,2]").Should().Be(new Point(1, 2));
-        //}
-
-        //[Test]
-        //public void DefaultConverter_GlobalFunc_ErrorCases()
-        //{
-        //    string errorMessage = LanguageResource.Converter_InvalidType;
-        //    object[] errorArgs = [typeof(Point).Name];
-
-        //    var c1 = new DefaultConverter<Point>();
-        //    DefaultConverter<Point>.GlobalSetFunc = x => { var tmp = JsonSerializer.Deserialize<int[]>(x); return new Point(tmp[0], tmp[1]); };
-
-        //    c1.Get("[1,2").Should().Be(Point.Empty);
-        //    c1.GetError.Should().BeTrue();
-        //    c1.GetErrorMessage.HasValue.Should().BeTrue();
-        //    c1.GetErrorMessage.Value.Item1.Should().Be(errorMessage);
-        //    c1.GetErrorMessage.Value.Item2.Should().BeEquivalentTo(errorArgs);
-        //}
-
         [Test]
         public void DefaultConverter_String_ValidCases()
         {
@@ -127,19 +63,6 @@ namespace MudBlazor.UnitTests.Utilities
             c2.Convert(null).Should().Be(null);
             c2.ConvertBack(null).Should().Be(null);
         }
-
-        //[Test]
-        //public void DefaultConverter_Bool_ErrorCases()
-        //{
-        //    var invalidInput = "a-z";
-        //    var booleanErrorMessage = LanguageResource.Converter_InvalidBoolean;
-
-        //    // Test invalid format for bool
-        //    TestInvalidFormat<bool>(invalidInput, booleanErrorMessage);
-
-        //    // Test invalid format for nullable bool
-        //    TestInvalidFormat<bool?>(invalidInput, booleanErrorMessage);
-        //}
 
         [Test]
         public void DefaultConverter_Numeric_ValidCases()
@@ -224,46 +147,6 @@ namespace MudBlazor.UnitTests.Utilities
             c16.Convert(null).Should().Be(null);
             c16.ConvertBack(null).Should().Be(null);
         }
-
-        //[Test]
-        //public void DefaultConverter_Numeric_ErrorCases()
-        //{
-        //    var invalidInput = "a-z";
-        //    var numberErrorMessage = LanguageResource.Converter_InvalidNumber;
-
-        //    // Test invalid format for various numeric types
-        //    TestInvalidFormat<sbyte>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<sbyte?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<byte>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<byte?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<short>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<short?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<ushort>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<ushort?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<int>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<int?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<uint>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<uint?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<long>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<long?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<ulong>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<ulong?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<float>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<float?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<double>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<double?>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<decimal>(invalidInput, numberErrorMessage);
-        //    TestInvalidFormat<decimal?>(invalidInput, numberErrorMessage);
-
-        //    // invalid format for type supplied
-        //    var c12 = new DefaultConverter<int?>();
-        //    c12.Format = "dd/mm/yy";
-        //    c12.Get(c12.Set(22)).Should().Be(null);
-        //    c12.GetError.Should().BeTrue();
-        //    c12.GetErrorMessage.HasValue.Should().BeTrue();
-        //    c12.GetErrorMessage.Value.Item1.Should().Be(numberErrorMessage);
-        //    c12.GetErrorMessage.Value.Item2.Should().BeEmpty();
-        //}
 
         [Test]
         public void DefaultConverter_Numeric_Culture_AffectsConversion()
@@ -389,19 +272,6 @@ namespace MudBlazor.UnitTests.Utilities
             c2.ConvertBack(null).Should().Be(null);
         }
 
-        //[Test]
-        //public void DefaultConverter_Guid_ErrorCases()
-        //{
-        //    var invalidInput = "a-z";
-        //    var guidErrorMessage = LanguageResource.Converter_InvalidGUID;
-
-        //    // Test invalid format for Guid
-        //    TestInvalidFormat<Guid>(invalidInput, guidErrorMessage);
-
-        //    // Test invalid format for nullable Guid
-        //    TestInvalidFormat<Guid?>(invalidInput, guidErrorMessage);
-        //}
-
         [Test]
         public void DefaultConverter_Enum_ValidCases()
         {
@@ -424,20 +294,6 @@ namespace MudBlazor.UnitTests.Utilities
         }
 
         //[Test]
-        //public void DefaultConverter_Enum_ErrorCases()
-        //{
-        //    var invalidInput = "a-z";
-        //    string enumErrorMessage = LanguageResource.Converter_NotValueOf;
-        //    object[] enumErrorArgs = [typeof(YesNoMaybe).Name];
-
-        //    // Test invalid format for Guid
-        //    TestInvalidFormat<YesNoMaybe>(invalidInput, enumErrorMessage, enumErrorArgs);
-
-        //    // Test invalid format for nullable Guid
-        //    TestInvalidFormat<YesNoMaybe?>(invalidInput, enumErrorMessage, enumErrorArgs);
-        //}
-
-        //[Test]
         //public void DefaultConverter_DateTime_ValidCases()
         //{
         //    var c1 = new DefaultConverter<DateTime>();
@@ -451,42 +307,27 @@ namespace MudBlazor.UnitTests.Utilities
         //    c2.ConvertBack(null).Should().Be(null);
         //}
 
-        //[Test]
-        //public void DefaultConverter_DateTime_Format_AffectsConversion()
-        //{
-        //    var dt1 = new DefaultConverter<DateTime>() { Format = "MM/dd/yyyy" };
-        //    dt1.Culture = new CultureInfo("en-US", false);
-        //    dt1.Set(new DateTime(2020, 11, 03)).Should().Be("11/03/2020");
-        //    dt1.Get("11/03/2020").Should().Be(new DateTime(2020, 11, 03));
-        //    dt1.Culture = new CultureInfo("pt-BR", false);
-        //    dt1.Format = "dd/MM/yyyy";
-        //    dt1.Set(new DateTime(2020, 11, 03)).Should().Be("03/11/2020");
-        //    dt1.Get("03/11/2020").Should().Be(new DateTime(2020, 11, 03));
+        [Test]
+        public void DefaultConverter_DateTime_Format_AffectsConversion()
+        {
+            var dt1 = new DefaultConverter<DateTime> { Format = () => "MM/dd/yyyy", Culture = () => new CultureInfo("en-US", false) };
+            dt1.Convert(new DateTime(2020, 11, 03)).Should().Be("11/03/2020");
+            dt1.ConvertBack("11/03/2020").Should().Be(new DateTime(2020, 11, 03));
+            dt1.Culture = () => new CultureInfo("pt-BR", false);
+            dt1.Format = () => "dd/MM/yyyy";
+            dt1.Convert(new DateTime(2020, 11, 03)).Should().Be("03/11/2020");
+            dt1.ConvertBack("03/11/2020").Should().Be(new DateTime(2020, 11, 03));
 
-        //    var dt2 = new DefaultConverter<DateTime?>() { Format = "MM/dd/yyyy" };
-        //    dt2.Culture = new CultureInfo("en-US", false);
-        //    dt2.Set(new DateTime(2020, 11, 03)).Should().Be("11/03/2020");
-        //    dt2.Set(null).Should().Be(null);
-        //    dt2.Get("11/03/2020").Should().Be(new DateTime(2020, 11, 03));
-        //    dt2.Get(null).Should().Be(null);
-        //    dt2.Culture = new CultureInfo("pt-BR", false);
-        //    dt2.Format = "dd/MM/yyyy";
-        //    dt2.Set(new DateTime(2020, 11, 03)).Should().Be("03/11/2020");
-        //    dt2.Get("03/11/2020").Should().Be(new DateTime(2020, 11, 03));
-        //}
-
-        //[Test]
-        //public void DefaultConverter_DateTime_ErrorCases()
-        //{
-        //    var invalidInput = "a-z";
-        //    var dtErrorMessage = LanguageResource.Converter_InvalidDateTime;
-
-        //    // Test invalid format for DateTime
-        //    TestInvalidFormat<DateTime>(invalidInput, dtErrorMessage);
-
-        //    // Test invalid format for nullable DateTime
-        //    TestInvalidFormat<DateTime?>(invalidInput, dtErrorMessage);
-        //}
+            var dt2 = new DefaultConverter<DateTime?> { Format = () => "MM/dd/yyyy", Culture = () => new CultureInfo("en-US", false) };
+            dt2.Convert(new DateTime(2020, 11, 03)).Should().Be("11/03/2020");
+            dt2.Convert(null).Should().Be(null);
+            dt2.ConvertBack("11/03/2020").Should().Be(new DateTime(2020, 11, 03));
+            dt2.ConvertBack(null).Should().Be(null);
+            dt2.Culture = () => new CultureInfo("pt-BR", false);
+            dt2.Format = () => "dd/MM/yyyy";
+            dt2.Convert(new DateTime(2020, 11, 03)).Should().Be("03/11/2020");
+            dt2.ConvertBack("03/11/2020").Should().Be(new DateTime(2020, 11, 03));
+        }
 
         [Test]
         public void DefaultConverter_TimeSpan_ValidCases()
@@ -518,142 +359,14 @@ namespace MudBlazor.UnitTests.Utilities
             converter.ConvertBack("01:02:03.456").Should().Be(preciseTime);
         }
 
-        //[Test]
-        //public void DefaultConverter_TimeSpan_ErrorCases()
-        //{
-        //    var invalidInput = "12:o1";
-        //    var overflowInput = "25:00";
-        //    var tmErrorMessage = LanguageResource.Converter_InvalidTimeSpan;
+        [Test]
+        public void DefaultConverter_NotImplementedType_ValidCases()
+        {
+            var notImplementedType = new object();
 
-        //    // Test invalid format for TimeSpan
-        //    TestInvalidFormat<TimeSpan>(invalidInput, tmErrorMessage);
-
-        //    // Test invalid format for nullable TimeSpan
-        //    TestInvalidFormat<TimeSpan?>(invalidInput, tmErrorMessage);
-
-        //    // Test timespan overflow for TimeSpan
-        //    TestInvalidFormat<TimeSpan>(overflowInput, tmErrorMessage);
-
-        //    // Test timespan overflow for nullable TimeSpan
-        //    TestInvalidFormat<TimeSpan?>(overflowInput, tmErrorMessage);
-        //}
-
-        //[Test]
-        //public void DefaultConverter_NotImplementedType_ValidCases()
-        //{
-        //    var notImplementedType = new object();
-
-        //    var c1 = new DefaultConverter<object>();
-        //    c1.Set(notImplementedType).Should().Be(notImplementedType.ToString());
-        //}
-
-        //[Test]
-        //public void DefaultConverter_NotImplementedType_ErrorCases()
-        //{
-        //    string errorMessage = LanguageResource.Converter_ConversionNotImplemented;
-        //    object[] errorArgs = [typeof(object)];
-
-        //    // Test invalid format
-        //    TestInvalidFormat<object>("a-z", errorMessage, errorArgs);
-        //}
-
-        //private void TestInvalidFormat<T>(string input, string expectedErrorMessage, object[] expectedErrorArguments = null)
-        //{
-        //    bool onErrorInvoked = false;
-
-        //    var converter = new DefaultConverter<T>
-        //    {
-        //        OnError = (string msg, object[] arguments) => onErrorInvoked = true,
-        //    };
-
-        //    converter.Get(input).Should().Be(default(T));
-
-        //    // Test GetError
-        //    converter.GetError.Should().BeTrue();
-
-        //    // Test OnError callback
-        //    onErrorInvoked.Should().BeTrue();
-
-        //    // Test GetErrorMessage
-        //    converter.GetErrorMessage.HasValue.Should().BeTrue();
-        //    converter.GetErrorMessage.Value.Item1.Should().Be(expectedErrorMessage);
-        //    if (expectedErrorArguments == null)
-        //    {
-        //        converter.GetErrorMessage.Value.Item2.Should().BeEmpty();
-        //    }
-        //    else
-        //    {
-        //        converter.GetErrorMessage.Value.Item2.Should().BeEquivalentTo(expectedErrorArguments);
-        //    }
-        //}
-
-        //[Test]
-        //public void DefaultConverter_GlobalCatch_ErrorCases()
-        //{
-        //    bool toBeThrow = true;
-
-        //    string exception = "test";
-        //    var convertFromString = new DefaultConverter<object>()
-        //    {
-        //        // The goal is to trigger the catch block in DefaultConverter without propagating the exception to the Converter.Get method.
-        //        OnError = (string msg, object[] arguments) =>
-        //        {
-        //            if (toBeThrow)
-        //            {
-        //                toBeThrow = false;
-        //                throw new FormatException(exception);
-        //            }
-        //        },
-        //    };
-
-        //    convertFromString.Get("a-z").Should().Be(null);
-        //    convertFromString.GetError.Should().BeTrue();
-        //    convertFromString.GetErrorMessage.HasValue.Should().BeTrue();
-        //    convertFromString.GetErrorMessage.Value.Item1.Should().Be(LanguageResource.Converter_ConversionError);
-        //    convertFromString.GetErrorMessage.Value.Item2.Should().BeEquivalentTo(new object[] { exception });
-
-        //    toBeThrow = true;
-
-        //    var convertToString = new DefaultConverter<object>();
-        //    DefaultConverter<object>.GlobalGetFunc = x => throw new FormatException(exception);
-
-        //    convertFromString.Set("a-z").Should().Be(null);
-        //    convertFromString.SetError.Should().BeTrue();
-        //    convertFromString.SetErrorMessage.HasValue.Should().BeTrue();
-        //    convertFromString.SetErrorMessage.Value.Item1.Should().Be(LanguageResource.Converter_ConversionFailed);
-        //    convertFromString.SetErrorMessage.Value.Item2.Should().BeEquivalentTo(new object[] { typeof(string).Name, typeof(object).Name, exception });
-        //    DefaultConverter<object>.GlobalGetFunc = null;
-        //}
-
-
-
-        //[Test]
-        //public void CustomlConverter_ValidCases()
-        //{
-        //    var conv = new MyTestConverter();
-        //    conv.Set(null).Should().Be("nada");
-        //    conv.Get("nada").Should().Be(null);
-        //    conv.Set(18).Should().Be("18");
-        //    conv.Get("18").Should().Be(18);
-        //}
-
-        // a custom converter used only in test cases
-        //private class MyTestConverter : DefaultConverter<int?>
-        //{
-        //    protected override int? ConvertFromString(string value)
-        //    {
-        //        if (value == "nada")
-        //            return null;
-        //        return base.ConvertFromString(value);
-        //    }
-
-        //    protected override string ConvertToString(int? arg)
-        //    {
-        //        if (arg == null)
-        //            return "nada";
-        //        return base.ConvertToString(arg);
-        //    }
-        //}
+            var c1 = new DefaultConverter<object>();
+            c1.Convert(notImplementedType).Should().Be(notImplementedType.ToString());
+        }
 
         private enum YesNoMaybe { Maybe, Yes, No }
     }
