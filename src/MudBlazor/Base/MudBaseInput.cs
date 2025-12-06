91ccdfe4ab9d43ba5a91142a2b9b7072ccf89891
ScarletKuro <19953225+ScarletKuro@users.noreply.github.com>
2025-12-07 01:25:39 +0200

    Fix another incorrect usage

--- src/MudBlazor/Base/MudBaseInput.cs
+++ src/MudBlazor/Base/MudBaseInput.cs
@@ -641,7 +641,7 @@ namespace MudBlazor
 
             _userAttributesId = UserAttributes.FirstOrDefault(userAttribute => userAttribute.Key.Equals("id", StringComparison.InvariantCultureIgnoreCase)).Value?.ToString();
 
-            if (InputId is null)
+            if (_inputIdState.Value is null)
             {
                 await UpdateInputIdStateAsync();
             }
