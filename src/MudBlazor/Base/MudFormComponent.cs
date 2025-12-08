f10186aed03a1dbfb2aefd0a6d8af8bd7ee10ccf
ScarletKuro <19953225+ScarletKuro@users.noreply.github.com>
2025-12-08 18:17:26 +0200

    nits

--- src/MudBlazor/Base/MudFormComponent.cs
+++ src/MudBlazor/Base/MudFormComponent.cs
@@ -838,7 +838,7 @@ namespace MudBlazor
 
         protected virtual T? ConvertGet(U? input)
         {
-            var converter = _converterState.Value;
+            var converter = GetConverter();
             if (converter is null)
             {
                 throw new InvalidOperationException(
@@ -856,7 +856,7 @@ namespace MudBlazor
 
         protected virtual U? ConvertSet(T? input)
         {
-            var converter = _converterState.Value;
+            var converter = GetConverter();
             if (converter is null)
             {
                 throw new InvalidOperationException(
