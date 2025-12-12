23a93efe9736e34c235eb0615971a3d8e051c6f6
Jan Haubold <9191057+91378246@users.noreply.github.com>
2025-12-12 16:15:02 +0100

    Revert namespace change

--- src/MudBlazor.UnitTests.Shared/Mocks/MockScrollServices.cs
+++ src/MudBlazor.UnitTests.Shared/Mocks/MockScrollServices.cs
@@ -1,62 +1,64 @@
-﻿namespace MudBlazor.UnitTests.Shared.Mocks;
-
-public class MockScrollListenerFactory : IScrollListenerFactory
+﻿namespace MudBlazor.UnitTests.Shared.Mocks
 {
-    public IScrollListener Create(string? selector, int reportRateMs = 100, bool fireOnStart = false)
+    public class MockScrollListenerFactory : IScrollListenerFactory
     {
-        return new MockScrollListener { Selector = selector, ReportRateMs = reportRateMs, FireOnStart = fireOnStart };
-    }
-}
-
-/// <summary>
-/// Mock for scroll listener
-/// </summary>
-public class MockScrollListener : IScrollListener
-{
-    public string? Selector { get; set; }
-    public int ReportRateMs { get; set; }
-    public bool FireOnStart { get; set; }
-
-    public event EventHandler<ScrollEventArgs>? OnScroll;
-
-    public MockScrollListener()
-    {
-        OnScroll?.Invoke(this, new ScrollEventArgs());
+        public IScrollListener Create(string? selector, int reportRateMs = 100, bool fireOnStart = false)
+        {
+            return new MockScrollListener { Selector = selector, ReportRateMs = reportRateMs, FireOnStart = fireOnStart };
+        }
     }
 
-    public void Dispose() { }
-}
+    /// <summary>
+    /// Mock for scroll listener
+    /// </summary>
+    public class MockScrollListener : IScrollListener
+    {
+        public string? Selector { get; set; }
+        public int ReportRateMs { get; set; }
+        public bool FireOnStart { get; set; }
 
-/// <summary>
-/// Mock for scroll manager
-/// </summary>
-public class MockScrollManager : IScrollManager
-{
-    public string? Selector { get; set; }
+        public event EventHandler<ScrollEventArgs>? OnScroll;
 
-    public ValueTask LockScrollAsync(string elementId, string cssClass) => ValueTask.CompletedTask;
+        public MockScrollListener()
+        {
+            OnScroll?.Invoke(this, new ScrollEventArgs());
+        }
 
-    public Task ScrollTo(int left, int top, ScrollBehavior scrollBehavior) => Task.CompletedTask;
+        public void Dispose() { }
+    }
 
-    public ValueTask ScrollToAsync(string? id, int left, int top, ScrollBehavior scrollBehavior) => ValueTask.CompletedTask;
+    /// <summary>
+    /// Mock for scroll manager
+    /// </summary>
+    public class MockScrollManager : IScrollManager
+    {
+        public string? Selector { get; set; }
 
-    public ValueTask ScrollIntoViewAsync(string? selector, ScrollBehavior behavior) => ValueTask.CompletedTask;
+        public ValueTask LockScrollAsync(string elementId, string cssClass) => ValueTask.CompletedTask;
 
-    public Task ScrollToFragment(string id, ScrollBehavior behavior) => Task.CompletedTask;
+        public Task ScrollTo(int left, int top, ScrollBehavior scrollBehavior) => Task.CompletedTask;
 
-    public ValueTask ScrollToFragmentAsync(string id, ScrollBehavior behavior) => ValueTask.CompletedTask;
+        public ValueTask ScrollToAsync(string? id, int left, int top, ScrollBehavior scrollBehavior) => ValueTask.CompletedTask;
 
-    public ValueTask ScrollToListItemAsync(string elementId) => ValueTask.CompletedTask;
+        public ValueTask ScrollIntoViewAsync(string? selector, ScrollBehavior behavior) => ValueTask.CompletedTask;
 
-    public Task ScrollToTop(ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => Task.CompletedTask;
+        public Task ScrollToFragment(string id, ScrollBehavior behavior) => Task.CompletedTask;
 
-    public ValueTask ScrollToTopAsync(string? id, ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => ValueTask.CompletedTask;
+        public ValueTask ScrollToFragmentAsync(string id, ScrollBehavior behavior) => ValueTask.CompletedTask;
 
-    public ValueTask ScrollToBottomAsync(string id, ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => ValueTask.CompletedTask;
+        public ValueTask ScrollToListItemAsync(string elementId) => ValueTask.CompletedTask;
 
-    public ValueTask ScrollToYearAsync(string elementId) => ValueTask.CompletedTask;
+        public Task ScrollToTop(ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => Task.CompletedTask;
 
-    public ValueTask UnlockScrollAsync(string elementId, string cssClass) => ValueTask.CompletedTask;
+        public ValueTask ScrollToTopAsync(string? id, ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => ValueTask.CompletedTask;
 
-    public ValueTask ScrollToVirtualizedItemAsync(string containerId, int itemIndex, double itemHeight, string targetItemId, ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => ValueTask.CompletedTask;
+        public ValueTask ScrollToBottomAsync(string id, ScrollBehavior scrollBehavior = ScrollBehavior.Auto) => ValueTask.CompletedTask;
+
+        public ValueTask ScrollToYearAsync(string elementId) => ValueTask.CompletedTask;
+
+        public ValueTask UnlockScrollAsync(string elementId, string cssClass) => ValueTask.CompletedTask;
+
+        public ValueTask ScrollToVirtualizedItemAsync(string containerId, int itemIndex, double itemHeight, string targetItemId, ScrollBehavior scrollBehavior = ScrollBehavior.Auto) =>
+            ValueTask.CompletedTask;
+    }
 }
--- src/MudBlazor/Services/Scroll/ScrollListener.cs
+++ src/MudBlazor/Services/Scroll/ScrollListener.cs
@@ -1,130 +1,132 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.JSInterop;
 
-namespace MudBlazor;
-#nullable enable
-/// <summary>
-/// Listens to scroll events on a specified element.
-/// </summary>
-internal sealed class ScrollListener : IScrollListener
+namespace MudBlazor
 {
-    private bool _disposed;
-    private readonly IJSRuntime _js;
-    private EventHandler<ScrollEventArgs>? _onScroll;
-    private DotNetObjectReference<ScrollListener>? _dotNetRef;
-    private ScrollEventArgs? _initialScrollEvent;
-    private bool _started;
-
-    /// <inheritdoc />
-    public string? Selector { get; set; }
-
-    /// <inheritdoc />
-    public int ReportRateMs { get; set; }
-
-    /// <inheritdoc />
-    public bool FireOnStart { get; set; }
-
+    #nullable enable
     /// <summary>
-    /// Initializes a new instance of the <see cref="ScrollListener"/> class with the specified JavaScript runtime.
+    /// Listens to scroll events on a specified element.
     /// </summary>
-    /// <param name="js">The JavaScript runtime.</param>
-    [DynamicDependency(nameof(RaiseOnScroll))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ScrollEventArgs))]
-    public ScrollListener(IJSRuntime js) : this(string.Empty, js) { }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ScrollListener"/> class with the specified selector and JavaScript runtime.
-    /// </summary>
-    /// <param name="selector">The CSS selector for the element to listen for scroll events.</param>
-    /// <param name="js">The JavaScript runtime.</param>
-    /// <param name="reportRateMs"></param>
-    /// <param name="fireOnStart"></param>
-    public ScrollListener(string? selector, IJSRuntime js, int reportRateMs = 100, bool fireOnStart = false)
+    internal sealed class ScrollListener : IScrollListener
     {
-        _js = js;
-        Selector = selector;
-        ReportRateMs = reportRateMs;
-        FireOnStart = fireOnStart;
-    }
+        private bool _disposed;
+        private readonly IJSRuntime _js;
+        private EventHandler<ScrollEventArgs>? _onScroll;
+        private DotNetObjectReference<ScrollListener>? _dotNetRef;
+        private ScrollEventArgs? _initialScrollEvent;
+        private bool _started;
 
-    /// <inheritdoc />
-    public event EventHandler<ScrollEventArgs> OnScroll
-    {
-        add => Subscribe(value);
-        remove => Unsubscribe(value);
-    }
+        /// <inheritdoc />
+        public string? Selector { get; set; }
 
-    private async void Subscribe(EventHandler<ScrollEventArgs> value)
-    {
-        var isFirstSubscriber = _onScroll == null;
-        _onScroll += value;
+        /// <inheritdoc />
+        public int ReportRateMs { get; set; }
 
-        if (isFirstSubscriber)
+        /// <inheritdoc />
+        public bool FireOnStart { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScrollListener"/> class with the specified JavaScript runtime.
+        /// </summary>
+        /// <param name="js">The JavaScript runtime.</param>
+        [DynamicDependency(nameof(RaiseOnScroll))]
+        [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ScrollEventArgs))]
+        public ScrollListener(IJSRuntime js) : this(string.Empty, js) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScrollListener"/> class with the specified selector and JavaScript runtime.
+        /// </summary>
+        /// <param name="selector">The CSS selector for the element to listen for scroll events.</param>
+        /// <param name="js">The JavaScript runtime.</param>
+        /// <param name="reportRateMs"></param>
+        /// <param name="fireOnStart"></param>
+        public ScrollListener(string? selector, IJSRuntime js, int reportRateMs = 100, bool fireOnStart = false)
         {
-            await Start();
+            _js = js;
+            Selector = selector;
+            ReportRateMs = reportRateMs;
+            FireOnStart = fireOnStart;
         }
 
-        if (_initialScrollEvent != null)
+        /// <inheritdoc />
+        public event EventHandler<ScrollEventArgs> OnScroll
         {
-            value.Invoke(this, _initialScrollEvent);
-        }
-    }
-
-    private void Unsubscribe(EventHandler<ScrollEventArgs> value)
-    {
-        _onScroll -= value;
-        if (_onScroll == null)
-        {
-            Cancel().ConfigureAwait(false);
-        }
-    }
-
-    /// <summary>
-    /// Invoked in JavaScript, in scroll-listener.js.
-    /// </summary>
-    /// <param name="e">The scroll event arguments.</param>
-    [JSInvokable]
-    public void RaiseOnScroll(ScrollEventArgs e)
-    {
-        // Store the first event if FireOnStart is enabled and we haven't started yet
-        if (FireOnStart && !_started)
-        {
-            _initialScrollEvent = e;
-            _started = true;
+            add => Subscribe(value);
+            remove => Unsubscribe(value);
         }
 
-        _onScroll?.Invoke(this, e);
-    }
-
-    /// <summary>
-    /// Subscribes to the scroll event in JavaScript.
-    /// </summary>
-    private ValueTask<bool> Start()
-    {
-        _dotNetRef = DotNetObjectReference.Create(this);
-        return _js.InvokeVoidAsyncWithErrorHandling("mudScrollListener.listenForScroll", _dotNetRef, Selector, ReportRateMs, FireOnStart);
-    }
-
-    /// <summary>
-    /// Unsubscribes from the scroll event in JavaScript.
-    /// </summary>
-    private async ValueTask Cancel()
-    {
-        await _js.InvokeVoidAsyncWithErrorHandling("mudScrollListener.cancelListener", Selector);
-        _started = false;
-        _initialScrollEvent = null;
-    }
-
-    /// <inheritdoc />
-    public void Dispose()
-    {
-        if (!_disposed)
+        private async void Subscribe(EventHandler<ScrollEventArgs> value)
         {
-            _disposed = true;
-            _dotNetRef?.Dispose();
+            var isFirstSubscriber = _onScroll == null;
+            _onScroll += value;
 
-            _onScroll = null;
+            if (isFirstSubscriber)
+            {
+                await Start();
+            }
+
+            if (_initialScrollEvent != null)
+            {
+                value.Invoke(this, _initialScrollEvent);
+            }
+        }
+
+        private void Unsubscribe(EventHandler<ScrollEventArgs> value)
+        {
+            _onScroll -= value;
+            if (_onScroll == null)
+            {
+                Cancel().ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// Invoked in JavaScript, in scroll-listener.js.
+        /// </summary>
+        /// <param name="e">The scroll event arguments.</param>
+        [JSInvokable]
+        public void RaiseOnScroll(ScrollEventArgs e)
+        {
+            // Store the first event if FireOnStart is enabled and we haven't started yet
+            if (FireOnStart && !_started)
+            {
+                _initialScrollEvent = e;
+                _started = true;
+            }
+
+            _onScroll?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Subscribes to the scroll event in JavaScript.
+        /// </summary>
+        private ValueTask<bool> Start()
+        {
+            _dotNetRef = DotNetObjectReference.Create(this);
+            return _js.InvokeVoidAsyncWithErrorHandling("mudScrollListener.listenForScroll", _dotNetRef, Selector, ReportRateMs, FireOnStart);
+        }
+
+        /// <summary>
+        /// Unsubscribes from the scroll event in JavaScript.
+        /// </summary>
+        private async ValueTask Cancel()
+        {
+            await _js.InvokeVoidAsyncWithErrorHandling("mudScrollListener.cancelListener", Selector);
+            _started = false;
             _initialScrollEvent = null;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                _dotNetRef?.Dispose();
+
+                _onScroll = null;
+                _initialScrollEvent = null;
+            }
         }
     }
 }
