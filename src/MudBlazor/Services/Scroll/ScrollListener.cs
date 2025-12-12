8c9f50c20000e4defbd0af8cdb616520ed063a78
Jan Haubold <9191057+91378246@users.noreply.github.com>
2025-12-12 17:00:05 +0100

    Format

--- src/MudBlazor/Services/Scroll/ScrollListener.cs
+++ src/MudBlazor/Services/Scroll/ScrollListener.cs
@@ -3,7 +3,7 @@ using Microsoft.JSInterop;
 
 namespace MudBlazor
 {
-    #nullable enable
+#nullable enable
     /// <summary>
     /// Listens to scroll events on a specified element.
     /// </summary>
