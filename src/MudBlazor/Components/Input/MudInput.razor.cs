300f1ffe6b7d20a869b0f0eb4c0dfddb4e66a81b
Artyom M. <artem.melnikov@live.com>
2025-12-16 12:07:37 +0200

    ups

--- src/MudBlazor/Components/Input/MudInput.razor.cs
+++ src/MudBlazor/Components/Input/MudInput.razor.cs
@@ -174,12 +174,12 @@ namespace MudBlazor
 
         private Task OnInputOrOnChangeAsync(string? input) => Immediate ? OnInput(input) : OnChange(input);
 
-        protected Task OnInput(string? args)
+        protected async Task OnInput(string? args)
         {
             _isFocused = true;
             _internalText = args;
             await OnInternalInputChanged.InvokeAsync(args);
-            return SetTextAndUpdateValueAsync(args);
+            await SetTextAndUpdateValueAsync(args);
         }
 
         protected async Task OnChange(string? args)
