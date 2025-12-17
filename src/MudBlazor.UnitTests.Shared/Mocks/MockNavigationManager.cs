c2adee88387f53ea4678074c0f4de3cd8694be21
Jan Haubold <9191057+91378246@users.noreply.github.com>
2025-12-17 20:48:21 +0100

    Pipeline

--- src/MudBlazor.UnitTests.Shared/Mocks/MockNavigationManager.cs
+++ src/MudBlazor.UnitTests.Shared/Mocks/MockNavigationManager.cs
@@ -14,5 +14,9 @@ public class MockNavigationManager
     protected override void NavigateToCore(string uri, bool forceLoad) =>
         WasNavigateInvoked = true;
 
+    protected override void SetNavigationLockState(bool value) =>
+        IsNavigationLocked = value;
+
     public bool WasNavigateInvoked { get; private set; }
+    public bool IsNavigationLocked { get; private set; }
 }
