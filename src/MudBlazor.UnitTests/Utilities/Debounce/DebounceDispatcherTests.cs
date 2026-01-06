b2dbd49737f2b408569c7881989d2de515f778ab
ScarletKuro <19953225+ScarletKuro@users.noreply.github.com>
2026-01-07 00:14:18 +0200

    nit

--- src/MudBlazor.UnitTests/Utilities/Debounce/DebounceDispatcherTests.cs
+++ src/MudBlazor.UnitTests/Utilities/Debounce/DebounceDispatcherTests.cs
@@ -814,7 +814,7 @@ public class DebounceDispatcherTests
         var dispatcher = new DebounceDispatcher(TimeSpan.FromMilliseconds(50));
 
         var tasks = new Task[200];
-        for (int i = 0; i < tasks.Length; i++)
+        for (var i = 0; i < tasks.Length; i++)
         {
             tasks[i] = Task.Run(async () =>
             {
