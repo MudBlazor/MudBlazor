a81f475cbc1d22506d1243074456da4005e7efdf
Jan Haubold <9191057+91378246@users.noreply.github.com>
2026-01-14 19:56:12 +0100

    Add flags

--- src/MudBlazor/Components/Card/MudCardActions.razor.cs
+++ src/MudBlazor/Components/Card/MudCardActions.razor.cs
@@ -14,6 +14,7 @@ namespace MudBlazor
     public partial class MudCardActions : MudComponentBase
     {
         protected string Classname => new CssBuilder("mud-card-actions")
+            .AddClass("mud-card-actions-padding", InnerPadding)
             .AddClass(Class)
             .Build();
 
@@ -23,5 +24,15 @@ namespace MudBlazor
         [Parameter]
         [Category(CategoryTypes.Card.Behavior)]
         public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// Adds padding of <c>8px</c> to all sides of the actions.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to <c>true</c>.
+        /// </remarks>
+        [Parameter]
+        [Category(CategoryTypes.Card.Appearance)]
+        public bool InnerPadding { get; set; } = true;
     }
 }
--- src/MudBlazor/Components/Card/MudCardContent.razor.cs
+++ src/MudBlazor/Components/Card/MudCardContent.razor.cs
@@ -14,6 +14,7 @@ namespace MudBlazor
     public partial class MudCardContent : MudComponentBase
     {
         protected string Classname => new CssBuilder("mud-card-content")
+            .AddClass("mud-card-content-padding", InnerPadding)
             .AddClass(Class)
             .Build();
 
@@ -23,5 +24,15 @@ namespace MudBlazor
         [Parameter]
         [Category(CategoryTypes.Card.Behavior)]
         public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// Adds padding of <c>16px</c> around all sides of the card content.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to <c>true</c>.
+        /// </remarks>
+        [Parameter]
+        [Category(CategoryTypes.Card.Appearance)]
+        public bool InnerPadding { get; set; } = true;
     }
 }
