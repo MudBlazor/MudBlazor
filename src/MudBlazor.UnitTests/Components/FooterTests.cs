b7dcd5d302c4570de382c81a8b3d7410a3e6ffba
Jihad Khawaja <khawajajihad@gmail.com>
2026-01-14 23:14:56 +0200

    fix: Correct comment formatting in footer component files

--- src/MudBlazor.UnitTests/Components/FooterTests.cs
+++ src/MudBlazor.UnitTests/Components/FooterTests.cs
@@ -1,4 +1,4 @@
-// Copyright (c) MudBlazor 2026
+﻿// Copyright (c) MudBlazor 2026
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
