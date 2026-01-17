45b9f539ea5a9499b66bf21bfa648e4768b81ab7
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-01-17 15:19:31 -0600

    fix FormComponentTest_ErrorTextTwoWayBindingAsync

--- src/MudBlazor.UnitTests/Components/FormTests.cs
+++ src/MudBlazor.UnitTests/Components/FormTests.cs
@@ -1917,13 +1917,13 @@ namespace MudBlazor.UnitTests.Components
             textField.GetState(x => x.ErrorText).Should().Be("Default value not changed by binding");
 
             // call validation on the textfield: now the error text should be null and the bound property aswell
-            await textField.ValidateAsync();
+            await comp.InvokeAsync(() => textField.ValidateAsync());
             textField.GetState(x => x.ErrorText).Should().BeNullOrEmpty();
             comp.Instance.BoundErrorText.Should().BeNullOrEmpty();
 
             // empty the input text and call validation: now the error text and the bound property should be the validation error message.
             await textInput.ChangeAsync("");
-            await textField.ValidateAsync();
+            await comp.InvokeAsync(() => textField.ValidateAsync());
             textField.GetState(x => x.ErrorText).Should().Be("EmptyOrWhitespace!");
             comp.Instance.BoundErrorText.Should().Be("EmptyOrWhitespace!");
             comp.Markup.Should().Contain("EmptyOrWhitespace!");
