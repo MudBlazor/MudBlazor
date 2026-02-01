7a661116fa5d740b94aa721d75c55d8b34d9245c
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-02-01 09:26:00 -0600

    tests

--- src/MudBlazor.UnitTests.Shared/Extensions/TestContextExtensions.cs
+++ src/MudBlazor.UnitTests.Shared/Extensions/TestContextExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Bunit;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Time.Testing;
 using MudBlazor.Services;
 using MudBlazor.UnitTests.Shared.Mocks;
 
@@ -11,6 +12,9 @@ namespace MudBlazor.UnitTests.Shared.Extensions
         public static void AddTestServices(this BunitContext ctx)
         {
             ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            var fakeTimeProvider = new FakeTimeProvider();
+            ctx.Services.AddSingleton<TimeProvider>(fakeTimeProvider);
+            ctx.Services.AddSingleton(fakeTimeProvider);
             ctx.Services.AddSingleton<NavigationManager>(new MockNavigationManager());
             ctx.Services.AddMudServices(options =>
             {
