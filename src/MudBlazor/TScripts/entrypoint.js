618bfbd5e3c5912c2183ba3e863299663d8dafa1
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-02-08 10:52:14 -0600

    revert

--- src/MudBlazor/TScripts/entrypoint.js
+++ src/MudBlazor/TScripts/entrypoint.js
@@ -1,11 +1,3 @@
-// Copyright (c) MudBlazor 2021
-// MudBlazor licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-/**
- * Bundles all JS interop modules exported as `window.mud*`.
- * Keep this list in sync with the TScripts directory (excluding this file).
- */
 import "./mudAAAlicense";
 import "./mudDragAndDrop";
 import "./mudElementReference";
--- src/MudBlazor/TScripts/mudAAAlicense.js
+++ src/MudBlazor/TScripts/mudAAAlicense.js
@@ -3,12 +3,3 @@
  * Copyright (c) 2021 MudBlazor
  * Licensed under MIT (https://github.com/MudBlazor/MudBlazor/blob/master/LICENSE)
  */
-
-// Copyright (c) MudBlazor 2021
-// MudBlazor licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-/**
- * License banner module imported first by `entrypoint.js`.
- * Keeps the distributed bundle license notice at the top.
- */
