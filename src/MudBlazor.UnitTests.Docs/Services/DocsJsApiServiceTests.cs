ed461641f4b4f7764119b664ad6f951a3a97566a
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-02-22 13:35:08 -0600

    fix import

--- src/MudBlazor.UnitTests.Docs/Services/DocsJsApiServiceTests.cs
+++ src/MudBlazor.UnitTests.Docs/Services/DocsJsApiServiceTests.cs
@@ -2,8 +2,7 @@
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Bunit;
-using FluentAssertions;
+using AwesomeAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Docs.Services;
 using MudBlazor.UnitTests.Mocks;
