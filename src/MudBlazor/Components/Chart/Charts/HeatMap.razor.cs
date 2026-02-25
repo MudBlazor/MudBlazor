a9f7dcbdc8d16e7da13f3eada7d52dba802cd5bc
Jan Haubold <9191057+91378246@users.noreply.github.com>
2026-02-25 19:31:59 +0100

    Remove dead code and syntax stuff

--- src/MudBlazor/Components/Chart/Charts/HeatMap.razor.cs
+++ src/MudBlazor/Components/Chart/Charts/HeatMap.razor.cs
@@ -279,10 +279,7 @@ namespace MudBlazor.Charts
                 {
                     var legend = new SvgLegend
                     {
-                        Index = row,
-                        Labels = _series[row].Name,
-                        Visible = _series[row].Visible,
-                        OnVisibilityChanged = EventCallback.Factory.Create<SvgLegend>(this, HandleLegendVisibilityChanged)
+                        Index = row, Labels = _series[row].Name, Visible = _series[row].Visible, OnVisibilityChanged = EventCallback.Factory.Create<SvgLegend>(this, HandleLegendVisibilityChanged)
                     };
                     _toggleLegend.Add(legend);
                 }
