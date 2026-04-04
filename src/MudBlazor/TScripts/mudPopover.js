8372f5bd4dc04abf3155b2c2b572dcb2bd6233e9
Nate <nate@nextgenerationsoftware.com.au>
2026-04-04 22:01:16 +1000

    implement popover api using javascript interop for provider.

--- src/MudBlazor/TScripts/mudPopover.js
+++ src/MudBlazor/TScripts/mudPopover.js
@@ -752,6 +752,18 @@ window.mudpopoverHelper = {
 
         return maxZIndex;
     },
+    
+    triggerPopoverState: function (element, open) {
+
+        // Showing an already open popover or hiding an already closed popover will error, so we check the current state before trying to change it
+        const isOpen = element.matches(":popover-open");
+
+        if (open && !isOpen) {
+            try { element.showPopover(); } catch { }
+        } else if (!open && isOpen) {
+            try { element.hidePopover(); } catch { }
+        }
+    },
 
     popoverOverlayUpdates: function () {
         let highestTickItem = null;
@@ -905,6 +917,25 @@ class MudPopover {
                 clearInterval(intervalId);
             }
         }, interval);
+    }
+
+    synchroniseAll(states) {
+        if (!Array.isArray(states)) return;
+
+        for (const s of states) {
+            const element = document.getElementById(s.id);
+            if (!element) continue;
+
+            window.mudpopoverHelper.triggerPopoverState(element, s.open);
+        }
+    }
+    synchronise(id, open) {
+        if (!id) return;
+
+        const element = document.getElementById(id);
+        if (!element) return;
+
+        window.mudpopoverHelper.triggerPopoverState(element, open);
     }
 
     /**
