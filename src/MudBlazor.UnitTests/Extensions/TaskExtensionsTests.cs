d83daa2e8f2506dae9227b77c9a21af191c24d9e
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-05-11 15:23:19 -0500

    Simplify unhandled exception handler isolation

--- src/MudBlazor.UnitTests/Extensions/TaskExtensionsTests.cs
+++ src/MudBlazor.UnitTests/Extensions/TaskExtensionsTests.cs
@@ -1,4 +1,3 @@
-﻿using System.Diagnostics;
 using AwesomeAssertions;
 using NUnit.Framework;
 
@@ -6,113 +5,120 @@ namespace MudBlazor.UnitTests.Extensions
 {
 
     [TestFixture]
-    [NonParallelizable]
     public class TaskExtensionsTests
     {
-        private Action<Exception> _originalExceptionHandler = null!;
-
         [SetUp]
         public void SetUp()
         {
-            _originalExceptionHandler = MudGlobal.UnhandledExceptionHandler;
+            MudGlobal.ClearUnhandledExceptionHandler();
         }
 
         [TearDown]
         public void TearDown()
         {
-            MudGlobal.UnhandledExceptionHandler = _originalExceptionHandler;
+            MudGlobal.ClearUnhandledExceptionHandler();
         }
 
-        private async Task AsyncTaskExceptionGenerator(string errorMessage)
+        private static async Task AsyncTaskExceptionGenerator(string errorMessage)
         {
-            await Task.Delay(10);
+            await Task.Yield();
             throw new Exception(errorMessage);
         }
 
-        private async ValueTask AsyncValueTaskExceptionGenerator(string errorMessage)
+        private static async ValueTask AsyncValueTaskExceptionGenerator(string errorMessage)
         {
-            await Task.Delay(10);
+            await Task.Yield();
             throw new Exception(errorMessage);
         }
 
-        private async ValueTask<TValue> AsyncValueTaskExceptionGenerator<TValue>(string errorMessage)
+        private static async ValueTask<TValue> AsyncValueTaskExceptionGenerator<TValue>(string errorMessage)
         {
-            await Task.Delay(10);
+            await Task.Yield();
             throw new Exception(errorMessage);
+        }
+
+        /// <summary>
+        /// Captures the exception message forwarded by a fire-and-forget operation in the current async flow.
+        /// </summary>
+        private static async Task<string> CaptureUnhandledException(Action invoke)
+        {
+            var exceptionTaskSource = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+            MudGlobal.UnhandledExceptionHandler = ex => exceptionTaskSource.TrySetResult(ex.Message);
+
+            invoke();
+
+            return await exceptionTaskSource.Task;
         }
 
         [Test]
+        [CancelAfter(5000)]
         public async Task Task_AndForget_ShouldForwardExceptionToGlobalHandler()
         {
-            string errorMessage = null;
-            MudGlobal.UnhandledExceptionHandler = ex => errorMessage = ex.Message;
             var task = AsyncTaskExceptionGenerator("Something bad is about to happen ...");
-            task.CatchAndLog();
-            var t = Stopwatch.StartNew();
-            while (errorMessage == null)
-            {
-                await Task.Delay(10);
-                if (t.Elapsed > TimeSpan.FromSeconds(5))
-                {
-                    Assert.Fail("The exception wasn't forwarded to the global exception handler in time!");
-                }
-            }
+            var errorMessage = await CaptureUnhandledException(() => task.CatchAndLog());
+
             errorMessage.Should().Be("Something bad is about to happen ...");
         }
 
         [Test]
+        [CancelAfter(5000)]
         public async Task ValueTask_AndForget_ShouldForwardExceptionToGlobalHandler()
         {
-            string errorMessage = null;
-            MudGlobal.UnhandledExceptionHandler = ex => errorMessage = ex.Message;
             var task = AsyncValueTaskExceptionGenerator("Something bad is about to happen ...");
-            task.CatchAndLog();
-            var t = Stopwatch.StartNew();
-            while (errorMessage == null)
-            {
-                await Task.Delay(10);
-                if (t.Elapsed > TimeSpan.FromSeconds(5))
-                {
-                    Assert.Fail("The exception wasn't forwarded to the global exception handler in time!");
-                }
-            }
+            var errorMessage = await CaptureUnhandledException(() => task.CatchAndLog());
+
             errorMessage.Should().Be("Something bad is about to happen ...");
         }
 
         [Test]
+        [CancelAfter(5000)]
         public async Task ValueTask_T_AndForget_ShouldForwardExceptionToGlobalHandler()
         {
-            string errorMessage = null;
-            MudGlobal.UnhandledExceptionHandler = ex => errorMessage = ex.Message;
             var task = AsyncValueTaskExceptionGenerator<bool>("Something bad is about to happen ...");
-            task.CatchAndLog();
-            var t = Stopwatch.StartNew();
-            while (errorMessage == null)
-            {
-                await Task.Delay(10);
-                if (t.Elapsed > TimeSpan.FromSeconds(5))
-                {
-                    Assert.Fail("The exception wasn't forwarded to the global exception handler in time!");
-                }
-            }
+            var errorMessage = await CaptureUnhandledException(() => task.CatchAndLog());
+
             errorMessage.Should().Be("Something bad is about to happen ...");
         }
 
         [Test]
+        [CancelAfter(5000)]
         public async Task Task_AndForget_ShouldNotFailIfGlobalHandlerIsNull()
         {
             MudGlobal.UnhandledExceptionHandler = null;
             var task = AsyncTaskExceptionGenerator("Something bad is about to happen ...");
             task.CatchAndLog();
-            var t = Stopwatch.StartNew();
-            while (!(task.IsCompleted || task.IsCanceled || task.IsFaulted))
+
+            var exception = Assert.ThrowsAsync<Exception>(async () => await task);
+            exception!.Message.Should().Be("Something bad is about to happen ...");
+        }
+
+        [Test]
+        public void UnhandledExceptionHandler_ShouldReturnDefaultConsoleHandlerWhenUnset()
+        {
+            MudGlobal.UnhandledExceptionHandler.Should().BeSameAs(MudGlobal.DefaultUnhandledExceptionHandler);
+        }
+
+        [Test]
+        [CancelAfter(5000)]
+        public async Task Task_AndForget_ShouldUseHandlerFromCurrentAsyncFlow()
+        {
+            static async Task<string> RunInSeparateFlow(string expectedMessage)
             {
-                await Task.Delay(10);
-                if (t.Elapsed > TimeSpan.FromSeconds(5))
+                return await Task.Run(async () =>
                 {
-                    Assert.Fail("The test task did not end in time, this should not happen!");
-                }
+                    return await CaptureUnhandledException(() => AsyncTaskExceptionGenerator(expectedMessage).CatchAndLog());
+                });
             }
+
+            var results = await Task.WhenAll(
+                RunInSeparateFlow("Something bad is about to happen in flow 1 ..."),
+                RunInSeparateFlow("Something bad is about to happen in flow 2 ..."));
+
+            results.Should().BeEquivalentTo(new[]
+            {
+                "Something bad is about to happen in flow 1 ...",
+                "Something bad is about to happen in flow 2 ..."
+            });
         }
     }
 }
--- src/MudBlazor/Services/MudGlobal.cs
+++ src/MudBlazor/Services/MudGlobal.cs
@@ -15,6 +15,11 @@ namespace MudBlazor;
 /// </summary>
 public static class MudGlobal
 {
+    private static readonly AsyncLocal<Action<Exception>> _unhandledExceptionHandler = new();
+    private static readonly AsyncLocal<bool> _unhandledExceptionHandlerIsSet = new();
+
+    internal static readonly Action<Exception> DefaultUnhandledExceptionHandler = exception => Console.Write(exception);
+
     /// <summary>
     /// Default settings for <see cref="MudMenu"/>.
     /// <br/>
@@ -54,5 +59,19 @@ public static class MudGlobal
     /// By default, exceptions are logged to the console via <see cref="Console.Write(object?)"/>.<br />
     /// To handle all .NET exceptions, see: <see href="https://learn.microsoft.com/aspnet/core/fundamentals/error-handling">Handle errors in ASP.NET Core</see>.
     /// </remarks>
-    public static Action<Exception> UnhandledExceptionHandler { get; set; } = (exception) => Console.Write(exception);
+    public static Action<Exception> UnhandledExceptionHandler
+    {
+        get => _unhandledExceptionHandlerIsSet.Value ? _unhandledExceptionHandler.Value : DefaultUnhandledExceptionHandler;
+        set
+        {
+            _unhandledExceptionHandler.Value = value;
+            _unhandledExceptionHandlerIsSet.Value = true;
+        }
+    }
+
+    internal static void ClearUnhandledExceptionHandler()
+    {
+        _unhandledExceptionHandler.Value = null;
+        _unhandledExceptionHandlerIsSet.Value = false;
+    }
 }
