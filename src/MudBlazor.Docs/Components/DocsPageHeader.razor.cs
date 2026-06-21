13d38b50f3d877071a2189e8f73a5bb18d9f2a8e
Omar <omarkurtovic19@gmail.com>
2026-06-21 10:45:13 +0200

    Update empty check.

--- src/MudBlazor.Docs/Components/DocsPageHeader.razor.cs
+++ src/MudBlazor.Docs/Components/DocsPageHeader.razor.cs
@@ -100,7 +100,7 @@ public sealed partial class DocsPageHeader
         {
             ComputedSubTitle = SubTitle;
         }
-        else if (!string.IsNullOrEmpty(DocumentedType.Summary))
+        else if (!string.IsNullOrWhiteSpace(DocumentedType.Summary))
         {
             ComputedSubTitle = DocumentedType.Summary;
         }
