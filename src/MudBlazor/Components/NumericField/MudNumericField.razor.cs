aef6f5f0b13b7c7fc1476d08ef5479f7af2830b7
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-06-25 22:42:10 -0500

    Update MudNumericField.razor.cs

--- src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
+++ src/MudBlazor/Components/NumericField/MudNumericField.razor.cs
@@ -1,4 +1,4 @@
-F﻿// Copyright (c) MudBlazor 2022
+﻿// Copyright (c) MudBlazor 2022
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
