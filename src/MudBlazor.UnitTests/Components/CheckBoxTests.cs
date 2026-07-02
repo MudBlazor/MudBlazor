MudBlazor/MudBlazor

commit 30dc3245c58843e5eb199dfc512d35c16e94a77e

Radio, Check Box, Switch: Unification and improved alignaments (#9472)

src/MudBlazor/Components/CheckBox/MudCheckBox.razor.cs
@@ -17,21 +17,22 @@ namespace MudBlazor
         [Inject]
         private IKeyInterceptorService KeyInterceptorService { get; set; } = null!;
 
-        protected string Classname => new CssBuilder("mud-input-control-boolean-input")
+        protected override string Classname => new CssBuilder("mud-input-control-boolean-input")
+            .AddClass($"mud-disabled", GetDisabledState())
+            .AddClass($"mud-readonly", GetReadOnlyState())
+            .AddClass("mud-input-with-content", ChildContent is not null)
             .AddClass(Class)
             .Build();
 
-        protected string LabelClassname => new CssBuilder("mud-checkbox")
+        protected override string LabelClassname => new CssBuilder("mud-checkbox")
             .AddClass($"mud-disabled", GetDisabledState())
             .AddClass($"mud-readonly", GetReadOnlyState())
-            .AddClass("flex-row-reverse", LabelPosition == LabelPosition.Start)
+            .AddClass($"mud-input-content-placement-{ConvertPlacement(LabelPlacement).ToDescriptionString()}")
             .Build();
 
-        protected string CheckBoxClassname => new CssBuilder("mud-button-root mud-icon-button")
-            .AddClass($"mud-{Color.ToDescriptionString()}-text", !GetDisabledState() && UncheckedColor == null || (UncheckedColor != null && BoolValue == true))
-            .AddClass($"mud-{UncheckedColor?.ToDescriptionString()}-text", !GetDisabledState() && UncheckedColor != null && BoolValue == false)
-            .AddClass($"hover:mud-{Color.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor == null || (UncheckedColor != null && BoolValue == true))
-            .AddClass($"hover:mud-{UncheckedColor?.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && BoolValue == false)
+        protected override string IconClassname => new CssBuilder("mud-button-root mud-icon-button")
+            .AddClass($"mud-{Color.ToDescriptionString()}-text hover:mud-{Color.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor == null || (UncheckedColor != null && BoolValue == true))
+            .AddClass($"mud-{UncheckedColor?.ToDescriptionString()}-text hover:mud-{UncheckedColor?.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && BoolValue == false)
             .AddClass($"mud-checkbox-dense", Dense)
             .AddClass($"mud-ripple mud-ripple-checkbox", Ripple && !GetReadOnlyState() && !GetDisabledState())
             .AddClass($"mud-disabled", GetDisabledState())
@@ -40,16 +41,6 @@ namespace MudBlazor
             .AddClass($"mud-checkbox-false", BoolValue == false)
             .AddClass($"mud-checkbox-null", BoolValue is null)
             .Build();
-
-        /// <summary>
-        /// The color of the checkbox.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to <see cref="Color.Default"/>.  Theme colors are supported.
-        /// </remarks>
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Appearance)]
-        public Color Color { get; set; } = Color.Default;
 
         /// <summary>
         /// The color of the checkbox when its <c>Value</c> is <c>false</c> or <c>null</c>.
@@ -62,26 +53,6 @@ namespace MudBlazor
         public Color? UncheckedColor { get; set; } = null;
 
         /// <summary>
-        /// The text to display next to the checkbox.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to <c>null</c>.
-        /// </remarks>
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Behavior)]
-        public string? Label { get; set; }
-
-        /// <summary>
-        /// The position of the <see cref="Label" /> text.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to <see cref="LabelPosition.End"/>.
-        /// </remarks>
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Behavior)]
-        public LabelPosition LabelPosition { get; set; } = LabelPosition.End;
-
-        /// <summary>
         /// Allows this checkbox to be controlled via the keyboard.
         /// </summary>
         /// <remarks>
@@ -92,16 +63,6 @@ namespace MudBlazor
         public bool KeyboardEnabled { get; set; } = true;
 
         /// <summary>
-        /// Shows a ripple effect when this checkbox is clicked.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to <c>true</c>.
-        /// </remarks>
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Appearance)]
-        public bool Ripple { get; set; } = true;
-
-        /// <summary>
         /// Uses compact padding.
         /// </summary>
         /// <remarks>
@@ -110,23 +71,6 @@ namespace MudBlazor
         [Parameter]
         [Category(CategoryTypes.FormComponent.Appearance)]
         public bool Dense { get; set; }
-
-        /// <summary>
-        /// The size of the checkbox.
-        /// </summary>
-        /// <remarks>
-        /// Defaults to <see cref="Size.Medium"/>.
-        /// </remarks>
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Appearance)]
-        public Size Size { get; set; } = Size.Medium;
-
-        /// <summary>
-        /// The content within this checkbox.
-        /// </summary>
-        [Parameter]
-        [Category(CategoryTypes.FormComponent.Behavior)]
-        public RenderFragment? ChildContent { get; set; }
 
         /// <summary>
         /// The icon to display for a checked state.

src/MudBlazor/Components/Radio/MudRadio.razor.cs
@@ -6,59 +6,37 @@ using MudBlazor.Utilities;
 namespace MudBlazor
 {
 #nullable enable
-    public partial class MudRadio<T> : MudComponentBase, IAsyncDisposable
+    public partial class MudRadio<T> : MudBooleanInput<T>
     {
         private IMudRadioGroup? _parent;
         private string _elementId = Identifier.Create("radio");
 
-        protected string Classname =>
-            new CssBuilder("mud-radio")
-                .AddClass("mud-disabled", GetDisabledState())
-                .AddClass("mud-readonly", GetReadOnlyState())
-                .AddClass($"mud-radio-content-placement-{ConvertPlacement(Placement).ToDescriptionString()}")
-                .AddClass("mud-radio-with-content", ChildContent is not null)
-                .AddClass(Class)
-                .Build();
+        protected override string Classname => new CssBuilder("mud-input-control-boolean-input")
+            .AddClass("mud-disabled", GetDisabledState())
+            .AddClass("mud-readonly", GetReadOnlyState())
+            .AddClass("mud-input-with-content", ChildContent is not null)
+            .AddClass(Class)
+            .Build();
 
-        protected string ButtonClassname =>
-            new CssBuilder("mud-button-root mud-icon-button")
-                .AddClass("mud-ripple mud-ripple-radio", Ripple && !GetDisabledState() && !GetReadOnlyState())
-                .AddClass($"mud-{Color.ToDescriptionString()}-text", !GetDisabledState() && (UncheckedColor == null || (UncheckedColor != null && Checked)))
-                .AddClass($"mud-{UncheckedColor?.ToDescriptionString()}-text", !GetDisabledState() && UncheckedColor != null && Checked == false)
-                .AddClass($"hover:mud-{UncheckedColor?.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && (UncheckedColor == null || (UncheckedColor != null && Checked)))
-                .AddClass($"hover:mud-{UncheckedColor?.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && Checked == false)
-                .AddClass("mud-radio-dense", Dense)
-                .AddClass("mud-disabled", GetDisabledState())
-                .AddClass("mud-readonly", GetReadOnlyState())
-                .AddClass("mud-checked", Checked)
-                .AddClass("mud-error-text", MudRadioGroup?.HasErrors)
-                .Build();
+        protected override string LabelClassname => new CssBuilder("mud-radio")
+            .AddClass($"mud-disabled", GetDisabledState())
+            .AddClass($"mud-readonly", GetReadOnlyState())
+            .AddClass($"mud-input-content-placement-{ConvertPlacement(LabelPlacement).ToDescriptionString()}")
+            .Build();
 
-        protected string RadioIconsClassNames =>
-            new CssBuilder("mud-radio-icons")
-                .AddClass($"mud-checked", Checked)
-                .Build();
-
-        protected string IconClassName =>
-            new CssBuilder("mud-icon-root mud-svg-icon")
-                .AddClass($"mud-icon-size-{Size.ToDescriptionString()}")
-                .Build();
-
-        protected string CheckedIconClassName =>
-            new CssBuilder("mud-icon-root mud-svg-icon mud-radio-icon-checked")
-                .AddClass($"mud-icon-size-{Size.ToDescriptionString()}")
-                .Build();
-
-        protected string ChildSpanClassName =>
-            new CssBuilder("mud-radio-content mud-typography mud-typography-body1")
-                .AddClass("mud-error-text", MudRadioGroup?.HasErrors)
-                .Build();
+        protected override string IconClassname => new CssBuilder("mud-button-root mud-icon-button")
+            .AddClass("mud-ripple mud-ripple-radio", Ripple && !GetDisabledState() && !GetReadOnlyState())
+            .AddClass($"mud-{Color.ToDescriptionString()}-text hover:mud-{Color.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && (UncheckedColor == null || (UncheckedColor != null && Checked)))
+            .AddClass($"mud-{UncheckedColor?.ToDescriptionString()}-text hover:mud-{UncheckedColor?.ToDescriptionString()}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && Checked == false)
+            .AddClass("mud-radio-dense", Dense)
+            .AddClass("mud-disabled", GetDisabledState())
+            .AddClass("mud-readonly", GetReadOnlyState())
+            .AddClass("mud-checked", Checked)
+            .AddClass("mud-error-text", MudRadioGroup?.HasErrors)
+            .Build();
 
         [Inject]
         private IKeyInterceptorService KeyInterceptorService { get; set; } = null!;
-
-        [CascadingParameter(Name = "RightToLeft")]
-        public bool RightToLeft { get; set; }
 
         /// <summary>
         /// The parent Radio Group
@@ -75,32 +53,11 @@ namespace MudBlazor
         }
 
         /// <summary>
-        /// The color of the component. It supports the theme colors.
-        /// </summary>
-        [Parameter]
-        [Category(CategoryTypes.Radio.Appearance)]
-        public Color Color { get; set; } = Color.Default;
-
-        /// <summary>
         /// The base color of the component in its none active/unchecked state. It supports the theme colors.
         /// </summary>
         [Parameter]
         [Category(CategoryTypes.Radio.Appearance)]
         public Color? UncheckedColor { get; set; } = null;
-
-        /// <summary>
-        /// The position of the child content.
-        /// </summary>
-        [Parameter]
-        [Category(CategoryTypes.Radio.Behavior)]
-        public Placement Placement { get; set; } = Placement.End;
-
-        /// <summary>
-        /// The value to associate to the button.
-        /// </summary>
-        [Parameter]
-        [Category(CategoryTypes.Radio.Behavior)]
-        public T? Value { get; set; }
 
         /// <summary>
         /// If true, compact padding will be applied.
@@ -110,50 +67,47 @@ namespace MudBlazor
         public bool Dense { get; set; }
 
         /// <summary>
-        /// The Size of the component.
+        /// The icon to display for a checked state.
         /// </summary>
+        /// <remarks>
+        /// Defaults to <see cref="Icons.Material.Filled.RadioButtonChecked"/>.
+        /// </remarks>
         [Parameter]
-        [Category(CategoryTypes.Radio.Appearance)]
-        public Size Size { get; set; } = Size.Medium;
+        [Category(CategoryTypes.FormComponent.Appearance)]
+        public string CheckedIcon { get; set; } = Icons.Material.Filled.RadioButtonChecked;
 
         /// <summary>
-        /// Gets or sets whether to show a ripple effect when the user clicks the button. Default is true.
+        /// The icon to display for an unchecked state.
         /// </summary>
+        /// <remarks>
+        /// Defaults to <see cref="Icons.Material.Filled.RadioButtonUnchecked"/>.
+        /// </remarks>
         [Parameter]
-        [Category(CategoryTypes.Radio.Appearance)]
-        public bool Ripple { get; set; } = true;
+        [Category(CategoryTypes.FormComponent.Appearance)]
+        public string UncheckedIcon { get; set; } = Icons.Material.Filled.RadioButtonUnchecked;
 
         /// <summary>
-        /// If true, the button will be disabled.
+        /// The icon to display for an indeterminate state.
         /// </summary>
+        /// <remarks>
+        /// Defaults to <see cref="Icons.Material.Filled.IndeterminateCheckBox"/>.
+        /// </remarks>
         [Parameter]
-        [Category(CategoryTypes.Radio.Behavior)]
-        public bool Disabled { get; set; }
+        [Category(CategoryTypes.FormComponent.Appearance)]
+        public string IndeterminateIcon { get; set; } = Icons.Material.Filled.IndeterminateCheckBox;
 
-        /// <summary>
-        /// Child content of component.
-        /// </summary>
-        [Parameter]
-        [Category(CategoryTypes.Radio.Behavior)]
-        public RenderFragment? ChildContent { get; set; }
-
-        private bool GetDisabledState() => Disabled || MudRadioGroup?.GetDisabledState() == true;
-
-        private bool GetReadOnlyState() => MudRadioGroup?.GetReadOnlyState() == true;
+        private string GetIcon()
+        {
+            return Checked switch
+            {
+                true => CheckedIcon,
+                false => UncheckedIcon
+            };
+        }
 
         internal bool Checked { get; private set; }
 
         internal MudRadioGroup<T>? MudRadioGroup => (MudRadioGroup<T>?)IMudRadioGroup;
-
-        private Placement ConvertPlacement(Placement placement)
-        {
-            return placement switch
-            {
-                Placement.Left => RightToLeft ? Placement.End : Placement.Start,
-                Placement.Right => RightToLeft ? Placement.Start : Placement.End,
-                _ => placement
-            };
-        }
 
         internal void SetChecked(bool value)
         {
@@ -176,7 +130,7 @@ namespace MudBlazor
 
         internal Task OnClickAsync()
         {
-            if (GetDisabledState() || (GetReadOnlyState()))
+            if (GetDisabledState() || GetReadOnlyState() || (MudRadioGroup?.GetReadOnlyState() ?? false))
             {
                 return Task.CompletedTask;
             }
@@ -191,7 +145,7 @@ namespace MudBlazor
 
         protected internal async Task HandleKeyDownAsync(KeyboardEventArgs keyboardEventArgs)
         {
-            if (GetDisabledState() || GetReadOnlyState())
+            if (GetDisabledState() || GetReadOnlyState() || (MudRadioGroup?.GetReadOnlyState() ?? false))
             {
                 return;
             }
@@ -243,20 +197,14 @@ namespace MudBlazor
             await base.OnAfterRenderAsync(firstRender);
         }
 
-        protected virtual async ValueTask DisposeAsyncCore()
+        protected override async ValueTask DisposeAsyncCore()
         {
+            await base.DisposeAsyncCore();
             MudRadioGroup?.UnregisterRadio(this);
             if (IsJSRuntimeAvailable)
             {
                 await KeyInterceptorService.UnsubscribeAsync(_elementId);
             }
-        }
-
-        /// <inheritdoc />
-        public async ValueTask DisposeAsync()
-        {
-            await DisposeAsyncCore();
-            GC.SuppressFinalize(this);
         }
     }
 }

commit 1a3d0cd1725a3ea6f9ff036e854216cb080b986c

MudCheckBox, MudSwitch, MudRadio: Honor Color/UncheckedColor when ReadOnly (#9524)
Read-only inputs were greyed like disabled ones because the text color and
hover classes shared one gate. Split them: the color stays applied when
read-only (only Disabled greys out), while the hover effect is still
suppressed whenever the input is not interactive.

Regressed by #9472, which re-merged the split introduced in #9904. Adds the
read-only color tests that were missing from the original fix.

src/MudBlazor/Components/CheckBox/MudCheckBox.razor.cs
@@ -33,8 +33,12 @@ namespace MudBlazor
             .Build();
 
         protected override string IconClassname => new CssBuilder("mud-button-root mud-icon-button")
-            .AddClass($"mud-{Color.ToStringFast(true)}-text hover:mud-{Color.ToStringFast(true)}-hover", (!GetReadOnlyState() && !GetDisabledState() && UncheckedColor == null) || (UncheckedColor != null && BoolValue == true))
-            .AddClass($"mud-{UncheckedColor?.ToStringFast(true)}-text hover:mud-{UncheckedColor?.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && BoolValue == false)
+            // The text color is kept when read-only; only Disabled greys the checkbox out.
+            // The hover effect is separated out so it can be suppressed whenever the checkbox is not interactive.
+            .AddClass($"mud-{Color.ToStringFast(true)}-text", !GetDisabledState() && (UncheckedColor == null || BoolValue == true))
+            .AddClass($"mud-{UncheckedColor?.ToStringFast(true)}-text", !GetDisabledState() && UncheckedColor != null && BoolValue == false)
+            .AddClass($"hover:mud-{Color.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && (UncheckedColor == null || BoolValue == true))
+            .AddClass($"hover:mud-{UncheckedColor?.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && BoolValue == false)
             .AddClass($"mud-checkbox-dense", Dense)
             .AddClass($"mud-ripple mud-ripple-checkbox", Ripple && !GetReadOnlyState() && !GetDisabledState())
             .AddClass($"mud-disabled", GetDisabledState())

src/MudBlazor/Components/Radio/MudRadio.razor.cs
@@ -38,8 +38,12 @@ namespace MudBlazor
 
         protected override string IconClassname => new CssBuilder("mud-button-root mud-icon-button")
             .AddClass("mud-ripple mud-ripple-radio", Ripple && !GetDisabledState() && !GetReadOnlyState())
-            .AddClass($"mud-{Color.ToStringFast(true)}-text hover:mud-{Color.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && (UncheckedColor == null || (UncheckedColor != null && Checked)))
-            .AddClass($"mud-{UncheckedColor?.ToStringFast(true)}-text hover:mud-{UncheckedColor?.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && Checked == false)
+            // The text color is kept when read-only; only Disabled greys the radio out.
+            // The hover effect is separated out so it can be suppressed whenever the radio is not interactive.
+            .AddClass($"mud-{Color.ToStringFast(true)}-text", !GetDisabledState() && (UncheckedColor == null || Checked))
+            .AddClass($"mud-{UncheckedColor?.ToStringFast(true)}-text", !GetDisabledState() && UncheckedColor != null && !Checked)
+            .AddClass($"hover:mud-{Color.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && (UncheckedColor == null || Checked))
+            .AddClass($"hover:mud-{UncheckedColor?.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && UncheckedColor != null && !Checked)
             .AddClass("mud-radio-dense", Dense)
             .AddClass("mud-disabled", GetDisabledState())
             .AddClass("mud-readonly", GetReadOnlyState())

src/MudBlazor/Components/Switch/MudSwitch.razor.cs
@@ -32,8 +32,12 @@ namespace MudBlazor
 
         protected string SwitchClassname => new CssBuilder("mud-button-root mud-icon-button mud-switch-base")
             .AddClass($"mud-ripple mud-ripple-switch", Ripple && !GetReadOnlyState() && !GetDisabledState())
-            .AddClass($"mud-{Color.ToStringFast(true)}-text hover:mud-{Color.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && BoolValue == true)
-            .AddClass($"mud-{UncheckedColor.ToStringFast(true)}-text hover:mud-{UncheckedColor.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && BoolValue == false)
+            // The text color is kept when read-only; only Disabled greys the switch out.
+            // The hover effect is separated out so it can be suppressed whenever the switch is not interactive.
+            .AddClass($"mud-{Color.ToStringFast(true)}-text", !GetDisabledState() && BoolValue == true)
+            .AddClass($"mud-{UncheckedColor.ToStringFast(true)}-text", !GetDisabledState() && BoolValue == false)
+            .AddClass($"hover:mud-{Color.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && BoolValue == true)
+            .AddClass($"hover:mud-{UncheckedColor.ToStringFast(true)}-hover", !GetReadOnlyState() && !GetDisabledState() && BoolValue == false)
             .AddClass($"mud-switch-disabled", GetDisabledState())
             .AddClass($"mud-readonly", GetReadOnlyState())
             .AddClass($"mud-checked", BoolValue)

src/MudBlazor.UnitTests/Components/CheckBoxTests.cs
@@ -373,6 +373,58 @@ namespace MudBlazor.UnitTests.Components
         }
 
         [Test]
+        [TestCase(Color.Secondary, Color.Error)]
+        [TestCase(Color.Info, Color.Warning)]
+        public void CheckBoxColor_ReadOnly_ShouldKeepColor(Color color, Color uncheckedColor)
+        {
+            // #9524: a read-only checkbox keeps Color/UncheckedColor (only Disabled greys out),
+            // while the interactive hover class stays suppressed.
+            var uncheckedComp = Context.Render<MudCheckBox<bool>>(x => x
+                .Add(c => c.Color, color)
+                .Add(c => c.UncheckedColor, uncheckedColor)
+                .Add(c => c.ReadOnly, true)
+                .Add(c => c.Value, false));
+            var uncheckedIcon = uncheckedComp.Find(".mud-button-root.mud-icon-button");
+            uncheckedIcon.ClassList.Should().Contain($"mud-{uncheckedColor.ToStringFast(true)}-text");
+            uncheckedIcon.ClassList.Should().NotContain($"hover:mud-{uncheckedColor.ToStringFast(true)}-hover");
+
+            var checkedComp = Context.Render<MudCheckBox<bool>>(x => x
+                .Add(c => c.Color, color)
+                .Add(c => c.UncheckedColor, uncheckedColor)
+                .Add(c => c.ReadOnly, true)
+                .Add(c => c.Value, true));
+            var checkedIcon = checkedComp.Find(".mud-button-root.mud-icon-button");
+            checkedIcon.ClassList.Should().Contain($"mud-{color.ToStringFast(true)}-text");
+            checkedIcon.ClassList.Should().NotContain($"hover:mud-{color.ToStringFast(true)}-hover");
+        }
+
+        [Test]
+        [TestCase(false)]
+        [TestCase(true)]
+        public void CheckBoxColor_ReadOnlyWithoutUncheckedColor_ShouldKeepColor(bool value)
+        {
+            // #9524: with only Color set, a read-only checkbox keeps that color in both states.
+            var comp = Context.Render<MudCheckBox<bool>>(x => x
+                .Add(c => c.Color, Color.Success)
+                .Add(c => c.ReadOnly, true)
+                .Add(c => c.Value, value));
+            comp.Find(".mud-button-root.mud-icon-button").ClassList.Should().Contain("mud-success-text");
+        }
+
+        [Test]
+        public void CheckBoxColor_Disabled_ShouldDropColorClass()
+        {
+            // #9524: Disabled (unlike ReadOnly) greys out, so no color utility class is emitted.
+            var comp = Context.Render<MudCheckBox<bool>>(x => x
+                .Add(c => c.Color, Color.Success)
+                .Add(c => c.UncheckedColor, Color.Error)
+                .Add(c => c.Disabled, true));
+            var icon = comp.Find(".mud-button-root.mud-icon-button");
+            icon.ClassList.Should().NotContain("mud-error-text");
+            icon.ClassList.Should().NotContain("mud-success-text");
+        }
+
+        [Test]
         public void CheckBoxDisabled()
         {
             var comp = Context.Render<CheckBoxLabelTest>();

src/MudBlazor.UnitTests/Components/RadioTests.cs
@@ -456,6 +456,31 @@ namespace MudBlazor.UnitTests.Components
         }
 
         [Test]
+        public void RadioColor_ReadOnly_ShouldKeepColor()
+        {
+            // #9524: a read-only radio keeps Color/UncheckedColor (only Disabled greys out),
+            // while the interactive hover class stays suppressed.
+            var comp = Context.Render<MudRadioGroup<int>>(self => self
+                .Add(x => x.ReadOnly, true)
+                .Add(x => x.Value, 1)
+                .AddChildContent<MudRadio<int>>(r => r
+                    .Add(x => x.Value, 1)
+                    .Add(x => x.Color, Color.Success)
+                    .Add(x => x.UncheckedColor, Color.Error))
+                .AddChildContent<MudRadio<int>>(r => r
+                    .Add(x => x.Value, 2)
+                    .Add(x => x.Color, Color.Success)
+                    .Add(x => x.UncheckedColor, Color.Error)));
+
+            var icons = comp.FindAll("span.mud-button-root");
+            // first radio is checked -> Color; second is unchecked -> UncheckedColor
+            icons[0].ClassList.Should().Contain("mud-success-text");
+            icons[0].ClassList.Should().NotContain("hover:mud-success-hover");
+            icons[1].ClassList.Should().Contain("mud-error-text");
+            icons[1].ClassList.Should().NotContain("hover:mud-error-hover");
+        }
+
+        [Test]
         public void RadioLabel()
         {
             var value = new DisplayNameLabelClass();

src/MudBlazor.UnitTests/Components/SwitchTests.cs
@@ -112,6 +112,32 @@ namespace MudBlazor.UnitTests.Components
         }
 
         [Test]
+        [TestCase(Color.Success, Color.Error)]
+        [TestCase(Color.Info, Color.Warning)]
+        public void SwitchColor_ReadOnly_ShouldKeepColor(Color color, Color uncheckedColor)
+        {
+            // #9524: a read-only switch keeps Color/UncheckedColor (only Disabled greys out),
+            // while the interactive hover class stays suppressed.
+            var offComp = Context.Render<MudSwitch<bool>>(x => x
+                .Add(c => c.Color, color)
+                .Add(c => c.UncheckedColor, uncheckedColor)
+                .Add(c => c.ReadOnly, true)
+                .Add(c => c.Value, false));
+            var offBase = offComp.Find(".mud-button-root.mud-icon-button.mud-switch-base");
+            offBase.ClassList.Should().Contain($"mud-{uncheckedColor.ToStringFast(true)}-text");
+            offBase.ClassList.Should().NotContain($"hover:mud-{uncheckedColor.ToStringFast(true)}-hover");
+
+            var onComp = Context.Render<MudSwitch<bool>>(x => x
+                .Add(c => c.Color, color)
+                .Add(c => c.UncheckedColor, uncheckedColor)
+                .Add(c => c.ReadOnly, true)
+                .Add(c => c.Value, true));
+            var onBase = onComp.Find(".mud-button-root.mud-icon-button.mud-switch-base");
+            onBase.ClassList.Should().Contain($"mud-{color.ToStringFast(true)}-text");
+            onBase.ClassList.Should().NotContain($"hover:mud-{color.ToStringFast(true)}-hover");
+        }
+
+        [Test]
         public void SwitchDisabled()
         {
             var comp = Context.Render<SwitchWithLabelTest>();