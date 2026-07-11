688dab5806409355367760f9e1e0960667438c87
Benjamin Raines <bennyraines08@gmail.com>
2026-07-11 18:32:32 -0400

    Update summary

--- src/MudBlazor/Utilities/CssStringBuilder.cs
+++ src/MudBlazor/Utilities/CssStringBuilder.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) MudBlazor 2021
+// Copyright (c) MudBlazor 2021
 // MudBlazor licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 namespace MudBlazor.Utilities;
 
 /// <summary>
-/// Base type for anything that evaluates to a CSS size/track value.
+/// Base type for constructing CSS input.
 /// </summary>
 public abstract class CssStringBuilder : IEquatable<CssStringBuilder>
 {
