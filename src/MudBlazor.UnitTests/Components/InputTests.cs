70cd916ab0b0e490dedd57b85a71653c8fcfe0a3
avalerio <avalerio@tnwops.com>
2026-07-27 15:17:36 -0500

    Add test for clear button mouse state.

--- src/MudBlazor.UnitTests/Components/InputTests.cs
+++ src/MudBlazor.UnitTests/Components/InputTests.cs
@@ -72,4 +72,32 @@ public class InputTests : BunitTest
             comp.Find("div.mud-input").ClassList.Should().NotContain("mud-input-full-width");
         }
     }
+
+    [Test]
+    public async Task MudInputIsClearingShouldRespectMouseState()
+    {
+        var comp = Context.Render<MudInput<string>>(parameters => parameters
+            .Add(x => x.Clearable, true)
+            .Add(x => x.Value, "Some value")
+        );
+
+        var button = comp.Find("div.mud-input .mud-input-clear-button");
+        button.Should().NotBeNull();
+        await button.MouseDownAsync();
+        comp.Instance.IsClearing.Should().BeTrue();
+        await button.ClickAsync();
+        comp.Instance.IsClearing.Should().BeFalse();
+
+        comp = Context.Render<MudInput<string>>(parameters => parameters
+            .Add(x => x.Clearable, true)
+            .Add(x => x.Value, "Some value")
+        );
+
+        button = comp.Find("div.mud-input .mud-input-clear-button");
+
+        await button.MouseDownAsync();
+        comp.Instance.IsClearing.Should().BeTrue();
+        await button.MouseLeaveAsync();
+        comp.Instance.IsClearing.Should().BeFalse();
+    }
 }
