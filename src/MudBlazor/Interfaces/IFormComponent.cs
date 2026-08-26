73f4b3faccd7deca369bedbbfc8fcea4ff39357d
Zac <me@zaca.dev>
2026-08-26 14:23:33 -0700

    whitespace formatting

--- src/MudBlazor/Interfaces/IFormComponent.cs
+++ src/MudBlazor/Interfaces/IFormComponent.cs
@@ -18,7 +18,7 @@ namespace MudBlazor.Interfaces
         /// Defaults to <c>false</c>.  When <c>true</c>, an error with the text in the implementors RequiredError will be shown during validation if no input was given.
         /// </remarks>
         bool Required { get; set; }
-        
+
         /// <summary>
         /// Displays an error.
         /// </summary>
@@ -26,7 +26,7 @@ namespace MudBlazor.Interfaces
         /// Defaults to <c>false</c>.  When <c>true</c>, the text in the implementors ErrorText field is displayed.
         /// </remarks>
         bool Error { get; set; }
-        
+
         /// <summary>
         /// Indicates any error, conversion error, or validation error with this input.
         /// </summary>
@@ -34,7 +34,7 @@ namespace MudBlazor.Interfaces
         /// When <c>true</c>, the <see cref="Error"/> property is <c>true</c>, or the implementors ConversionError is <c>true</c>, or one or more ValidationErrors exists.
         /// </remarks>
         bool HasErrors { get; }
-        
+
         /// <summary>
         /// Indicates whether the user has interacted with this input or the focus has been released.
         /// </summary>
@@ -42,14 +42,14 @@ namespace MudBlazor.Interfaces
         /// Defaults to <c>false</c>.  When <c>true</c>, the user has performed input, or focus has moved away from this input.  This property is typically used to show the implementors RequiredError text only after the user has interacted with this input.
         /// </remarks>
         bool Touched { get; }
-        
+
         /// <summary>
         /// Indicates that this input has a non-null, non-empty value.
         /// </summary>
         /// <remarks>Default implementation which indicates the input has been <see cref="Touched"/></remarks>
         /// <returns>True if some value exists, or false if null or empty</returns>
         bool HasValue() => Touched; // Default implementation for backwards compatibility; built-in inputs override this to report actual value presence.
-        
+
         /// <summary>
         /// The function used to detect problems with the input.
         /// </summary>
@@ -72,13 +72,13 @@ namespace MudBlazor.Interfaces
         /// </para>
         /// </remarks>
         object? Validation { get; set; }
-        
+
         /// <summary>
         /// Indicates that the <see cref="MudFormComponent{T,U}.For"/> field identifier expression is null
         /// </summary>
         /// <remarks>The <see cref="MudFormComponent{T,U}.For"/> expression is used to uniquely identify a form field to perform validation. </remarks>
         bool IsForNull { get; }
-        
+
         /// <summary>
         /// The list of problems with the current input value.
         /// </summary>
@@ -86,7 +86,7 @@ namespace MudBlazor.Interfaces
         /// When using a <see cref="MudForm"/>, this property is updated when validation has been performed.  Use the <see cref="Validation"/> property to control what validations are performed.
         /// </remarks>
         List<string> ValidationErrors { get; set; }
-        
+
         /// <summary>
         /// Causes validation to be performed for this input.
         /// </summary>
@@ -94,7 +94,7 @@ namespace MudBlazor.Interfaces
         /// When using a <see cref="MudForm"/>, the input is validated via the function set in the <see cref="Validation"/> property.
         /// </remarks>
         Task ValidateAsync();
-        
+
         /// <summary>
         /// Clears the input and any validation errors.
         /// </summary>
@@ -102,7 +102,7 @@ namespace MudBlazor.Interfaces
         /// When called, the <c>Value</c>, <see cref="Error"/>, <see cref="MudFormComponent{T,U}.ErrorText"/>, and <see cref="ValidationErrors"/> properties are all reset.
         /// </remarks>
         Task ResetAsync();
-        
+
         /// <summary>
         /// Clears any validation errors.
         /// </summary>
