0274dc0353d0e2b4bcfe29aef46e42592d359098
Daniel Chalmers <daniel.chalmers@outlook.com>
2026-09-13 18:51:02 -0500

    MudMenuItem: Render row element directly (#13801)

--- src/MudBlazor.UnitTests/Components/MenuTests.cs
+++ src/MudBlazor.UnitTests/Components/MenuTests.cs
@@ -1643,5 +1643,62 @@ namespace MudBlazor.UnitTests.Components
             activator.GetAttribute("aria-expanded").Should().Be("false");
             activator.HasAttribute("aria-controls").Should().BeFalse();
         }
+
+        /// <summary>
+        /// Menu items render an anchor with its link attributes when Href is set, and a div otherwise.
+        /// </summary>
+        [Test]
+        public async Task MenuItems_RenderAnchorWhenHrefIsSet()
+        {
+            var comp = Context.Render<MenuTest1>();
+            await comp.Find("button.mud-button-root").ClickAsync();
+
+            comp.FindAll("div.mud-menu-item").Count.Should().Be(2);
+            var links = comp.FindAll("a.mud-menu-item");
+            links.Count.Should().Be(2);
+            links[0].GetAttribute("href").Should().Be("https://www.test.com");
+            links[1].GetAttribute("target").Should().Be("_blank");
+        }
+
+        /// <summary>
+        /// A class or style supplied through UserAttributes keeps winning over the computed ones, as it did through the MudElement boundary.
+        /// </summary>
+        [Test]
+        public void MenuItem_UserAttributes_OverrideComputedClassAndStyle()
+        {
+            var comp = Context.Render<MudMenuItem>(parameters => parameters
+                .Add(x => x.Label, "Copy")
+                .Add(x => x.UserAttributes, new Dictionary<string, object>
+                {
+                    ["class"] = "user-class",
+                    ["style"] = "color:red",
+                }));
+
+            var row = comp.Find("div");
+            row.GetAttribute("class").Should().Be("user-class");
+            row.GetAttribute("style").Should().Be("color:red");
+        }
+
+        /// <summary>
+        /// The same precedence holds on the anchor row.
+        /// </summary>
+        [Test]
+        public void MenuItemWithHref_UserAttributes_OverrideComputedClassAndStyle()
+        {
+            var comp = Context.Render<MudMenuItem>(parameters => parameters
+                .Add(x => x.Label, "Docs")
+                .Add(x => x.Href, "/docs")
+                .Add(x => x.UserAttributes, new Dictionary<string, object>
+                {
+                    ["class"] = "user-class",
+                    ["style"] = "color:red",
+                }));
+
+            var row = comp.Find("a");
+            row.GetAttribute("class").Should().Be("user-class");
+            row.GetAttribute("style").Should().Be("color:red");
+            row.GetAttribute("href").Should().Be("/docs");
+        }
+
     }
 }
